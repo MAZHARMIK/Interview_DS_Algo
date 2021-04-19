a281bb482464f54e10c52065e8d5a016a86072a8
MAZHAR <iiestshibpur605@gmail.com>
2021-04-19 17:20:03 +0530

    Update Coin_Change_II_(3 Approaches).cpp

--- DP/Coin_Change_II_(3 Approaches).cpp
+++ DP/Coin_Change_II_(3 Approaches).cpp
@@ -69,7 +69,7 @@ public:
         for(int i = 0; i<n; i++) {
             //I select a coin coins[i] and find # ways to get coins[i], coins[i]+1...until amount
             int curr_coin = coins[i];
-            for(int j = curr_coin; j<=amount ; j++) { //we could start from j = 1; j<=amount just like rod cutting bottom up
+            for(int j = curr_coin; j<=amount ; j++) { //we could start from j = 1; j<=amount
                 int remain_amount = j-curr_coin; //Kis kis amount me tumne curr_coin add kara hoga to reach j
                 
                 //ways[remain_amount] = # ways to get amount "remain_amount"
