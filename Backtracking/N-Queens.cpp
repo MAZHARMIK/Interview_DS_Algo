19e84255bcf030bdee12dcfca1542433ba8b8f0e
MAZHAR <iiestshibpur605@gmail.com>
2021-05-22 15:22:29 +0530

    Added "Time Complexity Explanation"

--- Backtracking/N-Queens.cpp
+++ Backtracking/N-Queens.cpp
@@ -3,6 +3,18 @@
     Leetcode Link : https://leetcode.com/problems/n-queens/
 */
 
+/*
+    Time complexity: O(N!)
+    Unlike the brute force approach, we will only place queens on squares that aren't under attack.
+    For the first queen, we have NNN options. For the next queen, we won't attempt to place it in the
+    same column as the first queen, and there must be at least one square attacked diagonally by the
+    first queen as well. Thus, the maximum number of squares we can consider for the second queen is
+    (N−2). For the third queen, we won't attempt to place it in 2 columns already occupied by the first
+    2 queens, and there must be at least two squares attacked diagonally from the first 2 queens.
+    Thus, the maximum number of squares we can consider for the third queen is (N-4).
+    This pattern continues, resulting in an approximate time complexity of O(N!)
+*/
+
 //Approach-1 (Simple dfs)
 class Solution {
 public:
