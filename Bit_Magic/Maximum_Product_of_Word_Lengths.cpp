08d80f6d547fba4d669b9f39f5243d20dcc6acb3
MAZHAR <iiestshibpur605@gmail.com>
2021-05-27 15:17:13 +0530

    Added 2 approaches

--- Bit_Magic/Maximum_Product_of_Word_Lengths.cpp
+++ Bit_Magic/Maximum_Product_of_Word_Lengths.cpp
@@ -3,7 +3,7 @@
     Leetcode Link : https://leetcode.com/problems/maximum-product-of-word-lengths/
 */
 
-//Approach-1 (Simple as per the question) Time : O(n^2)
+//Approach-1 (Without Bit magic)
 class Solution {
 public:
     int maxProduct(vector<string>& words) {
@@ -42,4 +42,49 @@ public:
     }
 };
 
-//Approach-2
+//Approach-2 (With Bit magic)
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        int maxP = 0;
+        int n = words.size();
+        vector<int> numeric(n, 0);
+        /*
+            a = 100000....  
+            b = 010000....
+            c = 001000....
+            We are shifting 1 to left by character times
+            This way we will get value for every word.
+            Example : "abc"
+            int numericValue = 100000....  |  010000.... | 001000....
+            will be assigned to "abc"
+            Those having same numeric value will result in 000000 when we do & between them
+        */
+        
+        int k = 0;
+        for(const string& word : words) {
+            int numericValue = 0;
+            
+            for(const char& ch : word) {
+                int val = ch-'a';
+                //now left shift and add
+                
+                numericValue |= (1<<val);
+            }
+            numeric[k++] = numericValue; //numeric value of words[i]
+        }
+        
+        //Now, do & operation to find which words have no common letters
+        for(int i = 0; i<n-1; i++) {
+            for(int j = i+1; j<n; j++) {
+                if((numeric[i] & numeric[j]) == 0) { //means nothing common
+                    int l1 = words[i].length();
+                    int l2 = words[j].length();
+                    maxP = max(maxP, l1*l2);
+                }
+            }
+        }
+        
+        return maxP;
+    }
+};
