30ae80c739b4436846e7215fda110dbbbe3625fc
MAZHAR <iiestshibpur605@gmail.com>
2021-05-27 19:45:15 +0530

    Added simple approaches and explanation

--- Graph/All_Paths_From_Source_to_Target.cpp
+++ Graph/All_Paths_From_Source_to_Target.cpp
@@ -4,6 +4,7 @@
 */
 
 
+//Approach - 1
 class Solution {
 public:
     void DFS(vector<vector<int>>& adj, int start, vector<int>& temp,  vector<vector<int>>& result) {
@@ -26,6 +27,35 @@ public:
             temp.push_back(0);
             DFS(adj, x, temp, result);
         }
+        return result;
+    }
+};
+
+
+//Approach-2 (Similar but a little simple)
+class Solution {
+public:
+    int target;
+    void dfs(vector<vector<int>>& adj, int start, vector<int>& temp, vector<vector<int>>& result ) {
+        if(start == target) {
+            result.push_back(temp);
+            return;
+        }
+        
+        for(int &x : adj[start]) {
+            temp.push_back(x);
+            dfs(adj, x, temp, result);
+            temp.pop_back();
+        }
+    }
+    
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> result;
+        vector<int> temp;
+        target = graph.size()-1;
+        temp.push_back(0);
+        dfs(graph, 0, temp, result);
+        
         return result;
     }
 };
