ab5a1ffce6ea4a2dfa660ec7eae11e574d51539e
MAZHAR <iiestshibpur605@gmail.com>
2021-06-22 20:34:46 +0530

    Number of Matching Subsequences

---
+++ Arrays/Binary Search/Number of Matching Subsequences.cpp
@@ -0,0 +1,45 @@
+/*
+    Company Tags : Google
+    Leecode Link : https://leetcode.com/problems/number-of-matching-subsequences/
+    
+    //Other approach (using hash map) : Link : 
+*/
+
+class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+        map<char, vector<int>> mp;
+        int n = s.length();
+        
+        for(int i = 0; i<n; i++) {
+            mp[s[i]].push_back(i);
+        }
+
+        int count = 0;
+        for(string &word : words) {
+            int prev      = -1;
+            bool found = true;
+
+        for(char &ch : word) {
+            if(mp.find(ch) == mp.end()) {
+                found = false;
+                break;
+            }
+
+            auto it = upper_bound(begin(mp[ch]), end(mp[ch]), prev); //Binary Search (O(log(n))
+            
+            if(it == mp[ch].end()) {
+                found = false;
+                break;
+            }
+
+            prev = *it;
+        }
+        if(found)
+            count++;
+        }
+
+        return count;
+    }
+};
+
