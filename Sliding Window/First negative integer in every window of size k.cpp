6898a28159c48c583fc9fd934d014231233823a7
MAZHAR <iiestshibpur605@gmail.com>
2021-07-03 00:48:56 +0530

    First negative integer in every window of size k

---
+++ Sliding Window/First negative integer in every window of size k.cpp
@@ -0,0 +1,28 @@
+/*
+    Company Tags : Amazon
+    Qn Link      : https://practice.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1#
+*/
+
+typedef long long ll;
+vector<ll> printFirstNegativeInteger(ll A[], ll N, ll k) {
+    list<ll> dll;
+    vector<ll> result;
+    int i = 0, j = 0;
+    
+    while(j < N) {
+        if(A[j] < 0)
+            dll.push_back(A[j]);
+        
+        if(j-i+1 >= k) {
+            ll neg = dll.empty() ? 0 : dll.front();
+            result.push_back(neg);
+            if(A[i] < 0 && !dll.empty()) {
+                dll.pop_front();
+            }
+            i++;
+        }
+        j++;
+    }
+    return result;
+    
+ }
