5f7ef7cda58a6b4c354bf3d3935a68648653447c
MAZHAR <iiestshibpur605@gmail.com>
2021-07-03 01:09:14 +0530

    Count Occurences of Anagrams

---
+++ Sliding Window/Count Occurences of Anagrams.cpp
@@ -0,0 +1,38 @@
+/*
+    Company Tags : Amazon, Intuit, Microsoft
+    Qn Link      : https://practice.geeksforgeeks.org/problems/count-occurences-of-anagrams5839/1#
+*/
+
+
+class Solution{
+public:
+    bool allZero(vector<int>& count) {
+        return count==vector<int>(26, 0);
+    }
+
+	int search(string pat, string txt) {
+	    int k = pat.size();
+	    vector<int> count(26, 0);
+	    for(char &ch : pat) {
+	        count[ch-'a']++;
+	    }
+	    int i = 0, j = 0;
+	    int n = txt.size();
+	    int result = 0;
+	    while(j < n) {
+	        int idx = txt[j]-'a';
+	        count[idx]--;
+	        
+	        if(j - i + 1 >= k) {
+	            if(allZero(count)) {
+	                result++;
+	            }
+	            
+	            count[txt[i]-'a']++;
+	            i++;
+	        }
+	        j++;
+	    }
+	    return result;
+	}
+};
