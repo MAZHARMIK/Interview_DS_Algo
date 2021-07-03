0660ad5cf8beeba0d431ba1a7dec98acc7910b70
MAZHAR <iiestshibpur605@gmail.com>
2021-07-03 19:20:11 +0530

    Alternative qn added (interview)

--- Sliding Window/Longest K unique characters substring.cpp
+++ Sliding Window/Longest K unique characters substring.cpp
@@ -1,6 +1,17 @@
 /*
     Company Tags : Google, Amazon, SAP Labs
     Qn Link      : https://practice.geeksforgeeks.org/problems/longest-k-unique-characters-substring0853/1#
+    
+    Alternative question (Asked in an interview) :
+    John goes to mall with his mom. He sees that there many toys. He insists his mom to buy him toys.
+    His mom told him ok fine, you can pick the toys but in two conditions :
+    1) You can pick toys in line (you can't skip any toy in between) (Hint: She is wasking for a subarray)
+    2) You can have only two types of toys.
+    
+    Find maximum number of toys that can be picked up by John.
+    
+    So this question is nothing but : "Find the length of longest subarray/substring having k unique characters"
+    
 */
 
 //It's a Classical sliding window problem
