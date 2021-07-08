913bbb90c40d7699ecc8d4a20fe63896b8020d77
MAZHAR <iiestshibpur605@gmail.com>
2021-07-08 22:12:20 +0530

    Update Reverse Words in a String II.cpp

--- strings/Reverse Words in a String II.cpp
+++ strings/Reverse Words in a String II.cpp
@@ -1,7 +1,37 @@
 /*
     Company Tags  : Amazon, Microsoft, Uber
     Leetcode Link : https://leetcode.com/problems/reverse-words-in-a-string-ii/
+    
+    Since it's a Premium Qn, I have added Description and Example below
+    
+    Description
+
+    Given an input character array, reverse the array word by word.
+    A word is defined as a sequence of non-space characters.
+    The input character array does not contain leading or trailing
+    spaces and the words are always separated by a single space.
+    
+    Example
+
+    Example1
+
+    Input: s = "the sky is blue"
+
+    Output: "blue is sky the"
+
+    Example2
+
+    Input: "a b c"
+
+    Output: "c b a"
+
+    Challenge
+
+    Could you do it in-place without allocating extra space?
 */
+
+//Approach-0 (use stack to store words and then collect them to form reversed string) O(n) space solution
+
 
 //Approach-1 (O(n^2))
 class Solution {
