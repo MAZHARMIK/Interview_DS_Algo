4c2d0680756f0e7273a290ebcd4ed43ddff8c748
MAZHAR <iiestshibpur605@gmail.com>
2021-07-17 11:45:53 +0530

    Added "Minimum Lights to Activate"

---
+++ Arrays/Minimum Lights to Activate.cpp
@@ -0,0 +1,31 @@
+/*
+    Company Tags  : Media.net
+    Question Link : https://www.interviewbit.com/problems/minimum-lights-to-activate/
+*/
+
+int Solution::solve(vector<int> &A, int B) {
+    int n     = A.size();
+    int steps = 0;
+
+    for(int i = 0; i<n;) {
+        bool found = false;
+
+        //I need to start from right most so that I can go furthest when I find first '1'
+        int j = min(n-1, i+B-1);  
+        for(; j>=max(0, i-B+1); j--) {
+            if(A[j] == 1) {
+                steps++;
+                found = true;
+                break;
+            }
+        }
+        
+        if(!found) return -1;
+
+        //The point where I found '1' i.e. index 'j', it will lighten up bulbs till (j+B-1)
+        //So, I need to jump directly to j+B
+        i = j + B;
+    }
+
+    return steps;
+}
