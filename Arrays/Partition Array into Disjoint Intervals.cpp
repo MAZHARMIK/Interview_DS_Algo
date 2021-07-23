a24a95decb3c4870970847c001c5893a2f37b85c
MAZHAR <iiestshibpur605@gmail.com>
2021-07-23 10:58:51 +0530

    Added 3rd approach

--- Arrays/Partition Array into Disjoint Intervals.cpp
+++ Arrays/Partition Array into Disjoint Intervals.cpp
@@ -58,3 +58,48 @@ public:
         return -1;
     }
 };
+
+//Approach-3 (One loop solution with explained comments)
+/*
+	I noticed many people posting this solution with no elaborated comments.
+	So I have tried to put comments on lines which are really confusing.
+*/
+class Solution {
+public:
+    int partitionDisjoint(vector<int>& nums) {
+        int n = nums.size();
+        
+        int leftMax  = nums[0];
+        int currMax  = nums[0];
+        int len      = 1; //Qn says it is gauranteed that a partitioning exists
+        
+        for(int i = 1; i<n; i++) {
+            if(nums[i] < leftMax) {
+                /*
+                    If nums[i] is less than maximum to left, it must go to left partition
+                    so, increase the length of partition
+                */
+                len = i+1;
+                
+                leftMax = currMax;
+                /*
+                    why this ?
+                    Exampel : {5, 0, 3, 8, 6, 0, 10}
+                    Until i = 2, leftMax is 5
+                    Now, when we reach i = 3, we see that it's not less than leftMax (i.e. 8 > 5)
+                    So, we don't need to partition BUT we update our currMax = 8
+                    Now, we go to i = 4, our leftMax is still 5 and currMax = 8
+                    Now, we reach to i = 5, leftMax is still 5 and nums[i] is less than leftMax i.e. (0 < 5)
+                    so, we need to update our the partition length to (i+1)
+                    BUT, here's the catch, After i  = 5, we need to have the leftMax as 8 and not 5 anymore.
+                    That's why we update leftMax to currMax
+                    i.e. leftMax = currMax;
+                */
+            } else {
+                currMax = max(currMax, nums[i]); //Explained in above comment
+            }
+        }
+        
+        return len;
+    }
+};
