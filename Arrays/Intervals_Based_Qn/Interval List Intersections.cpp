df652d541a7b91b23f6bd27519414595612302e0
MAZHAR <iiestshibpur605@gmail.com>
2021-08-01 07:17:33 +0530

    Interval List Intersections

---
+++ Arrays/Intervals_Based_Qn/Interval List Intersections.cpp
@@ -0,0 +1,30 @@
+/*
+    Company Tags  : Not known yet. Please do let me know :-)
+    Leetcode Link : https://leetcode.com/problems/interval-list-intersections/
+*/
+
+class Solution {
+public:
+    typedef vector<int> V;
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        int m = firstList.size();
+        int n = secondList.size();
+        vector<V> result;
+        int i = 0, j = 0;
+        
+        while(i < m && j < n) {
+            
+            int x = max(firstList[i][0], secondList[j][0]);
+            int y = min(firstList[i][1], secondList[j][1]);
+            if(x <= y)
+                result.push_back({x, y}); //Add only when the interval is valid (increasing)
+            
+            if(firstList[i][1] > secondList[j][1])
+                j++;
+            else
+                i++;
+        }
+        
+        return result;
+    }
+};
