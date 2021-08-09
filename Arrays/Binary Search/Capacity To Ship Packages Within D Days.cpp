0bb95cc3fd81636f664384863be7fcd57da19130
MAZHAR <iiestshibpur605@gmail.com>
2021-08-09 15:37:45 +0530

    Capacity To Ship Packages Within D Days

---
+++ Arrays/Binary Search/Capacity To Ship Packages Within D Days.cpp
@@ -0,0 +1,61 @@
+/*
+    Company Tags : Facebook, Baidu
+    Leecode Link : https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
+*/
+
+class Solution {
+private:
+    int n;
+public:    
+    bool possible(int weight, vector<int>& weights, int days) {
+        int currDayCount  = 1;
+        int currSumWeight = 0;
+        for(int &w : weights) {
+            currSumWeight += w;
+            
+            if(currSumWeight > weight) {
+                currDayCount++;
+                currSumWeight = w;
+            }
+            
+        }
+        
+        return currDayCount<=days;
+        /*
+            Why currDayCount<=days ?
+            Because it is mentioned in the question that all the packages
+            need to be shipped "WITHIN" 'days' days
+            So, even if I am able to transfer all the weights within
+            days < (given days in input) I am good to go.
+        */
+    }
+    
+    int shipWithinDays(vector<int>& weights, int days) {
+            n     = weights.size();
+        int maxWt = *max_element(begin(weights), end(weights));
+        
+        if(n <= days)
+            return maxWt;
+        
+        int minWeight = accumulate(begin(weights), end(weights), 0);
+        
+        int high = minWeight;
+        int low  = maxWt;      //weight cannot be less than maximum weight
+        /*
+            Why ?
+            Because in worst case I will have to carry one weight per day.
+            So, in that case my ship's capacity must not be less than the maximum weight.
+        */
+
+        while(low < high) {
+            int mid = low + (high-low)/2;
+            
+            if(possible(mid, weights, days))
+                high = mid;
+            else
+                low  = mid+1;
+        }
+        
+        return high;
+    }
+};
