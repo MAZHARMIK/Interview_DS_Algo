b732b76992ca4869f805a038b93a0687e1ec0df8
MAZHAR <iiestshibpur605@gmail.com>
2021-08-29 23:33:24 +0530

    Added Bottom UP approach

--- DP/Maximum Profit in Job Scheduling.cpp
+++ DP/Maximum Profit in Job Scheduling.cpp
@@ -52,4 +52,45 @@ public:
     }
 };
 
-//Approach-2 (Bottom UP DP) - COMING SOON (I am travelling :-) )
+//Approach-2 (Bottom UP DP)
+class Solution {
+public:
+    int n;
+    int getNextIndex(vector<vector<int>>& array, int index, int target) {
+        int l = index, r = n-1;
+        int result = -1;
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            if(array[mid][0] >= target) {
+                result = mid;
+                r = mid-1;
+            } else {
+                l = mid+1;
+            }
+        }
+        return result;
+    }
+
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        n = startTime.size();
+        vector<vector<int>> array(n, vector<int>(3, 0));
+        
+        for(int i = 0; i<n; i++) {
+            array[i] = {startTime[i], endTime[i], profit[i]};
+        }
+        
+        sort(begin(array), end(array));
+        vector<int> t(n, 0);
+        t[n-1] = array[n-1][2];
+        
+        for(int i = n-2; i >= 0; i--) {
+            int next     = getNextIndex(array, i+1, array[i][1]);
+            int taken    = array[i][2] + (next == -1 ? 0 : t[next]);
+            int notTaken = t[i+1];
+            
+            t[i] = max(taken, notTaken);
+        }
+
+        return t[0];
+    }
+};
