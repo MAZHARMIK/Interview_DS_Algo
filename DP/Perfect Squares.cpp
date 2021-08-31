6e985b46d377fac6e75c0233d720fe0dffdf18ac
MAZHAR <iiestshibpur605@gmail.com>
2021-08-31 10:47:01 +0530

    Added 4 approaches

--- DP/Perfect Squares.cpp
+++ DP/Perfect Squares.cpp
@@ -1,7 +1,8 @@
 /*
     Company Tags  :  Google
+    Frequency     : 74%
     Leetcode Link : https://leetcode.com/problems/perfect-squares/
-    This question is nothing but just similar to of "Minimum Coins to get sum S (COIN CHANGE - Leetcode : 322"
+    This question is nothing but just similar to of "Minimum Coins to get sum S (COIN CHANGE - Leetcode : 322)
 */
 
 //Approach-1 (Recusrion + Memoization)
@@ -78,5 +79,58 @@ public:
         }
         int n = squares.size();
         return minSquares(squares, n, S);
+    }
+};
+
+
+//Approach-3 (Bottom UP DP : O(n) space)
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> t(n+1, INT_MAX);
+        //t[i] = min number of perfect squares to get i
+        t[0] = 0;
+        
+        for(int i = 1; i<=n; i++) {
+            
+            // For each i, it must be the sum of some number (i - j*j) and 
+            // a perfect square number (j*j).
+            for(int j = 1; j*j<=i; j++) {
+                int remain = i-j*j;
+                
+                t[i] = min(t[i], 1 + t[remain]);
+            }
+        }
+        
+        return t[n];
+    }
+};
+
+
+//Approach-4 (Recursion with Memoizatin without Coin Change concept)
+class Solution {
+public:
+    int t[10001];
+    
+    int minSquares(int n) {
+        if(n == 0)
+            return 0;
+        
+        if(t[n] != -1)
+            return t[n];
+        
+        //we can select only those perfect squares which can contribute to sum up to n
+        int minCount = INT_MAX;
+        for(int i = 1; i*i<=n; i++) {
+            minCount = min(minCount, 1 + minSquares(n-i*i)); //1 is for selectcing one quare number
+        }
+        
+        return t[n] = minCount;
+    }
+    
+    int numSquares(int n) {
+        memset(t, -1, sizeof(t));
+        
+        return minSquares(n);
     }
 };
