e26f103853f0f21e027f6c56b9d7d794b5682b9d
MAZHAR <iiestshibpur605@gmail.com>
2021-09-11 10:22:34 +0530

    Update and rename Max Stack to Max Stack.cpp

--- Design/Max Stack.cpp
+++ Design/Max Stack.cpp
@@ -1,7 +1,25 @@
 /*
     Company Tags  : LinkedIn
+    Frequency     : 12%
     Leetcode Link : https://leetcode.com/problems/max-stack/
     We can solve Min Stack in a similar way IF min stack requires PopMin()
+    
+    Since this is a Premimum qn, please find the Description below :
+    
+    Description :
+    Design a max stack that supports push, pop, top, peekMax and popMax.
+
+    push(x) -- Push element x onto stack.
+    pop() -- Remove the element on top of the stack and return it.
+    top() -- Get the element on the top.
+    peekMax() -- Retrieve the maximum element in the stack.
+    popMax() -- Retrieve the maximum element in the stack, and remove it. If you find more than one maximum elements, only remove the top-most one.
+    
+    Note:
+    1) -1e7 <= x <= 1e7
+    2) Number of operations won't exceed 10000.
+    3) The last four operations won't be called when stack is empty.
+
 */
 
 #include <list> //include this if compiler shows error like this :" list does not name a type "
