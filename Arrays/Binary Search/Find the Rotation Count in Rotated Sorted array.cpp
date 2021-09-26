d4c7adf777c860cf4d6ebff857621fa46ae930c3
MAZHAR <iiestshibpur605@gmail.com>
2021-09-26 17:48:57 +0530

    Find the Rotation Count in Rotated Sorted array

---
+++ Arrays/Binary Search/Find the Rotation Count in Rotated Sorted array.cpp
@@ -0,0 +1,22 @@
+/*
+    Company Tags      : Amazon, ABCO
+    Video Explanation : You can request for a video explanation by letting me know on LinkedIn. It's free
+    Question Link     : https://practice.geeksforgeeks.org/problems/rotation4723/1
+*/
+
+class Solution{
+public:	
+	int findKRotation(int arr[], int n) {
+	    int l = 0, r = n-1;
+	    while(l < r) {
+	        int mid = l + (r-l)/2;
+	        if(arr[mid] < arr[r]) {
+	            r = mid;
+	        } else {
+	            l = mid+1;
+	        }
+	    }
+	    return r;
+	}
+
+};
