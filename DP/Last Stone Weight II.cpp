2640e0b806083af942c1c806da249316005dff38
MAZHAR <iiestshibpur605@gmail.com>
2022-05-28 16:43:10 +0530

    Added recursive approach as well (Memoized)

--- DP/Last Stone Weight II.cpp
+++ DP/Last Stone Weight II.cpp
@@ -8,6 +8,31 @@
     NOTE: sum(S1) = sum of all elements in subset S1
 */
 
+//Approach-1 (Top Down : Recursive) - memoized
+class Solution {
+public:
+    int t[31][5051];
+    int minDiffSubset(vector<int>& stones, int n, int s1, int sum) {
+        if(n == 0)
+            return abs(sum-2*s1);
+        
+        if(t[n][s1] != -1)
+            return t[n][s1];
+        
+        return t[n][s1] = min(minDiffSubset(stones, n-1, s1+stones[n-1], sum),  //take
+                              minDiffSubset(stones, n-1, s1, sum));             //don't take
+    }
+    
+    int lastStoneWeightII(vector<int>& stones) {
+        memset(t, -1, sizeof(t));
+        int sum = accumulate(begin(stones), end(stones), 0);
+        int n   = stones.size();
+        return minDiffSubset(stones, n, 0, sum);
+    }
+};
+
+
+//Approach-2 (Bottom Up)
 class Solution {
 public:
     int minDiffSubset(vector<int>& stones, int n, int sum) {
