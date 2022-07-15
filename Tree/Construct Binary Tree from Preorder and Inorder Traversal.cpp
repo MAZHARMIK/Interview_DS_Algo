21cff9bafc336dd35b1243e57d8b31e1d71cd3aa
MAZHAR <iiestshibpur605@gmail.com>
2022-07-16 01:44:45 +0530

    Added basic approach also.

--- Tree/Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ Tree/Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -3,6 +3,42 @@
     Leetcode Link : https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 */
 
+
+//Basic Approach
+class Solution {
+public:
+    
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int start, int end, int& idx) {
+        if(start > end)
+            return NULL;
+        
+        int rootVal = preorder[idx];
+        int i = start;
+        
+        for(; i <= end; i++) {
+            if(inorder[i] == rootVal)
+                break;
+        }
+        
+        idx++;
+        TreeNode* root = new TreeNode(rootVal);
+        root->left  = solve(preorder, inorder, start, i-1, idx);
+        root->right = solve(preorder, inorder, i+1, end, idx);
+        
+        return root;
+    }
+    
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = preorder.size();
+        
+        int idx = 0;
+        
+        return solve(preorder, inorder, 0, n-1, idx);
+    }
+};
+
+
+//Optimized approach by using map to find index of root in inorder array
 class Solution {
 public:
     int index;
