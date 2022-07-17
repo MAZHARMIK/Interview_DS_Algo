ca4c91524f7fc8733eba68ad401fe5162f70886e
MAZHAR <iiestshibpur605@gmail.com>
2022-07-17 10:46:17 +0530

    Added comments for better understanding

--- Arrays/Binary Search/Find Minimum in Rotated Sorted Array.cpp
+++ Arrays/Binary Search/Find Minimum in Rotated Sorted Array.cpp
@@ -11,10 +11,12 @@ public:
         int l = 0, r = nums.size()-1;
         while(l < r) {
             mid = l + (r-l)/2;
-            if(nums[mid] < nums[r])
-                r  = mid;
-            else
-                l = mid+1;
+          
+            if(nums[mid] > nums[r]) { //something wrong on right side. Possibly my answer lies in right side.
+                l = mid+1;            //move right (Discarding mid, because it's greater than nums[r], so it can't be the minimum element)
+            } else {
+                r = mid;
+            }
         }
         return nums[l];
     }
