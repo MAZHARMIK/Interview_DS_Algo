bb301169f5c1e978de0b8ababaf13192d575337b
MAZHAR <iiestshibpur605@gmail.com>
2022-08-04 22:58:36 +0530

    Check Completeness of a Binary Tree

---
+++ Tree/Check Completeness of a Binary Tree.cpp
@@ -0,0 +1,39 @@
+/*
+    Company Tags  : Amazon
+    Leetcode Link : https://leetcode.com/problems/check-completeness-of-a-binary-tree/
+*/
+
+//Approach-1 : BFS
+
+//Asked in Amazon
+
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        //Using BFS
+        
+        queue<TreeNode*> que;
+        
+        que.push(root);
+        
+        bool past = false; //kya aapne past me NULL node dekha hai ?
+        
+        while(!que.empty()) {
+            TreeNode* node = que.front();
+            que.pop();
+            
+            if(node == NULL) {
+                past = true;
+            } else {
+                if(past == true) {
+                    return false;
+                }
+                
+                que.push(node->left);
+                que.push(node->right);
+            }
+        }
+        
+        return true;
+    }
+};
