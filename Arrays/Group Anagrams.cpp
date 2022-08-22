cbefab40ee4f7a3e0373ce8e441d833d0cfa6a2a
MAZHAR <iiestshibpur605@gmail.com>
2022-08-22 22:19:27 +0530

    Better solution added

--- Arrays/Group Anagrams.cpp
+++ Arrays/Group Anagrams.cpp
@@ -35,24 +35,41 @@ Time : O(n*k)  (n = size of input, k = maximum length of a string in the input v
 */
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<vector<int>, vector<string>> mp;
+    
+    string generate(string &s) {
+        int count[26] = {0};
         
-        vector<int> vec(26);
-        for(string str:strs) {
-            for(int i = 0; i<26; i++)
-                vec[i] = 0;
-            for(char c: str)
-                vec[c-'a']++;
+        for(char &ch : s) {
+            count[ch-'a']++;
+        }
+        
+        string new_s;
+        
+        for(int i = 0; i<26; i++) {
             
-            mp[vec].push_back(str);
+            if(count[i] > 0) {
+                new_s += string(count[i], i+'a');
+            }
+        }
+        
+        return new_s;
+    }
+    
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        
+        for(string &s : strs) {
+            string new_s = generate(s);
+            
+            mp[new_s].push_back(s);
         }
         
         vector<vector<string>> result;
-        for(auto it : mp) {
-            result.push_back(it.second);
+        for(auto &it : mp) {
+            result.push_back(std::move(it.second));
         }
         
         return result;
+        
     }
 };
