143c6f59a39c6a43587b12f162295d4adf461eaf
MAZHAR <iiestshibpur605@gmail.com>
2022-09-12 16:58:08 +0530

    Added company tags

--- Greedy/Bag of Tokens.cpp
+++ Greedy/Bag of Tokens.cpp
@@ -1,6 +1,7 @@
 /*
-    Company Tags  : Google
-    Leetcode Link : https://leetcode.com/problems/bag-of-tokens/
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Google
+    Leetcode Link               : https://leetcode.com/problems/bag-of-tokens/
 */
 
 class Solution {
@@ -12,19 +13,23 @@ public:
         int currScore = 0;
         int maxScore  = 0;
         int l = 0, r = n-1;
+
         //:GREEDY
         //While losing power, I will choose the smallest token
         //While gainin power, I will choose the largest token
+        
         while(l <= r) {
             if(P >= tokens[l]) {
                 currScore++;
                 maxScore = max(maxScore, currScore); //keep updating it
                 P -= tokens[l]; //choose smallest token
                 l++;
+                
             } else if(currScore >= 1) {
                 currScore--;
                 P += tokens[r]; //choose largest token
                 r--;
+                
             } else {
                 //no way further to increase score
                 return maxScore;
