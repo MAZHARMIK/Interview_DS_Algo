eb19bb0c980647c9fa830c4a81badde91512d09f
MAZHAR <iiestshibpur605@gmail.com>
2022-09-14 20:17:31 +0530

    Boats to Save People

---
+++ Greedy/Boats to Save People.cpp
@@ -0,0 +1,32 @@
+/*  
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Google
+    Leetcode Link               : https://leetcode.com/problems/boats-to-save-people/
+*/
+
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n = people.size();
+        
+        
+        //sort the array
+        sort(begin(people), end(people));
+        
+        int i = 0, j = n-1;
+        
+        int boats = 0;
+        
+        while(i <= j) {
+            if(people[j] + people[i] <= limit) {
+                i++;
+                j--;
+            } else {
+                j--;
+            }
+            boats += 1;
+        }
+        
+        return boats;
+    }
+};
