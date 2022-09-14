63b4ccc0b932eee39a8dadf0dca99ae0bbb5a541
MAZHAR <iiestshibpur605@gmail.com>
2022-09-14 09:45:12 +0530

    House Robber

---
+++ DP/House Robber.cpp
@@ -0,0 +1,76 @@
+/*
+    Company Tags    : Amazon, OYO Rooms, Paytm, Walmart, Google, Flipkart
+    Leetcode Link   : https://leetcode.com/problems/house-robber/
+    GfG Link        : https://practice.geeksforgeeks.org/problems/stickler-theif-1587115621/1
+    Also Famouse as : Stickler Thief
+*/
+
+//Approach-1 (Recur + Memo)
+class Solution {
+public:
+    int t[101];
+    int solve(vector<int>& nums, int i, int& n) {
+        if(i >= n)
+            return 0;
+        
+        if(t[i] != -1)
+            return t[i];
+        
+        int take = nums[i] + solve(nums, i+2, n); //steals ith house and moves to i+2 (because we can't steal adjacent)
+        int skip = solve(nums, i+1, n); //skips this house, now we can move to adjacent next house
+        
+        return t[i]=max(take, skip);
+    }
+    
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        memset(t, -1, sizeof(t));
+        return solve(nums, 0, n);
+    }
+};
+
+
+//Approach-2 (Bottom up)
+class Solution {
+public:
+    int t[101];
+    int solve(vector<int>& nums, int i, int& n) {
+        if(i >= n)
+            return 0;
+        
+        if(t[i] != -1)
+            return t[i];
+        
+        int take = nums[i] + solve(nums, i+2, n);
+        int skip = solve(nums, i+1, n);
+        
+        return t[i]=max(take, skip);
+    }
+    
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        
+        if(n == 1)
+            return nums[0];
+        
+        vector<int> t(n+1);
+        //t[i] = max profit till house i
+        
+        t[0] = 0;
+        t[1] = nums[0];
+        t[2] = max(nums[0], nums[1]);
+        
+        for(int i = 3; i<=n; i++) {
+            int curr_house = nums[i-1];
+          
+            t[i] = max(t[i-1],  curr_house + t[i-2]);
+            /*
+                If we skip this house, then we have money till previous house  =  t[i-1]
+                If we steal this house, then we can't take prev profit, we need (i-2)th house profit = t[i-2]
+            */
+        }
+        
+        return t[n];
+        
+    }
+};
