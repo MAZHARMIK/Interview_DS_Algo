8e8c3fd3c8937e82ece29d7c651bd884e056dae5
MAZHAR <iiestshibpur605@gmail.com>
2022-10-05 10:13:54 +0530

    Adde company tags and approach-2

--- Tree/Add One Row to Tree.cpp
+++ Tree/Add One Row to Tree.cpp
@@ -1,8 +1,47 @@
 /*
-    Company Tags  : Please let me know when you get it :-)
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags  : Google
     Leetcode Link : https://leetcode.com/problems/add-one-row-to-tree/
 */
 
+//Approach-1 : Using DFS
+class Solution {
+public:
+    
+    TreeNode* add(TreeNode* root, int val, int depth, int curr) {
+        if(!root)
+            return NULL;
+        
+        if(curr == depth-1) {
+            TreeNode* lTemp = root->left;
+            TreeNode* rTemp = root->right;
+            
+            root->left  = new TreeNode(val);
+            root->right = new TreeNode(val);
+            root->left->left = lTemp;
+            root->right->right = rTemp;
+            
+            return root;
+        }
+        
+        root->left  = add(root->left, val, depth, curr+1);
+        root->right = add(root->right, val, depth, curr+1);
+        
+        return root;
+    }
+    
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if(depth == 1) {
+            TreeNode* newRoot = new TreeNode(val);
+            newRoot->left = root;
+            return newRoot;
+        }
+        
+        return add(root, val, depth, 1);
+    }
+};
+
+//Approach-2 : Using BFS
 class Solution {
 public:
     TreeNode* addOneRow(TreeNode* root, int v, int d) {
