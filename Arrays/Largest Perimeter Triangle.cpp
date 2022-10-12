a06f02c1174125a35783da3be7efe1e136412899
MAZHAR <iiestshibpur605@gmail.com>
2022-10-12 20:26:40 +0530

    Largest Perimeter Triangle.cpp

---
+++ Arrays/Largest Perimeter Triangle.cpp
@@ -0,0 +1,25 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : C3.ai
+    Leetcode Link               : https://leetcode.com/problems/largest-perimeter-triangle/
+    
+*/
+
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        int n = nums.size();
+        
+        sort(begin(nums), end(nums));
+        
+        for(int i = n-3; i>=0; i--) {
+            //nums[i] < nums[i+1] < nums[i+2]
+            
+            if(nums[i] + nums[i+1] > nums[i+2]) //a + b > c
+                return nums[i] + nums[i+1] + nums[i+2];
+        }
+        
+        return 0;
+        
+    }
+};
