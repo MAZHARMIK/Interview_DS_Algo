0c24b2234c7359db20bc573789d6f0f460bc5fea
MAZHAR <iiestshibpur605@gmail.com>
2022-10-13 08:54:43 +0530

    Delete Node in a Linked List

---
+++ Linked List/Delete Node in a Linked List.cpp
@@ -0,0 +1,21 @@
+/*
+    MY YOUTUBE VIDEON ON THIS Qn : <soon>
+    Company Tags                 : Apple, Adobe, Microsoft, Samsung
+    Leetcode Link                : https://leetcode.com/problems/delete-node-in-a-linked-list/
+    Similar GfG Link             : https://practice.geeksforgeeks.org/problems/delete-a-node-in-single-linked-list/1
+*/
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* prev = NULL;
+        
+        while(node && node->next) {
+            node->val = node->next->val;
+            prev = node;
+            node = node->next;
+        }
+        delete(node);
+        prev->next = NULL;
+    }
+};
