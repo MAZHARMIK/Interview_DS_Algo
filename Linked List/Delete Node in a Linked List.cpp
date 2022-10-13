0f75564d5c6e57a3251737acb0c172bd04b7b11d
MAZHAR <iiestshibpur605@gmail.com>
2022-10-13 10:07:14 +0530

    Added YouTube video

--- Linked List/Delete Node in a Linked List.cpp
+++ Linked List/Delete Node in a Linked List.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEON ON THIS Qn : <soon>
+    MY YOUTUBE VIDEON ON THIS Qn : https://www.youtube.com/watch?v=TvLWOECe8mQ
     Company Tags                 : Apple, Adobe, Microsoft, Samsung
     Leetcode Link                : https://leetcode.com/problems/delete-node-in-a-linked-list/
     Similar GfG Link             : https://practice.geeksforgeeks.org/problems/delete-a-node-in-single-linked-list/1
