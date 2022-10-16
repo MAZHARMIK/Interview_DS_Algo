1a2e2b063905e626e2e0a2366347a5d60b9ad440
MAZHAR <iiestshibpur605@gmail.com>
2022-10-16 10:28:20 +0530

    Added YouTube video

--- DP/Minimum Difficulty of a Job Schedule.cpp
+++ DP/Minimum Difficulty of a Job Schedule.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=smTnSl6UKi0
     Company Tags                : Amazon
     Leetcode Link               : https://leetcode.com/problems/minimum-difficulty-of-a-job-schedule/
 */
