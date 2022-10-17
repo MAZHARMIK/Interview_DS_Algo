a11624a866215f0ef7c4c9dba8fe12d70eb4826c
MAZHAR <iiestshibpur605@gmail.com>
2022-10-17 07:21:22 +0530

    Check if the Sentence Is Pangram

---
+++ strings/Check if the Sentence Is Pangram.cpp
@@ -0,0 +1,51 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Zoho, Snapdeal
+    Leetcode Link               : https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+    GfG Link                    : https://practice.geeksforgeeks.org/problems/pangram-strings3155/1
+*/
+
+//Approach-1 (In 2 traversals)
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        vector<int> arr(26, 0);
+        
+        
+        for(char &ch : sentence) {
+            int index = ch - 'a';
+            arr[index]++;
+        }
+        
+        for(int &count : arr) {
+            if(count == 0)
+                return false;
+        }
+        
+        
+        return true;
+    }
+};
+
+
+//Approach-2 (In 1 traversal)
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        vector<int> arr(26, 0);
+        
+        int count = 0;
+        
+        for(char &ch : sentence) {
+            int index = ch - 'a';
+            
+            if(arr[index] == 0) {
+                arr[index]++;
+                count++;
+            }
+        }
+        
+        
+        return conut==26;
+    }
+};
