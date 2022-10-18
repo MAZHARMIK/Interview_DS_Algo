d2b5980f4cf1ce3b41abf04ffa00b381bdfb2be3
MAZHAR <iiestshibpur605@gmail.com>
2022-10-18 07:57:02 +0530

    Count and Say

---
+++ strings/Count and Say.cpp
@@ -0,0 +1,38 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags  : Facebook, Amazon, Microsoft, Zoho
+    Leetcode Link : https://leetcode.com/problems/count-and-say/
+    GfG Link      : https://practice.geeksforgeeks.org/problems/decode-the-pattern1138/1
+*/
+
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n == 1)
+            return "1";
+        
+        
+        string say = countAndSay(n-1);
+        
+        string result = "";
+        
+        // Just count and store in result and return
+        for(int i = 0; i<say.length(); i++) {
+            
+            int count = 1;
+            char ch = say[i];
+            
+            while(i < say.length()-1 && say[i] == say[i+1]) {
+                count++;
+                i++;
+            }
+            
+            result += to_string(count) + string(1, say[i]);
+            
+        }
+        
+        return result;
+        
+    }
+};
