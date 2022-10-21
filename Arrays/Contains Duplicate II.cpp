b5e328285a992fe2346e006cee6ddc0fbd646f70
MAZHAR <iiestshibpur605@gmail.com>
2022-10-21 08:12:52 +0530

    Contains Duplicate II

---
+++ Arrays/Contains Duplicate II.cpp
@@ -0,0 +1,24 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Airbnb, Palantir Technologies
+    Leetcode Link               : https://leetcode.com/problems/contains-duplicate-ii/
+*/
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        
+        for(int i = 0; i<nums.size(); i++) {
+            
+            if(mp.find(nums[i]) != mp.end() &&  //check if present in map and abs(i-j) <= k
+               abs(mp[nums[i]] - i) <= k)
+                return true;
+            else
+                mp[nums[i]] = i; //{number, index}
+            
+        }
+        
+        return false;
+    }
+};
