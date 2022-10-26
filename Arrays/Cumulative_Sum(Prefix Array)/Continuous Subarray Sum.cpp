d340c575934751b851e63cc1b180f7aacf727684
MAZHAR <iiestshibpur605@gmail.com>
2022-10-26 09:21:20 +0530

    Continuous Subarray Sum

---
+++ Arrays/Cumulative_Sum(Prefix Array)/Continuous Subarray Sum.cpp
@@ -0,0 +1,34 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Amazon, Facebook, Paytm
+    Leetcode Link               : https://leetcode.com/problems/continuous-subarray-sum/
+*/
+
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        unordered_map<int, int> mp;
+        
+        mp[0] = -1;
+        
+        int sum = 0;
+        
+        for(int i = 0; i<n; i++) {
+            sum += nums[i];
+            
+            int remainder = sum%k;
+            
+            if(mp.find(remainder) != mp.end()) {
+                
+                if(i - mp[remainder] >= 2)
+                    return true;
+                
+            } else {
+                mp[remainder] = i;
+            }
+        }
+        return false;
+    }
+};
