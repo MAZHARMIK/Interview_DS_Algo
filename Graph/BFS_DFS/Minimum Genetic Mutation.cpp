bd0891c5f3e95fc9718b41c4065d305f72058cd3
MAZHAR <iiestshibpur605@gmail.com>
2022-11-02 08:40:44 +0530

    Minimum Genetic Mutation

---
+++ Graph/BFS_DFS/Minimum Genetic Mutation.cpp
@@ -0,0 +1,48 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Twitter
+    Leetcode Link               : https://leetcode.com/problems/minimum-genetic-mutation/
+*/
+
+//Just like Word Ladder
+class Solution {
+public:
+    int minMutation(string start, string end, vector<string>& bank) {
+        unordered_set<string> bankset(bank.begin(), bank.end());
+        
+        unordered_set<string> visited;
+        queue<string> que;
+        que.push(start);
+        visited.insert(start);
+        
+        int level = 0;
+        
+        while(!que.empty()) {
+            int n = que.size();
+            
+            while(n--) {
+                string curr = que.front();
+                que.pop();
+                
+                if(curr ==  end)
+                    return level;
+                
+                for(char ch : "ACGT") {
+                    for(int i = 0; i<curr.length(); i++) {
+                        string neighbour = curr;
+                        neighbour[i] = ch;
+                        
+                        if(visited.find(neighbour) == visited.end() && 
+                           bankset.find(neighbour) != bankset.end()) {
+                            visited.insert(neighbour);
+                            que.push(neighbour);
+                        }
+                    }
+                }
+            }
+            level++;
+        }
+        
+        return -1;
+    }
+};
