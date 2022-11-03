fa1e384fd7381d56b76a01577f76f2c89e2d59a7
MAZHAR <iiestshibpur605@gmail.com>
2022-11-03 09:45:22 +0530

    Longest Palindrome by Concatenating Two Letter Words

---
+++ Greedy/Longest Palindrome by Concatenating Two Letter Words.cpp
@@ -0,0 +1,47 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags  : <Soon>
+    Leetcode Link : https://leetcode.com/problems/longest-palindrome-by-concatenating-two-letter-words/
+*/
+
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        unordered_map<string, int> mp;
+        
+        
+        //update map with frequency
+        for(string &word : words) {
+            mp[word]++;
+        }
+        
+        bool centerUsed = false; //for frequency one waale strings
+        
+        int result = 0;
+        
+        //start iterating on words one by one
+        for(string &word : words) {
+            string rev = word;
+            reverse(begin(rev), end(rev));
+            
+            if(rev != word) { //"ab" "ba"    ->.  {ab      ba}
+                if(mp[word] > 0 && mp[rev] > 0) {
+                    mp[word]--;
+                    mp[rev]--;
+                    result += 4;
+                }
+            } else { //"abcc      ccba"
+                if(mp[word] >= 2) {
+                    mp[word] -= 2;
+                    result += 4;
+                } else if(mp[word] == 1 && centerUsed == false) {
+                    mp[word]--;
+                    result += 2;
+                    centerUsed = true; //ab use hogaya hai center wala
+                }
+            }
+        }
+        
+        return result;
+    }
+};
