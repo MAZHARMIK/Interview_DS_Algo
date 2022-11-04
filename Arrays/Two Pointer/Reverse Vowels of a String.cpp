fb2d7aab0082d30a8383884a96a0348869ef88b8
MAZHAR <iiestshibpur605@gmail.com>
2022-11-04 07:43:13 +0530

    Reverse Vowels of a String

---
+++ Arrays/Two Pointer/Reverse Vowels of a String.cpp
@@ -0,0 +1,38 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Google, Zoho, Flipkart, 
+    Leetcode Link               : https://leetcode.com/problems/reverse-vowels-of-a-string/
+    GfG Link                    : https://practice.geeksforgeeks.org/problems/reversing-the-vowels5304/1
+*/
+
+//Simply using  Two pointer technique
+class Solution {
+public:
+    bool isVowel(char &ch) {
+        return ch =='a' || ch == 'e' ||
+               ch =='i' || ch == 'o' ||
+               ch == 'u' ||
+               ch =='A' || ch == 'E' ||
+               ch =='I' || ch == 'O' ||
+               ch == 'U';
+    }
+    string reverseVowels(string s) {
+        int n = s.length();
+        int i = 0;
+        int j = n-1;
+        
+        while(i < j) {
+            if(!isVowel(s[i])) i++;
+            
+            else if(!isVowel(s[j])) j--;
+            
+            else {
+                swap(s[i], s[j]);
+                i++;
+                j--;
+            }
+        }
+        
+        return s;
+    }
+};
