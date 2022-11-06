60f3281580f050902f3faa6b7038a74639c737b7
MAZHAR <iiestshibpur605@gmail.com>
2022-11-06 14:39:38 +0530

    Orderly Queue

---
+++ strings/Orderly Queue.cpp
@@ -0,0 +1,23 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Amazon
+    Leetcode Link               : https://leetcode.com/problems/orderly-queue/
+*/
+
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        if(k > 1) {
+            sort(begin(s), end(s));
+            return s;
+        }
+        
+        string result = s;
+        for(int i = 1; i<s.length(); i++) {
+            string temp = s.substr(i) + s.substr(0, i);
+            result = min(result, temp);
+        }
+        
+        return result;
+    }
+};
