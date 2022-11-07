2afdb7226b46feef75de7bd1f71be476c0a7794c
MAZHAR <iiestshibpur605@gmail.com>
2022-11-07 08:31:02 +0530

    Maximum 69 Number

---
+++ Greedy/Maximum 69 Number.cpp
@@ -0,0 +1,28 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Oracle (Phone Interview)
+    Leetcode Link               : https://leetcode.com/problems/maximum-69-number/
+*/
+
+//Without converting to string
+class Solution {
+public:
+    int maximum69Number (int num) {
+        int place = 0;
+        int index = -1;
+        int temp = num;
+        while(temp) {
+            int remain = temp%10;
+            if(remain == 6)
+                index = place;
+            
+            temp = temp/10;
+            place++;
+        }
+        
+        if(index == -1) return num;
+        
+        return num + 3*pow(10, index);
+        
+    }
+};
