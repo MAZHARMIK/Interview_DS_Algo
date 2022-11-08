ea880d622a96a61d2fd0776a1e9cdaf40d1a04b9
MAZHAR <iiestshibpur605@gmail.com>
2022-11-08 10:23:25 +0530

    Make The String Great

---
+++ strings/Make The String Great.cpp
@@ -0,0 +1,22 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Google, Apple, LinkedIn, Microsoft
+    Leetcode Link               : https://leetcode.com/problems/make-the-string-great/
+*/
+
+class Solution {
+public:
+    string makeGood(string s) {
+        string result = "";
+        
+        for(char &ch : s) {
+            if(ch + 32 == result.back() || ch - 32 == result.back()) {
+                result.pop_back();
+            } else {
+                result.push_back(ch);
+            }
+        }
+        
+        return result;
+    }
+};
