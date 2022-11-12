081b1d51577e5f2ca3112cdbae96204bd9090b27
MAZHAR <iiestshibpur605@gmail.com>
2022-11-12 10:21:06 +0530

    Added GfG Link

--- Design/Find Median from Data Stream.cpp
+++ Design/Find Median from Data Stream.cpp
@@ -3,6 +3,7 @@
     Company Tags                : Apple, AMAZON, VMWARE, GOOGLE, MICROSOFT, GOLDMAN SACHS, Adobe, Belzabar, SAP Labs, Yahoo,
                                   D-E-Shaw, Facebook, Flipkart, Google, Intuit, Microsoft, Morgan Stanley, Ola Cabs, Oracle, Samsung
     Leetcode Link               : https://leetcode.com/problems/find-median-from-data-stream/
+    GfG Link                    : https://practice.geeksforgeeks.org/problems/find-median-in-a-stream-1587115620/1
 */
 
 //Approach-1 (O(n^2) - TLE
