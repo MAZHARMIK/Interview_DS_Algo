e3af06eb44f12313fc4881a99cc15d83951e72cc
MAZHAR <iiestshibpur605@gmail.com>
2022-11-15 08:43:31 +0530

    Count Complete Tree Nodes

---
+++ Tree/Count Complete Tree Nodes.cpp
@@ -0,0 +1,46 @@
+/*
+    MY YOUTUBE VIDE ON THIS Qn : 
+    Company Tags               : Amazon, Microsoft, Salesforce, Facebook
+    Leetcode Link              : https://leetcode.com/problems/count-complete-tree-nodes/
+*/
+
+//Time Complexity : O(log(n) * log(n))
+class Solution {
+public:
+    int leftHeight(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        int lh = 0;
+        TreeNode* temp = root;
+        while(temp) {
+            temp = temp->left;
+            lh++;
+        }
+        return lh;
+    }
+    int rightHeight(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        int rh = 0;
+        TreeNode* temp = root;
+        while(temp) {
+            temp = temp->right;
+            rh++;
+        }
+        return rh;
+    }
+    int countNodes(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        int lh = leftHeight(root);
+        int rh = rightHeight(root);
+        if(lh == rh) {
+            return pow(2, lh) - 1;
+        }
+        
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
