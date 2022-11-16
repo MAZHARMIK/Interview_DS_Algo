d6688860f274bb95cacd24f4a295bcd9c6dcc17c
MAZHAR <iiestshibpur605@gmail.com>
2022-11-16 07:59:09 +0530

    Guess Number Higher or Lower

---
+++ Arrays/Binary Search/Guess Number Higher or Lower.cpp
@@ -0,0 +1,27 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Microsoft, Google(with variation)
+    Leetcode Link               : https://leetcode.com/problems/guess-number-higher-or-lower/
+*/
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int l = 1, r = n;
+        
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            int val = guess(mid);
+            
+            if(val == -1) {
+                r = mid-1;
+            } else if(val == 1) {
+                l = mid+1;
+            } else {
+                return mid;
+            }
+        }
+        
+        return -1;
+    }
+};
