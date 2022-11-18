3f30354f703952a7adc09602b80e15d2203d4209
MAZHAR <iiestshibpur605@gmail.com>
2022-11-18 22:20:17 +0530

    Added Ugly Number

---
+++ Mathematical/Ugly Number.cpp
@@ -0,0 +1,22 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Facebook, Amazon
+    Leetcode Link               : https://leetcode.com/problems/ugly-number/
+*/
+
+class Solution {
+public:
+    bool isUgly(int n) {
+        while(n > 1) {
+            if(n%2 == 0)
+                n /= 2;
+            else if(n%3 == 0)
+                n /= 3;
+            else if(n%5 == 0)
+                n /= 5;
+            else
+                return false;
+        }
+        return n == 1;
+    }
+};
