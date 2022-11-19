f5dcf5808eafeb544f546ed054f16e9c84d41040
MAZHAR <iiestshibpur605@gmail.com>
2022-11-19 18:50:25 +0530

    Added YouTube video

--- Mathematical/Erect the Fence.cpp
+++ Mathematical/Erect the Fence.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=et3dtYzVGQQ
     Company Tags                : Google
     Leetcode Link               : https://leetcode.com/problems/erect-the-fence/
 */
