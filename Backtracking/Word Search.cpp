b590239b7b81285cfae853d53e7aa29de23502bc
MAZHAR <iiestshibpur605@gmail.com>
2022-11-24 08:42:55 +0530

    Word Search

---
+++ Backtracking/Word Search.cpp
@@ -0,0 +1,50 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Apple, Amazon, Microsoft, Intuit
+    Leetcode Link               : https://leetcode.com/problems/word-search/
+*/
+
+class Solution {
+public:
+    int l, m, n;
+    vector<vector<int>> directions{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    bool find(vector<vector<char>>& board, int i, int j, string &word, int idx) {
+        if(idx >= l)
+            return true;
+        
+        if(i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[idx])
+            return false;
+        
+        char temp = board[i][j];
+        board[i][j] = '$';
+        
+        for(auto& dir : directions) {
+            int i_ = i + dir[0];
+            int j_ = j + dir[1];
+            
+            if(find(board, i_, j_, word, idx+1))
+                return true;
+        }
+        
+        board[i][j] = temp;
+        return false;
+    }
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        m = board.size();
+        n = board[0].size();
+        l = word.length();
+        if(m*n < l)
+            return false;
+        
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                if(board[i][j] == word[0] && find(board, i, j, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+};
