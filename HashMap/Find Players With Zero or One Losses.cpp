e7fb6209c4972a38e4718c497e96e5137e9e8e00
MAZHAR <iiestshibpur605@gmail.com>
2022-11-28 15:54:31 +0530

    Find Players With Zero or One Losses

---
+++ HashMap/Find Players With Zero or One Losses.cpp
@@ -0,0 +1,40 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Google
+    Leetcode Link               : https://leetcode.com/problems/find-players-with-zero-or-one-losses/
+*/
+
+class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        unordered_map<int, int> lost;
+        
+        for(auto &it : matches) {
+            int lose = it[1];
+            lost[lose]++;
+        }
+        
+        vector<int> notLost;
+        vector<int> oneLos;
+        
+        for(auto &it : matches) {
+            int lose = it[1];
+            int win  = it[0];
+            
+            if(lost[lose] == 1) {
+                oneLos.push_back(lose);
+            }
+            if(lost.find(win) == lost.end()) {
+                notLost.push_back(win);
+                lost[win] = 2;
+            }
+            
+        }
+        
+        sort(begin(notLost), end(notLost));
+        sort(begin(oneLos), end(oneLos));
+        
+        return {notLost, oneLos};
+        
+    }
+};
