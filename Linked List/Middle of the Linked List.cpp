77dcc27dd75dedc86872ab54ff25aba5c7a8fd0d
MAZHAR <iiestshibpur605@gmail.com>
2022-12-05 08:47:50 +0530

    Middle of the Linked List

---
+++ Linked List/Middle of the Linked List.cpp
@@ -0,0 +1,22 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Adobe, Amazon, Flipkart, GE, Hike, IgniteWorld, MAQ Software, Microsoft, Morgan Stanley, 
+                                  Nagarro, Netskope, Payu, Qualcomm, Samsung, SAP Labs, Veritas, VMWare, Wipro, Zoho
+    Leetcode Link               : https://leetcode.com/problems/middle-of-the-linked-list/
+    GfG Link                    : https://practice.geeksforgeeks.org/problems/finding-middle-element-in-a-linked-list/1
+*/
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* first  = head;
+        ListNode* second = head;
+        
+        while(first && second && second->next) {
+            first = first->next;
+            second = second->next->next;
+        }
+        
+        return first;
+    }
+};
