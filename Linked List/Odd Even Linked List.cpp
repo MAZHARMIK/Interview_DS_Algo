d2cf7703ad11fa2ceab6d2ac91a518d07fc4d36a
MAZHAR <iiestshibpur605@gmail.com>
2022-12-06 11:17:16 +0530

    Commented for better understanding

--- Linked List/Odd Even Linked List.cpp
+++ Linked List/Odd Even Linked List.cpp
@@ -4,28 +4,44 @@
     Leetcode Link               : https://leetcode.com/problems/odd-even-linked-list/
 */
 
-//Approach-1
+//Approach-1 (Time : O(n), Space : O(1))
 class Solution {
 public:
-    ListNode *oddEvenList(ListNode * head) {
+    ListNode* oddEvenList(ListNode* head) {
+        
         if(head == NULL || head->next == NULL)
             return head;
         
-        ListNode* odd = head;
-        ListNode* even = head->next;
-        ListNode* evenTemp = head->next;
-        while(even && even->next) {
-            odd->next = even->next;
-            even->next = even->next->next;
+        
+        /*
+            Story : 
+            1. Hume odd and even ko segregate karna hai
+            2. To let's take two pointers to point to ODD and EVEN nodes
+            3. Unko alag alag segregate kardenge
+            4. Last me connect kardenge odd ko even se
             
-            odd  = odd->next;
-            even = even->next;
+        */
+        
+        ListNode* ODD  = head;
+        ListNode* EVEN = head->next;
+        
+        ListNode* evenStart = head->next;
+        
+        while(EVEN != NULL && EVEN->next != NULL) {
+            ODD->next  = EVEN->next;
+            EVEN->next = EVEN->next->next; //fat sakt hai ye code
+            
+            ODD  = ODD->next;
+            EVEN = EVEN->next;
         }
-        odd->next = evenTemp;
+        
+        ODD->next = evenStart;
+        
+        
         return head;
+        
     }
 };
-
 
 //Approach-2
 class Solution {
