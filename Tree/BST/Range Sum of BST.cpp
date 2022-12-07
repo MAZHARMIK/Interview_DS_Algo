c91047882672c758544cabd861a7f264eb7f3be6
MAZHAR <iiestshibpur605@gmail.com>
2022-12-07 09:51:26 +0530

    Range Sum of BST

---
+++ Tree/BST/Range Sum of BST.cpp
@@ -0,0 +1,26 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Google, Amazon, D.E. Shaw 
+    Leetcode Link               : https://leetcode.com/problems/range-sum-of-bst/
+    GfG Link                    : https://practice.geeksforgeeks.org/problems/count-bst-nodes-that-lie-in-a-given-range/1
+*/
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(!root)
+            return 0;
+        
+        //Within Range
+        if(root->val >= low && root->val <= high) {
+            return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+        }
+        
+        //When outside the range (Less than low) - Go right
+        if(root->val < low)
+            return rangeSumBST(root->right, low, high);
+
+        //When outside the range (Greater than high) - Go left
+        return rangeSumBST(root->left, low, high);
+    }
+};
