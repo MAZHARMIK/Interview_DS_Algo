ecdc1d3a41ecde20a75258363deb3851e8535162
MAZHAR <iiestshibpur605@gmail.com>
2022-12-08 09:40:42 +0530

    Leaf-Similar Trees

---
+++ Tree/Leaf-Similar Trees.cpp
@@ -0,0 +1,33 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Meta, Microsoft, Twitter, Amazon, Goldman Sachs
+    Leetcode Link               : https://leetcode.com/problems/leaf-similar-trees/
+*/
+
+
+class Solution {
+public:
+    
+    void inOrder(TreeNode* root, string& s) {
+        if(root == NULL) return;
+        
+        //leaf node
+        if(root->left == NULL && root->right == NULL) {
+            s += to_string(root->val) + "_";
+            return;
+        }
+        
+        inOrder(root->left, s);
+        inOrder(root->right, s);
+    }
+    
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        string s1 = "";
+        string s2 = "";
+        
+        inOrder(root1, s1);
+        inOrder(root2, s2);
+        
+        return s1==s2;
+    }
+};
