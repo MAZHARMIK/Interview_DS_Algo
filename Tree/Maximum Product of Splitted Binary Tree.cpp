7cfd6fc7479aae2fbeac255bc8fdd0e20bb9a776
MAZHAR <iiestshibpur605@gmail.com>
2022-12-10 10:03:52 +0530

    Added YouTube Link

--- Tree/Maximum Product of Splitted Binary Tree.cpp
+++ Tree/Maximum Product of Splitted Binary Tree.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=ITySQXe9Ybg
     Company Tags                : Google, Facebok, Amazon(Phone Interview)
     Leetcode Link               : https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/
 */
