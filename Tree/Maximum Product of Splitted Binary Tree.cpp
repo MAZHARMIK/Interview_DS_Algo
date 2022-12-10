8e1296fa5af9892a5f71360cad890c06db50f1a3
MAZHAR <iiestshibpur605@gmail.com>
2022-12-10 08:20:56 +0530

    Maximum Product of Splitted Binary Tree

---
+++ Tree/Maximum Product of Splitted Binary Tree.cpp
@@ -0,0 +1,40 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Google, Facebok, Amazon(Phone Interview)
+    Leetcode Link               : https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/
+*/
+
+
+class Solution {
+public:
+    
+    long M = 1e9+7;
+    long totalSum = 0, maxP = 0;
+    
+    int findTotalSum(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        int leftSubtreeSum  = findTotalSum(root->left);
+        int rightSubtreeSum = findTotalSum(root->right);
+        int sum             = root->val + leftSubtreeSum + rightSubtreeSum;
+        
+        maxP = max(maxP, (totalSum - sum) * sum);
+        
+        return sum;
+    }
+    
+    int maxProduct(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        maxP = 0;
+        
+        totalSum = findTotalSum(root);
+        
+        
+        findTotalSum(root);
+        
+        return maxP%M;
+    }
+};
