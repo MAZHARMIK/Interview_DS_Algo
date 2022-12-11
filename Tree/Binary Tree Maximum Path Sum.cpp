dc061fff144d9147a0a5ad779bdb649c44378a11
MAZHAR <iiestshibpur605@gmail.com>
2022-12-11 11:11:25 +0530

    Binary Tree Maximum Path Sum

---
+++ Tree/Binary Tree Maximum Path Sum.cpp
@@ -0,0 +1,42 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn :<soon>
+    Company Tags                : Google, Amazon, Meta, Flipkart
+    Leetcode Link               : https://leetcode.com/problems/binary-tree-maximum-path-sum/
+    GfG Link                    : https://practice.geeksforgeeks.org/problems/maximum-path-sum-from-any-node/1
+*/
+
+class Solution {
+public:
+    
+    int maxSum;
+    
+    int solve(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        
+        int l = solve(root->left);
+        int r = solve(root->right);
+        
+        int neeche_hi_milgaya_answer = l + r + root->val; //(1)
+        
+        int koi_ek_acha = max(l, r) + root->val; //(2)
+        
+        int only_root_acha = root->val; //(3)
+        
+        maxSum = max({maxSum, neeche_hi_milgaya_answer, koi_ek_acha, only_root_acha});
+        
+        
+        //most important part
+        return max(koi_ek_acha, only_root_acha);
+        
+    }
+    
+    int maxPathSum(TreeNode* root) {
+        maxSum = INT_MIN;
+        
+        solve(root);
+        
+        return maxSum;
+        
+    }
+};
