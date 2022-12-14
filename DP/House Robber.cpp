c1f9a94330476b5cadbf28c0c8a308856264d982
MAZHAR <iiestshibpur605@gmail.com>
2022-12-14 12:05:47 +0530

    Added YouTube Link

--- DP/House Robber.cpp
+++ DP/House Robber.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=SI6Pm8AKqnQ
     Company Tags                : Amazon, OYO Rooms, Paytm, Walmart, Google, Flipkart, LinkedIn, Airbnb
     Leetcode Link               : https://leetcode.com/problems/house-robber/
     GfG Link                    : https://practice.geeksforgeeks.org/problems/stickler-theif-1587115621/1
@@ -50,10 +50,10 @@ public:
         for(int i = 3; i<=n; i++) {
             int curr_house = nums[i-1];
           
-            t[i] = max(t[i-1],  curr_house + t[i-2]);
+            t[i] = max(t[i-1],  curr_house + t[i-2]); //max(skip, steal)
             /*
-                If we skip this house, then we have money till previous house  =  t[i-1]
-                If we steal this house, then we can't take prev profit, we need (i-2)th house profit = t[i-2]
+                SKIP  : If we skip this house,  then we have money till previous house  =  t[i-1]
+                STEAL : If we steal this house, then we can't take prev profit, we can take till (i-2)th house profit = t[i-2]
             */
         }
         
