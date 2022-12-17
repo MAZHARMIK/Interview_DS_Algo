68062412d1fdfae25cfb0e0789641a0cf13d08dd
MAZHAR <iiestshibpur605@gmail.com>
2022-12-17 08:02:29 +0530

    Added fancy lambda solution

--- Stack/Evaluate Reverse Polish Notation.cpp
+++ Stack/Evaluate Reverse Polish Notation.cpp
@@ -4,6 +4,7 @@
     Leetcode Link               : https://leetcode.com/problems/evaluate-reverse-polish-notation/
 */
 
+//Basic approach
 class Solution {
 public:
     int operate(int &a, int &b, const string &s) {
@@ -32,6 +33,42 @@ public:
                 st.pop();
                 
                 result = operate(a, b, s);
+                st.push(result);
+            } else {
+                st.push(stoi(s));
+            }
+        }
+        return st.top();
+    }
+};
+
+
+/*
+    Using fancy Lambda on unordered_map
+    For my fancy STL collection with easy exlanation with examples, see the link below -
+    https://github.com/MAZHARMIK/Cpp-STL-Quick-Help/blob/main/README.md
+*/
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        int result = 0;
+        
+        unordered_map<string, function<int (int, int)> > mp = {
+            {"+", [](int a, int b) {return a + b; } },
+            {"-", [](int a, int b) {return a - b; } },
+            {"*", [](int a, int b) {return (long)a * (long)b; } },
+            {"/", [](int a, int b) {return a / b; } },
+        };
+        
+        for(const string& s:tokens) {
+            if(s == "+" || s == "-" || s == "*" || s == "/") {
+                int b = st.top();
+                st.pop();
+                int a = st.top();
+                st.pop();
+                
+                result = mp[s](a, b);
                 st.push(result);
             } else {
                 st.push(stoi(s));
