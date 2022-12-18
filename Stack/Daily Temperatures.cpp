087ab9beae4630dd29b83345ba5ff6975d7c9e30
MAZHAR <iiestshibpur605@gmail.com>
2022-12-18 10:03:00 +0530

    Daily Temperatures

---
+++ Stack/Daily Temperatures.cpp
@@ -0,0 +1,32 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Google, Meta, Tokopedia
+    Leetcode Link               : https://leetcode.com/problems/daily-temperatures/
+*/
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        stack<int> st;
+        
+        vector<int> result(n);
+        
+        for(int i = n-1; i>=0; i--) {
+            while(!st.empty() && temperatures[i] >= temperatures[st.top()]) {
+                st.pop();
+            }
+            
+            if(st.empty()) {
+                result[i] = 0;
+            } else {
+                result[i] = st.top() - i; //days
+            }
+            
+            st.push(i);
+        }
+        
+        return result;
+        
+    }
+};
