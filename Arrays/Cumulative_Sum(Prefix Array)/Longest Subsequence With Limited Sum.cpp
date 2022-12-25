b0fc9e183f610b29b740b17aac48263bd2d22f7e
MAZHAR <iiestshibpur605@gmail.com>
2022-12-25 09:33:57 +0530

    Longest Subsequence With Limited Sum

---
+++ Arrays/Cumulative_Sum(Prefix Array)/Longest Subsequence With Limited Sum.cpp
@@ -0,0 +1,72 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : <soon>
+    Leetcode Link               : https://leetcode.com/problems/longest-subsequence-with-limited-sum/
+*/
+
+//Approach-1
+//Using Self Written Binary Search : O(m+n)*log(n)
+class Solution {
+public:
+    
+    int binarySearch(vector<int>& nums, int n, int target) {
+        int l = 0, r = n-1;
+        int mid;
+        int result_idx = -1;
+        
+        while(l <= r) {
+            mid = l + (r-l)/2;
+            if(nums[mid] <= target) {
+                result_idx = mid;
+                l++;
+            } else if(nums[mid] > target) {
+                r--;
+            }
+        }
+        return result_idx+1;
+    }
+    
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        int n = nums.size();
+        
+        sort(begin(nums), end(nums));
+        for(int i = 1; i<n; i++) {
+            nums[i] += nums[i-1];
+        }
+        
+        int m = queries.size();
+        vector<int> result(m);
+        
+        for(int i = 0; i<m; i++) {
+            auto it = binarySearch(nums, n, queries[i]);
+            
+            result[i] = it;
+        }
+        return result;
+    }
+};
+
+
+//Approach-2
+//Using upper_bound STL in C++ : O(m+n)*log(n)
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        int n = nums.size();
+        
+        sort(begin(nums), end(nums));
+        for(int i = 1; i<n; i++) {
+            nums[i] += nums[i-1];
+        }
+        
+        int m = queries.size();
+        vector<int> result(m);
+        
+        for(int i = 0; i<m; i++) {
+            auto it = upper_bound(begin(nums), end(nums), queries[i]) - begin(nums);
+            
+            result[i] = it;
+        }
+        return result;
+    }
+};
