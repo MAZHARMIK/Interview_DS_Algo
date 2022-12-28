e85cea91940fd8dde0599e28d378acbfdc2a5e5c
MAZHAR <iiestshibpur605@gmail.com>
2022-12-28 11:49:08 +0530

    Remove Stones to Minimize the Total

---
+++ Heap/Remove Stones to Minimize the Total.cpp
@@ -0,0 +1,35 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn :
+    Company Tags                : Google, Miscrosoft, Netflix, Salesforce, Meta
+    Leetcode Link               : https://leetcode.com/problems/remove-stones-to-minimize-the-total/
+*/
+
+//Time Complexity : O(n + k*log(n))
+class Solution {
+public:
+    int minStoneSum(vector<int>& piles, int k) {
+        int n = piles.size();
+        
+        priority_queue<int> pq;
+        int sum = 0;
+        for(int i = 0 ; i<n; i++) {
+            pq.push(piles[i]);
+            sum += piles[i];
+        }
+        
+       for(int i = 1; i<=k; i++) {
+           int curr = pq.top();
+           pq.pop();
+           
+           int remove = curr/2;
+           sum -= remove;
+           
+           int remaining = curr-remove;
+           
+           pq.push(remaining);
+       }
+        
+        return sum;
+        
+    }
+};
