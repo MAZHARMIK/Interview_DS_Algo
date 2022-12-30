ebecd11728a0c32ab9d44e8505cf3f91c440c34e
MAZHAR <iiestshibpur605@gmail.com>
2022-12-30 14:39:19 +0530

    Added BFS

--- Graph/All Paths From Source to Target.cpp
+++ Graph/All Paths From Source to Target.cpp
@@ -33,7 +33,7 @@ public:
 };
 
 
-//Approach-2 (Similar but a little simple)
+//Approach-2 (Similar to approach-1 but a little simple)
 class Solution {
 public:
     int target;
@@ -56,6 +56,45 @@ public:
         target = graph.size()-1;
         temp.push_back(0);
         dfs(graph, 0, temp, result);
+        
+        return result;
+    }
+};
+
+
+//Approach-3 (Using BFS)
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int n = graph.size();
+        
+        int source = 0;
+        int target = n-1;
+        
+        vector<vector<int>> result;
+        vector<int> temp;
+        
+        queue<vector<int>> que;
+        temp.push_back(0);
+        que.push(temp);
+        
+        while(!que.empty()) {
+            vector<int> currPath = que.front();
+            que.pop();
+            
+            int lastNode = currPath.back();
+            
+            if(lastNode == target) {
+                result.push_back(currPath);
+            } else {
+                for(int &v : graph[lastNode]) {
+                    vector<int> path = currPath;
+                    path.push_back(v);
+                    que.push(path);
+                }
+            }
+        }
+      
         
         return result;
     }
