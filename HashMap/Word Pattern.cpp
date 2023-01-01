a02983e6a196a866f1f6188c872657492f099f03
MAZHAR <iiestshibpur605@gmail.com>
2023-01-01 13:14:22 +0530

    Added 2 approaches

--- HashMap/Word Pattern.cpp
+++ HashMap/Word Pattern.cpp
@@ -39,4 +39,43 @@ public:
 };
 
 
-//Approach-2 ()
+//Approach-2 (map of char/word to index+1)
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char, int> charToIndex;
+        unordered_map<string, int> wordToIndex;
+        
+        stringstream ss(s);
+        string token;
+        int countTokens = 0;
+        int i = 0; 
+        int n = pattern.size();
+        
+        while(ss >> token) {
+            countTokens++;
+            
+            cout << charToIndex[pattern[i]] << ", " << wordToIndex[token] << endl;
+            
+            if (i == n || charToIndex[pattern[i]] != wordToIndex[token])
+                return false;
+            
+            charToIndex[pattern[i]] = wordToIndex[token] = i+1; 
+            /*
+                Why i+1 ?
+                Because by default, if the key is not in map, it's value is returned as 0
+                Dry-run this :
+                "abba"
+                "dog cat cat fish"
+
+            */
+            
+            i++;
+        }
+        
+        if(countTokens != pattern.size() || i != n)
+            return false;
+        
+        return true;
+    }
+};
