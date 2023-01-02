0492313d2dee17f784a4809dbb3538201cd6089c
MAZHAR <iiestshibpur605@gmail.com>
2023-01-02 09:20:32 +0530

    Detect Capital

---
+++ strings/Detect Capital.cpp
@@ -0,0 +1,56 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : GOOGLE
+    Leetcode Link               : https://leetcode.com/problems/detect-capital/
+*/
+
+//Approach-1 (Simple Checks)
+//NOTE :  YOU can also use isupper() C++ function to check capital
+class Solution {
+public:
+    bool allCapitals(string word) {
+        for(char ch:word) {
+            if(ch <'A' || ch >'Z')
+                return false;
+        }
+        return true;
+    }
+    bool allSmall(string word) {
+        for(char ch:word) {
+            if(ch <'a' || ch >'z')
+                return false;
+        }
+        return true;
+    }
+    
+    bool detectCapitalUse(string word) {
+        if(allCapitals(word) || allSmall(word) || allSmall(word.substr(1)))
+            return true;
+        return false;
+    }
+};
+
+
+//Approach-2 (Small and simple)
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int countCapitals = 0;
+        
+        for(char &ch : word) {
+            if(isupper(ch))
+                countCapitals++;
+        }
+        
+        if(countCapitals == 0)
+            return true;
+        
+        if(countCapitals == word.length())
+            return true;
+        
+        if(countCapitals == 1 && isupper(word[0]))
+            return true;
+        
+        return false;
+    }
+};
