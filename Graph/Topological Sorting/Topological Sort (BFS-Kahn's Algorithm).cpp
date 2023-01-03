9d489442d575aa688530f4a8bab406403165a2ee
MAZHAR <iiestshibpur605@gmail.com>
2023-01-03 15:03:14 +0530

    Added GfG Link

--- Graph/Topological Sorting/Topological Sort (BFS-Kahn's Algorithm).cpp
+++ Graph/Topological Sorting/Topological Sort (BFS-Kahn's Algorithm).cpp
@@ -1,5 +1,6 @@
 /*
     Company Tags                        : Accolite, Amazon, Flipkart, Microsoft, Moonfrog Labs, OYO Rooms
+    GfG Link                            : https://practice.geeksforgeeks.org/problems/topological-sort/1#
     Related Leetcode Qn                 : Course Schedule, Course Schedule II
     Course Schedule (Leetcode - 207)    : https://leetcode.com/problems/course-schedule/
     Course Schedule II (Leetcode - 210) : https://leetcode.com/problems/course-schedule-ii/
@@ -8,81 +9,43 @@
     NOTE: This can also be used to check if the graph contains cycle or not (Please see Line:56 below)
 */
 
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <map>
-using namespace std;
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int N, vector<int> adj[]) {
 
-void addEdge(map<int, vector<int>>& adj, int u, int v) {
-    adj[u].push_back(v);
-}
-
-void topologicalSortBFS(map<int, vector<int>>& adj, int V) {
-    vector<bool> visited(V, false);
-
-    vector<int> indegree(V, 0);
-
-    for(auto vec: adj) {
-        for(auto x : vec.second) {
-            indegree[x]++;
-        }
-    }
-    queue<int> que;
-    for(int i = 0; i<V; i++) {
-        if(indegree[i] == 0) {
-            visited[i] = true;
-            que.push(i);
-        }
-    }
-    vector<int> result;
-    int count = 0;
-    while(!que.empty()) {
-        int curr = que.front();
-        que.pop();
-        count++;
-
-        for(auto x:adj[curr]) {
-            if(visited[x] == false)
-                indegree[x]--;
-            if(indegree[x] == 0) {
-                que.push(x);
-                visited[x] = true;
+        vector<int> indegree(N, 0);
+    
+        for(int u = 0; u<N; u++) {
+            for(int &v : adj[u]) {
+                indegree[v]++;
             }
         }
-
-        result.push_back(curr);
-    }
-    if(count != V) {
-        cout << "No topological sorting possible. There is a cycle\n";
-        return;
-    }
-    for(int i : result)
-        cout << i << " ";
-}
-
-int main() {
-
-    int V = 6;
-    map<int, vector<int>> adj;
-    addEdge(adj, 5, 2);
-    addEdge(adj, 5, 0);
-    addEdge(adj, 4, 0);
-    addEdge(adj, 4, 1);
-    addEdge(adj, 2, 3);
-    addEdge(adj, 3, 1);
-
-    cout << "This is the graph : \n";
-    for(int i = 0; i<V; i++) {
-        cout << i << " -> ";
-        for(int i:adj[i]) {
-            cout << i << " ";
+        
+        queue<int> que;
+        for(int i = 0; i<N; i++) {
+            if(indegree[i] == 0) {
+                que.push(i);
+            }
         }
-        cout << endl;
-    }
-
-    cout << "\nThis is the topological Sort using Kahn's Algorithm: \n";
-    topologicalSortBFS(adj, V);
+        vector<int> result;
+        int count = 0;
+        while(!que.empty()) {
+            int curr = que.front();
+            que.pop();
+            count++;
     
-return 0;
-}
+            for(auto x:adj[curr]) {
+                indegree[x]--;
+                if(indegree[x] == 0) {
+                    que.push(x);
+                }
+            }
+    
+            result.push_back(curr);
+        }
+
+        return result;
+	}
+};
