5052dac7e68014a3baf6ff258313591c38dc0afe
MAZHAR <iiestshibpur605@gmail.com>
2023-01-04 10:17:12 +0530

    Minimum Rounds to Complete All Tasks

---
+++ Greedy/Minimum Rounds to Complete All Tasks.cpp
@@ -0,0 +1,35 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : 
+    Leetcode Link               : https://leetcode.com/problems/minimum-rounds-to-complete-all-tasks/
+*/
+
+//Greedy
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        unordered_map<int, int> mp;
+        
+        for(int &x : tasks) {
+            mp[x]++;
+        }
+        
+        int round = 0;
+        
+        for(auto &it : mp) {
+            int count = it.second;
+            
+            if(count == 1)
+                return -1;
+            
+            if(count % 3 == 0)
+                round += count/3;
+            else
+                round += count/3+1;
+        }
+        
+        return round;
+    }
+};
+
+
