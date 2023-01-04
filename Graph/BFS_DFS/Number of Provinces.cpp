51ab209dd36a4e5f88ee3162d7db89ab3b45fce4
MAZHAR <iiestshibpur605@gmail.com>
2023-01-04 23:54:16 +0530

    Number of Provinces

---
+++ Graph/BFS_DFS/Number of Provinces.cpp
@@ -0,0 +1,181 @@
+/*
+      MY YOUTUBE VIDEO ON THIS Qn : <soon>
+      Company Tags                : 
+      Leetcode Link               : https://leetcode.com/problems/number-of-provinces/
+      
+*/
+
+//Approach-1 : [ Using DFS - Creating our own Map as graph ]
+class Solution {
+public:
+    void dfs(unordered_map<int, vector<int>> &adj, int u, vector<bool>& visited) {
+        visited[u] = true;
+        
+        //Visit neighbours
+        for(int &v : adj[u]) {
+            if(!visited[v]) {
+                dfs(adj, v, visited);
+            }
+        }
+    }
+    
+    
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        
+        unordered_map<int, vector<int>> adj;
+        
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                if(isConnected[i][j] == 1) {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+        
+        vector<bool> visited(n, false);
+        int count = 0;
+        
+        for(int i = 0; i<n; i++) {
+            if(!visited[i]) {
+                count++;
+                dfs(adj, i, visited);
+            }
+        }
+        
+        return count;
+        
+    }
+};
+
+
+//Approach-2 : [ Using DFS - Creating given input graph]
+class Solution {
+public:
+    int n;
+    void dfs(vector<vector<int>> &adj, int u, vector<bool>& visited) {
+        visited[u] = true;
+        
+        //Visit neighbours
+        for(int v = 0; v < n; v++) {
+            if(adj[u][v] == 1 && !visited[v]) {
+                dfs(adj, v, visited);
+            }
+        }
+    }
+    
+    
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        n = isConnected.size();
+        
+        vector<bool> visited(n, false);
+        int count = 0;
+        
+        for(int i = 0; i<n; i++) {
+            if(!visited[i]) {
+                count++;
+                dfs(isConnected, i, visited);
+            }
+        }
+        
+        return count;
+        
+    }
+};
+
+
+//Approach-3 : [ Using BFS : Creating our own Map as graph ]
+class Solution {
+public:
+
+    void bfs(unordered_map<int, vector<int>>& adj, int u, vector<bool>& visited) {
+        queue<int> que;
+        que.push(u);
+        visited[u] = true;
+        
+        while(!que.empty()) {
+            int u = que.front();
+            que.pop();
+            
+           //Visit neighbours
+            for(int &v : adj[u]) {
+                if(!visited[v]) {
+                    visited[v] = true;
+                    que.push(v);
+                }
+            } 
+        }
+    }
+    
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        
+        unordered_map<int, vector<int>> adj;
+        
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                if(isConnected[i][j] == 1) {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+        
+        vector<bool> visited(n, false);
+        int count = 0;
+        
+        for(int i = 0; i<n; i++) {
+            if(!visited[i]) {
+                bfs(adj, i, visited);
+                count++;
+            }
+        }
+        
+        return count;
+        
+    }
+};
+
+
+//Approach-4 : [ Using BFS - Creating given input graph]
+class Solution {
+public:
+    int n;
+    void bfs(vector<vector<int>>& adj, int u, vector<bool>& visited) {
+        queue<int> que;
+        que.push(u);
+        visited[u] = true;
+        
+        
+        while(!que.empty()) {
+            int u = que.front();
+            que.pop();
+            
+            //Visit neighbours
+            for(int v = 0; v<n; v++) {
+                if(adj[u][v] == 1 && !visited[v]) {
+                    visited[v] = true;
+                    que.push(v);
+                }
+            } 
+        }
+    }
+    
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        n = isConnected.size();
+        
+        vector<bool> visited(n, false);
+        int count = 0;
+        
+        for(int i = 0; i<n; i++) {
+            if(!visited[i]) {
+                bfs(isConnected, i, visited);
+                count++;
+            }
+        }
+        
+        return count;
+        
+    }
+};
