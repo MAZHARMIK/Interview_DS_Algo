d5a9f24aaa05defadb9db67fc2e924f1e6854c05
MAZHAR <iiestshibpur605@gmail.com>
2023-01-04 15:41:21 +0530

    Added clean solution and updated YouTube link

---
+++ Graph/Detect cycle in a directed graph (BFS).cpp
@@ -0,0 +1,48 @@
+/*
+  MY YOUTUBE VIDEO ON THIS Qn : <soon>
+  Company Tags                : Flipkart, Amazon, Microsoft, Samsung, MakeMyTrip, Oracle, Adobe, BankBazaar
+  Gfg Link                    : https://practice.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1
+*/
+
+
+class Solution {
+  public:
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int N, vector<int> adj[]) {
+        queue<int> que;
+	    vector<int> indegree(N, 0);
+	    int count = 0;
+	    //1
+	    for(int u = 0; u<N; u++) {
+	        for(int &v : adj[u]) {
+	            indegree[v]++;
+	        }
+	    }
+	    
+	    //2. Fill que, indegree with 0
+	    for(int i = 0; i<N; i++) {
+	        if(indegree[i] == 0) {
+	            que.push(i);
+	            count++;
+	        }
+	    }
+	    
+	    //3. Simple BFS
+	    while(!que.empty()) {
+	        int u = que.front();
+	        que.pop();
+	        
+	        for(int &v : adj[u]) {
+	            indegree[v]--;
+	            
+	            if(indegree[v] == 0) {
+	                que.push(v);
+	                count++;
+	            }
+	            
+	        }
+	    }
+	    
+	    return count != N;
+    }
+};
