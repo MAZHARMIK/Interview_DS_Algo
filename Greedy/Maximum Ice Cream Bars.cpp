74273335d8460bdf213d66859dd034cfb4e84283
MAZHAR <iiestshibpur605@gmail.com>
2023-01-06 09:14:23 +0530

    Maximum Ice Cream Bars

---
+++ Greedy/Maximum Ice Cream Bars.cpp
@@ -0,0 +1,25 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Apply, Amazon, Meta, Microsoft
+    Leetcode Link               : https://leetcode.com/problems/maximum-ice-cream-bars/
+*/
+
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(begin(costs), end(costs));
+        
+        int count = 0;
+        
+        for(int &cost : costs) {
+            if(cost > coins)
+                return count;
+            else {
+                count++;
+                coins -= cost;
+            }
+        }
+        
+        return count;
+    }
+};
