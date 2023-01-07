35a1f0f1d1e50f6137bbf0c570128f49922988b1
MAZHAR <iiestshibpur605@gmail.com>
2023-01-07 09:14:32 +0530

    Gas Station

---
+++ Greedy/Gas Station.cpp
@@ -0,0 +1,73 @@
+/*
+    My YOUTUBE VIDEO ON THIS Qn : <soon>
+    
+    This question is also popular as "Circular Tour"
+    Company Tags                : Google, Amazon, Uber, Microsoft, FactSet, Morgan Stanley, Zoho, Flipkart
+    Leetcode Link               : https://leetcode.com/problems/gas-station/
+*/
+
+//Approach-1 (Brute Force (O(n^2)) - TLE
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+
+        for(int i = 0; i<n; i++) {
+            
+            if(gas[i] < cost[i])
+                continue;
+            
+            int j = (i+1)%n;
+            
+            int currGas = gas[i];
+                
+            currGas = currGas - cost[i] + gas[j];
+            
+            while(j != i) {
+                if(currGas < cost[j])
+                    break;
+
+                currGas = currGas - cost[j] + gas[j];
+                
+                j = (j+1)%n;
+            }
+            
+            if(i == j)
+                return i;
+        }
+        
+        
+        return -1; 
+    }
+};
+
+
+//Approach-2 (Greedy O(n)) : Accepted
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+
+        int sumGas = accumulate(begin(gas), end(gas), 0);
+        
+        int sumCost = accumulate(begin(cost), end(cost), 0);
+        
+        if(sumGas < sumCost)
+            return -1;
+        
+        int total  = 0;
+        int result = 0;
+        
+        for(int i = 0; i<n; i++) {
+            
+            total += gas[i] - cost[i];
+            
+            if(total < 0) {
+                total = 0;
+                result = i+1;
+            }
+            
+        }
+        return result;
+    }
+};
