ec9e7b75d7d29f917918a04a0b352f0f4b87b875
MAZHAR <iiestshibpur605@gmail.com>
2023-01-10 16:34:10 +0530

    Added DFS approach as well

--- Graph/Topological Sorting/Course Schedule.cpp
+++ Graph/Topological Sorting/Course Schedule.cpp
@@ -5,6 +5,7 @@
     GfG Link                    : https://practice.geeksforgeeks.org/problems/prerequisite-tasks/1?
 */
 
+//Approach-1 (Using BFS Cycle Check - Kahn's Algorithm (Topological Sort)
 class Solution {
 public:
     
@@ -65,5 +66,50 @@ public:
         //if cycle is present, not possible
         
         return topologicalSortCheck(adj, numCourses, indegree);
+    }
+};
+
+
+//Approach-2 (Using DFS Cycle Check)
+class Solution {
+public:
+    bool isCycleDFS(unordered_map<int, vector<int>> &adj, int u, vector<bool>& visited, vector<bool>& inRecursion) {
+        visited[u] = true;
+        inRecursion[u] = true;
+        
+        
+        for(int &v : adj[u]) {
+            //if not visited, then we check for cycle in DFS
+            if(visited[v] == false && isCycleDFS(adj, v, visited, inRecursion))
+                return true;
+            else if(inRecursion[v] == true)
+                return true;
+        }
+        
+        inRecursion[u] = false;
+        return false;
+        
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        unordered_map<int, vector<int>> adj;
+        vector<bool> visited(numCourses, false);
+        vector<bool> inRecursion(numCourses, false);
+        
+        for(auto &vec : prerequisites) {
+            int a = vec[0];
+            int b = vec[1];
+            
+            //b ---> a
+            adj[b].push_back(a);
+        }
+        
+        for(int i = 0; i<numCourses; i++) {
+            if(!visited[i] && isCycleDFS(adj, i, visited, inRecursion))
+                return false;
+        }
+        
+        return true;
     }
 };
