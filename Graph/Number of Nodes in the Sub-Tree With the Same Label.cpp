8fc3328bed27b534528e480a4f13d7de404809ab
MAZHAR <iiestshibpur605@gmail.com>
2023-01-12 15:02:26 +0530

    Update Number of Nodes in the Sub-Tree With the Same Label.cpp

--- Graph/Number of Nodes in the Sub-Tree With the Same Label.cpp
+++ Graph/Number of Nodes in the Sub-Tree With the Same Label.cpp
@@ -7,24 +7,27 @@
 //Approach-1 (DFS - adding counts from children at every node)
 class Solution {
 public:
-    void DFS(unordered_map<int, vector<int>> &adj, int u, int parent, vector<int>& result, string &labels, vector<int>& count) {
-        
+    vector<int> DFS(unordered_map<int, vector<int>> &adj, int u, int parent, vector<int>& result, string &labels) {
+        vector<int> my_count(26, 0);
         char mylabel = labels[u];
         
-        int count_before_exploring_my_subtree = count[mylabel-'a'];
+        my_count[mylabel - 'a'] = 1;
         
-        
-        //explore my subtree children
-        count[mylabel-'a']++;
         for(int &v : adj[u]) {
             if(v == parent)
                 continue;
-            
-            DFS(adj, v, u, result, labels, count);
+        
+            vector<int> child_count(26, 0);
+            child_count = DFS(adj, v, u, result, labels);
+
+            for(int i = 0; i<26; i++) {
+                my_count[i] += child_count[i];
+            }
         }
         
-        int count_after_exploring_my_subtree = count[mylabel-'a'];
-        result[u] = count_after_exploring_my_subtree - count_before_exploring_my_subtree;
+        result[u] = my_count[mylabel - 'a'];
+        
+        return my_count;
     }
     
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
@@ -39,9 +42,8 @@ public:
         }
         
         vector<int> result(n, 0);
-        vector<int> count(26, 0);
         
-        DFS(adj, 0, -1, result, labels, count);
+        DFS(adj, 0, -1, result, labels);
         
         return result;
         
