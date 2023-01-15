90b9c2a89668764ab589788ca7c940f11a3b99c9
MAZHAR <iiestshibpur605@gmail.com>
2023-01-15 19:25:42 +0530

    Disjoint set (Union-Find)

---
+++ Graph/Disjoint Set/Disjoint set (Union-Find).cpp
@@ -0,0 +1,24 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=AsAdKHkITBQ&t=1625s
+    Company Tags                : Google, Facebook, Apple, Amazon, Netflix, Flipkart
+    GfG Link                    : https://practice.geeksforgeeks.org/problems/disjoint-set-union-find/1
+*/
+
+/*Complete the functions below*/
+int find(int A[],int x)
+{
+       if(A[x] == x)
+        return x;
+        
+        return A[x] = find(A, A[x]);
+}
+void unionSet(int A[],int X,int Z)
+{
+	int x = find(A, X);
+	int y = find(A, Z);
+	
+	if(x == y)
+	    return;
+	
+	A[x] = y;
+}
