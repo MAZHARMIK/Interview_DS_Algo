a92c4528cca5bf8cf95b7543af32170fbd91e208
MAZHAR <iiestshibpur605@gmail.com>
2023-01-17 12:40:15 +0530

    Flip String to Monotone Increasing

---
+++ DP/Flip String to Monotone Increasing.cpp
@@ -0,0 +1,56 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : GOOGLE
+    Leetcode Link               : https://leetcode.com/problems/flip-string-to-monotone-increasing/
+*/
+
+//Approach-1 (Using Recursion + Memoization)
+class Solution {
+public:
+    int n;
+    
+    int solve(string &s, int curr_index, int prev_val, vector<vector<int>>& t) {
+        if(curr_index >= n)
+            return 0;
+        
+        if(t[curr_index][prev_val] != -1)
+            return t[curr_index][prev_val];
+        
+        int flip    = INT_MAX;
+        int no_flip = INT_MAX;
+
+        if(s[curr_index] == '0') {
+            
+            if(prev_val == 1) {
+                flip = 1 + solve(s, curr_index+1, 1, t);
+            } else {
+                flip = 1 + solve(s, curr_index+1, 1, t);
+                no_flip = solve(s, curr_index+1, 0, t);
+            }
+        }
+
+        else if(s[curr_index] == '1')
+        {
+            if(prev_val == 1) {
+                no_flip = solve(s, curr_index+1, 1, t);
+            } else {
+                flip    = 1 + solve(s, curr_index+1, 0, t);
+                no_flip = solve(s, curr_index+1, 1, t);
+            }
+        }
+
+        return t[curr_index][prev_val] =  min(flip, no_flip);
+    }
+    
+    int minFlipsMonoIncr(string s) {
+        n = s.length();
+        
+        vector<vector<int>> t(n+1, vector<int>(2, -1));
+        
+        return solve(s, 0, 0, t);
+    }
+};
+
+
+//Approach-2 (Better Optimal Solution)
+//soon
