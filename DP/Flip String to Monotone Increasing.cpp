68f6e75e45c3124cc45aa93377faffe4272c57d1
MAZHAR <iiestshibpur605@gmail.com>
2023-01-17 18:59:12 +0530

    Added optimal approach

--- DP/Flip String to Monotone Increasing.cpp
+++ DP/Flip String to Monotone Increasing.cpp
@@ -54,4 +54,20 @@ public:
 
 
 //Approach-2 (Better Optimal Solution)
-//soon
+class Solution {
+public:
+    int minFlipsMonoIncr(string s) {
+        int count_of_ones = 0;
+        int flips = 0;
+        
+        for(char &ch : s) {
+            if(ch == '1')
+                count_of_ones++;
+            else {
+                flips = min(flips+1, count_of_ones);
+            }
+        }
+        
+        return flips;
+    }
+};
