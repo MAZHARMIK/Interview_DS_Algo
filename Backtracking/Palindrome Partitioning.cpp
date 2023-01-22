15a00705f063dc44ec9d52fd3fdf176d8e497b39
MAZHAR <iiestshibpur605@gmail.com>
2023-01-22 14:34:34 +0530

    clean code

--- Backtracking/Palindrome Partitioning.cpp
+++ Backtracking/Palindrome Partitioning.cpp
@@ -8,34 +8,53 @@
 //Whenever a question asks for "Generating all possible" something, think about Backtracking once
 class Solution {
 public:
-    bool isPallindrome(string& s, int l, int r) {
+    int n;
+    
+    bool isPalindrome(string &s, int l, int r) {
+        
         while(l < r) {
             if(s[l] != s[r])
                 return false;
             l++;
             r--;
         }
+        
         return true;
+        
     }
-    void solve(string s, int idx, vector<string> curr, vector<vector<string>>& result) {
-        if(idx >= s.length()) {
+    
+    void backtrack(string &s, int idx, vector<string> curr, vector<vector<string>> &result) {
+        
+        if(idx == n) {
             result.push_back(curr);
             return;
         }
         
-        for(int i = idx; i<s.length(); i++) {
-            if(isPallindrome(s, idx, i)) {
+        
+        for(int i = idx; i<n; i++) {
+            
+            if(isPalindrome(s, idx, i)) {
+                
                 curr.push_back(s.substr(idx, i-idx+1));
-                solve(s, i+1, curr, result);
+                
+                backtrack(s, i+1, curr, result);
+                
                 curr.pop_back();
+                
             }
+            
         }
+        
     }
     
     vector<vector<string>> partition(string s) {
+        n = s.length();
         vector<vector<string>> result;
         vector<string> curr;
-        solve(s, 0, curr, result);
+        
+        backtrack(s, 0, curr, result);
+        
         return result;
+        
     }
 };
