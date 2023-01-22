1ae02400d9bc7b5f98328bd17b38b82d78b64401
MAZHAR <iiestshibpur605@gmail.com>
2023-01-22 11:23:18 +0530

    Added GfG Link

--- DP/Palindrome Partitioning.cpp
+++ DP/Palindrome Partitioning.cpp
@@ -2,6 +2,7 @@
     MY YOUTUBE VIDEO ON THIS Qn : <soon>
     Company Tags                : Google, Amazon, Microsoft, Meta
     Leetcode Link               : https://leetcode.com/problems/palindrome-partitioning/
+    GfG Link                    : https://practice.geeksforgeeks.org/problems/find-all-possible-palindromic-partitions-of-a-string/1
 */
 
 //Whenever a question asks for "Generating all possible" something, think about Backtracking once
@@ -30,6 +31,7 @@ public:
             }
         }
     }
+    
     vector<vector<string>> partition(string s) {
         vector<vector<string>> result;
         vector<string> curr;
