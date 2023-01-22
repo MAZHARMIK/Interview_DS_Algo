941c5deae7e477f004caa32fd942b84017b31411
MAZHAR <iiestshibpur605@gmail.com>
2023-01-22 20:54:56 +0530

    Detect Cycle using DSU

---
+++ Graph/Disjoint Set/Detect Cycle using DSU.cpp
@@ -0,0 +1,56 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Compnay Tags                : Google, Microsoft
+    GfG Link                    : https://practice.geeksforgeeks.org/problems/detect-cycle-using-dsu/1
+*/
+
+class Solution
+{
+    public:
+    vector<int> parent;
+    vector<int> rank;
+    
+    int find(int x) {
+        if(parent[x] == x)
+            return x;
+        
+        return parent[x] = find(parent[x]);
+    }
+    
+    void Union(int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+ 
+    
+        if(rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else if(rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        } else {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
+    
+    //Function to detect cycle using DSU in an undirected graph.
+	int detectCycle(int V, vector<int>adj[]) {
+	    parent.resize(V);
+	    rank.resize(V, 0);
+	    
+	    for(int i = 0; i<V; i++)
+	        parent[i] = i;
+	       
+	    for(int u = 0; u<V; u++) {
+	        for(int &v : adj[u]) {
+	            if(u < v) {
+    	            if(find(u) == find(v))
+    	                return true;
+    	            else {
+    	                Union(u, v);
+    	            }
+	            }
+	        }
+	    }
+	    return false;
+	}
+};
