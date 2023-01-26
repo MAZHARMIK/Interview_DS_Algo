9d1f8c2cf475ef06511e336bee1104638edeb2ba
MAZHAR <iiestshibpur605@gmail.com>
2023-01-26 10:23:00 +0530

    Cheapest Flights Within K Stops

---
+++ Graph/Cheapest Flights Within K Stops.cpp
@@ -0,0 +1,59 @@
+/*
+      MY YOUTUBE VIDEO ON THIS Qn : <soon>
+      Company Tags                : GOOGLE
+      Leetcode Link               : https://leetcode.com/problems/cheapest-flights-within-k-stops/
+      GfG Link                    : https://practice.geeksforgeeks.org/problems/cheapest-flights-within-k-stops/1
+*/
+
+//Approach-1 (BFS)
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<int> distance(n, INT_MAX);
+        
+        unordered_map<int, vector<pair<int, int>>> adj;
+        
+        for(vector<int> &vec : flights) {
+            int u    = vec[0];
+            int v    = vec[1];
+            int cost = vec[2];
+            
+            adj[u].push_back({v, cost});
+        }
+        
+        queue<pair<int, int>> que;
+        que.push({src, 0});
+        distance[src] = 0;
+        
+        int level = 0;
+        
+        while(!que.empty() && level <= k) {
+            int N = que.size();
+            
+            while(N--) {
+                int u = que.front().first;
+                int d = que.front().second;
+                que.pop();
+                
+                for(pair<int, int> &P : adj[u]) {
+                    
+                    int v    = P.first;
+                    int cost = P.second;
+                    
+                    if(distance[v] > d + cost) {
+                        distance[v] = d + cost;
+                        que.push({v, d+cost});
+                    }
+                    
+                }
+                
+            }
+            level++;
+        }
+        
+        return distance[dst] == INT_MAX ? -1 : distance[dst];
+    }
+};
+
+
+//Other Approaches coming soon with youTube video link
