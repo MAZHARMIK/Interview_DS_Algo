8d4e26c99ccbc09c26956c9d3b64c0d6797ed96e
MAZHAR <iiestshibpur605@gmail.com>
2023-01-29 16:27:51 +0530

    Added YouTube Link

--- Design/LRU Cache.cpp
+++ Design/LRU Cache.cpp
@@ -1,6 +1,6 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : <soon>
-    Company Tags                : Adobe, Microsoft, Amazon, Citygroup, Twitch
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=81h8O-0U5oo
+    Company Tags                : Adobe, Microsoft, Amazon, Citygroup, Twitch and many more (see my video above to find all companies)
     Leetcode Link               : https://leetcode.com/problems/lru-cache/
 */
 
