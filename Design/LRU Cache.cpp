018608afa2d7d8ae8a3d1d5f1ded22ee75226d6a
MAZHAR <iiestshibpur605@gmail.com>
2023-01-29 15:33:51 +0530

    Updated brute force with small improvement

--- Design/LRU Cache.cpp
+++ Design/LRU Cache.cpp
@@ -7,52 +7,52 @@
 //Approach-1 (Brute Force)
 class LRUCache {
 public:
-    vector<pair<int,int>> cache;
-    int cap ;
-    LRUCache(int capacity) {
-        cap = capacity;
-        
-    }
     
+    vector<pair<int, int>> cache;
+    int n;
+    
+    LRUCache(int capacity) {
+        n = capacity;
+    }
     
     int get(int key) {
         
-        for(int i=0;i<cache.size();i++){
-            if(cache[i].first==key){
+        for(int i = 0; i<cache.size(); i++) {
+            
+            if(cache[i].first == key) {
                 int val = cache[i].second;
-                pair<int,int> temp = cache[i];
+                
+                pair<int, int> temp = cache[i];
                 cache.erase(cache.begin()+i);
                 cache.push_back(temp);
                 
-                return val;             
+                return val;
             }
+            
         }
+        
         return -1;
         
     }
     
     void put(int key, int value) {
-        bool found= false;
-        for(int i=0;i<cache.size();i++){
-            if(cache[i].first==key){
-                found =true;
+        
+        for(int i = 0; i<cache.size(); i++) {
+            
+            if(cache[i].first == key) {
                 cache.erase(cache.begin()+i);
-                cache.push_back({key,value});
-                break;
+                cache.push_back({key, value});
+                return;
             }
         }
-        if(found)
-            return ;
-        else{
-            if(cache.size() == cap){
-                cache.erase(cache.begin());
-                cache.push_back({key,value});
-            }
-            else{
-                cache.push_back({key,value});
-            }
+        
+        if(cache.size() == n) {
+            cache.erase(cache.begin());
+            cache.push_back({key, value});
+        } else {
+            cache.push_back({key, value});
         }
-      
+        
     }
 };
 
