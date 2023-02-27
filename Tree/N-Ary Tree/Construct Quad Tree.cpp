8a5a9e756a3922231685f734c0169e348a8b3dbd
MAZHAR <iiestshibpur605@gmail.com>
2023-02-27 10:58:43 +0530

    Added YouTube video

--- Tree/N-Ary Tree/Construct Quad Tree.cpp
+++ Tree/N-Ary Tree/Construct Quad Tree.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=tzABQFpl6Mk
     Company Tags                : GOOGLE
     Leetcode Link               : https://leetcode.com/problems/construct-quad-tree/
 */
