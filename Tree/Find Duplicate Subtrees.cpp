bbf89f0fd5d64261b94a9fd1bd8e348cd8772e70
MAZHAR <iiestshibpur605@gmail.com>
2023-02-28 09:47:10 +0530

    Find Duplicate Subtrees

---
+++ Tree/Find Duplicate Subtrees.cpp
@@ -0,0 +1,35 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : GOOGLE
+    Leetcode Link               : https://leetcode.com/problems/find-duplicate-subtrees/
+*/
+
+class Solution {
+public:
+    
+        
+    string DFS(TreeNode* root, unordered_map<string, int>& mp, vector<TreeNode*>& res){
+        if(root == NULL) 
+            return "NULL";
+        
+        string s = to_string(root->val) + "," + DFS(root->left, mp, res) + "," + DFS(root->right, mp, res);
+        
+        if(mp[s] == 1)
+            res.push_back(root);
+        
+        mp[s]++;
+        
+        return s;
+    }
+    
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        unordered_map<string, int> mp;
+        
+        vector<TreeNode*>res;
+        
+        DFS(root, mp, res);
+        
+        return res;
+    }
+
+};
