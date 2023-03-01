6bb10efa4df9a92dd9c3dbd6c46812ce73b84207
MAZHAR <iiestshibpur605@gmail.com>
2023-03-01 12:46:50 +0530

    Sort an Array

---
+++ Arrays/Sort an Array.cpp
@@ -0,0 +1,33 @@
+/*
+    MY YUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags               : Microsoft, Goldman Sachs, Cisco
+    Leetcode Link              : https://leetcode.com/problems/sort-an-array/
+*/
+
+//solving using Counting Sort (O(n+k))
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int minE = *min_element(begin(nums), end(nums));
+        int maxE = *max_element(begin(nums), end(nums));
+        
+        unordered_map<int, int> mp;
+        
+        for(int &num : nums)
+            mp[num]++;
+        
+        int i = 0;
+        
+        for(int num = minE; num <= maxE; num++) {
+            
+            while(mp[num] > 0) {
+                nums[i] = num;
+                i++;
+                mp[num]--;
+            }
+            
+        }
+        
+        return nums;
+    }
+};
