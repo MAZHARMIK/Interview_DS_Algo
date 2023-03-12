9e9f209fd0d2ba0391b197874cba8e1619e57010
MAZHAR <iiestshibpur605@gmail.com>
2023-03-12 08:16:34 +0530

    Added Better and clean approach

---
+++ Linked List/Merge k Sorted Lists.cpp
@@ -0,0 +1,65 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : VMWare, Amazon, Uber, Google, Twitter, LinkedIn, Airbnb, Facebook, Microsoft, IXL
+    Leetcode Link               : https://leetcode.com/problems/merge-k-sorted-lists/
+*/
+
+
+//Approach-1 (Using merge sort) - Time : O(log(n) * n * k)
+/*
+    Where n = total number of lists
+    and k = average length of every linkedlist
+    Why ?
+    Because, The merge in the higher layer has more nodes, 
+    the lists are longer than the lists in the bottoms. 
+    But the number of operation of merge is the same in every level(which is n*k). 
+    and log(n) comes from "partitionAndMerge" function. We will have log(n) levels and on each level we do a merge.
+*/
+class Solution {
+public:
+    ListNode* mergeTwoSortedLists(ListNode* l1, ListNode* l2) {
+        if(!l1)
+            return l2;
+        if(!l2)
+            return l1;
+
+        if(l1->val <= l2->val) {
+            l1->next = mergeTwoSortedLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoSortedLists(l1, l2->next);
+            return l2;
+        }
+        
+        return NULL;
+    }
+    
+    ListNode* partitionAndMerge(int start, int end, vector<ListNode*>& lists) {
+        if(start == end)
+            return lists[start];
+        
+        if(start > end)
+            return NULL;
+        
+        int mid = start + (end-start)/2;
+        
+        ListNode* l1 = partitionAndMerge(start, mid, lists);
+        ListNode* l2 = partitionAndMerge(mid+1, end, lists);
+        
+        return mergeTwoSortedLists(l1, l2);
+    }
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+        int n = lists.size();
+        
+        if(n == 0)
+            return NULL;
+        
+        return partitionAndMerge(0, n-1, lists);
+        
+    }
+};
+
+
+//Approach-2 - (Using Priority Queue) -> SOON
