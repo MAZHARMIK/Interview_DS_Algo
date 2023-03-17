f9aec6b7083d6a6f7b158bfcdba1717b9d9c7981
MAZHAR <iiestshibpur605@gmail.com>
2023-03-17 15:27:50 +0530

    Implement Trie (Prefix Tree)

---
+++ Trie/Implement Trie (Prefix Tree).cpp
@@ -0,0 +1,77 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Microsoft
+    Leetcode Link               : https://leetcode.com/problems/implement-trie-prefix-tree/
+*/
+
+class Trie {
+public:
+    
+    struct trieNode { 
+        trieNode *children[26]; 
+        bool isEndOfWord; 
+    };
+    
+    trieNode* getNode() {
+        trieNode* newNode = new trieNode();
+        newNode->isEndOfWord = false;
+        
+        for (int i = 0; i < 26; i++) {
+			newNode->children[i] = NULL;
+		}
+        
+        return newNode;
+    }
+    
+    /** Initialize your data structure here. */
+    trieNode* root;
+    
+    Trie() {
+        root = getNode();
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        trieNode *crawler = root; 
+        
+        for (int i = 0; i < word.length(); i++) { 
+            int index = word[i] - 'a'; 
+            if (!crawler->children[index]) 
+                crawler->children[index] = getNode();
+            
+            crawler = crawler->children[index]; 
+        }
+        
+        crawler->isEndOfWord = true; 
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        trieNode *crawler = root; 
+        
+        for (int i = 0; i < word.length(); i++) { 
+            int index = word[i] - 'a'; 
+            if (!crawler->children[index]) 
+                return false; 
+            crawler = crawler->children[index]; 
+        }
+        return (crawler != NULL && crawler->isEndOfWord); 
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        trieNode *crawler = root;
+        int i = 0;
+        for (i = 0; i < prefix.length(); i++) { 
+            int index = prefix[i] - 'a'; 
+            
+            if (!crawler->children[index]) 
+                return false; 
+            
+            crawler = crawler->children[index]; 
+        }
+        if(i==prefix.length())
+            return true;
+        return false;
+    }
+};
