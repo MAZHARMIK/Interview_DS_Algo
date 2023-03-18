d8281dd473882729fcc51b241365be0fa6b57e3c
MAZHAR <iiestshibpur605@gmail.com>
2023-03-18 21:51:37 +0530

    Design Browser History

---
+++ Design/Design Browser History.cpp
@@ -0,0 +1,54 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : META
+    Leetcode Link               : https://leetcode.com/problems/design-browser-history/
+*/
+
+class BrowserHistory {
+public:
+    stack<string> past; //back
+    stack<string> future; //future
+    string curr;
+    
+    BrowserHistory(string homepage) {
+        curr = homepage;
+    }
+    
+    void visit(string url) {
+        
+        past.push(curr);
+        curr = url;
+        
+        future = stack<string>();
+        
+    }
+    
+    string back(int steps) {
+        
+        while(steps > 0 && !past.empty()) {
+            
+            future.push(curr);
+            curr = past.top();
+            past.pop();
+            steps--;
+            
+        }
+        
+        return curr;
+    }
+    
+    string forward(int steps) {
+        
+        while(steps > 0 && !future.empty()) {
+            
+            past.push(curr);
+            curr = future.top();
+            future.pop();
+            steps--;
+            
+        }
+        
+        return curr;
+        
+    }
+};
