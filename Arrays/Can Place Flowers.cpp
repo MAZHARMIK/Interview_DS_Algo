4f163bee15b53516abc1f983e28d2511b23e7d84
MAZHAR <iiestshibpur605@gmail.com>
2023-03-20 19:08:10 +0530

    Can Place Flowers

---
+++ Arrays/Can Place Flowers.cpp
@@ -0,0 +1,36 @@
+/*
+      MY YOUTUBE VIDEO ON THIS Qn : <soon>
+      Company Tags                : 
+      Leetcode Link               : https://leetcode.com/problems/can-place-flowers/
+*/
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int l = flowerbed.size();
+        
+        
+        
+        for(int i = 0; i<l; i++) {
+            
+            if(flowerbed[i] == 0) {
+                
+                bool leftEmpty   = (i==0) || (flowerbed[i-1] == 0);
+                
+                bool rightEmpty  = (i == l-1) || (flowerbed[i+1] == 0);
+                
+                if(leftEmpty && rightEmpty) {
+                    flowerbed[i] = 1;
+                    n--;
+                    if(n <= 0)
+                        return true;
+                }
+                
+            }
+            
+        }
+        
+        return n <= 0;
+        
+    }
+};
