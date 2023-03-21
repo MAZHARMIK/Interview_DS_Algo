b85e3968fa5a26ffb6ba8495fd444acbd792ce87
MAZHAR <iiestshibpur605@gmail.com>
2023-03-21 12:29:57 +0530

    Number of Zero-Filled Subarrays

---
+++ Arrays/Number of Zero-Filled Subarrays.cpp
@@ -0,0 +1,63 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : META
+    Leetcode Link               : https://leetcode.com/problems/number-of-zero-filled-subarrays/
+*/
+
+//Approach-1 (Using simple math to calculate count of subarrays)
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long result = 0;
+        
+        int n = nums.size();
+        
+        int i = 0;
+        while(i < n) {
+            
+            long long zeros = 0;
+            
+            if(nums[i] == 0) {
+                
+                while(i < n && nums[i] == 0) {
+                    i++;
+                    zeros++;
+                }
+                
+            } else {
+                    i++;
+            }
+            result += (zeros)*(zeros+1)/2;
+        }
+        
+        return result;
+        
+    }
+};
+
+//Approach-2 (Better and clean approach using observation)
+//Watch my YouTube video above to see the explanation on why this works
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long result = 0;
+        
+        int n = nums.size();
+        
+        int countSubArray = 0;
+        
+        for(int i = 0; i<n; i++) {
+            
+            if(nums[i] == 0)
+                countSubArray += 1;
+            else
+                countSubArray = 0;
+            
+            result += countSubArray;
+            
+        }
+        
+        return result;
+        
+    }
+};
