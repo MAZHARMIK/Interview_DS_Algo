6c8cf2c2a07b08d2cc48392e915d8b59a818264b
MAZHAR <iiestshibpur605@gmail.com>
2023-03-22 20:33:19 +0530

    Added YouTube link

--- Graph/BFS_DFS/Minimum Score of a Path Between Two Cities.cpp
+++ Graph/BFS_DFS/Minimum Score of a Path Between Two Cities.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=blUPwDof-r4
     Company Tags                : GOOGLE
     Leetcode Link               : https://leetcode.com/problems/minimum-score-of-a-path-between-two-cities/
 */
