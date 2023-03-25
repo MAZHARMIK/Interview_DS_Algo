202dee8016dd456b06c34a3a20525e8ff54aec33
MAZHAR <iiestshibpur605@gmail.com>
2023-03-25 12:06:45 +0530

    Count Unreachable Pairs of Nodes in an Undirected Graph

---
+++ Graph/BFS_DFS/Count Unreachable Pairs of Nodes in an Undirected Graph.cpp
@@ -0,0 +1,110 @@
+/*
+      MY YOUTUBE VIDEO ON THIS Qn : <soon>
+      Company Tags                : MICROSOFT
+      Leetcode Link               : https://leetcode.com/problems/count-unreachable-pairs-of-nodes-in-an-undirected-graph/
+*/
+
+
+//Using DFS - (Remaining Nodes concept)
+class Solution {
+public:
+    
+    void dfs(int u, unordered_map<int, vector<int>> &adj, vector<bool>& visited, long long &sizeOfComponent) {
+        visited[u] = true;
+        sizeOfComponent++;
+        
+        for(int &v : adj[u]) {
+            
+            if(!visited[v]) {
+                dfs(v, adj, visited, sizeOfComponent);
+            }
+            
+        }
+    }
+    
+    long long countPairs(int n, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adj;
+        
+        for(auto &vec : edges) {
+            
+            int u = vec[0];
+            int v = vec[1];
+            
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
+        vector<bool> visited(n, false);
+        long long remainingNodes = n;
+        
+        long long result = 0;
+        
+        for(int i = 0; i<n; i++) {
+            if(!visited[i]) {
+                
+                long long sizeOfComponent = 0;
+                
+                dfs(i, adj, visited, sizeOfComponent);
+                
+                result += (sizeOfComponent) * (remainingNodes-sizeOfComponent);
+                
+                remainingNodes -= sizeOfComponent;
+                
+            }
+        }
+        
+        return result;
+    }
+};
+
+
+//Using DFS (Calculating duplicates and divide by 2)
+class Solution {
+public:
+    
+    void dfs(int u, unordered_map<int, vector<int>> &adj, vector<bool>& visited, long long &sizeOfComponent) {
+        visited[u] = true;
+        sizeOfComponent++;
+        
+        for(int &v : adj[u]) {
+            
+            if(!visited[v]) {
+                dfs(v, adj, visited, sizeOfComponent);
+            }
+            
+        }
+    }
+    
+    long long countPairs(int n, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adj;
+        
+        for(auto &vec : edges) {
+            
+            int u = vec[0];
+            int v = vec[1];
+            
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
+        vector<bool> visited(n, false);
+        
+        long long result = 0;
+        
+        for(int i = 0; i<n; i++) {
+            if(!visited[i]) {
+                
+                long long sizeOfComponent = 0;
+                
+                dfs(i, adj, visited, sizeOfComponent);
+                
+                result += (sizeOfComponent) * (n-sizeOfComponent);
+                
+            }
+        }
+        
+        return result/2;
+    }
+};
+
+//Using BFS and DSU will be provided soon
