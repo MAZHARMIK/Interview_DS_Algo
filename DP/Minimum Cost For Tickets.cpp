494657e1c03a181a697f733d38cdade87ea6115b
MAZHAR <iiestshibpur605@gmail.com>
2023-03-28 14:41:13 +0530

    Added one more approach

--- DP/Minimum Cost For Tickets.cpp
+++ DP/Minimum Cost For Tickets.cpp
@@ -3,6 +3,8 @@
     Company Tags                : GOOGLE
     Leetcode link               : https://leetcode.com/problems/minimum-cost-for-tickets/
 */
+
+
 //Approach-1 (Recursion + Memoized)  [Time : O(max_day)] [Space : O(n)] //n = size of days vector
 class Solution {
 public:
@@ -81,5 +83,61 @@ public:
     
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         return bottomUp(days, costs);     
+    }
+};
+
+
+//Approach-3 (Moving from i = n-1 to 0
+//Simply replacing recursive calls with t
+class Solution {
+public:
+    
+    int solve_bottom_up(vector<int>& days, vector<int>& costs, int n ) {
+        
+        vector<int> t(n+1, INT_MAX);
+        t[n] = 0;
+        
+        for(int i = n-1; i>=0; i--) {
+            
+            //1 day pass
+            int cost_1 = costs[0] + t[i+1];
+
+
+            //7 day pass
+            int j = i;
+            int max_day = days[i] + 7;
+
+            while(j < n && days[j] < max_day) {
+                j++;
+            }
+
+            int cost_7 = costs[1] + t[j];
+            
+            
+            //30 day pass
+            j = i;
+            max_day = days[i] + 30;
+
+            while(j < n && days[j] < max_day) {
+                j++;
+            }
+
+            int cost_30 = costs[2] + solve(days, costs, n, j);
+
+            t[i] = min({cost_1, cost_7, cost_30});
+        }
+        
+        return t[0];
+    }
+    
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int n = days.size();
+        
+        memset(t, -1, sizeof(t));
+        
+        //return solve(days, costs, n, 0);
+        
+        return solve_bottom_up(days, costs, n);
+        
     }
 };
