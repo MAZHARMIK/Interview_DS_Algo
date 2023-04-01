ffe7e4332d11f01b28678ffb55a02f4b70bf0ec0
MAZHAR <iiestshibpur605@gmail.com>
2023-04-01 14:26:00 +0530

    Binary Search

---
+++ Arrays/Binary Search/Binary Search.cpp
@@ -0,0 +1,57 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Everyone :-)
+    Leetcode Link               : https://leetcode.com/problems/binary-search/
+*/
+
+//Iterative : O(log(n))
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        
+        int l = 0, r = n-1;
+        
+        int mid = 0;
+        
+        while(l <= r) {
+            
+            mid = l + (r-l)/2;
+            
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                l = mid+1;
+            else
+                r = mid-1;
+        }
+        
+        return -1;
+    }
+};
+
+
+//Recursive : O(log(n))
+class Solution {
+public:
+    
+    int solve(vector<int>& nums, int l, int r, int target) {
+        if(l > r)
+            return -1;
+        
+        int mid = l + (r-l)/2;
+        
+        if(nums[mid] == target)
+            return mid;
+        else if(nums[mid] < target)
+            return solve(nums, mid+1, r, target);
+        else
+            return solve(nums, l, mid-1, target);
+    }
+    
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        
+        return solve(nums, 0, n-1, target);
+    }
+};
