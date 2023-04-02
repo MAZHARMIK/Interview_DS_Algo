699ab0b9bcac6078afb7e8c3d0ddb6a5b1d4a0e7
MAZHAR <iiestshibpur605@gmail.com>
2023-04-02 13:13:44 +0530

    Added YouTube Link

--- Arrays/Binary Search/Successful Pairs of Spells and Potions.cpp
+++ Arrays/Binary Search/Successful Pairs of Spells and Potions.cpp
@@ -1,5 +1,5 @@
 /*
-      MY YOUTUBE VIDEO ON THIS Qn : <soon>
+      MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=QZI4TCVckpA
       Company Tags                : MICROSOFT
       Leetcode Link               : https://leetcode.com/problems/successful-pairs-of-spells-and-potions/
 */
