10346f498bb05c2428dd06b7f0e9746923aeac8d
MAZHAR <iiestshibpur605@gmail.com>
2023-04-04 09:00:36 +0530

    Optimal Partition of String

---
+++ Greedy/Optimal Partition of String.cpp
@@ -0,0 +1,25 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : AMAZON
+    Leetcode Link               : https://leetcode.com/problems/optimal-partition-of-string/
+*/
+
+class Solution {
+public:
+    int partitionString(string s) {
+        vector<int> lastSeen(26, -1);
+        
+        int count = 0;
+        int substringStart = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (lastSeen[s[i] - 'a'] >= substringStart) {
+                count++;
+                substringStart = i;
+            }
+            lastSeen[s[i] - 'a'] = i;
+        }
+
+        return count+1;
+    }
+};
