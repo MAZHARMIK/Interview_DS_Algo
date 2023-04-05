b9c75fb394b09f48d7a293dc8200c93dc36c6efe
MAZHAR <iiestshibpur605@gmail.com>
2023-04-05 12:05:24 +0530

    Minimize Maximum of Array

---
+++ Arrays/Binary Search/Minimize Maximum of Array.cpp
@@ -0,0 +1,52 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Meta
+    Leetcode Link               : https://leetcode.com/problems/minimize-maximum-of-array/
+*/
+
+class Solution {
+public:
+    
+    bool isValid(vector<int> &nums, int mid_max, int n) {
+        
+        vector<long long> arr(begin(nums), end(nums));
+        
+        for(int i = 0; i<n-1; i++) {
+            
+            if(arr[i] > mid_max)
+                return false;
+            
+            long long buffer = mid_max - arr[i];
+            
+            arr[i+1] = arr[i+1] - buffer;   
+            
+        }
+        
+        return arr[n-1] <= mid_max;
+        
+    }
+    
+    int minimizeArrayValue(vector<int>& nums) {
+        int n = nums.size();
+        
+        int maxL = 0;
+        int maxR = *max_element(begin(nums), end(nums));
+        
+        int result;
+        
+        while(maxL <= maxR) {
+            
+            int mid_max = maxL + (maxR-maxL)/2;
+            
+            if(isValid(nums, mid_max, n)) {
+                result = mid_max;
+                maxR   = mid_max-1;
+            } else {
+                maxL = mid_max+1;
+            }
+            
+        }
+        
+        return result;
+    }
+};
