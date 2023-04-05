a47b5958baed88c079685ba39fe4fd8aeac797d8
MAZHAR <iiestshibpur605@gmail.com>
2023-04-05 13:19:05 +0530

    Added YouTube Link

--- Arrays/Binary Search/Minimize Maximum of Array.cpp
+++ Arrays/Binary Search/Minimize Maximum of Array.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=lWU1wI7koGk
     Company Tags                : Meta
     Leetcode Link               : https://leetcode.com/problems/minimize-maximum-of-array/
 */
