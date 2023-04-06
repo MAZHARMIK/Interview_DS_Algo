bbbf997c981d8268c01ed2e870b4fa8eed55a2ec
MAZHAR <iiestshibpur605@gmail.com>
2023-04-06 20:09:35 +0530

    Number of Closed Islands

---
+++ Arrays/2-D Array/DFS/Number of Closed Islands.cpp
@@ -0,0 +1,53 @@
+/*
+      MY YOUTUBE VIDEO ON THIS Qn : <soon>
+      COMPANY TAGS                : GOOGLE
+      Leetcode Link               : https://leetcode.com/problems/number-of-closed-islands/
+*/
+
+//Approach-1 (Using DFS)
+class Solution {
+public:
+    
+    int m, n;
+    bool dfs(vector<vector<int>> &grid, int r, int c) {
+        
+        if(r < 0 || r >= m || c < 0 || c >= n)
+            return false; //Not closed
+        
+        if(grid[r][c] == 1)
+            return true; //closed here
+        
+        grid[r][c] = 1;
+        
+        bool left  = dfs(grid, r, c-1);
+        bool right = dfs(grid, r, c+1);
+        bool up    = dfs(grid, r-1, c);
+        bool down  = dfs(grid, r+1, c);
+        
+        return left && right && up && down;
+        
+    }
+    
+    int closedIsland(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        
+        int count = 0;
+        
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                
+                if(grid[i][j] == 0) {
+                    count += dfs(grid, i, j);
+                }
+                
+            }
+        }
+        
+        return count;
+    }
+};
+
+
+//Approach-2 (Using BFS)
+//soon I will share BFS too
