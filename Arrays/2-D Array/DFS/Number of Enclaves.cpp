e53deeadd5ad18e15fad4f60990c0f5076b41f20
MAZHAR <iiestshibpur605@gmail.com>
2023-04-07 10:02:05 +0530

    Number of Enclaves

---
+++ Arrays/2-D Array/DFS/Number of Enclaves.cpp
@@ -0,0 +1,73 @@
+/*
+      MY YOUTUBE VIDEO ON THIS Qn : <soon>
+      Company Tags                : AMAZON
+      Leetcode Link               : https://leetcode.com/problems/number-of-enclaves/
+*/
+
+//Approach-1 (DFS)
+class Solution {
+public:
+    
+    int m, n;
+    void dfs(vector<vector<int>> &grid, int r, int c) {
+        
+        if(r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == 0)
+            return;
+
+        
+        grid[r][c] = 0;
+        
+        
+        dfs(grid, r, c-1);
+        dfs(grid, r, c+1);
+        dfs(grid, r-1, c);
+        dfs(grid, r+1, c);
+        
+    }
+    
+    int numEnclaves(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        
+        for (int i = 0; i < m; ++i) {
+            // First column.
+            if (grid[i][0] == 1) {
+                dfs(grid, i, 0);
+            }
+            // Last column.
+            if (grid[i][n - 1] == 1) {
+                dfs(grid, i, n - 1);
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            // First row.
+            if (grid[0][i] == 1) {
+                dfs(grid, 0, i);
+            }
+            // Last row.
+            if (grid[m - 1][i] == 1) {
+                dfs(grid, m - 1, i);
+            }
+        }
+        
+        
+        
+        int count = 0;
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                
+                if(grid[i][j] == 1) {
+                    count += 1;
+                }
+                
+            }
+        }
+        
+        return count;
+    }
+};
+
+
+//Approach-2 (Using BFS)
+//will soon share here
