cf7a3fc8b369323268f15e86ab6e8dd085faae34
MAZHAR <iiestshibpur605@gmail.com>
2023-04-08 11:38:17 +0530

    Added YouTube Link

--- Graph/Clone_Graph.cpp
+++ Graph/Clone_Graph.cpp
@@ -1,28 +1,9 @@
 /*
-    Company Tags     : Facebook, Amazon, Google
-    Leetcode Qn Link : https://leetcode.com/problems/clone-graph/
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Google, Facebook, Amazon
+    Leetcode Qn Link            : https://leetcode.com/problems/clone-graph/
 */
 
-/*
-    // Definition for a Node.
-    class Node {
-    public:
-        int val;
-        vector<Node*> neighbors;
-        Node() {
-            val = 0;
-            neighbors = vector<Node*>();
-        }
-        Node(int _val) {
-            val = _val;
-            neighbors = vector<Node*>();
-        }
-        Node(int _val, vector<Node*> _neighbors) {
-            val = _val;
-            neighbors = _neighbors;
-        }
-    };
-*/
 //Approach-1 (DFS)
 class Solution {
 public:
