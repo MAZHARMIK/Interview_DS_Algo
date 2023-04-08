d4a4455e05de33f6a29eeba8f7da6a19460cb5a1
MAZHAR <iiestshibpur605@gmail.com>
2023-04-09 00:16:08 +0530

    Dijkstra’s Algorithm(set)

---
+++ Graph/Dijkstra’s Algorithm(set).cpp
@@ -0,0 +1,48 @@
+/*
+	MY YOUTUBE VIDEO ON THIS Qn   : <soon>
+	Company Tags		          : Flipkart, Microsoft
+	GfG Link		              : https://practice.geeksforgeeks.org/problems/implementing-dijkstra-set-1-adjacency-matrix/1
+*/
+
+//Time : O(E * log(V))
+//E = number of edges
+//V = number of vertices
+class Solution
+{
+	public:
+	   //Function to find the shortest distance of all the vertices
+    	   //from the source vertex S.
+	    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S) {
+            set<pair<int, int>> pq;
+
+            vector<int> result(V, INT_MAX);
+
+            result[S] = 0;
+            pq.insert({0, S});
+
+            while(!pq.empty()) {
+
+                auto &it = *pq.begin();
+                int d  = it.first;
+                int node = it.second;
+                pq.erase(it);
+
+                for(auto &vec : adj[node]) {
+
+                    int adjNode = vec[0];
+                    int dist    = vec[1];
+
+                    if(d + dist < result[adjNode]) {
+
+                        result[adjNode] = d + dist;
+                        pq.insert({d+dist, adjNode});
+
+                    }
+
+                }
+
+            }
+
+            return result;
+	    }
+};
