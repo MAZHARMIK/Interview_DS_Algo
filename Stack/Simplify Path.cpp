1951c3ceeab3de410c6b3bc0406afb719563d80e
MAZHAR <iiestshibpur605@gmail.com>
2023-04-12 20:43:58 +0530

    Added YouTube link

--- Stack/Simplify Path.cpp
+++ Stack/Simplify Path.cpp
@@ -1,5 +1,5 @@
 /*
-      MY YOUTUBE VIDEO ON THIS Qn : <soon>
+      MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=ZV-Hi1e1KL8
       Company Tags                : META
       Leetcode Link               : https://leetcode.com/problems/simplify-path/
 */
