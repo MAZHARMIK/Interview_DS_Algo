e21addd82c303d98a18ea9fe87f5931dc5160d8d
MAZHAR <iiestshibpur605@gmail.com>
2023-04-12 19:35:16 +0530

    Simplify Path

---
+++ Stack/Simplify Path.cpp
@@ -0,0 +1,67 @@
+/*
+      MY YOUTUBE VIDEO ON THIS Qn : <soon>
+      Company Tags                : META
+      Leetcode Link               : https://leetcode.com/problems/simplify-path/
+*/
+
+//Approach-1 (Using Stack)
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string token = "";
+        
+        stringstream ss(path);
+        stack<string> st;
+        
+        while(getline(ss, token, '/')) {
+
+            if(token == "" || token == ".") continue;
+            
+            if (token != "..") 
+                st.push(token);
+            else if (!st.empty()) 
+                st.pop();
+        }
+        
+        string result = "";
+        
+        while(!st.empty()){ // add all the stack elements
+            result="/"+st.top()+result;
+            st.pop();
+        }
+        
+        if(result.length()==0) // if no directory or file is present
+            result="/"; // minimum root directory must be present in result
+        
+        return result;
+    }
+};
+
+//Approach-2 (Using vector as stack)
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string token = "";
+        
+        stringstream ss(path);
+        vector<string> st;
+        
+        while(getline(ss, token, '/')) {
+
+            if(token == "" || token == ".") continue;
+            
+            if (token != "..") 
+                st.push_back(token);
+            else if (!st.empty()) 
+                st.pop_back();
+        }
+        
+        string result = "";
+        
+        for(auto &token : st) {
+            result += "/" + token;
+        }
+        
+        return result.empty() ? "/" : result;
+    }
+};
