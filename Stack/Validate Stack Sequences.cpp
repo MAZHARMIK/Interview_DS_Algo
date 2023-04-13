4c7aded2b47cdecd1e6997499d6ed1ab7688c214
MAZHAR <iiestshibpur605@gmail.com>
2023-04-13 12:49:17 +0530

    Added YouTube link

--- Stack/Validate Stack Sequences.cpp
+++ Stack/Validate Stack Sequences.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=HEh7EtNSXtg
     Company Tags                : AMAZON
     Leetcode Link               : https://leetcode.com/problems/validate-stack-sequences/
 */
