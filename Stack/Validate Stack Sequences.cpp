ea85314223734cba1ec4a0eeb5db143afc4ce90c
MAZHAR <iiestshibpur605@gmail.com>
2023-04-13 10:17:27 +0530

    Validate Stack Sequences

---
+++ Stack/Validate Stack Sequences.cpp
@@ -0,0 +1,31 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : AMAZON
+    Leetcode Link               : https://leetcode.com/problems/validate-stack-sequences/
+*/
+
+
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> st;
+        
+        int n = pushed.size();
+        
+        int i = 0, j = 0;
+        
+        while(i < n && j < n) {
+            
+            st.push(pushed[i]);
+            
+            while(!st.empty() && j < n && st.top() == popped[j]) {
+                st.pop();
+                j++;
+            }
+            i++;
+            
+        }
+        
+        return st.empty();
+    }
+};
