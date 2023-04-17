37a8618915f2560685d02ecfc7a45e99c7e90d54
MAZHAR <iiestshibpur605@gmail.com>
2023-04-17 09:35:28 +0530

    Update Remove Zero Sum Consecutive Nodes from Linked List.cpp

--- Linked List/Remove Zero Sum Consecutive Nodes from Linked List.cpp
+++ Linked List/Remove Zero Sum Consecutive Nodes from Linked List.cpp
@@ -4,16 +4,6 @@
     Leetcode Link               : https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/
 */
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* removeZeroSumSublists(ListNode* head) {
