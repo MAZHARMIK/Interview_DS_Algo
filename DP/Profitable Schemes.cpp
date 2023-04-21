d60f7630e9b469478b5fe84e85f167e34e635a56
MAZHAR <iiestshibpur605@gmail.com>
2023-04-21 20:43:59 +0530

    Added YouTube Link

--- DP/Profitable Schemes.cpp
+++ DP/Profitable Schemes.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=ay3MHrS7YbQ
     Company Tags                : AMAZON
     Leetcode Link               : https://leetcode.com/problems/profitable-schemes/
 */
