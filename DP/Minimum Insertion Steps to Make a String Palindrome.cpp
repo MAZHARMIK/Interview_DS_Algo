8d16e013c7960b68d8a2b2568a1217215d5f80d8
MAZHAR <iiestshibpur605@gmail.com>
2023-04-24 17:40:50 +0530

    Added YouTube Link

--- DP/Minimum Insertion Steps to Make a String Palindrome.cpp
+++ DP/Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -1,5 +1,6 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    MY YOUTUBE VIDEO ON THIS Qn : Recur + Memo - https://www.youtube.com/watch?v=99S-xuAbwoc
+                                  Bottom Up - <soon>
     Company Tags                : Google, Amazon, Microsoft, Airtel
     Leetcode Link               : https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/
 */
@@ -35,4 +36,33 @@ public:
 
 //Approach-2 (Bottom Up - coming soon)
 
-//Approach-3 (Using LCS) - Coming Soon
+//Approach-3 (Using LCS) - Video Coming Soon
+class Solution {
+public:
+    int t[501][501];
+    
+    int LCS(string& s1, string& s2, int m, int n) {
+        if(m == 0 || n == 0)
+            return t[m][n] = 0;
+        
+        if(t[m][n] != -1)
+            return t[m][n];
+        
+        if(s1[m-1] == s2[n-1])
+            return t[m][n] = 1 + LCS(s1, s2, m-1, n-1);
+        
+        return t[m][n] = max(LCS(s1, s2, m, n-1), LCS(s1, s2, m-1, n));
+    }
+    
+    int minInsertions(string s) {
+        int m = s.length();
+        
+        memset(t, -1, sizeof(t));
+        string temp = s;
+        reverse(begin(temp), end(temp));
+        
+        int lcs_length = LCS(s, temp, m, m);
+        
+        return m - lcs_length;
+    }
+};
