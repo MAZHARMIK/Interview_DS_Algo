7afb9eef627a169bdc925ff44889b18fd600d544
MAZHAR <iiestshibpur605@gmail.com>
2023-04-25 16:33:31 +0530

    Added YouTube Link

--- HashSet/Smallest Number in Infinite Set.cpp
+++ HashSet/Smallest Number in Infinite Set.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=qiSjBraBzLM
     Company Tags                : Microsoft
     Leetcode Link               : https://leetcode.com/problems/smallest-number-in-infinite-set/
 */
