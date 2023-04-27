0223594b0244afb03b042569301d3de90c8c33d3
MAZHAR <iiestshibpur605@gmail.com>
2023-04-27 08:24:23 +0530

    Bulb Switcher

---
+++ Mathematical/Bulb Switcher.cpp
@@ -0,0 +1,12 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : AMAZON
+    Leetcode Link               : https://leetcode.com/problems/bulb-switcher/
+*/
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n); //explained in my video link above
+    }
+};
