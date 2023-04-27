dc35bb4eb856b2f5edf3be7e2a7dda7d5bbe1b48
MAZHAR <iiestshibpur605@gmail.com>
2023-04-27 17:25:30 +0530

    Added YouTube Link

--- Mathematical/Bulb Switcher.cpp
+++ Mathematical/Bulb Switcher.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=86t-bvaiF7k
     Company Tags                : AMAZON
     Leetcode Link               : https://leetcode.com/problems/bulb-switcher/
 */
