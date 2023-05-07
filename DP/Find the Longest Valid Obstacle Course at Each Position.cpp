1496246e7b7998ceef673ef5d63026aa73134c2a
MAZHAR <iiestshibpur605@gmail.com>
2023-05-07 17:59:19 +0530

    Improved and better naming

--- DP/Find the Longest Valid Obstacle Course at Each Position.cpp
+++ DP/Find the Longest Valid Obstacle Course at Each Position.cpp
@@ -30,19 +30,24 @@ class Solution {
 public:
     vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
         int n = obstacles.size();
+        vector<int> result(n, 1);
+        vector<int> LIS;
+
         
-        // lis[i] records the lowest increasing sequence of length i + 1.
-        vector<int> answer(n, 1), lis;
-        
-        for (int i = 0; i < n; ++i) {
-            // Find the rightmost insertion position idx.
-            int idx = upper_bound(lis.begin(), lis.end(), obstacles[i]) - lis.begin();
-            if (idx == lis.size())
-                lis.push_back(obstacles[i]);
-            else
-                lis[idx] = obstacles[i];
-            answer[i] = idx + 1;
+        for(int i = 0; i<n; i++) {
+            
+            int idx = upper_bound(begin(LIS), end(LIS), obstacles[i]) - begin(LIS);
+            
+            if(idx == LIS.size()) {
+                LIS.push_back(obstacles[i]);
+            } else {
+                LIS[idx] = obstacles[i];
+            }
+            
+            result[i] = idx+1;
+            
         }
-        return answer;
+        
+        return result;
     }
 };
