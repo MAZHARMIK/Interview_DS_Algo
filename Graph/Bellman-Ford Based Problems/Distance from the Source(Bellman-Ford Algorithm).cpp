befc78041510f97468a6cae57f07bae2079f1831
MAZHAR <iiestshibpur605@gmail.com>
2023-05-17 23:53:52 +0530

    Create Distance from the Source(Bellman-Ford Algorithm).cpp

---
+++ Graph/Bellman-Ford Based Problems/Distance from the Source(Bellman-Ford Algorithm).cpp
@@ -0,0 +1,43 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Amazon, Microsoft (Generally asked as a follow up qn of Dijkstra's Algorithm)
+    GfG Link                    : https://practice.geeksforgeeks.org/problems/distance-from-the-source-bellman-ford-algorithm/1
+*/
+
+class Solution {
+  public:
+    vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
+        vector<int> result(V, 1e8);
+        result[S] = 0;
+        
+        for(int c = 1; c<=V-1; c++) {
+            
+            for(auto &edge : edges) {
+                
+                int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
+                
+                if(result[u] != 1e8 && result[u] + w < result[v]) {
+                    result[v] = result[u] + w;
+                }
+                
+            }
+            
+        }
+        
+        //Now detect negative cycle
+        for(auto &edge : edges) {
+            
+            int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
+                
+                if(result[u] != 1e8 && result[u] + w < result[v]) {
+                    return {-1};
+                }
+        }
+        
+        return result;
+    }
+};
