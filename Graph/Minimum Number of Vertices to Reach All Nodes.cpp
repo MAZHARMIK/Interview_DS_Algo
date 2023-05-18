2ea81424415084f83001f30effb8cde9ac1358ba
MAZHAR <iiestshibpur605@gmail.com>
2023-05-18 08:08:15 +0530

    Create Minimum Number of Vertices to Reach All Nodes.cpp

---
+++ Graph/Minimum Number of Vertices to Reach All Nodes.cpp
@@ -0,0 +1,29 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : GOOGLE
+    Leetcode Link               : https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/
+*/
+
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<bool> indegree(n, false);
+        
+        for(auto &edge : edges) {
+            
+            int u = edge[0];
+            int v = edge[1];
+            
+            indegree[v] = true;
+        }
+        
+        vector<int> result;
+        
+        for(int i = 0; i<n; i++) {
+            if(indegree[i] == false)
+                result.push_back(i);
+        }
+        
+        return result;
+    }
+};
