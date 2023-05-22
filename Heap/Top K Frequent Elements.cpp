81c28da8b0bde14ed157f0d49e9aef4267830ad9
MAZHAR <iiestshibpur605@gmail.com>
2023-05-22 09:12:14 +0530

    Top K FTop K Frequent Elements.cpp

---
+++ Heap/Top K Frequent Elements.cpp
@@ -0,0 +1,84 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn :  <soon>
+    Company Tags                :  Amazon, Accolite
+    Leetcode Qn Link            :  https://leetcode.com/problems/top-k-frequent-elements/
+*/
+
+//Approach-1 (Using min-heap) - TC : O(nlog(n-k))
+class Solution {
+public:
+    typedef pair<int, int> p;
+    
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        //min-heap
+        priority_queue<p, vector<p>, greater<p>> pq;
+        
+        //count frequency of each element
+        // Worst Case - n distinct elements are stored, so, space O(n)
+        unordered_map<int, int> mp;
+        for(int i : nums)
+            mp[i]++;
+        
+        //Push in min-heap and maintain size k
+        for(auto it:mp) {
+            pq.push({it.second, it.first});
+            
+            if(pq.size() > k)
+                pq.pop();
+        }
+        
+        
+        //Pick all top K elements
+        vector<int> result;
+        while(!pq.empty()) {
+            result.push_back(pq.top().second);
+            pq.pop();
+        }
+        return result;
+    }
+};
+
+
+//Approach-2 (Using Bucket Sort) - TC : O(n) - We visit all elements of nums only once.
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        unordered_map<int, int> mp;
+        
+        for(int &num : nums) {
+            mp[num]++;
+        }
+        
+        //index = frequency
+        //Value will be elements
+        //bucket[i] = elements occuring ith time
+        vector<vector<int>> bucket(n+1);
+        
+        for(auto &it : mp) {
+            int element = it.first;
+            int freq    = it.second;
+            
+            bucket[freq].push_back(element);
+        }
+        
+        //Pick from right to left to find max frequency elements
+        vector<int> result;
+        for(int i = n; i >= 0; i--) {
+            
+            if(bucket[i].size() == 0) continue;
+            
+            int size = bucket.size();
+            while(bucket[i].size() > 0 && k > 0) {
+                result.push_back(bucket[i].back());
+                bucket[i].pop_back();
+                k--;
+            }
+            
+        }
+        
+        return result;
+    }
+};
