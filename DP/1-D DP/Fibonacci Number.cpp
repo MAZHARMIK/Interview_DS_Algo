e7893503ae61cde594a4a72e792909346c3385e9
MAZHAR <iiestshibpur605@gmail.com>
2023-05-23 23:25:36 +0530

    Fibonacci Number

--- DP/1-D DP/Fibonacci Number.cpp
+++ DP/1-D DP/Fibonacci Number.cpp
@@ -50,3 +50,30 @@ public:
         
     }
 };
+
+
+//Approach-3 (Constant Space Complexity)
+class Solution {
+public:
+    int fib(int n) {
+        if( n<=1)
+            return n;
+        
+        
+        int c;
+        int a = 0, b = 1;
+        
+        for(int i = 1; i<n; i++) {
+            
+            c = a+b;
+            
+            a = b;
+            b = c;
+             
+        }
+        
+        
+        return c;
+    }
+    
+};
