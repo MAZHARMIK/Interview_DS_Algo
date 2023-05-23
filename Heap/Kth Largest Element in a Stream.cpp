bf74a389913b4137f17da65c26f25e2de2a975e7
MAZHAR <iiestshibpur605@gmail.com>
2023-05-23 16:02:37 +0530

    Kth Largest Element in a Stream

---
+++ Heap/Kth Largest Element in a Stream.cpp
@@ -0,0 +1,31 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Amazon
+    Leetcode Qn Link            : https://leetcode.com/problems/kth-largest-element-in-a-stream/
+*/
+
+//Approach - (Using min-heap)
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int K;
+    
+    KthLargest(int k, vector<int>& nums) {
+        K = k;
+        for(int &x : nums) {
+            pq.push(x);
+            
+            if(pq.size() > k)
+                pq.pop();
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        
+        if(pq.size() > K)
+            pq.pop();
+        
+        return pq.top();
+    }
+};
