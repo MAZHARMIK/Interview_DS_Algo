d3ee9860f16b4ec2300d1fb74f912f48f41a174b
MAZHAR <iiestshibpur605@gmail.com>
2023-05-24 18:53:06 +0530

    Added YouTube Link

--- Heap/Maximum Subsequence Score.cpp
+++ Heap/Maximum Subsequence Score.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=z9oUzKhEYJU
     Company Tags                : Amazon
     Leetcode Link               : https://leetcode.com/problems/maximum-subsequence-score/
 */
