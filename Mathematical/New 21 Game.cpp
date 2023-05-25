bc475a699714e61ec26d83d5dcd176538fdb6e28
MAZHAR <iiestshibpur605@gmail.com>
2023-05-26 00:20:17 +0530

    Added YouTube Link

--- Mathematical/New 21 Game.cpp
+++ Mathematical/New 21 Game.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=x19SmqQbhWk
     Company Tags                : Google
     Leetcode Link               : https://leetcode.com/problems/new-21-game/
 */
