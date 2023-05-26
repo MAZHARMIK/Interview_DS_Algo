0e9752203ce9800afd480e54f00962b9700023c3
MAZHAR <iiestshibpur605@gmail.com>
2023-05-26 20:50:58 +0530

    improved naming

--- DP/Game Strategy/Stone Game II.cpp
+++ DP/Game Strategy/Stone Game II.cpp
@@ -7,33 +7,31 @@
 //Recursion + Memo
 class Solution {
 public:
-    int t[2][101][101];
-    int n;
     
-    int solve(vector<int>& piles, int person, int i, int M) {
+    int n;
+    int t[2][101][101]; //t[2][n+1][n+1] - 2*(n+1)*(n+1) ~n^2 * n ~ O(n^3)
+    int solveForAlice(vector<int>& piles, int person, int i, int M) {
+        
         if(i >= n)
             return 0;
         
         if(t[person][i][M] != -1)
             return t[person][i][M];
         
-        int result;
-        if(person == 0)
-            result = -1;
-        else
-            result = 1000000;
+        int result = (person == 1) ? -1 : INT_MAX;
         
-        int sum = 0;
+        int stones = 0;
         
-        for (int x = 1; x <= min(2 * M, n - i); x++) {
-            sum += piles[i + x - 1];
+        for(int x = 1; x <= min(2*M, n-i); x++) {
             
-            if (person == 0) {
-                result = max(result, sum + solve(piles, 1, i + x, max(M, x)));
+            stones += piles[i+x-1];
+            
+            if(person == 1) { //Alice
+                result = max(result, stones + solveForAlice(piles, 0, i+x, max(M, x)));
+            } else { //Bob
+                result = min(result, solveForAlice(piles, 1, i+x, max(M, x)));
             }
-            else {
-                result = min(result, solve(piles, 0, i + x, max(M, x)));
-            }
+            
         }
         
         return t[person][i][M] = result;
@@ -41,10 +39,10 @@ public:
     }
     
     int stoneGameII(vector<int>& piles) {
-        memset(t, -1, sizeof(t));
-        
         n = piles.size();
         
-        return solve(piles, 0, 0, 1);
+        memset(t, -1, sizeof(t));
+        
+        return solveForAlice(piles, 1, 0, 1);
     }
 };
