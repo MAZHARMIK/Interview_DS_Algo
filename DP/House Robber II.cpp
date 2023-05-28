f4a85141925e0ef2d8bc073b8ff272e03b764ad1
MAZHAR <iiestshibpur605@gmail.com>
2023-05-28 20:56:22 +0530

    House Robber II.cpp

--- DP/House Robber II.cpp
+++ DP/House Robber II.cpp
@@ -1,6 +1,7 @@
 /*
-    Company Tags  : Airbnb, Microsoft
-    Leetcode Link : https://leetcode.com/problems/house-robber-ii/
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Airbnb, Microsoft
+    Leetcode Link               : https://leetcode.com/problems/house-robber-ii/
 */
 
 
@@ -40,7 +41,53 @@ public:
     }
 };
 
-//Approach-2 (Bottom UP)
+
+
+//Approach-2 (Using Bottom Up similar to House Robber-1 and just taking two cases
+/*
+    Case-1 (Take from 1st House - Hence skip the last house)
+    Case-2 (Take from 2nd House - Hence take the last house)
+*/
+class Solution {
+public:
+    int n;
+    
+    int rob(vector<int>& nums) {
+        n = nums.size();
+        if(n == 1)
+            return nums[0];
+        
+        vector<int> t(n+1, 0);
+        //t[i] = Max money gained from i houses
+        
+        int result1 = 0;
+        int result2 = 0;
+        
+        //Case-1 (Take from 1st House - Hence skip the last house)
+        t[0] = 0;
+        for(int i = 1; i<=n-1; i++) {
+            t[i] = max(t[i-1] , nums[i-1]+ ((i-2 >= 0) ? t[i-2] : 0 ));
+        }
+        result1 = t[n-1];
+        
+        t.clear();
+        //Case-2 (Take from 2nd House - Hence take the last house)
+        t[0] = 0;
+        t[1] = 0;
+        for(int i = 2; i<=n; i++) {
+            t[i] = max(t[i-1] , nums[i-1]+ ((i-2 >= 0) ? t[i-2] : 0 ));
+        }
+        result2 = t[n];
+        
+        return max(result1, result2);
+        
+    }
+};
+
+
+
+
+//Approach-3 (Converting Bottom Up to constant space)
 class Solution {
 public:
     
