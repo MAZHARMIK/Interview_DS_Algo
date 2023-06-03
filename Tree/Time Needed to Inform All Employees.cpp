9c3526f0b6025c643022907c4036d20696601882
MAZHAR <iiestshibpur605@gmail.com>
2023-06-03 12:24:37 +0530

    Added YouTube Link

--- Tree/Time Needed to Inform All Employees.cpp
+++ Tree/Time Needed to Inform All Employees.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=4fiZOJpis9g
     Company Tags                : AMAZON
     Leetcode Link               : https://leetcode.com/problems/time-needed-to-inform-all-employees/
 */
