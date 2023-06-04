58eb0faff84dd7dcf287514cc3bbf00bfc02288e
MAZHAR <iiestshibpur605@gmail.com>
2023-06-04 12:07:08 +0530

    Floyd Warshall.cpp

---
+++ Graph/Floyd Warshall/Floyd Warshall.cpp
@@ -0,0 +1,43 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : Samsung
+    GfG Link                    : https://practice.geeksforgeeks.org/problems/implementing-floyd-warshall2042/1
+*/
+
+//Time Complexity - O(n^3)
+class Solution {
+  public:
+	void shortest_distance(vector<vector<int>>& grid){
+        int n = grid.size();
+        
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                
+                if(grid[i][j] == -1)
+                    grid[i][j] = 100000;
+                
+            }
+        }
+        
+        for(int via = 0; via < n; via++) {
+            
+            for(int i = 0; i<n; i++) {
+                for(int j = 0; j<n; j++) {
+                    
+                   grid[i][j] = min(grid[i][j], grid[i][via] + grid[via][j]);
+                    
+                }
+            }
+        }
+        
+        
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                
+                if(grid[i][j] == 100000)
+                    grid[i][j] = -1;
+                
+            }
+        }
+	}
+};
