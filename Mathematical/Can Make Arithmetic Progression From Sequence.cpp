e0d4e34462e26c1b963206d8c748039895dde52f
MAZHAR <iiestshibpur605@gmail.com>
2023-06-06 08:15:19 +0530

    updated

--- Mathematical/Can Make Arithmetic Progression From Sequence.cpp
+++ Mathematical/Can Make Arithmetic Progression From Sequence.cpp
@@ -12,7 +12,7 @@ public:
         
         sort(begin(arr), end(arr));
         
-        int d = arrr[1] - arr[0];
+        int d = arr[1] - arr[0];
         
         for(int i = 2; i<n; i++) {
             if(arr[i] - arr[i-1] != d)
