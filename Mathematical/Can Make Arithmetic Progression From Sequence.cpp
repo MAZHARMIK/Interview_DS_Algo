43b16b990a964a094af2e9d7f1f0ec765f005a3a
MAZHAR <iiestshibpur605@gmail.com>
2023-06-06 22:39:52 +0530

    Added 3rd Approach as well

--- Mathematical/Can Make Arithmetic Progression From Sequence.cpp
+++ Mathematical/Can Make Arithmetic Progression From Sequence.cpp
@@ -56,5 +56,39 @@ public:
 
 
 //Approach-3 (Using O(1) Space and O(n) Time)
-//Kindly let me know if you want the solution for this as well. 
-//I will make a video on this too
+class Solution {
+public:
+    bool canMakeArithmeticProgression(std::vector<int>& arr) {
+        
+        int n = arr.size();
+
+        int min_el = *min_element(begin(arr), end(arr)); //a0
+        int max_el = *max_element(begin(arr), end(arr)); //a0+(n-1)*d
+        
+        if((max_el - min_el)%(n-1) != 0)
+            return false;
+        
+        int d = (max_el - min_el)/(n-1);
+        
+        int i = 0;
+        while (i < n) {
+            if(arr[i] == min_el + i*d) {
+                i++;
+            } else {
+                
+                if((arr[i] - min_el) % d != 0)
+                    return false;
+                
+                int j = (arr[i] - min_el)/d;
+                
+                if(arr[i] == arr[j])
+                    return false;
+                
+                swap(arr[i], arr[j]);
+                
+            }
+        }
+        
+        return true;
+    }
+};
