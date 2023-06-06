3721549eba1726f78ae1ef3ebde5335bff8bc861
MAZHAR <iiestshibpur605@gmail.com>
2023-06-06 08:01:37 +0530

    Create Can Make Arithmetic Progression From Sequence.cpp

---
+++ Mathematical/Can Make Arithmetic Progression From Sequence.cpp
@@ -0,0 +1,43 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : <soon>
+    Company Tags                : GOOGLE
+    Leetcode Link               : https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/
+*/
+
+//Approach-1 (Using Sorting (O(nlogn))
+
+//Approach-2 (Using Mathematival AP property - O(n) Time, O(n) Space))
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        int n = arr.size();
+        
+        unordered_set<int> st(begin(arr), end(arr));
+        
+        int min_el = *min_element(begin(arr), end(arr)); //a0
+        int max_el = *max_element(begin(arr), end(arr)); //a0+(n-1)*d
+        
+        if((max_el - min_el)%(n-1))
+            return false;
+        
+        int d = (max_el - min_el)/(n-1);
+        
+        int i = 0; 
+        //a0 , a0+d , a0 + 2d
+        
+        while(i < n) {
+            int num = min_el + i*d;
+            if(st.find(num) == st.end())
+                return false;
+            
+            i++;
+        }
+        
+        return true;
+    }
+};
+
+
+//Approach-3 (Using O(1) Space and O(n) Time)
+//Kindly let me know if you want the solution for this as well. 
+//I will make a video on this too
