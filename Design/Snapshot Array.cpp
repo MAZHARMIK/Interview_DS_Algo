0e45555f9e56313f6c3ced8f085bea049c6eed8e
MAZHAR <iiestshibpur605@gmail.com>
2023-06-11 17:46:21 +0530

    Snapshot Array.cpp

---
+++ Design/Snapshot Array.cpp
@@ -0,0 +1,103 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Microsoft, Amazon
+    Leetcode Link               : https://leetcode.com/problems/snapshot-array/
+*/
+
+//Approach-1 : Brute Force - Store all states of array for all snap ids
+//You can write it on your own. A very simple one.
+
+
+//Approach-2 : Use Own Binary Search - O(nlogn)
+class SnapshotArray {
+public:
+    
+    int snap_id;
+    
+    vector<vector<pair<int, int>>> vec;
+    
+    SnapshotArray(int length) {
+        snap_id = 0;
+        
+        vec.resize(length);
+        
+        for(int i = 0; i < length; i++) {
+            
+            vec[i].push_back(make_pair(0, 0));
+            
+        }
+    }
+    
+    void set(int index, int val) {
+        
+        vec[index].push_back(make_pair(snap_id, val));
+        
+    }
+    
+    int snap() {
+        return snap_id++;
+    }
+    
+    int get(int index, int snap_id) {
+        
+        int l = 0, r = vec[index].size()-1;
+        
+        int result = 0;
+        while(l <= r) {
+            
+            int mid = l + (r-l)/2;
+            
+            if(vec[index][mid].first <= snap_id) {
+                result = vec[index][mid].second;
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+            
+        }
+        
+        return result;
+        
+    }
+};
+
+
+//Approach-3 : Use upper_bound - O(nlogn)
+class SnapshotArray {
+public:
+    
+    int snap_id;
+    
+    vector<vector<pair<int, int>>> vec;
+    
+    SnapshotArray(int length) {
+        snap_id = 0;
+        
+        vec.resize(length);
+        
+        for(int i = 0; i < length; i++) {
+            
+            vec[i].push_back(make_pair(0, 0));
+            
+        }
+    }
+    
+    void set(int index, int val) {
+        
+        vec[index].push_back(make_pair(snap_id, val));
+        
+    }
+    
+    int snap() {
+        return snap_id++;
+    }
+    
+    int get(int index, int snap_id) {
+        
+        auto it = upper_bound(begin(vec[index]), end(vec[index]), make_pair(snap_id, INT_MAX));
+        //pair(snap_id, val)
+        
+        return prev(it)->second;
+        
+    }
+};
