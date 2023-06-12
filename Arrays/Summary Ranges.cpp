74e036dee79c42528808e12b4461d28b3d12d4c9
MAZHAR <iiestshibpur605@gmail.com>
2023-06-12 07:48:47 +0530

    Summary Ranges

---
+++ Arrays/Summary Ranges.cpp
@@ -0,0 +1,35 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : GOOGLE
+    Leetcode Link               : https://leetcode.com/problems/summary-ranges/
+*/
+
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0)
+            return {};
+        
+        vector<string> result;
+        
+        for(int i = 0; i<n; i++) {
+            
+            int start = nums[i];
+            
+            while(i+1 < n && nums[i]+1 == nums[i+1]) {
+                i++;
+            }
+            
+            if(start != nums[i]) {
+                result.push_back(to_string(start) + "->" + to_string(nums[i]));
+            } else {
+                result.push_back(to_string(start));
+            }
+            
+        }
+        
+        return result;
+        
+    }
+};
