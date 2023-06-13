3a2907bfd85186aa05d8c282145ad7d219104d3a
MAZHAR <iiestshibpur605@gmail.com>
2023-06-13 08:57:52 +0530

    Equal Row and Column Pairs

--- HashMap/Equal Row and Column Pairs.cpp
+++ HashMap/Equal Row and Column Pairs.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=QWhQxJYppgo
     Company Tags                : Google, Microsoft
     Leetcode Link               : https://leetcode.com/problems/equal-row-and-column-pairs/
 */
