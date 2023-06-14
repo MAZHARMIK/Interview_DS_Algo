bdebc4a4c3ea3e0f67e9fe86bd02a5c596fc2626
MAZHAR <iiestshibpur605@gmail.com>
2023-06-14 08:34:08 +0530

    Minimum Absolute Difference in BST

---
+++ Tree/BST/Minimum Absolute Difference in BST.cpp
@@ -0,0 +1,35 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : GOOGLE
+    Leetcode Link               : https://leetcode.com/problems/minimum-absolute-difference-in-bst/submissions/
+*/
+
+//Using Simple InOrder traversal - T.C : O(n)
+class Solution {
+public:
+    
+    int minDiff = INT_MAX;
+    
+    void inOrder(TreeNode* root, TreeNode* &prev) {
+        
+        if(root == NULL)
+            return;
+        
+        inOrder(root->left, prev);
+        
+        if(prev != NULL) {
+            minDiff = min(minDiff, root->val - prev->val);
+        }
+        
+        prev = root;
+        
+        inOrder(root->right, prev);
+        
+    }
+    
+    int getMinimumDifference(TreeNode* root) {
+        TreeNode* prev = NULL;
+        inOrder(root, prev);
+        return minDiff;
+    }
+};
