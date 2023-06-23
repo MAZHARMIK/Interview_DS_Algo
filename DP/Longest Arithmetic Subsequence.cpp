f053ab6931f1fbf4442bf890841ef4dfeb7e0c59
MAZHAR <iiestshibpur605@gmail.com>
2023-06-23 19:15:56 +0530

    simplified like LIS

--- DP/Longest Arithmetic Subsequence.cpp
+++ DP/Longest Arithmetic Subsequence.cpp
@@ -67,7 +67,7 @@ public:
 
         int result = 0;
         
-        for (int i=0; i<n; i++) {
+        for (int i = 1; i<n; i++) {
             
             for (int j=0; j<i; j++) {
                 
