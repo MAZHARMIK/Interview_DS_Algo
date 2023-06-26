e0f0f7d4acb670865009203e25c204ad43c93a47
MAZHAR <iiestshibpur605@gmail.com>
2023-06-26 15:47:56 +0530

    Added YouTube Link

--- Heap/Total Cost to Hire K Workers.cpp
+++ Heap/Total Cost to Hire K Workers.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn - 
+    MY YOUTUBE VIDEO ON THIS Qn - https://www.youtube.com/watch?v=ODuICq8exLo
     Company Tags                - META
     Leetcode Link               - https://leetcode.com/problems/total-cost-to-hire-k-workers/
 */
