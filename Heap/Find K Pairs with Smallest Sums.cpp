595a829356cbc706cc770ee6ec7736e3855d34f5
MAZHAR <iiestshibpur605@gmail.com>
2023-06-27 16:27:09 +0530

    Improved Brute Force

--- Heap/Find K Pairs with Smallest Sums.cpp
+++ Heap/Find K Pairs with Smallest Sums.cpp
@@ -5,7 +5,55 @@
 */
 
 
-//Complete Brute Force using MinHeap - O(m*n*log(k))
+//Complete Brute Force using MinHeap - TIME COMPLEXITY = O(m*n*log(k)) - TLE
+class Solution {
+public:
+    typedef pair<int, pair<int, int>> P;
+    
+    struct lambda {
+        bool operator()(P &p1, P &p2){
+            return p1.first < p2.first; //Max heap
+        }
+    };
+    
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        priority_queue<P, vector<P>, lambda> pq;
+        
+        int m = nums1.size();
+        int n = nums2.size();
+        
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                
+                int sum = nums1[i] + nums2[j];
+
+                if(pq.size() < k) {
+                    pq.push({sum, {i, j}});
+                } else if(pq.top().first > sum) {
+                    pq.pop();
+                    pq.push({sum, {i, j}});
+                }
+            }
+        }
+        
+        vector<vector<int>> result;
+        
+        while(!pq.empty()) {
+            auto temp = pq.top(); 
+            pq.pop();
+            
+            int i = temp.second.first;
+            int j = temp.second.second;
+            result.push_back({nums1[i], nums2[j]});
+        }
+        
+        return result;
+    }
+};
+
+
+//Approach-2 (Improved BRUTE FORCE) 
+//Complete Brute Force using MinHeap - TIME COMPLEXITY < O(m*n*log(k)) - ACCEPTED
 class Solution {
 public:
     typedef pair<int, pair<int, int>> P;
@@ -33,7 +81,7 @@ public:
                     pq.pop();
                     pq.push({sum, {i, j}});
                 } else {
-                    break;
+                    break; //Breaking from here (slight improvement)
                 }
             }
         }
@@ -53,5 +101,4 @@ public:
     }
 };
 
-
-//Approach-2 (Optimising the code above) - 
+//Approach-3 (Optimal approach)
