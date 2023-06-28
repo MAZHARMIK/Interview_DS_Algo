136d50f2c3630048a18c3da9c6c2e6e96450bdf5
MAZHAR <iiestshibpur605@gmail.com>
2023-06-28 08:05:33 +0530

    Path with Maximum Probability

---
+++ Graph/Dijkstra'a Based Problems/Path with Maximum Probability.cpp
@@ -0,0 +1,49 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : 
+    Leetcode Link               : https://leetcode.com/problems/path-with-maximum-probability/
+*/
+
+//Using Dijkstra's Algorithm - TIME - E(log(V))
+class Solution {
+public:
+    typedef pair<double, int> P;
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+        unordered_map<int, vector<pair<int, double>>> adj;
+        vector<double> result(n, 0);
+        
+        for(int i=0;i<edges.size();i++)
+        {
+            int u       = edges[i][0];
+            int v       = edges[i][1];
+            double prob = succProb[i];
+            adj[u].push_back({v, prob});
+            adj[v].push_back({u, prob});
+        }
+
+        priority_queue<P> pq; //max heap because we need maximum probability
+
+		result[start] = 1;
+		pq.push({1.0, start});
+
+		while(!pq.empty()) {
+
+		    int curnode    = pq.top().second;
+            double curProb = pq.top().first;
+            pq.pop();
+
+            for(auto &child : adj[curnode])
+            {
+                if(result[child.first] < curProb*child.second) //we want maximum this time
+                {
+                    result[child.first] = curProb*child.second;
+                    pq.push({result[child.first], child.first});
+                }
+            }
+
+		}
+
+		return result[end];
+
+    }
+};
