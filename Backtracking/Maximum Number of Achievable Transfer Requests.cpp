fed3c4b0f555b872fadc9555e408d04b868d2452
MAZHAR <iiestshibpur605@gmail.com>
2023-07-02 11:28:43 +0530

    Maximum Number of Achievable Transfer Requests

---
+++ Backtracking/Maximum Number of Achievable Transfer Requests.cpp
@@ -0,0 +1,60 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : GOOGLE, ADOBE
+    Leetcode Link               : https://leetcode.com/problems/maximum-number-of-achievable-transfer-requests/
+*/
+
+
+/*
+Time complexity: O((2^m) * N)
+
+We iterate over every two possibilities for each of the M requests 
+This is equal to 2^M possibilities. 
+At the end we will iterate over N buildings to check if the employee change is zero which takes O(N)
+*/
+
+class Solution {
+public:
+    int m;
+    int result = INT_MIN;
+    
+    void solve(int idx, int count, int n, vector<int>& resultant, vector<vector<int>>& requests) {
+        if(idx == m) {
+            
+            int allZero = true;
+            for(int &x : resultant) {
+                if(x != 0) {
+                    allZero = false;
+                    break;
+                }
+            }
+            
+            if(allZero) {
+                result = max(result, count);
+            }
+            return;
+        }
+        
+        
+        int from = requests[idx][0];
+        int to   = requests[idx][1];
+        
+        resultant[from]--;
+        resultant[to]++;
+        solve(idx+1, count+1, n, resultant, requests);
+        
+        resultant[from]++;
+        resultant[to]--;
+        solve(idx+1, count, n, resultant, requests);
+    }
+    
+    int maximumRequests(int n, vector<vector<int>>& requests) {
+        m = requests.size();
+        
+        vector<int> resultant(n, 0);
+        
+        solve(0, 0, n, resultant, requests);
+        
+        return result;
+    }
+};
