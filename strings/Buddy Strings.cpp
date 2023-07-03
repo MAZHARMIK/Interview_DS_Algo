9c02c52cac222f235269425f0e5ee398e13dcdd6
MAZHAR <iiestshibpur605@gmail.com>
2023-07-03 08:06:16 +0530

    Added YouTube Link

--- strings/Buddy Strings.cpp
+++ strings/Buddy Strings.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=LTrZCwuCoiE
     Company Tags                : GOOGLE, META
     Leetcode Link               : https://leetcode.com/problems/buddy-strings/
 */
