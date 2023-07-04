34d972bdd5223da531bb07e8b45f1c81617c0510
MAZHAR <iiestshibpur605@gmail.com>
2023-07-04 15:11:45 +0530

    Multiple Approaches + Google

---
+++ Bit_Magic/Single Number II.cpp
@@ -0,0 +1,55 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn   : 
+    Company Tags                  : Google, Amazon, Microsoft, Ola Cabs, Qualcomm
+    Leetcode Link                 : https://leetcode.com/problems/single-number-ii/
+*/
+
+//Approach-1
+/*
+    Simply maintain a unordered_map. 
+    Find the element having frequency 1
+    Space Complexity = O(n)
+    Time Complexity = O(n)
+*/
+
+//Approach-2
+/*
+    Sort + Smart Traversal
+    Time - O(nlogn)
+    Space - No extra space
+*/
+
+//Approach - 3 (Time Complexity : O(32*n))
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int result = 0;
+        
+        for(int i = 0; i<32; i++) {
+            
+            int temp = (1 << i);
+            
+            int countOne  = 0;
+            int countZero = 0;
+            
+            for(int &num : nums) {
+                
+                if((num&temp) == 0) {
+                    countZero++;
+                } else {
+                    countOne++;
+                }
+                
+            }
+            
+            if(countOne % 3 == 1) {
+                result = (result | temp);
+            }
+            
+        }
+        
+        return result;
+        
+    }
+};
