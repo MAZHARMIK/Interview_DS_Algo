dfbde98926ac23bda3728319f9bfe92f8db7532c
MAZHAR <iiestshibpur605@gmail.com>
2023-07-08 14:39:28 +0530

    Added YouTube Link

--- Arrays/Put Marbles in Bags.cpp
+++ Arrays/Put Marbles in Bags.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=L-KWU8W3OqE
     Company Tags                : AMAZON
     Leetcode Link               : https://leetcode.com/problems/put-marbles-in-bags/
 */
