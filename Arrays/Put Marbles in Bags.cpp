fe68403307b23da09aeb41918c99b86f193eaee2
MAZHAR <iiestshibpur605@gmail.com>
2023-07-08 11:30:54 +0530

    Put Marbles in Bags

---
+++ Arrays/Put Marbles in Bags.cpp
@@ -0,0 +1,30 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : AMAZON
+    Leetcode Link               : https://leetcode.com/problems/put-marbles-in-bags/
+*/
+
+//T.C : O(nlog(n))
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        int n = weights.size();
+        
+        vector<int> pairSum(n-1, 0);
+        
+        for(int i = 0; i<n-1; i++) {
+            pairSum[i] = weights[i] + weights[i+1];
+        }
+        
+        sort(begin(pairSum), end(pairSum));
+        long maxSum = 0;
+        long minSum = 0;
+        
+        for(int i = 0; i<k-1; i++) {
+            minSum += pairSum[i];
+            maxSum += pairSum[n-i-2];
+        }
+        
+        return maxSum-minSum;
+    }
+};
