b3d789ef0f8ac287c6cec0b767a96158d80680c4
MAZHAR <iiestshibpur605@gmail.com>
2023-07-13 19:57:23 +0530

    cleaner solution

--- DP/1-D DP/Maximum Alternating Subsequence Sum.cpp
+++ DP/1-D DP/Maximum Alternating Subsequence Sum.cpp
@@ -5,7 +5,7 @@
 
 */
 
-//Approach-1 (Recursion + Memo)
+//Approach-1 (Recursion + Memo) (Time Complexity - n*2 : We visit all states of t once)
 class Solution {
 public:
     int n;
@@ -15,18 +15,17 @@ public:
         if(idx >= n) {
             return 0;
         }
-        
         if(t[idx][iseven] != -1)
             return t[idx][iseven];
         
         long long skip = solve(idx+1, nums, iseven);
         
-        long long take = 0;
-        if(iseven == true) {
-            take = solve(idx+1, nums, !iseven) + nums[idx];
-        } else {
-            take = solve(idx+1, nums, !iseven) - nums[idx];
+        long long val = nums[idx];
+        if(iseven == false) {
+            val = -val;
         }
+        
+        long long take = solve(idx+1, nums, !iseven) + val;
         
         return t[idx][iseven] = max(skip, take);
         
