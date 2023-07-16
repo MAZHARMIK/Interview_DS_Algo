6e3d45e47b7bcaf707c6e2a3a2f9b2c71557aba0
MAZHAR <iiestshibpur605@gmail.com>
2023-07-16 22:05:01 +0530

    Adde multiple approaches

--- DP/Bit Magic DP/Smallest Sufficient Team.cpp
+++ DP/Bit Magic DP/Smallest Sufficient Team.cpp
@@ -71,4 +71,74 @@ public:
     }
 };
 
-//More approaches coming soon
+//Approach-2 (Using vector<vector<int>> instead of unordered_map to memoize)
+Also guys, Yes, you can use a vector as well to store your past result as DP instead of unordered_map
+Find the C++ code below : 
+
+class Solution {
+public:
+    
+    int m, n;
+    int target_mask;
+    vector<int> result;
+    vector<vector<int>> t;
+    
+    void solve(vector<int> &people , int idx , vector<int> &temp, int mask ) {
+        
+       if(idx == people.size()) {
+           if(mask == target_mask) {
+               if(result.size() == 0 || result.size() >= temp.size()) {
+                   //vector<int> copy = temp;
+                   result = temp;
+               }
+           }
+           return;
+       }
+        
+        if(t[idx][mask] != -1) {
+            if(t[idx][mask] <= temp.size())
+               return ; 
+        }
+        
+        if(result.size() != 0 && temp.size() >= result.size()) 
+            return; 
+        
+        solve(people , idx + 1 , temp , mask ); // no
+
+        if( (mask | people[idx]) != mask) {
+            temp.push_back(idx);
+            solve(people, idx + 1, temp , mask | people[idx]);
+
+            temp.pop_back();
+            t[idx][mask] = (temp.size() != 0 ) ? temp.size() : -1;
+        }
+
+    }
+    
+    vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {
+        n = req_skills.size();                                                                                          
+        m = people.size();
+        
+        unordered_map<string, int> skills; //skill -> unique number
+        for (int i = 0; i < n; ++i)
+            skills[req_skills[i]] = i;
+        
+        // represent each person by a single bitmasked number (k'th bit represents if a person has the k'th skill or not)
+        vector<int> people_skill;
+        for (auto &v: people) {
+            int skill_bit = 0;
+            for (string &skill: v)
+                skill_bit |= 1 << skills[skill];
+            
+            people_skill.push_back(skill_bit);
+        }
+        
+        // required set of skills mask (All set to 1s)
+        target_mask = pow(2, n) - 1;
+
+        t.resize(62, vector<int>(target_mask+1, -1));
+        vector<int> temp;
+        solve(people_skill, 0, temp, 0);
+        return result;
+    }
+};
