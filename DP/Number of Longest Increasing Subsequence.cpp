1c3e57ddd7ce099bc977f01f0e7385f77ade0f42
MAZHAR <iiestshibpur605@gmail.com>
2023-07-21 08:49:23 +0530

    Better code

---
+++ DP/Number of Longest Increasing Subsequence.cpp
@@ -0,0 +1,45 @@
+/*
+    Company Tags  : META
+    Leetcode Link : https://leetcode.com/problems/number-of-longest-increasing-subsequence/
+*/
+
+//Using same code of LIS - (T.C : O(n^2))
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> t(n, 1);
+        vector<int> count(n, 1);
+        
+        for(int i = 0; i<n; i++) {
+            
+            for(int j = 0; j<i; j++) {
+                
+                if(nums[j] < nums[i]) {
+                    
+                    if(t[i] == t[j] + 1)
+                        count[i] += count[j];
+                    else if(t[i] < t[j] + 1){
+                        t[i] = t[j] + 1;
+                        count[i] = count[j];
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+        int maxVal = *max_element(begin(t), end(t));
+        
+        int result = 0;
+        for(int i = 0; i<n; i++) {
+            if(t[i] == maxVal) {
+                result += count[i];
+            }
+        }
+        
+        return result;
+    }
+};
