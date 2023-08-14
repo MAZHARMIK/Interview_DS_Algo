54f2215a86f3988005467ef9c6fe40696648d4bc
MAZHAR <iiestshibpur605@gmail.com>
2023-08-14 18:28:24 +0530

    Update Kth Largest Element in an Array.cpp

--- Heap/Kth Largest Element in an Array.cpp
+++ Heap/Kth Largest Element in an Array.cpp
@@ -36,7 +36,48 @@ public:
 
 //Approach-3 (Using QuickSelect Algorithm
 //Time complexity: O(n) on average, O(n2) in the worst case (See video above for explanation)
-
+class Solution {
+public:
+    
+    //Hoare's partition
+    int partition(vector<int>& nums,int left,int right){
+        int pivot = nums[left];
+        int l     = left+1;
+        int r     = right;
+        
+        while(l<=r){
+            if(nums[l] < pivot && nums[r] > pivot)
+                swap(nums[l++],nums[r--]);
+            
+            if(nums[l] >= pivot) 
+                ++l;
+            
+            if(nums[r] <= pivot) 
+                --r;
+        }
+        swap(nums[left], nums[r]);
+        return r;
+    }
+    int findKthLargest(vector<int>& nums, int k) {
+        
+        //partition rule: >=pivot   pivot   <=pivot
+        int left=0, right=nums.size()-1, idx=0;
+        
+        while(true) {
+            
+            idx = partition(nums,left,right);
+            
+            if(idx == k-1) //kth element
+                break;
+            else if(idx < k-1)
+                left = idx+1;
+            else
+                right = idx-1;
+        }
+        
+        return nums[idx];
+    }
+};
 
 
 
