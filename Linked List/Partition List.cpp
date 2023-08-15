1ee0650ee9d453094f69e2a2084c23e24fe90f54
MAZHAR <iiestshibpur605@gmail.com>
2023-08-15 10:04:39 +0530

    Added Java Code

--- Linked List/Partition List.cpp
+++ Linked List/Partition List.cpp
@@ -4,6 +4,7 @@
     Leetcode Link               : https://leetcode.com/problems/partition-list/
 */
 
+************************************************************ C++ ************************************************************
 //Time : O(n), we visit each node only once
 class Solution {
 public:
@@ -31,3 +32,36 @@ public:
         return small->next;
     }
 };
+
+
+************************************************************ JAVA ************************************************************
+//Time : O(n), we visit each node only once
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode small = new ListNode(0);
+        ListNode large = new ListNode(0);
+        
+        ListNode smallP = small;
+        ListNode largeP = large;
+        
+        while(head != null) {
+            
+            if(head.val < x) {
+                smallP.next = head;
+                smallP = smallP.next;
+            } else {
+                largeP.next = head;
+                largeP = largeP.next;
+            }
+            
+            head = head.next;
+            
+        }
+        
+        
+        smallP.next = large.next;
+        largeP.next = null;
+        
+        return small.next;
+    }
+}
