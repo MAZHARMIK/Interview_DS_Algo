33cca43dcd750d6cea3689c4d47289a0383f521c
MAZHAR <iiestshibpur605@gmail.com>
2023-08-22 08:06:42 +0530

    Excel Sheet Column Title

---
+++ Mathematical/Excel Sheet Column Title.cpp
@@ -0,0 +1,29 @@
+/*
+      MY YOUTUBE VIDEO ON THIS Qn : 
+      Company Tags                : Facebook, Zenefits, Microsoft
+      Leetcode Link               : https://leetcode.com/problems/excel-sheet-column-title/
+*/
+
+//T.C : O(log(columnNumber))
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        
+        string s;
+        
+        while(columnNumber) {
+            columnNumber--;
+            
+            int remain = columnNumber%26;
+            char ch    = remain + 'A';
+            s.push_back(ch);
+            
+            columnNumber /= 26;
+            
+        }
+        
+        reverse(begin(s), end(s));
+        return s;
+        
+    }
+};
