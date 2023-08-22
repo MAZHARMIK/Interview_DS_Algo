4184e8c7a8688a7728b0b0553ae62e7e874c5f1b
MAZHAR <iiestshibpur605@gmail.com>
2023-08-22 09:42:45 +0530

    Added Java Code

--- Mathematical/Excel Sheet Column Title.cpp
+++ Mathematical/Excel Sheet Column Title.cpp
@@ -4,6 +4,7 @@
       Leetcode Link               : https://leetcode.com/problems/excel-sheet-column-title/
 */
 
+****************************** C++ ******************************
 //T.C : O(log(columnNumber))
 class Solution {
 public:
@@ -27,3 +28,25 @@ public:
         
     }
 };
+
+
+
+
+****************************** JAVA ******************************
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder result = new StringBuilder();
+        
+        while(columnNumber > 0) {
+            columnNumber--;
+            int remain = columnNumber%26;
+            
+
+            result.append((char)((remain + 'A')));
+            columnNumber = (columnNumber) / 26;
+        }
+        
+        // Reverse it, as we appended characters in reverse order.
+        return result.reverse().toString();
+    }
+}
