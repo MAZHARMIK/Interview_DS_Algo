145cf2400a820b86fa5336c700787d8837dfc1ab
MAZHAR <iiestshibpur605@gmail.com>
2023-08-24 12:39:27 +0530

    Added YouTube Link

--- strings/Text Justification.cpp
+++ strings/Text Justification.cpp
@@ -1,5 +1,5 @@
 /*
-      MY YOUTUBE VIDEO ON THIS Qn :  
+      MY YOUTUBE VIDEO ON THIS Qn :  https://www.youtube.com/watch?v=jpU2LVaDa4g
       Company Tags                : GOOGLE
       Leetcode Link               : https://leetcode.com/problems/text-justification/
 */
