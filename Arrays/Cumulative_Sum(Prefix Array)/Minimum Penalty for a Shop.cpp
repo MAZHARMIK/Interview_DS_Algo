338aa5c637225efa7421b30a514fc37e40c3d477
MAZHAR <iiestshibpur605@gmail.com>
2023-08-29 15:05:05 +0530

    Added YouTube Link

--- Arrays/Cumulative_Sum(Prefix Array)/Minimum Penalty for a Shop.cpp
+++ Arrays/Cumulative_Sum(Prefix Array)/Minimum Penalty for a Shop.cpp
@@ -1,5 +1,5 @@
 /*
-      MY YOUTUBE VIDEO ON THIS Qn : 
+      MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=XHgelA78SGs
       Company Tags                : META
       Leetcode Link               : https://leetcode.com/problems/minimum-penalty-for-a-shop/
 */
