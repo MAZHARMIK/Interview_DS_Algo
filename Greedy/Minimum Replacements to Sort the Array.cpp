1d66dde90d9f82638623f1a3c59e1d59d1fb12e7
MAZHAR <iiestshibpur605@gmail.com>
2023-08-30 13:16:09 +0530

    Added Java code

--- Greedy/Minimum Replacements to Sort the Array.cpp
+++ Greedy/Minimum Replacements to Sort the Array.cpp
@@ -4,6 +4,7 @@
       Leetcode Link               : https://leetcode.com/problems/minimum-replacements-to-sort-the-array/
 */
 
+************************************************** C++ **************************************************
 //Using Greedy Approach - T.C : O(n) , S.C : O(1)
 class Solution {
 public:
@@ -26,3 +27,31 @@ public:
         return result;
     }
 };
+
+
+************************************************** JAVA **************************************************
+class Solution {
+    public long minimumReplacement(int[] nums) {
+        int n = nums.length;
+        
+        long operations = 0;
+
+        for (int i = n - 2; i >= 0; i--) {
+            
+            if (nums[i] <= nums[i + 1]) {
+                continue;
+            }
+
+            long parts = (long)(nums[i]) / (long)nums[i + 1];
+            
+            if(nums[i] % nums[i+1] != 0)
+                parts += 1;
+
+            operations += parts - 1;
+
+            nums[i] = nums[i] / (int)parts;
+        }
+
+        return operations;
+    }
+}
