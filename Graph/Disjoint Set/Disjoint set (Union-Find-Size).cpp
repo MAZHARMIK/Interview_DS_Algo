efcac9da60238b79d83c82c49f5d0c80a5e5edb6
MAZHAR <iiestshibpur605@gmail.com>
2023-08-30 17:52:42 +0530

    DSU by Size

--- Graph/Disjoint Set/Disjoint set (Union-Find-Size).cpp
+++ Graph/Disjoint Set/Disjoint set (Union-Find-Size).cpp
@@ -3,32 +3,65 @@
     Company Tags                : Google, Facebook, Apple, Amazon, Netflix, Flipkart
 */
 
-/*Complete the functions below*/
-vector<int> parent;
-vector<int> size;
+#include <bits/stdc++.h>
+using namespace std;
+class DSU {
+    vector<int> parent;
+	vector<int> size;
+public:
+    DSU(int n) {
+    	parent.resize(n);
+        size.resize(n);
 
-int find (int x) {
-    if (x == parent[x]) 
-        return x;
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
 
-    return parent[x] = find(parent[x]);
-}
+    int find(int x) {
+        if (x == parent[x])
+            return x;
+            
+        return parent[x] = find(parent[x]);
+    }
 
-void Union (int x, int y) {
-    int x_parent = find(x);
-    int y_parent = find(y);
-
-    if (x_parent == y_parent) 
-        return;
-
-    if(size[x_parent] > rank[y_parent]) {
-        parent[y_parent] = x_parent;
-        size[x_parent]  += size[y_parent];
-    } else if(rank[x_parent] < rank[y_parent]) {
-        parent[x_parent] = y_parent;
-        size[y_parent]  += size[x_parent];
+    void Union (int x, int y) {
+	    int x_parent = find(x);
+	    int y_parent = find(y);
+	
+	    if (x_parent == y_parent) 
+	        return;
+	
+	    if(size[x_parent] > size[y_parent]) {
+	        parent[y_parent] = x_parent;
+	        size[x_parent]  += size[y_parent];
+	    } else if(size[x_parent] < size[y_parent]) {
+	        parent[x_parent] = y_parent;
+	        size[y_parent]  += size[x_parent];
+	    } else {
+	        parent[x_parent] = y_parent;
+	        size[y_parent]  += size[x_parent];
+	    }
+	}
+};
+int main() {
+    DSU dsu(6);
+    dsu.Union(0, 1);
+    dsu.Union(0, 2);
+    
+    // if 0 and 3 are not on same Component yet
+    if (dsu.find(0) == dsu.find(3)) {
+        cout << "In same component" << endl;
     } else {
-        parent[x_parent] = y_parent;
-        size[y_parent]  += size[x_parent];
+    	cout << "Not in same component" << endl;
+    }
+    
+    //Now we will Union 0 and 3
+    dsu.Union(0, 3);
+    if (dsu.find(0) == dsu.find(3)) {
+        cout << "In same component" << endl;
+    } else {
+    	cout << "Not in same component" << endl;
     }
 }
