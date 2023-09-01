70cca89768a9cc7c4d5d1bdbc681c03db08f6696
MAZHAR <iiestshibpur605@gmail.com>
2023-09-01 06:54:05 +0530

    Counting Bits

---
+++ Bit_Magic/Counting Bits.cpp
@@ -0,0 +1,45 @@
+/*
+      MY YOUTUBE VIDEO ON THIS Qn : 
+      Company Tags                : 
+      Leetcode Link               : https://leetcode.com/problems/counting-bits/
+*/
+
+//Approach-1 (Using built-in function) - __builtin_popcount - O(nlog(n))
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> result(n+1);
+        
+        for(int i = 0; i<n+1; i++) {
+            result[i] = __builtin_popcount(i); //log(n)
+        }
+        
+        return result;
+    }
+};
+
+//Approach-2 - T.C : O(n)
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> result(n+1);
+        
+        if(n == 0)
+            return result;
+        
+        result[1] = 1;
+        
+        for(int i = 2; i < n+1; i++) {
+            
+            if(i%2 == 0) {
+                result[i] = result[i/2];
+            } else {
+                result[i] = result[i/2] + 1;
+            }
+            
+        }
+        
+        return result;
+        
+    }
+};
