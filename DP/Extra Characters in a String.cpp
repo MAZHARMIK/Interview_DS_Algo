5577d251f0042d6455109e3698e2c7cc540cfac9
MAZHAR <iiestshibpur605@gmail.com>
2023-09-03 08:57:26 +0530

    Added JAVA Code

--- DP/Extra Characters in a String.cpp
+++ DP/Extra Characters in a String.cpp
@@ -4,7 +4,7 @@
       Leetcode Link               : https://leetcode.com/problems/extra-characters-in-a-string/
 */
 
-
+************************************************************ C++ ************************************************************
 //Approach-1 (Recursion + Memo)
 class Solution {
 public:
@@ -53,4 +53,52 @@ public:
         return solve(0, s);
     }
 };
+
+
 //Approach-2 (Bottom Up) - SOOON
+
+
+
+
+************************************************************ JAVA ************************************************************
+//Approach-1 (Recursion + Memo)
+class Solution {
+    int n;
+    int[] t;
+    public int minExtraChar(String s, String[] dictionary) {
+        n = s.length();
+        t = new int[n];
+        
+        Arrays.fill(t, -1);
+        
+        Set<String> set = new HashSet<String>();
+        
+        for(String s1 : dictionary) {
+            set.add(s1);
+        }
+        
+        return solve(s, set, 0);
+    }
+
+    public int solve(String s, Set<String> set, int idx) {
+        if(idx >= n) 
+            return 0;
+        
+        if(t[idx] != -1) 
+            return t[idx];
+        
+        int minChars = n;
+        
+        for(int i = idx; i < n; i++) {
+            
+            String temp = s.substring(idx, i+1);
+            
+            if(set.contains(temp)) {
+                minChars = Math.min(minChars, solve(s, set, i+1));
+            } else {
+                minChars = Math.min(minChars, temp.length() + solve(s, set, i+1));
+            }
+        }
+        return t[idx] = minChars;
+    }
+}
