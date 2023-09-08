171f404182f9194a967d21b2ae56147842a7327c
MAZHAR <iiestshibpur605@gmail.com>
2023-09-08 23:08:54 +0530

    Added JAVA code

--- Arrays/Pascal's Triangle.cpp
+++ Arrays/Pascal's Triangle.cpp
@@ -4,6 +4,7 @@
     Leetcode Link               : https://leetcode.com/problems/pascals-triangle/
 */
 
+/*************************************************** C++ ********************************************************/
 //Approach-1 (Doing simply just like Pascal triangle is formed)
 class Solution {
 public:
@@ -54,3 +55,26 @@ public:
         return result;
     }
 };
+
+
+/*************************************************** JAVA ********************************************************/
+//Approach-1 (Doing simply just like Pascal triangle is formed)
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        for(int i = 0; i<numRows; i++) {
+            List<Integer> row = new ArrayList<>(i+1);
+            for(int j = 0; j <= i; j++) {
+                if(j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    int num = result.get(i-1).get(j) + result.get(i-1).get(j-1);
+                    row.add(num);
+                }
+            }
+            result.add(row);
+        }
+        return result;
+    }
+}
