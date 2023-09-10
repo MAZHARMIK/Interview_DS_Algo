d9cff8ea01a28dc48602b6b256ff9b6e3128caf8
MAZHAR <iiestshibpur605@gmail.com>
2023-09-10 11:31:31 +0530

    Added Java code

--- Mathematical/Count All Valid Pickup and Delivery Options.cpp
+++ Mathematical/Count All Valid Pickup and Delivery Options.cpp
@@ -4,6 +4,7 @@
       Leetcode Link               : https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/
 */
 
+/**************************************************************** C++ ****************************************************************/
 class Solution {
 public:
     int M = 1e9+7;
@@ -27,3 +28,25 @@ public:
         return result;
     }
 };
+
+
+/**************************************************************** JAVA ****************************************************************/
+class Solution {
+    public int M = 1000000007;
+    
+    public int countOrders(int n) {
+        if(n == 1)
+            return 1;
+        long result = 1; 
+        for(int i = 2; i<=n; i++) {
+            int spaces = (i-1)*2 + 1;
+            
+            int possibility = spaces * (spaces+1)/2;
+            
+            result *= possibility;
+            
+            result %= M;
+        }
+        return (int)result;
+    }
+}
