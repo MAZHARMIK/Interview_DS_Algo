7939ed3af799cf250bb696b43b7ae988e0a92b55
MAZHAR <iiestshibpur605@gmail.com>
2023-09-13 17:25:46 +0530

    Update Candy.cpp

--- Greedy/Candy.cpp
+++ Greedy/Candy.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=YUT13Koh5Jg
     Company Tags                : Amazon
     Leetcode Link               : https://leetcode.com/problems/candy/
 */
