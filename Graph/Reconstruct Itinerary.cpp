5dad95d65f22f7e35cd8892d23c29471965ac0f3
MAZHAR <iiestshibpur605@gmail.com>
2023-09-14 18:47:12 +0530

    Added Java Code

--- Graph/Reconstruct Itinerary.cpp
+++ Graph/Reconstruct Itinerary.cpp
@@ -4,6 +4,7 @@
       Leetcode Link               : https://leetcode.com/problems/reconstruct-itinerary/
 */
 
+/******************************************************** C++ ********************************************************/
 //Approach-1 (Using adj of string to vector and sorting it)
 //T.C : O(V^2)
 class Solution {
@@ -92,3 +93,48 @@ public:
         return result;
     }
 };
+
+
+/******************************************************** JAVA ********************************************************/
+class Solution {
+    Map<String,List<String>> graph = new HashMap();
+    List<String> result = new ArrayList();
+    int numTickets = 0;
+    public List<String> findItinerary(List<List<String>> tickets) {
+        numTickets = tickets.size();
+        for(List<String> ticket:tickets){
+            graph.put(ticket.get(0),new ArrayList());
+        }
+        for(List<String> ticket:tickets){
+            graph.get(ticket.get(0)).add(ticket.get(1));
+        }
+        for(Map.Entry<String,List<String>> edges:graph.entrySet()){
+            Collections.sort(edges.getValue());
+        }
+
+        List<String> path = new ArrayList();
+        DFS("JFK",path);
+        
+        return result;
+    }
+    
+    public boolean DFS(String fromAirport,List<String> path){
+        path.add(fromAirport);
+        if(path.size() == numTickets+1){
+            result = path;
+            return true;
+        }
+        
+        List<String> neighbors = graph.get(fromAirport);
+        for(int i=0;neighbors!=null && i<neighbors.size();i++){
+            String toAirport = neighbors.get(i);
+            neighbors.remove(toAirport);
+            if(DFS(toAirport,path)){
+                return true;
+            }
+            neighbors.add(i,toAirport);
+        }
+        path.remove(path.size()-1);
+        return false;
+    }
+}
