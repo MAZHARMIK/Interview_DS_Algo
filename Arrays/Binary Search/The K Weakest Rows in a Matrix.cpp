ea4e39957c312275b8ac01b6add20ed13956650b
MAZHAR <iiestshibpur605@gmail.com>
2023-09-18 07:49:12 +0530

    Added JAVA Code

--- Arrays/Binary Search/The K Weakest Rows in a Matrix.cpp
+++ Arrays/Binary Search/The K Weakest Rows in a Matrix.cpp
@@ -4,7 +4,8 @@
     Leetcode Link               : https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
 */
 
-//Approach-1 (Using Binary Search)
+/***************************************** C++ *****************************************/
+//Approach-1 (Using Sorting + Binary Search)
 //T.C : O(m*log(n)) + O(m*log(m))
 //S.C : O(m) to store count of ones and corresponding row in the vector
 class Solution {
@@ -56,7 +57,7 @@ public:
     }
 };
 
-//Approach-2 (Using Priority_queue - max-heap of size k)
+//Approach-2 (Using Priority_queue - max-heap of size k + Binary Search)
 //Why max-heap - So that when the size of mac-heap reaches more than k, we pop the bigger elements because we need answer in descending order of weakness of rows
 //T.C : //O(m*log(n) + m*log(k))
 //S.C : O(k) to store count of ones and corresponding row in the min-heap
@@ -116,3 +117,94 @@ public:
         return result;
     }
 };
+
+
+/***************************************** JAVA *****************************************/
+//Approach-1 (Using Sorting + Binary Search)
+//T.C : O(m*log(n)) + O(m*log(m))
+//S.C : O(m) to store count of ones and corresponding row in the vector
+class Solution 
+{
+    public int[] kWeakestRows(int[][] mat, int k)  {
+        List<int[]> countOnes = new ArrayList<>();
+        int ind = 0;
+        for(int[] a: mat) {
+            int soldiers = binarySearch(a);
+            countOnes.add(new int[]{ind++, soldiers});
+        }
+        
+        countOnes.sort((x1, x2) -> (x1[1]-x2[1]));
+        
+        int[] res = new int[k];
+        
+        for(int i=0;i<k;i++)
+            res[i] = countOnes.get(i)[0];
+        
+        return res;
+    }
+    
+    public int binarySearch(int[] a) {
+        
+        int n = a.length;
+        int l = 0, r = n-1;
+        int result = -1;
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            if(a[mid] == 1) {
+                result = mid;
+                l = mid+1;
+            }
+            else
+                r = mid-1;
+        }
+        return result+1;
+    }
+}
+
+
+//Approach-2 (Using max-heap + Binary Search)
+//Why max-heap - So that when the size of mac-heap reaches more than k, we pop the bigger elements because we need answer in descending order of weakness of rows
+//T.C : //O(m*log(n) + m*log(k))
+//S.C : O(k) to store count of ones and corresponding row in the min-heap
+class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+        
+        //max-heap
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] != b[0] ? b[0] - a[0] : b[1] - a[1]);
+        
+        int[] ans = new int[k];
+        
+        for (int i = 0; i < mat.length; i++) {
+            pq.offer(new int[] {numOnes(mat[i], 0, n-1), i});
+            //pq.offer(new int[] {binarySearch(mat[i], 0, n-1), i});
+            if (pq.size() > k)
+                pq.poll();
+        }
+        
+        while (k > 0)
+            ans[--k] = pq.poll()[1];
+        
+        return ans;
+    }
+    
+    private int numOnes(int[] arr, int l, int r) {
+
+        int mid;
+        int result = -1;
+        while(l <= r) {
+            mid = l + (r-l)/2;
+            
+            if(arr[mid] == 1) {
+                result = mid;
+                l = mid+1;
+            } else if(arr[mid] == 0) {
+                r = mid-1;
+            }
+            
+        }
+
+        return result+1;
+    }
+}
