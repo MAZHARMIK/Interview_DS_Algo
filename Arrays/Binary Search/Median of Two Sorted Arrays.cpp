8e5c46a2d552fcc98702768089ab3bb97b3c200e
MAZHAR <iiestshibpur605@gmail.com>
2023-09-21 11:25:00 +0530

    Median of Two Sorted Arrays

--- Arrays/Binary Search/Median of Two Sorted Arrays.cpp
+++ Arrays/Binary Search/Median of Two Sorted Arrays.cpp
@@ -1,9 +1,12 @@
 /*
-    Company Tags  : Google, Microsoft, Apple, Zenefits, Yahoo, Adobe, Dropbox
-    Leetcode Link : https://leetcode.com/problems/median-of-two-sorted-arrays/
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Google, Microsoft, Apple, Zenefits, Yahoo, Adobe, Dropbox
+    Leetcode Link               : https://leetcode.com/problems/median-of-two-sorted-arrays/
 */
 
-//Approach-1 (O(nlong))
+//Approach-1 - Brute Force
+//T.C : O(m+n)
+//S.C : O(m+n)
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
@@ -25,48 +28,79 @@ public:
         
         int size = m+n;
         
-        if(size%2 != 0) return temp[size/2];
+        if(size%2 != 0)
+            return temp[size/2];
         
-        return (temp[size/2]+temp[max(0, size/2-1)])/(double(2));
+        return (temp[size/2]+temp[(size/2)-1])/2.0;
         
     }
 };
 
-//Approach-2 (O(log(min(m, n)))
-//Source : Tushar Roy's Explanation : https://www.youtube.com/watch?v=LPFhl65R7ww
+
+//Approach-2 - Optimising Space from Approach-1
+//T.C : O(m+n)
+//S.C : O(1)
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size() > nums2.size())
-            return findMedianSortedArrays(nums2, nums1);
-        
         int m = nums1.size();
         int n = nums2.size();
+        int size = m+n;
         
-        int low = 0, high = m;
-        while(low <= high) {
-            
-            int Px = low + (high-low)/2;
-            int Py = (m+n+1)/2 - Px;
-            
-            int maxLeftX  = (Px == 0) ? INT_MIN : nums1[Px-1];
-            int minRightX = (Px == m) ? INT_MAX : nums1[Px];
-            
-            int maxLeftY  = (Py == 0) ? INT_MIN : nums2[Py-1];
-            int minRightY = (Py == n) ? INT_MAX : nums2[Py];
-            
-            if(maxLeftX <= minRightY && maxLeftY <= minRightX) {
-                if((m+n)%2 == 0)
-                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY))/(double)2;
-                
-                return max(maxLeftX, maxLeftY);
-            } else if(maxLeftX > minRightY) {
-                high = Px-1;
+        int idx1 = (size/2)-1;
+        int element1 = -1;
+        int idx2 = size/2;
+        int element2 = -1;
+        
+        int i = 0, j = 0, k = 0;
+        
+        while(i < m && j < n) {
+            if(nums1[i] < nums2[j]) {
+                if(k == idx1) {
+                    element1 = nums1[i];
+                }
+                if(k == idx2) {
+                    element2 = nums1[i];
+                }
+                i++;
             } else {
-                low = Px+1;
+                if(k == idx1) {
+                    element1 = nums2[j];
+                }
+                if(k == idx2) {
+                    element2 = nums2[j];
+                }
+                j++;
             }
+            k++;
         }
         
-        return -1;
+        while(i < m) {
+            if(k == idx1) {
+                element1 = nums1[i];
+            }
+            if(k == idx2) {
+                element2 = nums1[i];
+            }
+            i++;
+            k++;
+        }
+        
+        while(j < n) {
+            if(k == idx1) {
+                element1 = nums2[j];
+            }
+            if(k == idx2) {
+                element2 = nums2[j];
+            }
+            j++;
+            k++;
+        }
+
+        if(size%2 == 1)
+            return element2;
+        
+        return (element1 + element2)/2.0;
+        
     }
 };
