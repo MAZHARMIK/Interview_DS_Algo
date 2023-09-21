1763129edd0b8c851e87ed17c1f62d8e3e310bfd
MAZHAR <iiestshibpur605@gmail.com>
2023-09-21 17:49:13 +0530

    Added YouTube link

--- Arrays/Binary Search/Median of Two Sorted Arrays.cpp
+++ Arrays/Binary Search/Median of Two Sorted Arrays.cpp
@@ -1,5 +1,6 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : Part-1 (Brute & Better) - https://www.youtube.com/watch?v=6D9T2ZY8h5c
+                                  Part-1 (Binary Search)  - 
     Company Tags                : Google, Microsoft, Apple, Zenefits, Yahoo, Adobe, Dropbox
     Leetcode Link               : https://leetcode.com/problems/median-of-two-sorted-arrays/
 */
@@ -102,5 +103,46 @@ public:
         
         return (element1 + element2)/2.0;
         
+    }
+};
+
+
+//Approach-3 (Using Binary Search)
+//T.C : O(log(min(m, n))
+//S.C : O(1)
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if(nums1.size() > nums2.size())
+            return findMedianSortedArrays(nums2, nums1);
+        
+        int m = nums1.size();
+        int n = nums2.size();
+        
+        int low = 0, high = m;
+        while(low <= high) {
+            
+            int Px = low + (high-low)/2;
+            int Py = (m+n+1)/2 - Px;
+            
+            int x1  = (Px == 0) ? INT_MIN : nums1[Px-1];
+            int x3 = (Px == m) ? INT_MAX : nums1[Px];
+            
+            int x2  = (Py == 0) ? INT_MIN : nums2[Py-1];
+            int x4 = (Py == n) ? INT_MAX : nums2[Py];
+            
+            if(x1 <= x4 && x2 <= x3) {
+                if((m+n)%2 == 0)
+                    return (max(x1, x2) + min(x3, x4))/2.0;
+                
+                return max(x1, x2);
+            } else if(x1 > x4) {
+                high = Px-1;
+            } else {
+                low = Px+1;
+            }
+        }
+        
+        return -1;
     }
 };
