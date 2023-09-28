91ffb5c9d12b76e137195c8943d80b95b2233b28
MAZHAR <iiestshibpur605@gmail.com>
2023-09-28 07:28:50 +0530

    Sort Array By Parit

---
+++ Arrays/Sort Array By Parity.cpp
@@ -0,0 +1,90 @@
+/*
+      MY YOUTUBE VIDEO ON THIS Qn : 
+      Company Tags                : META
+      Leetcode Link               : https://leetcode.com/problems/sort-array-by-parity/
+*/
+
+/************************************************************ C++ ************************************************************/
+//Approach-1 (Using simple iteration)
+//T.C : O(n)
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int j = 0;
+        
+        for(int i = 0; i<nums.size(); i++) {
+            
+            if(nums[i]%2 == 0) {//even
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+        
+        return nums;
+    }
+};
+
+//Approach-2 (Writing your own custom comparator for sorting)
+class Solution {
+public:
+    
+    static bool comparator(int a, int b) {
+        return a%2 < b%2; //a%2 will be 0 if it's even and since we do a%2 < b%2, hence a will come first if it's even
+    }
+    
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        sort(begin(nums), end(nums), comparator);
+        
+        return nums;
+    }
+};
+
+/************************************************************ JAVA ************************************************************/
+//Approach-1 (Using simple iteration)
+//T.C : O(n)
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int j = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) { // even
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+
+        return nums;
+    }
+}
+
+
+//Approach-2 (Writing your own custom comparator for sorting)
+//T.C : O(nlog(n))
+class Solution {
+    
+    static boolean comparator(int a, int b) {
+        return a%2 < b%2; //a%2 will be 0 if it's even and since we do a%2 < b%2, hence a will come first if it's even
+    }
+    
+    public int[] sortArrayByParity(int[] nums) {
+        
+        Integer[] numsWrapper = new Integer[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            numsWrapper[i] = nums[i];
+        }
+        
+        Arrays.sort(numsWrapper, new Comparator<Integer>() {
+            public int compare(Integer a, Integer b) {
+                return comparator(a, b) ? -1 : 1;
+            }
+        });
+        
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = numsWrapper[i];
+        }
+        
+        return nums;
+    }
+}
