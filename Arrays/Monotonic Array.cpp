ccdcd0bccd589958578f7317581946f0371adfff
MAZHAR <iiestshibpur605@gmail.com>
2023-09-29 08:34:05 +0400

    Monotonic Array

---
+++ Arrays/Monotonic Array.cpp
@@ -0,0 +1,50 @@
+/*
+      MY YOUTUBE VIDEO ON THIS Qn : 
+      Company Tags                : META (3 Times)
+      Leetcode Link               : https://leetcode.com/problems/monotonic-array/
+*/
+
+/*************************** C++ ***************************/
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int n = nums.size();
+        
+        bool increasing = false;
+        bool decreasing = false;
+        
+        for(int i = 0; i < n-1; i++) {
+            if(nums[i] < nums[i+1]) increasing = true;
+            
+            if(nums[i] > nums[i+1]) decreasing = true;
+
+        }
+        
+        if(increasing == true && decreasing == true)
+                return false;
+        
+        return true;
+    }
+};
+
+/*************************** JAVA ***************************/
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        int n = nums.length;
+        
+        boolean increasing = false;
+        boolean decreasing = false;
+        
+        for(int i = 0; i < n - 1; i++) {
+            if(nums[i] < nums[i+1]) {
+                increasing = true;
+            }
+            
+            if(nums[i] > nums[i+1]) {
+                decreasing = true;
+            }
+        }
+        
+        return !(increasing && decreasing);
+    }
+}
