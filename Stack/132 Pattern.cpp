712b94033375ceac0bad1454ce9ef34432c92ee5
MAZHAR <iiestshibpur605@gmail.com>
2023-09-30 13:17:15 +0400

    Update 132 Pattern.cpp

--- Stack/132 Pattern.cpp
+++ Stack/132 Pattern.cpp
@@ -51,9 +51,9 @@ public:
 //Approach-3 --> Using Monotonic stack
 //T.C : O(n) - We don't visit any element more than once
 /*
-    We are only storing one item in the stack, which is our ideal candidate for s2 (number that needs to be the largest). 
-    If we find a number that is bigger than what we thought was our ideal candidate for s2; we pop out our stack 
-    and store the value in s3 (mid value number), then we store the new ideal candidate for s2 in the stack. 
+    We are only storing one item in the stack, which is our ideal candidate for num2 (number that needs to be the largest). 
+    If we find a number that is bigger than what we thought was our ideal candidate for num2; we pop out our stack 
+    and store the value in num3 (mid value number), then we store the new ideal candidate for num2 in the stack. 
     On the next ith iteration, if nums[i] is actually less than s3, then we are done!
 */
 class Solution {
