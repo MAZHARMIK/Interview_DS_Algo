537bd7b6ba7a62dcb2314e904583f14e7a60563a
MAZHAR <iiestshibpur605@gmail.com>
2023-10-02 08:41:05 +0400

    Remove Colored Pieces if Both Neighbors are the Same Color

---
+++ Greedy/Remove Colored Pieces if Both Neighbors are the Same Color.cpp
@@ -0,0 +1,50 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Google
+    Leetcode Link               : https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color/
+
+*/
+
+/**************************************** C++ ****************************************/
+class Solution {
+public:
+    bool winnerOfGame(string colors) {
+        int n = colors.length();
+        int alice = 0;
+        int bob   = 0;
+        
+        for(int i = 1; i<n; i++) {
+            
+            if(colors[i-1] == colors[i] && colors[i] == colors[i+1]) {
+                if(colors[i] == 'A')
+                    alice++;
+                else
+                    bob++;
+            }
+        }
+        
+        return alice > bob;
+    }
+};
+
+
+/**************************************** JAVA ****************************************/
+public class Solution {
+    public boolean winnerOfGame(String colors) {
+        int n = colors.length();
+        int alice = 0;
+        int bob = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+            if (colors.charAt(i - 1) == colors.charAt(i) && colors.charAt(i) == colors.charAt(i + 1)) {
+                if (colors.charAt(i) == 'A') {
+                    alice++;
+                } else {
+                    bob++;
+                }
+            }
+        }
+
+        return alice > bob;
+    }
+}
