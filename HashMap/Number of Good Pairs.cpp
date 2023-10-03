370fdb821bd21180956bde13b6e0e5cfa19f9abd
MAZHAR <iiestshibpur605@gmail.com>
2023-10-03 13:11:44 +0400

    Added YouTube Link

--- HashMap/Number of Good Pairs.cpp
+++ HashMap/Number of Good Pairs.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=RSu9uQ-OGb0
     Company Tags                : META
     Leetcode Link               : https://leetcode.com/problems/number-of-good-pairs/
 */
