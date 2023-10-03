d0101016409d1cbe6a74b0613a73345de185c711
MAZHAR <iiestshibpur605@gmail.com>
2023-10-03 10:28:01 +0400

    Added Java code and company tags

--- HashMap/Number of Good Pairs.cpp
+++ HashMap/Number of Good Pairs.cpp
@@ -1,6 +1,6 @@
 /*
     MY YOUTUBE VIDEO ON THIS Qn : 
-    Company Tags                : 
+    Company Tags                : META
     Leetcode Link               : https://leetcode.com/problems/number-of-good-pairs/
 */
 
