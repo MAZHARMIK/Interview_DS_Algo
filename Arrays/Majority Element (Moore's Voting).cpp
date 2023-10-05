68a97c6afa5cc2c82555e52d5d92767837c65c80
MAZHAR <iiestshibpur605@gmail.com>
2023-10-05 11:28:31 +0400

    Added JAVA code

--- Arrays/Majority Element (Moore's Voting).cpp
+++ Arrays/Majority Element (Moore's Voting).cpp
@@ -1,29 +1,54 @@
 /*
-    Company Tags     : Amazon, Accolite, D-E-Shaw, FactSet, MakeMyTrip, Microsoft, Samsung
-    Leetcode Qn Link : https://leetcode.com/problems/majority-element/
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Amazon, Accolite, D-E-Shaw, FactSet, MakeMyTrip, Microsoft, Samsung
+    Leetcode Qn Link            : https://leetcode.com/problems/majority-element/
 */
 
+/********************************************** C++ **********************************************/
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        int count = 1;
-        int maj = nums[0];
+        int count = 0;
+        int maj = NULL;
         
-        for(int i = 1; i<n; i++) {
-            if(nums[i] == maj) {
-                count++;
-            } else {
-                count--;
-            }
-            
+        for(int i = 0; i<n; i++) {
             if(count == 0) {
                 count = 1;
                 maj = nums[i];
             }
+            else if(nums[i] == maj) {
+                count++;
+            } else {
+                count--;
+            }
         }
         
-        //Since it is gauranteen that majority element exists, so we don't check for freq of "maj" and return
+        //Since it is gauranteed that majority element exists, so we don't check for freq of "maj" and return
         return maj;
     }
 };
+
+
+/********************************************** JAVA **********************************************/
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        Integer maj = null;
+        
+        for (int i = 0; i < n; i++) {
+            if (count == 0) {
+                count = 1;
+                maj = nums[i];
+            } else if (nums[i] == maj) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        
+        // Since it is guaranteed that the majority element exists, so we don't check for the frequency of "maj" and return
+        return maj;
+    }
+}
