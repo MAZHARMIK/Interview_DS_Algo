2de4ede2267406a9fc1a79927ba1a261984089d7
MAZHAR <iiestshibpur605@gmail.com>
2023-10-05 14:36:04 +0400

    Added YouTube Link

--- Arrays/Majority Element (Moore's Voting).cpp
+++ Arrays/Majority Element (Moore's Voting).cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=Q6L5SoS-fTo
     Company Tags                : Amazon, Accolite, D-E-Shaw, FactSet, MakeMyTrip, Microsoft, Samsung
     Leetcode Qn Link            : https://leetcode.com/problems/majority-element/
 */
@@ -29,6 +29,29 @@ public:
     }
 };
 
+//////OR you can also code it like this (Eaxct similar to Majority Element-II)
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        int maj = NULL;
+
+        for(int i = 0; i<n; i++) {
+            if(nums[i] == maj) {
+                count++;
+            } else if(count == 0) {
+                maj = nums[i];
+                count = 1;
+            } else {
+                count--;
+            }
+
+        }
+
+        return maj;
+    }
+};
 
 /********************************************** JAVA **********************************************/
 class Solution {
@@ -49,6 +72,29 @@ class Solution {
         }
         
         // Since it is guaranteed that the majority element exists, so we don't check for the frequency of "maj" and return
+        return maj;
+    }
+}
+
+
+//////OR you can also code it like this (Eaxct similar to Majority Element-II)
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        Integer maj = null;
+
+        for (int i = 0; i < n; i++) {
+            if (maj != null && nums[i] == maj) {
+                count++;
+            } else if (count == 0) {
+                maj = nums[i];
+                count = 1;
+            } else {
+                count--;
+            }
+        }
+
         return maj;
     }
 }
