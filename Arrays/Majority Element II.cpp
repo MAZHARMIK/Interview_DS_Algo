48752590486bb78b18e663ecd19713f2226034cb
MAZHAR <iiestshibpur605@gmail.com>
2023-10-05 10:05:11 +0400

    Added JAVA code

--- Arrays/Majority Element II.cpp
+++ Arrays/Majority Element II.cpp
@@ -1,8 +1,10 @@
 /*
-    Company Tags     : Amazon, Google
-    Leetcode Qn Link : https://leetcode.com/problems/majority-element-ii/
+    MY YOUTUBE VIDEO ON THIS Qn : soon today
+    Company Tags                : Amazon, Google
+    Leetcode Qn Link            : https://leetcode.com/problems/majority-element-ii/
 */
 
+/**************************************************** C++ ****************************************************/
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
@@ -50,3 +52,55 @@ public:
 
     }
 };
+
+
+
+/**************************************************** JAVA ****************************************************/
+public class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        
+        Integer maj1 = null;
+        int count1 = 0;
+
+        Integer maj2 = null;
+        int count2 = 0;
+        
+        for (int num : nums) {
+            if (maj1 != null && num == maj1) {
+                count1++;
+            } else if (maj2 != null && num == maj2) {
+                count2++;
+            } else if (count1 == 0) {
+                maj1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                maj2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (maj1 != null && num == maj1) {
+                count1++;
+            } else if (maj2 != null && num == maj2) {
+                count2++;
+            }
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        if (count1 > Math.floor(n / 3)) {
+            result.add(maj1);
+        }
+        if (count2 > Math.floor(n / 3)) {
+            result.add(maj2);
+        }
+        
+        return result;
+    }
+}
