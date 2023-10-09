4d82b4bf21fca17279e7457633424a9129a54539
MAZHAR <iiestshibpur605@gmail.com>
2023-10-09 10:52:08 +0400

    Added JAVA code

--- Arrays/Binary Search/Find First and Last Position of Element in Sorted Array.cpp
+++ Arrays/Binary Search/Find First and Last Position of Element in Sorted Array.cpp
@@ -86,3 +86,97 @@ public:
 
 
 /********************************************** JAVA **********************************************/
+//Approach-1 (Using Two Custom Binary Search)
+//T.C : O(nlogn)
+class Solution {
+    public int findFirstPosition(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        int result = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                result = mid; // possibly my answer
+                r = mid - 1; // but lets look at left more
+            } else if (nums[mid] > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return result;
+    }
+
+    public int findLastPosition(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        int result = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                result = mid; // possibly my answer
+                l = mid + 1; // but lets look at right more
+            } else if (nums[mid] > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return result;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int firstPosition = findFirstPosition(nums, target);
+        int lastPosition = findLastPosition(nums, target);
+        return new int[]{firstPosition, lastPosition};
+    }
+}
+
+
+//Approach-2 (Using Own written lower and upper bound in java)
+//T.C : O(nlogn)
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int l = lowerBound(nums, target);
+        int r = upperBound(nums, target);
+        
+        if (l == nums.length || nums[l] != target) {
+            return new int[]{-1, -1};
+        }
+        
+        return new int[]{l, r - 1};
+    }
+    
+    private int lowerBound(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        return left;
+    }
+    
+    private int upperBound(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        return left;
+    }
+}
