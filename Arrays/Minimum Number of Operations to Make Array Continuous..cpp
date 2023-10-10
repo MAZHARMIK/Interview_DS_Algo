99004f6cc9c3d0fdd7925a4e652807f6ab7dc7e1
MAZHAR <iiestshibpur605@gmail.com>
2023-10-10 16:12:36 +0400

    Update Minimum Number of Operations to Make Array Continuous..cpp

--- Arrays/Minimum Number of Operations to Make Array Continuous..cpp
+++ Arrays/Minimum Number of Operations to Make Array Continuous..cpp
@@ -15,10 +15,6 @@ public:
     int minOperations(vector<int>& nums) {
         int n = nums.size();
         
-        unordered_set<int> st(begin(nums), end(nums));
-        
-        vector<int> temp(begin(st), end(st));
-        
         int result = INT_MAX;
         
         for(int i = 0; i < n; i++) {
