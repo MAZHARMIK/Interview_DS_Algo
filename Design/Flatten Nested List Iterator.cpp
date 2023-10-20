8e6c21a96215a85e75f6b67dbda8f8e41b61f475
MAZHAR <iiestshibpur605@gmail.com>
2023-10-20 14:21:41 +0530

    Added YouTube Link

--- Design/Flatten Nested List Iterator.cpp
+++ Design/Flatten Nested List Iterator.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=0PFE_7S3X-U
     Company Tags                : Google, Twitter, Facebook, Netflix
     Leetcode Link               : https://leetcode.com/problems/flatten-nested-list-iterator/
 */
