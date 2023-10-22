8b3be2b5e9de442a86302ce1b436c77c8806181a
MAZHAR <iiestshibpur605@gmail.com>
2023-10-22 10:54:55 +0530

    Maximum Score of a Good Subarray

---
+++ Greedy/Maximum Score of a Good Subarray.cpp
@@ -0,0 +1,78 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : AMAZON
+    Leetcode Link               : https://leetcode.com/problems/maximum-score-of-a-good-subarray/
+*/
+
+/****************************************** C++ ******************************************/
+class Solution {
+public:
+    int maximumScore(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        int i = k;
+        int j = k;
+        int curMin = nums[k];
+        
+        int result = nums[k];
+        
+        while(i > 0 || j < n-1) {
+            int leftValue;
+            int rightValue;
+            
+            if(i > 0) {
+                leftValue = nums[i-1];
+            } else {
+                leftValue = 0;
+            }
+            
+            if(j < n-1) {
+                rightValue = nums[j+1];
+            } else {
+                rightValue = 0;
+            }
+            
+            if(leftValue < rightValue) { //Check my video for intuition behind this
+                j++;
+                curMin = min(curMin, nums[j]);
+            } else {
+                i--;
+                curMin = min(curMin, nums[i]);
+            }
+            
+            result = max(result, curMin * (j-i+1));
+        }
+        
+        return result;
+    }
+};
+
+
+
+/****************************************** JAVA ******************************************/
+class Solution {
+    public int maximumScore(int[] nums, int k) {
+        int n = nums.length;
+        int i = k;
+        int j = k;
+        int curMin = nums[k];
+        int result = nums[k];
+        
+        while (i > 0 || j < n - 1) {
+            int leftValue = (i > 0) ? nums[i - 1] : 0;
+            int rightValue = (j < n - 1) ? nums[j + 1] : 0;
+
+            if (leftValue < rightValue) {
+                j++;
+                curMin = Math.min(curMin, nums[j]);
+            } else {
+                i--;
+                curMin = Math.min(curMin, nums[i]);
+            }
+
+            result = Math.max(result, curMin * (j - i + 1));
+        }
+        
+        return result;
+    }
+}
