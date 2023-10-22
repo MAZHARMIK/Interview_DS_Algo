916d79ae735d17106af76c0cc51369fd5ddf0d45
MAZHAR <iiestshibpur605@gmail.com>
2023-10-22 12:28:21 +0530

    Update Maximum Score of a Good Subarray.cpp

--- Greedy/Maximum Score of a Good Subarray.cpp
+++ Greedy/Maximum Score of a Good Subarray.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=2XMBLBdgqW8
     Company Tags                : AMAZON
     Leetcode Link               : https://leetcode.com/problems/maximum-score-of-a-good-subarray/
 */
