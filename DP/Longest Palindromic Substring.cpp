a3fe6d6700696485053b8c8181692c638126eabc
MAZHAR <iiestshibpur605@gmail.com>
2023-10-27 07:49:16 +0530

    Added JAVA code as well

--- DP/Longest Palindromic Substring.cpp
+++ DP/Longest Palindromic Substring.cpp
@@ -52,7 +52,48 @@ public:
     }
 };
 
-//Approach 2 - Using Bottom Up (Elaborated for simplicity)
+//If you notice above, in the recursion part, we don't have any choice. We only have to check s[l] == s[r]
+//So, we can simply solve it using a loop
+//Approach 2 - Looping simply
+//T.C : O(n^3)
+class Solution {
+public:
+    bool solve(string &s, int l, int r){
+        
+        while(l <= r) {
+            if(s[l] != s[r])
+                return false;
+            l++;
+            r--;
+        }
+        
+        return true;
+    }
+    
+    string longestPalindrome(string s) {
+        int n = s.length();
+        
+        int maxlen = INT_MIN;
+        int startingIndex = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                
+                if(solve(s, i, j)) {
+                    if(j-i+1 > maxlen){
+                        startingIndex = i;
+                        maxlen = j-i+1;
+                    }
+                }
+                
+            }
+        }
+
+        return s.substr(startingIndex, maxlen);
+    }
+};
+
+//Approach 3 - Using Bottom Up (Elaborated for simplicity)
 //T.C : O(n^2)
 //S.C : O(n^2)
 class Solution {
@@ -106,7 +147,7 @@ public:
 };
 
 
-//Approach 3 :  (Simplifiying Approach-2 above)
+//Approach 4 :  (Simplifiying Approach-2 above)
 //Simplified solution
 class Solution {
 public:
@@ -186,7 +227,43 @@ public class Solution {
 }
 
 
-//Approach 2 - Using Bottom Up (Elaborated for simplicity)
+//If you notice above, in the recursion part, we don't have any choice. We only have to check s[l] == s[r]
+//So, we can simply solve it using a loop
+//Approach 2 - Looping simply
+//T.C : O(n^3)
+public class Solution {
+    private boolean solve(String s, int l, int r) {
+        while (l <= r) {
+            if (s.charAt(l) != s.charAt(r))
+                return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        int maxlen = Integer.MIN_VALUE;
+        int startingIndex = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (solve(s, i, j)) {
+                    if (j - i + 1 > maxlen) {
+                        startingIndex = i;
+                        maxlen = j - i + 1;
+                    }
+                }
+            }
+        }
+
+        return s.substring(startingIndex, startingIndex + maxlen);
+    }
+}
+
+
+//Approach 3 - Using Bottom Up (Elaborated for simplicity)
 //T.C : O(n^2)
 //S.C : O(n^2)
 public class Solution {
@@ -233,7 +310,7 @@ public class Solution {
 }
 
 
-//Approach 3 :  (Simplifiying Approach-2 above)
+//Approach 4 :  (Simplifiying Approach-2 above)
 //Simplified solution
 public class Solution {
     public String longestPalindrome(String s) {
