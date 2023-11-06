529c56b715ed316d162150b83bd19b98e001afb3
MAZHAR <iiestshibpur605@gmail.com>
2023-11-06 09:18:12 +0530

    Added YouTube Link

--- Design/Seat Reservation Manager.cpp
+++ Design/Seat Reservation Manager.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=fjwHZNvbr7g
     Company Tags                : META
     Leetcode Link               : https://leetcode.com/problems/seat-reservation-manager/
 */
