ac1fd5a809377b4ffb2921dcc96a45041ece31fa
MAZHAR <iiestshibpur605@gmail.com>
2023-11-07 22:03:52 +0530

    Update Maximum Balanced Subsequence Sum.cpp

--- DP/LIS & Variants/Maximum Balanced Subsequence Sum.cpp
+++ DP/LIS & Variants/Maximum Balanced Subsequence Sum.cpp
@@ -74,6 +74,41 @@ public:
 };
 
 
+//Approach-3 (Using Optimal LIS - Similar to Patience Sorting) - Accepted
+//Time : O(nlogn)
+class Solution {
+public:
+    long long maxBalancedSubsequenceSum(vector<int>& nums) {
+        int n = nums.size();
+        map<int, long long> mp;
+
+        long long ans = INT_MIN;
+        
+        for(int i = 0; i < n; i++){
+            auto it = mp.upper_bound(nums[i]-i); //Find the element just greater than nums[i]-i;
+
+            long long cur_ans = nums[i];
+
+            if(it != mp.begin()) {
+                it--;
+                cur_ans += (*it).second;
+            }
+            mp[nums[i]-i] = max(mp[nums[i]-i], cur_ans);
+
+            it = mp.upper_bound(nums[i]-i);
+
+            while(it != mp.end() && (*it).second <= cur_ans) {
+                 mp.erase(it++);
+            }
+
+            ans = max(ans, cur_ans);
+        }
+
+        return ans;
+    }
+};
+
+
 /********************************************************************************** JAVA **********************************************************************************/
 //Approach-1 (Using LIS) - Recursion (TLE) ---> 316 / 345 testcases passed
 //T.C : O(n^2) - prev index for every i
@@ -152,5 +187,39 @@ class Solution {
         }
 
         return maxSum > maxEl ? maxSum : maxEl;
+    }
+}
+
+
+//Approach-3 (Using Optimal LIS - Similar to Patience Sorting) - Accepted
+//Time : O(nlogn)
+class Solution {
+    public long maxBalancedSubsequenceSum(int[] nums) {
+        int n = nums.length;
+        int [] arr = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = nums[i]-i;
+        }
+        TreeMap<Integer, Long> map = new TreeMap<>();
+        long ans = Integer.MIN_VALUE;
+        for(int i = 0; i<n; i++){
+            if(nums[i]<=0){
+                ans = Math.max(ans, nums[i]);
+            }
+            else{
+                long temp = nums[i];
+                if(map.floorKey(arr[i])!=null){
+                    temp += map.get(map.floorKey(arr[i]));
+                }
+                while(map.ceilingKey(arr[i])!=null && map.get(map.ceilingKey(arr[i]))<temp){
+                    map.remove(map.ceilingKey(arr[i]));
+                }
+                if(map.floorKey(arr[i])==null || map.get(map.floorKey(arr[i]))<temp){
+                    map.put(arr[i], temp);
+                }
+                ans = Math.max(ans, temp);
+            }
+        }
+        return ans;
     }
 }
