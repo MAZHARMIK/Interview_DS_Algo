9da4252d7bbb0d27be1edcdbfa989c716223fbf0
MAZHAR <iiestshibpur605@gmail.com>
2023-11-09 23:00:54 +0530

    Added YouTube Video Link

--- DP/LIS & Variants/Maximum Balanced Subsequence Sum.cpp
+++ DP/LIS & Variants/Maximum Balanced Subsequence Sum.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn :
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=JrG4tbq6efg
     Company Tags                : Will soon update
     Leetcode Link               : https://leetcode.com/problems/maximum-balanced-subsequence-sum
 */
