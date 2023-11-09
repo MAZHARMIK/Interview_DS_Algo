73ec5737ed39ade2dcd33db2bbd4c52f11db7eec
MAZHAR <iiestshibpur605@gmail.com>
2023-11-09 08:53:17 +0530

    Update Count Number of Homogenous Substrings.cpp

--- strings/Count Number of Homogenous Substrings.cpp
+++ strings/Count Number of Homogenous Substrings.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=qEJR7ox0mNI
     Company Tags                : Will update soon
     Leetcode Link               : https://leetcode.com/problems/count-number-of-homogenous-substrings/
 */
