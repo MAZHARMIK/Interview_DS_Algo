d63685af2012e33e554f424193d4aa1df5e9a9f0
MAZHAR <iiestshibpur605@gmail.com>
2023-11-13 20:24:22 +0530

    Added JAVA code

--- Trie/Implement Trie (Prefix Tree).cpp
+++ Trie/Implement Trie (Prefix Tree).cpp
@@ -4,6 +4,8 @@
     Leetcode Link               : https://leetcode.com/problems/implement-trie-prefix-tree/
 */
 
+
+/**************************************************** C++ ****************************************************/
 class Trie {
 public:
     
@@ -75,3 +77,67 @@ public:
         return false;
     }
 };
+
+
+/**************************************************** JAVA ****************************************************/
+class Trie {
+    static class TrieNode {
+        TrieNode[] children;
+        boolean isEndOfWord;
+
+        TrieNode() {
+            children = new TrieNode[26];
+            isEndOfWord = false;
+        }
+    }
+
+    private TrieNode root;
+
+    Trie() {
+        root = new TrieNode();
+    }
+
+    private TrieNode getNode() {
+        return new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode crawler = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (crawler.children[index] == null)
+                crawler.children[index] = getNode();
+
+            crawler = crawler.children[index];
+        }
+
+        crawler.isEndOfWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode crawler = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (crawler.children[index] == null)
+                return false;
+            crawler = crawler.children[index];
+        }
+        return crawler != null && crawler.isEndOfWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode crawler = root;
+        int i = 0;
+        for (i = 0; i < prefix.length(); i++) {
+            int index = prefix.charAt(i) - 'a';
+
+            if (crawler.children[index] == null)
+                return false;
+
+            crawler = crawler.children[index];
+        }
+        return i == prefix.length();
+    }
+}
