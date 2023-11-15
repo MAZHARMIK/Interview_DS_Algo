62b95a2546277989cf85438d7c392a2f6df314c2
MAZHAR <iiestshibpur605@gmail.com>
2023-11-15 07:11:38 +0530

    Maximum Element After Decreasing and Rearranging

---
+++ Greedy/Maximum Element After Decreasing and Rearranging.cpp
@@ -0,0 +1,57 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Will update soon
+    Leetcode Link               : https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging/
+*/
+
+/******************************************************************** C++ ********************************************************************/
+//Apprpach-1 (Simply doing like asked in the Qn)
+//T.C : O(nlogn)
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(begin(arr), end(arr));
+
+        int n = arr.size();
+        int maxEl = 1;
+
+        for(int i = 0; i < n; i++) {
+
+            if(i == 0) {
+                arr[i] = 1;
+            } else if(arr[i] - arr[i-1] > 1) {
+                arr[i] = arr[i-1] + 1;
+            }
+
+            maxEl = max(maxEl, arr[i]);
+        }
+
+        return maxEl;
+
+    }
+};
+
+
+/******************************************************************** JAVA ********************************************************************/
+//Apprpach-1 (Simply doing like asked in the Qn)
+//T.C : O(nlogn)
+public class Solution {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+
+        int n = arr.length;
+        int maxEl = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                arr[i] = 1;
+            } else if (arr[i] - arr[i - 1] > 1) {
+                arr[i] = arr[i - 1] + 1;
+            }
+
+            maxEl = Math.max(maxEl, arr[i]);
+        }
+
+        return maxEl;
+    }
+}
