4faca03d2b2a0d22a1bf465f99e55921f920e914
MAZHAR <iiestshibpur605@gmail.com>
2023-11-18 09:51:12 +0530

    Added 3 approaches

--- Sliding Window/Frequency of the Most Frequent Element.cpp
+++ Sliding Window/Frequency of the Most Frequent Element.cpp
@@ -5,7 +5,61 @@
 */
 
 /************************************************ C++ *******************************************************/
-//Approach-1 (Using sliding window)
+//Approach-1 (Using Binary Search)
+//T.C : O(nlogn)
+//S.C : O(n) for prefixSum for effienctly calculating windowSum
+class Solution {
+public:
+    
+    int findBest(int target_idx, int k, vector<int>& nums, vector<long>& prefixSum) {
+        int target = nums[target_idx];
+        
+        int i = 0;
+        int j = target_idx;
+        int result = target_idx;
+        
+        while(i <= j) {
+            int mid = i + (j-i)/2;
+            
+            long count      = (target_idx - mid + 1);
+            long windowSum  = (count * target);
+            long currSum    = prefixSum[target_idx] - prefixSum[mid] + nums[mid];
+            
+            int ops         = windowSum - currSum;
+            
+            if(ops > k) {
+                i = mid+1;
+            } else {
+                result = mid;
+                j = mid-1;
+            }
+        }
+        
+        return target_idx-result+1;
+    }
+    
+    int maxFrequency(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        sort(begin(nums), end(nums));
+        vector<long> prefixSum(n);
+        prefixSum[0] = nums[0];
+        
+        for(int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i-1] + nums[i];
+        }
+        
+        int result = 0;
+        
+        for(int i = 0; i<n; i++) {
+            result = max(result, findBest(i, k, nums, prefixSum));
+        }
+        
+        return result;
+    }
+};
+
+//Approach-2 (Using sliding window)
 //T.C : O(nlogn)
 //S.C : O(1)
 class Solution {
@@ -37,7 +91,7 @@ public:
     }
 };
 
-//Approach-2 (Improved sliding window)
+//Approach-3 (Improved sliding window)
 //T.C : O(nlogn)
 //S.C : O(1)
 class Solution {
@@ -70,8 +124,62 @@ public:
 };
 
 
-/************************************************ JAVA *******************************************************/\
-//Approach-1 (Using sliding window)
+/************************************************ JAVA *******************************************************/
+//Approach-1 (Using Binary Search)
+//T.C : O(nlogn)
+//S.C : O(n) for prefixSum for effienctly calculating windowSum
+public class Solution {
+    
+    private int findBest(int targetIdx, int k, int[] nums, long[] prefixSum) {
+        int target = nums[targetIdx];
+        
+        int i = 0;
+        int j = targetIdx;
+        int result = targetIdx;
+        
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            
+            long count = (targetIdx - mid + 1);
+            long windowSum = (count * target);
+            long currSum = prefixSum[targetIdx] - prefixSum[mid] + nums[mid];
+            
+            int ops = (int) (windowSum - currSum);
+            
+            if (ops > k) {
+                i = mid + 1;
+            } else {
+                result = mid;
+                j = mid - 1;
+            }
+        }
+        
+        return targetIdx - result + 1;
+    }
+    
+    public int maxFrequency(int[] nums, int k) {
+        int n = nums.length;
+        
+        Arrays.sort(nums);
+        long[] prefixSum = new long[n];
+        prefixSum[0] = nums[0];
+        
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        }
+        
+        int result = 0;
+        
+        for (int i = 0; i < n; i++) {
+            result = Math.max(result, findBest(i, k, nums, prefixSum));
+        }
+        
+        return result;
+    }
+}
+
+
+//Approach-2 (Using sliding window)
 //T.C : O(nlogn)
 //S.C : O(1)
 public class Solution {
@@ -103,10 +211,9 @@ public class Solution {
 }
 
 
-//Approach-2 (Improved sliding window)
+//Approach-3 (Improved sliding window)
 //T.C : O(nlogn)
 //S.C : O(1)
-
 public class Solution {
     public int maxFrequency(int[] nums, int k) {
         Arrays.sort(nums);
