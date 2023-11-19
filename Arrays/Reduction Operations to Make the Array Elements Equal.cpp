9883ec29ebdff202c0903668824dbf9c383f3e83
MAZHAR <iiestshibpur605@gmail.com>
2023-11-19 07:34:59 +0530

    Reduction Operations to Make the Array Elements Equal

---
+++ Arrays/Reduction Operations to Make the Array Elements Equal.cpp
@@ -0,0 +1,59 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : will soon update
+    Leetcode Link               : https://leetcode.com/problems/reduction-operations-to-make-the-array-elements-equal
+*/
+
+/****************************************************************** C++ ******************************************************************/
+//Approach - Using sorting
+//T.C : O(nlogn)
+//S.C : O(1)
+class Solution {
+public:
+    int reductionOperations(vector<int>& nums) {
+        int n = nums.size();
+        
+        sort(begin(nums), end(nums));
+        
+        int count = 0;
+        
+        for(int i = n-1; i > 0; i--) {
+            if(nums[i] == nums[i-1]) {
+                continue;
+            }
+            
+            count += n-i;
+        }
+        
+        
+        return count;
+        
+        
+    }
+};
+
+
+/****************************************************************** JAVA ******************************************************************/
+//Approach - Using sorting
+//T.C : O(nlogn)
+//S.C : O(1)
+public class Solution {
+    public int reductionOperations(int[] nums) {
+        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        int count = 0;
+
+        for (int i = n - 1; i > 0; i--) {
+            if (nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            count += n - i;
+        }
+
+        return count;
+    }
+}
+
