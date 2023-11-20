c69a58858000b091e4d9ab21ed70eea847135a72
MAZHAR <iiestshibpur605@gmail.com>
2023-11-20 08:28:41 +0530

    Added YouTube Link

--- Arrays/Cumulative_Sum(Prefix Array)/Minimum Amount of Time to Collect Garbage.cpp
+++ Arrays/Cumulative_Sum(Prefix Array)/Minimum Amount of Time to Collect Garbage.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=SGJMTu9SZHs
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/minimum-amount-of-time-to-collect-garbage/
 */
