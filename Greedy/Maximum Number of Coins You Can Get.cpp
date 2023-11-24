90e8b87a22dfb976edd227ec7667711a71b30915
MAZHAR <iiestshibpur605@gmail.com>
2023-11-24 08:13:36 +0530

    Added YouTube link

--- Greedy/Maximum Number of Coins You Can Get.cpp
+++ Greedy/Maximum Number of Coins You Can Get.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=H762GDF08mg
     Company Tags                : will soon update
     Leetcode Link               : https://leetcode.com/problems/maximum-number-of-coins-you-can-get/
 */
