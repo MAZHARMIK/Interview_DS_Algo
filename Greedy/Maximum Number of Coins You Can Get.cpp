3cf6367ee4514d30f7b0c33b56f76737673a0996
MAZHAR <iiestshibpur605@gmail.com>
2023-11-24 07:04:27 +0530

    Maximum Number of Coins You Can Get

---
+++ Greedy/Maximum Number of Coins You Can Get.cpp
@@ -0,0 +1,103 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : will soon update
+    Leetcode Link               : https://leetcode.com/problems/maximum-number-of-coins-you-can-get/
+*/
+
+/****************************************************************** C++ *****************************************************************************/
+//Approach-1 (Using Greedy + 3 pointers)
+//T.C : O(nlogn) - Sorting
+//S.C : O(1)
+//NOTE - You could also use q deque to push and pop from either side
+class Solution {
+public:
+    int maxCoins(vector<int>& piles) {
+        int n = piles.size();
+        
+        sort(begin(piles), end(piles));
+        
+        int result = 0;
+        
+        int Bob = 0, Me = n-2, Alice = n-1;
+        
+        while(Me > Bob) {
+            
+            result += piles[Me];
+        
+            Me    -= 2;    
+            Bob   += 1;
+            Alice -= 2; //(You can remove this becasue it's required. I Used it for the sake of completeness)
+        }
+        
+        return result;
+    }
+};
+
+
+//Approach-2 (Using Greedy without 3 pointers)
+//T.C : O(nlogn) - Sorting
+//S.C : O(1)
+class Solution {
+public:
+    int maxCoins(vector<int>& piles) {
+        int n = piles.size();
+        
+        sort(begin(piles), end(piles));
+        
+        int result = 0;
+
+        
+        for(int i = n/3; i <n; i+=2) {
+            result += piles[i];
+        }
+        
+        return result;
+    }
+};
+
+
+/****************************************************************** JAVA *****************************************************************************/
+//Approach-1 (Using Greedy + 3 pointers)
+//T.C : O(nlogn) - Sorting
+//S.C : O(1)
+//NOTE - You could also use q deque to push and pop from either side
+public class Solution {
+    public int maxCoins(int[] piles) {
+        int n = piles.length;
+
+        Arrays.sort(piles);
+
+        int result = 0;
+
+        int Bob = 0, Me = n - 2, Alice = n - 1;
+
+        while (Me > Bob) {
+            result += piles[Me];
+
+            Me -= 2;
+            Bob += 1;
+            Alice -= 2; // (You can remove this because it's required. I Used it for the sake of completeness)
+        }
+
+        return result;
+    }
+}
+
+//Approach-2 (Using Greedy without 3 pointers)
+//T.C : O(nlogn) - Sorting
+//S.C : O(1)
+public class Solution {
+    public int maxCoins(int[] piles) {
+        int n = piles.length;
+
+        Arrays.sort(piles);
+
+        int result = 0;
+
+        for (int i = n / 3; i < n; i += 2) {
+            result += piles[i];
+        }
+
+        return result;
+    }
+}
