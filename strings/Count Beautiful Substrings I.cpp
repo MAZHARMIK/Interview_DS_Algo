d2cecf8bdb3dcd2bbf1fce1c60512068d739a998
MAZHAR <iiestshibpur605@gmail.com>
2023-11-27 10:45:19 +0530

    Added YouTube Link

--- strings/Count Beautiful Substrings I.cpp
+++ strings/Count Beautiful Substrings I.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=ipobT-2_DkQ
     Company Tags                : will soon update
     Leetcode Link               : https://leetcode.com/problems/count-beautiful-substrings-i/
 */
