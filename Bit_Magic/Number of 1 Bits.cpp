8a2bc61e8d4c79aefe96677ebeb21b34aa5d0d4c
MAZHAR <iiestshibpur605@gmail.com>
2023-11-29 08:29:13 +0530

    Added YouTube Link

--- Bit_Magic/Number of 1 Bits.cpp
+++ Bit_Magic/Number of 1 Bits.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=YOyajw-L0pc
     Company Tags                : Apple, Microsoft, Amazon
     Leetcode Link               : https://leetcode.com/problems/number-of-1-bits/
 */
