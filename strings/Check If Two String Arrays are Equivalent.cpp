f955a84b8fb63b62d67706ffee4204c934bcaba6
MAZHAR <iiestshibpur605@gmail.com>
2023-12-01 10:32:19 +0530

    Added JAVA code

--- strings/Check If Two String Arrays are Equivalent.cpp
+++ strings/Check If Two String Arrays are Equivalent.cpp
@@ -4,7 +4,10 @@
     Leetcode Link               : https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
 */
 
+/************************************************************** C++ **************************************************************/
 //Approach-1 (Simple concatenation)
+//T.C : O(n*k) - n and m = length of word1 and word2 respectively
+//S.C : O(n+k)
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
@@ -23,6 +26,8 @@ public:
 
 
 //Approach-2 (Using comparison character by character using index)
+//T.C : O(n*k) - n and m = length of word1 and word2 respectively
+//S.C : O(1)
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
@@ -58,3 +63,61 @@ public:
         
     }
 };
+
+
+/************************************************************** JAVA **************************************************************/
+//Approach-1 (Simple concatenation)
+//T.C : O(n*k) - n and m = length of word1 and word2 respectively
+//S.C : O(n+k)
+public class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder s1 = new StringBuilder();
+        StringBuilder s2 = new StringBuilder();
+
+        for (String s : word1) {
+            s1.append(s);
+        }
+
+        for (String s : word2) {
+            s2.append(s);
+        }
+
+        return s1.toString().equals(s2.toString());
+    }
+}
+
+
+
+
+//Approach-2 (Using comparison character by character using index)
+//T.C : O(n*k) - n and m = length of word1 and word2 respectively
+//S.C : O(1)
+public class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        int m = word1.length;
+        int n = word2.length;
+
+        int w1i = 0, i = 0;
+        int w2i = 0, j = 0;
+
+        while (w1i < m && w2i < n) {
+            if (word1[w1i].charAt(i) != word2[w2i].charAt(j))
+                return false;
+
+            i++;
+            j++;
+
+            if (i == word1[w1i].length()) {
+                i = 0;
+                w1i++;
+            }
+
+            if (j == word2[w2i].length()) {
+                j = 0;
+                w2i++;
+            }
+        }
+
+        return w1i == m && w2i == n;
+    }
+}
