17c8f040fd18e7df604de8950a9ee6794c444b2c
MAZHAR <iiestshibpur605@gmail.com>
2023-12-04 07:54:17 +0530

    Largest 3-Same-Digit Number in String

---
+++ strings/Easy Tagged/Largest 3-Same-Digit Number in String.cpp
@@ -0,0 +1,40 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : will update soon
+    Leetcode Link               : https://leetcode.com/problems/largest-3-same-digit-number-in-string
+*/
+
+
+/******************************************************************** C++ ********************************************************************/
+//Simple iteration and check
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        char maxChar = ' ';
+        
+        for(int i = 2; i<num.length(); i++) {
+            if(num[i] == num[i-1] && num[i] == num[i-2]) {
+                maxChar = max(maxChar, num[i]);
+            }
+        }
+        
+        return maxChar == ' ' ? "" : string(3, maxChar);
+    }
+};
+
+
+/******************************************************************** JAVA ********************************************************************/
+//Simple iteration and check
+public class Solution {
+    public String largestGoodInteger(String num) {
+        char maxChar = ' ';
+        
+        for (int i = 2; i < num.length(); i++) {
+            if (num.charAt(i) == num.charAt(i - 1) && num.charAt(i) == num.charAt(i - 2)) {
+                maxChar = (char) Math.max(maxChar, num.charAt(i));
+            }
+        }
+        
+        return maxChar == ' ' ? "" : new String(new char[] { maxChar, maxChar, maxChar });
+    }
+}
