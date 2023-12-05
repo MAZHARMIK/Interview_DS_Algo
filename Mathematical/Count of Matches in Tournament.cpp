6950d2efcd2fb0d2be00db9389fd4b1d214a93bf
MAZHAR <iiestshibpur605@gmail.com>
2023-12-05 07:20:00 +0530

    Count of Matches in Tournament

---
+++ Mathematical/Count of Matches in Tournament.cpp
@@ -0,0 +1,72 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : MICROSOFT
+    Leetcode Link               : https://leetcode.com/problems/count-of-matches-in-tournament
+*/
+
+/******************************************************** C++ ********************************************************/
+//Approach-1 (Doing simple simulation asking in code)
+//T.C : O(log(n))
+//S.C : O(1)
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int result = 0;
+        
+        while(n > 1) {
+            if(n%2 == 0) {
+                result += n/2;
+                n /= 2;
+            } else {
+                result += (n-1)/2;
+                n = ((n-1)/2) + 1;
+            }
+        }
+        
+        return result;
+    }
+};
+
+
+//Approach-2 (Using simple math)
+//T.C : O(1)
+//S.C : O(1)
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        return n-1;
+    }
+};
+
+
+/******************************************************** JAVA ********************************************************/
+//Approach-1 (Doing simple simulation asking in code)
+//T.C : O(log(n))
+//S.C : O(1)
+class Solution {
+    public int numberOfMatches(int n) {
+        int result = 0;
+        
+        while (n > 1) {
+            if (n % 2 == 0) {
+                result += n / 2;
+                n = n / 2;
+            } else {
+                result += (n - 1) / 2;
+                n = (n - 1) / 2 + 1;
+            }
+        }
+        
+        return result;
+    }
+}
+
+
+//Approach-2 (Using simple math)
+//T.C : O(1)
+//S.C : O(1)
+class Solution {
+    public int numberOfMatches(int n) {
+        return n - 1;
+    }
+}
