9ddf757276fd62555af802075afe90911c5f007a
MAZHAR <iiestshibpur605@gmail.com>
2023-12-05 08:29:35 +0530

    Update Count of Matches in Tournament.cpp

--- Mathematical/Count of Matches in Tournament.cpp
+++ Mathematical/Count of Matches in Tournament.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=rTZdxOlHwk0
     Company Tags                : MICROSOFT
     Leetcode Link               : https://leetcode.com/problems/count-of-matches-in-tournament
 */
