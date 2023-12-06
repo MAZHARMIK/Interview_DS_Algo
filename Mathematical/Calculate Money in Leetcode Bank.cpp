0f461063cda1d2bcaedcafd8f85ef738b9c08f1c
MAZHAR <iiestshibpur605@gmail.com>
2023-12-06 09:39:02 +0530

    Added YouTube Link

--- Mathematical/Calculate Money in Leetcode Bank.cpp
+++ Mathematical/Calculate Money in Leetcode Bank.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn :
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=TAmv6Csq3Ss
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/calculate-money-in-leetcode-bank/
 */
