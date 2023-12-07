ec1aef80c98ef373d1235c8ca594c2a9f47e5ab2
MAZHAR <iiestshibpur605@gmail.com>
2023-12-07 06:50:36 +0530

    Largest Odd Number in String

---
+++ strings/Easy Tagged/Largest Odd Number in String.cpp
@@ -0,0 +1,44 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Amazon
+    Leetcode Link               : https://leetcode.com/problems/largest-odd-number-in-string/
+*/
+
+/****************************************************************** C++ *****************************************************************************/
+//Approach - Simple Greedy
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int n = num.length();
+        
+        for(int i = n-1; i>=0; i--) {
+            
+            if((num[i] - '0') % 2 != 0) {
+                return num.substr(0, i+1);
+            }
+        }
+        
+        return "";
+    }
+};
+
+
+/****************************************************************** JAVA *****************************************************************************/
+//Approach - Simple Greedy
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public String largestOddNumber(String num) {
+        int n = num.length();
+        
+        for (int i = n - 1; i >= 0; i--) {
+            if (Character.getNumericValue(num.charAt(i)) % 2 != 0) {
+                return num.substring(0, i + 1);
+            }
+        }
+        
+        return "";
+    }
+}
