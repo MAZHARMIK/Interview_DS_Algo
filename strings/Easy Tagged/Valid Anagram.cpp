13afa4235883fa53135bd1849cdcb0a165a5dddc
MAZHAR <iiestshibpur605@gmail.com>
2023-12-16 09:35:29 +0530

    Update Valid Anagram.cpp

--- strings/Easy Tagged/Valid Anagram.cpp
+++ strings/Easy Tagged/Valid Anagram.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS QN : 
+    MY YOUTUBE VIDEO ON THIS QN : https://www.youtube.com/watch?v=1tmEKyRAMuY
     Company Tags                : Uber
     Leetcode Link               : https://leetcode.com/problems/valid-anagram/
     
@@ -37,6 +37,13 @@ public:
         for(char &ch : t) {
             count[ch-'a']--;
         }
+        //NOTE : You can also do above in only one for loop.
+        /*
+            for(int i = 0; i<n; i++) {
+                count[s[i]-'a']++;
+                count[t[i]-'a']--;
+            }
+        */
         
         // Check if all elements in the array are 0 --> You can write a for loop as well
         //This is just for learning purpose
