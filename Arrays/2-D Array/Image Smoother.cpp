4a0faff557b2fa52a950d84ff06d0226ba148318
MAZHAR <iiestshibpur605@gmail.com>
2023-12-19 10:45:43 +0530

    Added YouTube Link

--- Arrays/2-D Array/Image Smoother.cpp
+++ Arrays/2-D Array/Image Smoother.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=QXnw71MA_pU
     Company Tags                : AMAZON
     Leetcode Link               : https://leetcode.com/problems/image-smoother/
 */
