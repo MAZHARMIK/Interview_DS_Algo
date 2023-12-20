57d2f43a5fd434a14d0e603d455647d68610599a
MAZHAR <iiestshibpur605@gmail.com>
2023-12-20 06:43:26 +0530

    Buy Two Chocolates

---
+++ Arrays/Leetcode Easy/Buy Two Chocolates.cpp
@@ -0,0 +1,57 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : will update soon
+    Leetcode Link               : https://leetcode.com/problems/buy-two-chocolates/
+*/
+
+//Approach (Simply find min and second min to check 2 chocolates price)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        int minPrice    = INT_MAX;
+        int secMinPrice = INT_MAX;
+        
+        for(int &price : prices) {
+            if(price < minPrice) {
+                secMinPrice = minPrice;
+                minPrice    = price;
+            } else {
+                secMinPrice = min(secMinPrice, price);
+            }
+        }
+        
+        if(minPrice + secMinPrice > money)
+            return money;
+        
+        return money - (minPrice + secMinPrice);
+    }
+};
+
+
+
+//Approach (Simply find min and second min to check 2 chocolates price)
+//T.C : O(n)
+//S.C : O(1)
+public class Solution {
+    public int buyChoco(int[] prices, int money) {
+        int minPrice = Integer.MAX_VALUE;
+        int secMinPrice = Integer.MAX_VALUE;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                secMinPrice = minPrice;
+                minPrice = price;
+            } else {
+                secMinPrice = Math.min(secMinPrice, price);
+            }
+        }
+
+        if (minPrice + secMinPrice > money) {
+            return money;
+        }
+
+        return money - (minPrice + secMinPrice);
+    }
+}
