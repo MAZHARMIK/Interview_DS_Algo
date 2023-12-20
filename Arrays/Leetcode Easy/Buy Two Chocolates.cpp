19a5c49942570c42e7c7349f6e0efc8bcf73a247
MAZHAR <iiestshibpur605@gmail.com>
2023-12-20 10:48:09 +0530

    Added YouTube Link

--- Arrays/Leetcode Easy/Buy Two Chocolates.cpp
+++ Arrays/Leetcode Easy/Buy Two Chocolates.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=MihxAUNFac0
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/buy-two-chocolates/
 */
