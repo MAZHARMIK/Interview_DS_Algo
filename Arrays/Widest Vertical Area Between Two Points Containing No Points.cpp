c2e07cf7b080cccbad555b378c01c5e8aca65bc6
MAZHAR <iiestshibpur605@gmail.com>
2023-12-21 10:53:56 +0530

    Added YouTube Link

--- Arrays/Widest Vertical Area Between Two Points Containing No Points.cpp
+++ Arrays/Widest Vertical Area Between Two Points Containing No Points.cpp
@@ -1,5 +1,5 @@
 /*
-      MY YOUTUBE VIDEO ON THIS Qn :
+      MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=QZuxYdVNoIU
       Company Tags                : GOOGLE
       Leetcode Link               : https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/
 */
