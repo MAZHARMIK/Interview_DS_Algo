f74c48336efa5a35a6c684302f6e3257967b7272
MAZHAR <iiestshibpur605@gmail.com>
2023-12-21 06:39:31 +0530

    Widest Vertical Area Between Two Points Containing No Points

---
+++ Arrays/Widest Vertical Area Between Two Points Containing No Points.cpp
@@ -0,0 +1,44 @@
+/*
+      MY YOUTUBE VIDEO ON THIS Qn :
+      Company Tags                : GOOGLE
+      Leetcode Link               : https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/
+*/
+
+/************************************************************** C++ **************************************************************/
+//T.C : O(nlog(n))
+//S.C : O(1)
+class Solution {
+public:
+    int maxWidthOfVerticalArea(vector<vector<int>>& points) {
+        int n = points.size();
+        sort(begin(points), end(points));
+        
+        int maxVal = 0;
+        
+        for(int i = 1; i<n; i++) {
+            maxVal = max(maxVal, points[i][0] - points[i-1][0]);
+        }
+        
+        return maxVal;
+        
+    }
+};
+
+
+/************************************************************** JAVA **************************************************************/
+//T.C : O(nlog(n))
+//S.C : O(1)
+class Solution {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        int n = points.length;
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int maxVal = 0;
+
+        for (int i = 1; i < n; i++) {
+            maxVal = Math.max(maxVal, points[i][0] - points[i - 1][0]);
+        }
+
+        return maxVal;
+    }
+}
