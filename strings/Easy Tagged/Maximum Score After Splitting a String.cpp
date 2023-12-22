9668239cb5ff2709957fe050b50ec85701bed05c
MAZHAR <iiestshibpur605@gmail.com>
2023-12-22 10:13:44 +0530

    Added YouTube Link

--- strings/Easy Tagged/Maximum Score After Splitting a String.cpp
+++ strings/Easy Tagged/Maximum Score After Splitting a String.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=H618dJqxpsM
     Company Tags                : will soon update
     Leetcode Link               : https://leetcode.com/problems/maximum-score-after-splitting-a-string/
 */
