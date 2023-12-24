5be9217f7c1c650b28f0e7123b8e30349febd969
MAZHAR <iiestshibpur605@gmail.com>
2023-12-24 10:39:28 +0530

    Added YouTube Link

--- strings/Easy Tagged/Minimum Changes To Make Alternating Binary String.cpp
+++ strings/Easy Tagged/Minimum Changes To Make Alternating Binary String.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=YB4oUR2Skfk
     Company Tags                : MICROSOFT
     Leetcode Link               : https://leetcode.com/problems/minimum-changes-to-make-alternating-binary-string/
 */
