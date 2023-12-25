381cc9e2373f1712fa8f59770bde6da6ab471c10
MAZHAR <iiestshibpur605@gmail.com>
2023-12-25 08:27:48 +0530

    Decode Ways

--- DP/Decode Ways.cpp
+++ DP/Decode Ways.cpp
@@ -1,12 +1,58 @@
 /*
-    Company Tags     : Facebook, Uber, Google, Facebook, Microsoft
-    Leetcode Link    : https://leetcode.com/problems/decode-ways/
-    Four approaches  : Memoized, Top Down, Better Top Down, O(1) space DP
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Facebook, Uber, Google, Facebook, Microsoft
+    Leetcode Link               : https://leetcode.com/problems/decode-ways/
+    Four approaches             : Memoized, Top Down, Better Top Down, O(1) space DP
 */
 
+/************************************************************************ C++ ***************************************************************/
+//Approach-1 (Using Recursion + Memoization)
+//T.C : O(n) after memoization (without memoization - O(2^n)
+//S.C : O(101) ~= O(1)
 class Solution {
 public:
-    int ways_better_top_down(string&s, int n) {
+    int t[101];
+    int solve(int i, string &s, int &n) {
+        if(t[i] != -1) {
+            return t[i];
+        }
+
+        if(i == n) {
+            return t[i] = 1; //one valid split done
+        }
+
+        if(s[i] == '0') {
+            return t[i] = 0; //not possible to split
+        }
+
+        int result     = solve(i+1, s, n);
+        
+        if(i+1 < n) {
+            if(s[i] == '1' || (s[i] == '2' && s[i+1] <= '6'))
+                result += solve(i+2, s, n);
+        }
+
+        
+        return t[i] = result;
+
+    }
+
+    int numDecodings(string s) {
+        int n = s.length();
+        
+        memset(t, -1, sizeof(t));
+        return solve(0, s, n);
+        
+
+    }
+};
+
+//Approach-2 (Bottom Up Way-1)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int bottom_up_1(string&s, int n) {
         //simply simulate the memoized version
         vector<int> t(n+1, 0);
         //t[i] = ways to decode string of length i
@@ -24,28 +70,20 @@ public:
         return t[0];
     }
     
-    int ways_memoized(string &s, int n, int idx, vector<int>& t) {
-        if(idx > n)
-            return 0;
-        if(t[idx] != -1)
-            return t[idx];
-        if(idx == n)
-            return t[idx] = 1;
-        
-        if(s[idx] == '0')
-            return t[idx] = 0;
-        
-        int result = ways_memoized(s, n, idx+1, t); //length 1
-        //for length 2, we need to check if it doesn't cross 27
-        if(idx < n-1) {
-            if(s[idx] == '1' || (s[idx] == '2' && s[idx+1] < '7')) {
-                result += ways_memoized(s, n, idx+2, t);
-            }
-        }
-        return t[idx] = result;
+    int numDecodings(string &s) {
+        int n = s.length();
+        vector<int> t(n+1, -1);
+        return bottom_up_1(s, n);
     }
-    
-    int ways_top_down(string&s, int n) {
+};
+
+
+//Approach-3 (Using Bottom Up Way-2)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int bottom_up_2(string&s, int n) {
         vector<int> t(n+1, 0);
         //t[i] = ways to decode string of length i
         t[0] = 1;
@@ -64,17 +102,14 @@ public:
     
     int numDecodings(string &s) {
         int n = s.length();
-        if(n == 0)
-            return 0;
         vector<int> t(n+1, -1);
-        //return ways_memoized(s, n, 0, t);
-        //return ways_top_down(s, n);
-        return ways_better_top_down(s, n);
+        return bottom_up_2(s, n);
     }
 };
 
-
-//Approach-4 (O(1) space DP)
+//Approach-4 
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     int numDecodings(string s) {
@@ -99,3 +134,132 @@ public:
         return last1;
     }
 };
+
+
+
+/************************************************************************ JAVA ***************************************************************/
+//Approach-1 (Using Recursion + Memoization)
+//T.C : O(n) after memoization (without memoization - O(2^n)
+//S.C : O(101) ~= O(1)
+public class Solution {
+    private int[] t;
+
+    public int solve(int i, String s, int n) {
+        if (t[i] != -1) {
+            return t[i];
+        }
+
+        if (i == n) {
+            return t[i] = 1; // one valid split done
+        }
+
+        if (s.charAt(i) == '0') {
+            return t[i] = 0; // not possible to split
+        }
+
+        int result = solve(i + 1, s, n);
+
+        if (i + 1 < n) {
+            if (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i + 1) <= '6'))
+                result += solve(i + 2, s, n);
+        }
+
+        return t[i] = result;
+    }
+
+    public int numDecodings(String s) {
+        int n = s.length();
+        t = new int[101];
+        Arrays.fill(t, -1);
+        return solve(0, s, n);
+    }
+}
+
+
+//Approach-2 (Bottom Up Way-1)
+//T.C : O(n)
+//S.C : O(n)
+public class Solution {
+    public int bottomUp1(String s, int n) {
+        int[] t = new int[n + 1];
+        Arrays.fill(t, 0);
+        t[n] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                t[i] = 0;
+            } else {
+                t[i] = t[i + 1];
+                if (i < n - 1 && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i + 1) < '7'))) {
+                    t[i] += t[i + 2];
+                }
+            }
+        }
+        return t[0];
+    }
+
+    public int numDecodings(String s) {
+        int n = s.length();
+        return bottomUp1(s, n);
+    }
+}
+
+
+//Approach-3 (Using Bottom Up Way-2)
+//T.C : O(n)
+//S.C : O(n)
+public class Solution {
+    public int bottomUp2(String s, int n) {
+        int[] t = new int[n + 1];
+        Arrays.fill(t, 0);
+        t[0] = 1;
+        t[1] = s.charAt(0) == '0' ? 0 : 1;
+
+        for (int i = 2; i <= n; i++) {
+            if (s.charAt(i - 1) != '0') {
+                t[i] += t[i - 1];
+            }
+
+            if (s.charAt(i - 2) == '1' || (s.charAt(i - 2) == '2' && s.charAt(i - 1) < '7')) {
+                t[i] += t[i - 2];
+            }
+        }
+        return t[n];
+    }
+
+    public int numDecodings(String s) {
+        int n = s.length();
+        return bottomUp2(s, n);
+    }
+}
+
+
+//Approach-4 
+//T.C : O(n)
+//S.C : O(1)
+public class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        if (n == 1) {
+            return s.charAt(0) == '0' ? 0 : 1;
+        }
+        if (s.charAt(0) == '0') {
+            return 0;
+        }
+
+        int last1 = 1, last2 = 1;
+
+        for (int i = 1; i < n; i++) {
+            int count = s.charAt(i) != '0' ? last1 : 0;
+
+            if (s.charAt(i - 1) == '1' || (s.charAt(i - 1) == '2' && s.charAt(i) < '7')) {
+                count += last2;
+            }
+
+            last2 = last1;
+            last1 = count;
+        }
+
+        return last1;
+    }
+}
