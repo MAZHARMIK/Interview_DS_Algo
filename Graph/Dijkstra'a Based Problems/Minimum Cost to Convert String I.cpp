798f2db265ac5afb26d70ce4ed361a49393b9f77
MAZHAR <iiestshibpur605@gmail.com>
2023-12-25 21:36:21 +0530

    Added YouTube Link

--- Graph/Dijkstra'a Based Problems/Minimum Cost to Convert String I.cpp
+++ Graph/Dijkstra'a Based Problems/Minimum Cost to Convert String I.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=M8WnAIhTjmQ
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/minimum-cost-to-convert-string-i/
 */
@@ -10,51 +10,62 @@
 //S.C : O(V+E) -> We create a graph, where V = number of vertices and E = number of edges
 class Solution {
 public:
-    void dijkstra(unordered_map<char, vector<pair<char, int>>>& adj, char source, vector<vector<int>>& distances) {
-        
+    void dijkstra(char &source, unordered_map<char, vector<pair<char, int>>> &adj, vector<vector<long long>> &costMatrix) {
+
+        //min-heap
         priority_queue<pair<int, char>, vector<pair<int, char>>, greater<pair<int, char>>> pq;
+
         pq.push({0, source});
-        
-        while (!pq.empty()) {
-            int d     = pq.top().first;
-		    char node = pq.top().second;
-		    pq.pop();
-            
-            for (auto &child : adj[node]) {
-                char adjNode  = child.first;
-			    int distance  = child.second;
-                
-                if (distances[source - 'a'][adjNode - 'a'] > distance + d) {
-                    distances[source - 'a'][adjNode - 'a'] = distance + d;
-                    pq.push({distance + d, adjNode});
+
+        while(!pq.empty()) {
+            int d        = pq.top().first;
+            char adjNode = pq.top().second;
+            pq.pop();
+
+            for(auto &it : adj[adjNode]) {
+                char adjNode = it.first;
+                int cost      = it.second;
+
+                if(costMatrix[source - 'a'][adjNode - 'a'] > d + cost) {
+                    costMatrix[source - 'a'][adjNode - 'a'] = d + cost;
+                    pq.push({d + cost, adjNode});
                 }
             }
+
         }
         return;
     }
-
+    //V = # of vertices
+    //E = # of edges
+    //O(n * (V+E)log(V))
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
         unordered_map<char, vector<pair<char, int>>> adj;
 
-        for (int i = 0; i < original.size(); i++) {
+        for(int i = 0; i < original.size(); i++) {
             adj[original[i]].push_back({changed[i], cost[i]});
         }
 
-        vector<vector<int>> distances(26, vector<int>(26, INT_MAX));
+        vector<vector<long long>> costMatrix(26, vector<long long>(26, INT_MAX));
 
-        //Populate distances using Dijkstra's
-        for (auto it : original)
-            dijkstra(adj, it, distances);
+        //Populate the costMatrix using Dijkstra's Algorithm
+        for(int i = 0; i < source.length(); i++) { //n
+            char ch = source[i];
+            dijkstra(ch, adj, costMatrix);
+        }
 
         long long ans = 0;
-        for (int i = 0; i < source.size(); i++) {
-            
-            if (source[i] == target[i]) continue;
 
-            if (distances[source[i] - 'a'][target[i] - 'a'] == INT_MAX) 
+        for(int i = 0; i < source.length(); i++) {
+            if(source[i] == target[i]) {
+                continue;
+            }
+
+            if(costMatrix[source[i] - 'a'][target[i] - 'a'] == INT_MAX) {
                 return -1;
-            
-            else ans += distances[source[i] - 'a'][target[i] - 'a'];
+            }
+
+            ans += costMatrix[source[i] - 'a'][target[i] - 'a'];
+
         }
 
         return ans;
