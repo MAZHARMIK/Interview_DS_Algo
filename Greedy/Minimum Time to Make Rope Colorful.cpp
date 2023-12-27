eb659a5f0060e64be073962a44dc301084fb64ae
MAZHAR <iiestshibpur605@gmail.com>
2023-12-27 07:03:25 +0530

    Added  JAVA Code

--- Greedy/Minimum Time to Make Rope Colorful.cpp
+++ Greedy/Minimum Time to Make Rope Colorful.cpp
@@ -4,6 +4,10 @@
     Leetcode Link               : https://leetcode.com/problems/minimum-time-to-make-rope-colorful/
 */
 
+/************************************************************************ C++ ************************************************************************************/
+//Approach - Greedily moving ahead
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
@@ -31,3 +35,35 @@ public:
         return time;
     }
 };
+
+
+
+/************************************************************************ JAVA ************************************************************************************/
+//Approach - Greedily moving ahead
+//T.C : O(n)
+//S.C : O(1)
+public class Solution {
+    public int minCost(String colors, int[] neededTime) {
+        int n = colors.length();
+        
+        int time = 0;
+        int prevMax = 0;
+        
+        for (int i = 0; i < n; i++) {
+            
+            if (i > 0 && colors.charAt(i) != colors.charAt(i - 1)) {
+                prevMax = 0;
+            }
+
+            int curr = neededTime[i];
+            
+            // Greedily select the minimum between prevMax and curr
+            time += Math.min(prevMax, curr);
+            
+            // Update prevMax to the maximum between prevMax and curr
+            prevMax = Math.max(prevMax, curr);
+        }
+        
+        return time;
+    }
+}
