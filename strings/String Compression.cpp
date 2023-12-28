99bc5c70d7e7d9b4cbb5202be6e9a7d1e0a7787a
MAZHAR <iiestshibpur605@gmail.com>
2023-12-28 06:33:37 +0530

    Added JAVA code

--- strings/String Compression.cpp
+++ strings/String Compression.cpp
@@ -1,11 +1,14 @@
 /*
-    MY YOUTUBE VIDE ON THIS Qn : https://www.youtube.com/watch?v=I7drewKcN1Y&t=5s
+    MY YOUTUBE VIDE ON THIS Qn : https://www.youtube.com/watch?v=I7drewKcN1Y
     Company Tags               : Snapchat, GoDaddy, Lyft, Expedia, Microsoft, Bloomberg, Amazon, CouponDunia, FactSet, Goldman Sachs, VMWare, Zoho
     Leetcode Link              : https://leetcode.com/problems/string-compression/
     GfG Link                   : https://practice.geeksforgeeks.org/problems/run-length-encoding/1
 
 */
 
+/************************************************************ C++ ************************************************************************/
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     int compress(vector<char>& chars) {
@@ -41,3 +44,40 @@ public:
         return index;
     }
 };
+
+
+/************************************************************ JAVA ************************************************************************/
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public int compress(char[] chars) {
+        int n = chars.length;
+
+        int i = 0;
+        int index = 0;
+
+        while (i < n) {
+            char curr = chars[i];
+
+            int count = 0;
+            // Find count of duplicates
+            while (i < n && chars[i] == curr) {
+                i++;
+                count++;
+            }
+
+            // Assign it to chars and move index ahead to add the count
+            chars[index++] = curr;
+
+            // Add the count
+            if (count > 1) {
+                String countStr = Integer.toString(count);
+                for (char ch : countStr.toCharArray()) {
+                    chars[index++] = ch;
+                }
+            }
+        }
+
+        return index;
+    }
+}
