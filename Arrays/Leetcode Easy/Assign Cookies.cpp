5d6f75fe450f7b01812cf2200ea605dd63647013
MAZHAR <iiestshibpur605@gmail.com>
2024-01-01 07:41:36 +0530

    Assign Cookies

---
+++ Arrays/Leetcode Easy/Assign Cookies.cpp
@@ -0,0 +1,61 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Amazon, Meta
+    Leetcode Link               : https://leetcode.com/problems/assign-cookies/
+*/
+
+
+/**************************************************** C++ ****************************************************/
+//Approach (Sort and choose)
+//T.C : O(mlogm + nlogn)
+//S.C : O(1)
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(begin(g), end(g));
+        sort(begin(s), end(s));
+        
+        int m = g.size();
+        int n = s.size();
+        
+        int i = 0;
+        int j = 0;
+        
+        while(i < m && j < n) {
+            if(g[i] <= s[j]) {
+                i++;
+            }
+            j++;
+        }
+        
+        return i;
+    }
+};
+
+
+
+/**************************************************** JAVA ****************************************************/
+//Approach (Sort and choose)
+//T.C : O(mlogm + nlogn)
+//S.C : O(1)
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        
+        int m = g.length;
+        int n = s.length;
+        
+        int i = 0;
+        int j = 0;
+        
+        while (i < m && j < n) {
+            if (g[i] <= s[j]) {
+                i++;
+            }
+            j++;
+        }
+        
+        return i;
+    }
+}
