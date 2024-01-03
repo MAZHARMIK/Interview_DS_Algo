2045d2a545f9e3f5b1b15515fdf71b502ed68b54
MAZHAR <iiestshibpur605@gmail.com>
2024-01-03 06:51:01 +0530

    Number of Laser Beams in a Bank

---
+++ strings/Number of Laser Beams in a Bank.cpp
@@ -0,0 +1,67 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : AMAZON
+    Leetcode Link               : https://leetcode.com/problems/number-of-laser-beams-in-a-bank/
+*/
+
+/****************************************************** C++ ******************************************************/
+//Approach (Simple iterate and count)
+//T.C : O(m*n) - Total number of characters in all string
+//S.C : O(1)
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int n = bank.size();
+        
+        int prevDeviceCount = 0;
+        int result    = 0;
+        
+        for(int i = 0; i<n; i++) {
+            
+            int currDeviceCount = 0;
+            for(char &ch : bank[i]) {
+                if(ch == '1') {
+                    currDeviceCount++;
+                }
+            }
+            
+            result += (prevDeviceCount * currDeviceCount);
+            
+            prevDeviceCount = currDeviceCount == 0 ? prevDeviceCount : currDeviceCount;
+            
+        }
+        
+        return result;
+    }
+};
+
+
+
+/****************************************************** JAVA ******************************************************/
+//Approach (Simple iterate and count)
+//T.C : O(m*n) - Total number of characters in all string
+//S.C : O(1)
+class Solution {
+    public int numberOfBeams(String[] bank) {
+        int n = bank.length;
+
+        int prevDeviceCount = 0;
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            int currDeviceCount = 0;
+            for (char ch : bank[i].toCharArray()) {
+                if (ch == '1') {
+                    currDeviceCount++;
+                }
+            }
+
+            result += (prevDeviceCount * currDeviceCount);
+
+            prevDeviceCount = (currDeviceCount == 0) ? prevDeviceCount : currDeviceCount;
+        }
+
+        return result;
+    }
+}
