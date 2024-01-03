eaa7f5ea2aacb3938dba8b4fe8374954272eff6d
MAZHAR <iiestshibpur605@gmail.com>
2024-01-03 10:34:36 +0530

    Added YouTube Link

--- strings/Number of Laser Beams in a Bank.cpp
+++ strings/Number of Laser Beams in a Bank.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=3-c1Gc-5QnE
     Company Tags                : AMAZON
     Leetcode Link               : https://leetcode.com/problems/number-of-laser-beams-in-a-bank/
 */
