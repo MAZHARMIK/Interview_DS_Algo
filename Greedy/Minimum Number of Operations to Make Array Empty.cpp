1b8af3c71f700a699b88839e8013f5937a297a89
MAZHAR <iiestshibpur605@gmail.com>
2024-01-04 07:00:15 +0530

    Minimum Number of Operations to Make Array Empty

---
+++ Greedy/Minimum Number of Operations to Make Array Empty.cpp
@@ -0,0 +1,67 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : will update soon
+    Leetcode Link               : https://leetcode.com/problems/minimum-number-of-operations-to-make-array-empty/
+*/
+
+
+/******************************************************************* C++ *********************************************************************/
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        
+        unordered_map<int, int> mp;
+        
+        for(int i = 0; i < n; i++) {
+            mp[nums[i]]++;
+        }
+        
+        int result = 0;
+        
+        for(auto &it : mp) {
+            int freq = it.second;
+            
+            if(freq == 1) { //we can only remove equal elements
+                return -1;
+            }
+            
+            result += ceil((double)freq/3);
+        }
+        
+        return result;
+    }
+};
+
+
+/******************************************************************* JAVA *********************************************************************/
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public int minOperations(int[] nums) {
+        int n = nums.length;
+
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
+        }
+
+        int result = 0;
+
+        for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
+            int freq = entry.getValue();
+
+            if (freq == 1) {
+                // we can only remove equal elements
+                return -1;
+            }
+
+            result += Math.ceil((double) freq / 3);
+        }
+
+        return result;
+    }
+}
