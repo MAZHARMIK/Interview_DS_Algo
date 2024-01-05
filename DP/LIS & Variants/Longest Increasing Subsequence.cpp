c8e8041ba8299cc1b2ce395d1c3db2eff8171cd8
MAZHAR <iiestshibpur605@gmail.com>
2024-01-05 07:57:16 +0530

    Longest Increasing Subsequence

--- DP/LIS & Variants/Longest Increasing Subsequence.cpp
+++ DP/LIS & Variants/Longest Increasing Subsequence.cpp
@@ -269,6 +269,52 @@ class Solution {
     }
 }
 
+
+//Approacj-4 (Using concept of Patience Sorting (O(nlogn))
+//T.C : O(nlogn)
+//S.C : O(n)
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        List<Integer> sorted = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            /*
+                Why lower bound?
+                We want an increasing subsequence, and hence
+                we want to eliminate the duplicates as well.
+                lower_bound returns the index of "next greater or equal to."
+            */
+            int index = binarySearch(sorted, nums[i]);
+
+            if (index == sorted.size())
+                sorted.add(nums[i]); // greatest: so insert it
+            else
+                sorted.set(index, nums[i]); // replace
+        }
+
+        return sorted.size();
+    }
+
+    private int binarySearch(List<Integer> sorted, int target) {
+        int left = 0, right = sorted.size();
+        int result = sorted.size();
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (sorted.get(mid) < target) {
+                left = mid + 1;
+            } else {
+                result = mid;
+                right = mid;
+            }
+        }
+        return result;
+    }
+}
+
+	
 //Approach-5 (Using same code of Leetcode-2926(Maximum Balanced Subsequence Sum) (YouTube - https://www.youtube.com/watch?v=JrG4tbq6efg)
 //T.C : O(nlogn)
 //S.C : O(n)
