7dae4d364ac7d2d5ae2073f3376096bb02d35df5
MAZHAR <iiestshibpur605@gmail.com>
2024-01-06 07:31:57 +0530

    Added JAVA code also

--- DP/Maximum Profit in Job Scheduling.cpp
+++ DP/Maximum Profit in Job Scheduling.cpp
@@ -116,3 +116,116 @@ public:
 	    return dp[n - 1];
     }
 };
+
+
+/******************************************************************* JAVA *******************************************************************/
+//Approach-1 (Recur + Memo)
+//T.C : O(nlogn), where n is the number of jobs
+//S.C : O(n), where n is the number of jobs.
+class Solution {
+    private int[] memo;
+    private int n;
+
+    // Find the first job whose starting point >= currentJob's end point
+    private int getNextIndex(int[][] array, int l, int currentJobEnd) {
+        int r = n - 1;
+        int result = n + 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (array[mid][0] >= currentJobEnd) { // we can take this task
+                result = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return result;
+    }
+
+    private int solve(int[][] array, int i) {
+        if (i >= n)
+            return 0;
+
+        if (memo[i] != -1)
+            return memo[i];
+
+        int next = getNextIndex(array, i + 1, array[i][1]);
+
+        int taken = array[i][2] + solve(array, next);
+        int notTaken = solve(array, i + 1);
+
+        return memo[i] = Math.max(taken, notTaken);
+    }
+
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        n = startTime.length;
+
+        memo = new int[n];
+        Arrays.fill(memo, -1);
+
+        int[][] array = new int[n][3]; // {s, e, p}
+
+        for (int i = 0; i < n; i++) {
+            array[i][0] = startTime[i];
+            array[i][1] = endTime[i];
+            array[i][2] = profit[i];
+        }
+
+        Arrays.sort(array, Comparator.comparingInt(vec -> vec[0]));
+
+        return solve(array, 0);
+    }
+}
+
+
+
+//Approach-2 (Bottom Up DP) - Video coming soon
+//T.C :  O(n^2), where n is the number of jobs
+//S.C : O(n)
+class Solution {
+    class Job {
+        int start;
+        int end;
+        int profit;
+
+        public Job(int start, int end, int profit) {
+            this.start = start;
+            this.end = end;
+            this.profit = profit;
+        }
+    }
+
+    private static int compareJobs(Job m, Job n) {
+        return Integer.compare(m.end, n.end);
+    }
+
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int n = startTime.length;
+        int[] dp = new int[n];
+        Job[] jobs = new Job[n];
+
+        for (int i = 0; i < n; i++) {
+            jobs[i] = new Job(startTime[i], endTime[i], profit[i]);
+        }
+
+        Arrays.sort(jobs, Comparator.comparingInt(j -> j.end));
+
+        dp[0] = jobs[0].profit;
+
+        for (int i = 1; i < n; i++) {
+            int prev = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                if (jobs[i].start >= jobs[j].end) {
+                    prev = dp[j];
+                    break;
+                }
+            }
+            dp[i] = Math.max(prev + jobs[i].profit, dp[i - 1]);
+        }
+
+        return dp[n - 1];
+    }
+}
