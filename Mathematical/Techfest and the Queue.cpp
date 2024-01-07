969fadf56b41fd57daf26e7b88449dbe0b871d15
MAZHAR <iiestshibpur605@gmail.com>
2024-01-07 12:51:50 +0530

    Update Techfest and the Queue.cpp

--- Mathematical/Techfest and the Queue.cpp
+++ Mathematical/Techfest and the Queue.cpp
@@ -40,13 +40,22 @@ public:
 
 
 /************************************************************************ JAVA ****************************************************************************/
+//JAVA code gives TLE so I have used static array "resultCache" to store the result for different values so that they can be used in future large inputs.
 //T.C : O((b - a + 1) * sqrt(x) * log(x)).
 //S.C : O(1)
 class Solution {
+    private static final int MAX_SIZE = 2*100001;
+    private static long[] resultCache = new long[MAX_SIZE];
+
+    // Function to count prime factors
     public static long countSum(long x) {
+        if (resultCache[(int)x] != 0) {
+            return resultCache[(int)x];
+        }
+
         long count = 0;
-	int temp = x;
-	    
+        long temp = x;
+
         for (int i = 2; i * i <= temp; i++) {
             while (x % i == 0) {
                 x /= i;
@@ -58,15 +67,18 @@ class Solution {
             count++;
         }
 
+        // Store the result in the cache
+        resultCache[(int)temp] = count;
+
         return count;
     }
-    
+
+    // Function to calculate sum of powers
     public static long sumOfPowers(long a, long b) {
         long result = 0;
 
-        while (a <= b) {
-            result += countSum(a);
-            a++;
+        for (long i = a; i <= b; i++) {
+            result += countSum(i);
         }
 
         return result;
