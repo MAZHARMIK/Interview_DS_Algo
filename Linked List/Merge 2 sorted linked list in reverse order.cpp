2ab8e71b6e09bc029c894b5a3a2de92173e99fa8
MAZHAR <iiestshibpur605@gmail.com>
2024-01-08 21:45:16 +0530

    Merge 2 sorted linked list in reverse order

---
+++ Linked List/Merge 2 sorted linked list in reverse order.cpp
@@ -0,0 +1,62 @@
+/*
+      MY YOUTUBE VIDEO ON THIS Qn : 
+      Company Tags                : Microsoft
+      GfG Link                    : https://www.geeksforgeeks.org/problems/merge-2-sorted-linked-list-in-reverse-order/1
+*/
+
+/************************************************************ C++ **************************************************************/
+//T.C : O(m+n)
+//T.C : O(1) (Ignoring recursion stack space)
+class Solution
+{
+    public:
+    
+    struct Node* mergeTwoLists(Node *list1,Node *list2) {
+        if(list1 == NULL) {
+            return list2;
+        }
+
+        if(list2 == NULL) {
+            return list1;
+        }
+
+        struct Node* result;
+        if(list1->data < list2->data) {
+            result = list1;
+            result->next = mergeTwoLists(list1->next, list2);
+        } else {
+            result = list2;
+            result->next = mergeTwoLists(list1, list2->next);
+        }
+
+        return result;
+    }
+    
+    struct Node* reverseList(struct Node* head) {
+        if(head == NULL || head->next == NULL) {
+            return head;
+        }
+
+        struct Node* last = reverseList(head->next); //last = future head
+
+        head->next->next = head;
+        head->next = NULL;
+
+        return last;
+    }
+    
+    struct Node * mergeResult(Node *node1,Node *node2) {
+        struct Node* result = mergeTwoLists(node1, node2);
+        
+        
+        result = reverseList(result);
+        
+        return result;
+        
+    }  
+};
+
+
+/************************************************************ JAVA **************************************************************/
+//T.C : O(m+n)
+//T.C : O(1) (Ignoring recursion stack space)
