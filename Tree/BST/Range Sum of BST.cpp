b64947270e8b1cea724ef912ce1fdffe42b29597
MAZHAR <iiestshibpur605@gmail.com>
2024-01-08 06:57:30 +0530

    Added JAVA code also

--- Tree/BST/Range Sum of BST.cpp
+++ Tree/BST/Range Sum of BST.cpp
@@ -5,6 +5,10 @@
     GfG Link                    : https://practice.geeksforgeeks.org/problems/count-bst-nodes-that-lie-in-a-given-range/1
 */
 
+
+/****************************************************************** C++ ******************************************************************/
+//T.C : O(n)
+//S.C : O(1) (Ignoging recursion stack space)
 class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
@@ -24,3 +28,28 @@ public:
         return rangeSumBST(root->left, low, high);
     }
 };
+
+
+/****************************************************************** JAVA ******************************************************************/
+//T.C : O(n)
+//S.C : O(1) (Ignoging recursion stack space)
+public class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+
+        // Within Range
+        if (root.val >= low && root.val <= high) {
+            return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+        }
+
+        // When outside the range (Less than low) - Go right
+        if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        }
+
+        // When outside the range (Greater than high) - Go left
+        return rangeSumBST(root.left, low, high);
+    }
+}
