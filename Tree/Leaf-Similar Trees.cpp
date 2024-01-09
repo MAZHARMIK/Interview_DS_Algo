84acc8b181b69ab739fb30672c25b687029ac68f
MAZHAR <iiestshibpur605@gmail.com>
2024-01-09 07:14:02 +0530

    Added JAVA Code also

--- Tree/Leaf-Similar Trees.cpp
+++ Tree/Leaf-Similar Trees.cpp
@@ -4,7 +4,9 @@
     Leetcode Link               : https://leetcode.com/problems/leaf-similar-trees/
 */
 
-
+/******************************************************** C++ ********************************************************/
+//T.C : O(n)
+//S.C : O(n) (string s) - I am ignoring the recursion stack space here
 class Solution {
 public:
     
@@ -31,3 +33,32 @@ public:
         return s1==s2;
     }
 };
+
+
+/******************************************************** JAVA ********************************************************/
+//T.C : O(n)
+//S.C : O(n) (string s) - I am ignoring the recursion stack space here
+public class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        StringBuilder s1 = new StringBuilder();
+        StringBuilder s2 = new StringBuilder();
+
+        inOrder(root1, s1);
+        inOrder(root2, s2);
+
+        return s1.toString().equals(s2.toString());
+    }
+
+    private void inOrder(TreeNode root, StringBuilder s) {
+        if (root == null) return;
+
+        // Leaf node
+        if (root.left == null && root.right == null) {
+            s.append(root.val).append("_");
+            return;
+        }
+
+        inOrder(root.left, s);
+        inOrder(root.right, s);
+    }
+}
