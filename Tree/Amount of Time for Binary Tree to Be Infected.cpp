9e55d24a4b696e5e6be9ad57c0940b74dec61a0c
MAZHAR <iiestshibpur605@gmail.com>
2024-01-10 19:37:22 +0530

    Added one pass solution

--- Tree/Amount of Time for Binary Tree to Be Infected.cpp
+++ Tree/Amount of Time for Binary Tree to Be Infected.cpp
@@ -66,7 +66,39 @@ public:
 //Approach-2 (Using DFS - one pass solution)
 //T.C : O(n)
 //S.C : O(n) - Max depth of recursion call stack
-//soon coming
+class Solution {
+public:
+    int result = INT_MIN;
+
+    int solve(TreeNode* root, int start) {
+        if(root == NULL) {
+            return 0;
+        }
+
+        int LH = solve(root->left, start);
+        int RH = solve(root->right, start);
+
+        if(root->val == start) {
+            result = max(LH, RH);
+            return -1;
+        } else if(LH >= 0 && RH >= 0) {
+            return max(LH, RH) + 1;
+        } else {
+            int d = abs(LH) + abs(RH);
+            result = max(result, d);
+
+            return min(LH, RH) - 1;
+        }
+
+        return 0;
+    }
+
+    int amountOfTime(TreeNode* root, int start) {
+        solve(root, start);
+
+        return result;
+    }
+};
 
 
 /******************************************************************** JAVA ********************************************************************/
@@ -128,4 +160,32 @@ public class Solution {
 //Approach-2 (Using DFS - one pass solution)
 //T.C : O(n)
 //S.C : O(n) - Max depth of recursion call stack
-//soon coming
+public class Solution {
+    private int result = Integer.MIN_VALUE;
+
+    public int solve(TreeNode root, int start) {
+        if (root == null) {
+            return 0;
+        }
+
+        int LH = solve(root.left, start);
+        int RH = solve(root.right, start);
+
+        if (root.val == start) {
+            result = Math.max(LH, RH);
+            return -1;
+        } else if (LH >= 0 && RH >= 0) {
+            return Math.max(LH, RH) + 1;
+        } else {
+            int d = Math.abs(LH) + Math.abs(RH);
+            result = Math.max(result, d);
+
+            return Math.min(LH, RH) - 1;
+        }
+    }
+
+    public int amountOfTime(TreeNode root, int start) {
+        solve(root, start);
+        return result;
+    }
+}
