fd635c3664dc434e9c11f7c49ede7a9401d7ce62
MAZHAR <iiestshibpur605@gmail.com>
2024-01-12 07:47:07 +0530

    Added JAVA Code

--- strings/Determine if String Halves Are Alike.cpp
+++ strings/Determine if String Halves Are Alike.cpp
@@ -4,7 +4,10 @@
     Leetcode Link               : https://leetcode.com/problems/determine-if-string-halves-are-alike/
 */
 
-//Approach-1 (Using Set)
+/******************************************************** C++ ********************************************************/
+//Approach-1 (without Using Set)
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     bool isVowel(char &ch) {
@@ -36,7 +39,9 @@ public:
 };
 
 
-//Approach-2 (Using Set)
+//Approach-1 (without Using Set)
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
 public:
     bool halvesAreAlike(string s) {
@@ -65,3 +70,71 @@ public:
         return countL == countR;
     }
 };
+
+
+/******************************************************** JAVA ********************************************************/
+//Approach-1 (without Using Set)
+//T.C : O(n)
+//S.C : O(1)
+public class Solution {
+    public boolean isVowel(char ch) {
+        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || 
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U');
+    }
+
+    public boolean halvesAreAlike(String s) {
+        int n = s.length();
+
+        int mid = n / 2;
+
+        int i = 0, j = mid;
+
+        int countL = 0;
+        int countR = 0;
+
+        while (i < n / 2 && j < n) {
+            if (isVowel(s.charAt(i))) countL++;
+
+            if (isVowel(s.charAt(j))) countR++;
+
+            i++;
+            j++;
+        }
+
+        return countL == countR;
+    }
+}
+
+
+//Approach-1 (without Using Set)
+//T.C : O(n)
+//S.C : O(n)
+public class Solution {
+    public boolean halvesAreAlike(String s) {
+        int n = s.length();
+
+        int mid = n / 2;
+
+        int i = 0, j = mid;
+
+        int countL = 0;
+        int countR = 0;
+
+        Set<Character> vowels = new HashSet<>();
+        String vowelString = "aeiouAEIOU";
+        for (char c : vowelString.toCharArray()) {
+            vowels.add(c);
+        }
+
+        while (i < n / 2 && j < n) {
+            if (vowels.contains(s.charAt(i))) countL++;
+
+            if (vowels.contains(s.charAt(j))) countR++;
+
+            i++;
+            j++;
+        }
+
+        return countL == countR;
+    }
+}
