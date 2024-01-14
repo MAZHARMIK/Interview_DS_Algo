37f8759148d49af3064e2c10310c8bdf70e69cb4
MAZHAR <iiestshibpur605@gmail.com>
2024-01-14 07:44:25 +0530

    Added JAVA code also

--- strings/Determine if Two Strings Are Close.cpp
+++ strings/Determine if Two Strings Are Close.cpp
@@ -4,6 +4,9 @@
     Leecode Link                : https://leetcode.com/problems/determine-if-two-strings-are-close/
 */
 
+/*************************************************************************** C++ **********************************************************************/
+//T.C : O(n)
+//S.C : O(26) ~ O(1)
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
@@ -45,3 +48,42 @@ public:
         
     }
 };
+
+
+/*************************************************************************** JAVA **********************************************************************/
+//T.C : O(n)
+//S.C : O(26) ~ O(1)
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        
+        if (m != n)
+            return false;
+        
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        
+        for (int i = 0; i < m; i++) {
+            char ch1 = word1.charAt(i);
+            char ch2 = word2.charAt(i);
+            
+            freq1[ch1-'a']++;
+            freq2[ch2-'a']++;
+        }
+        
+        // 1st point - jo char word1 me hai, wo char word2 me bhi hona chaie
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i] != 0 && freq2[i] != 0) || (freq1[i] == 0 && freq2[i] == 0))
+                continue;
+            
+            return false;
+        }
+        
+        // 2nd point = match freq
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+        
+        return Arrays.equals(freq1, freq2);
+    }
+}
