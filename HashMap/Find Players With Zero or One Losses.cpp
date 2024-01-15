4fc15c217e3cf8d405d3e2209e99bed9e4f1d6b8
MAZHAR <iiestshibpur605@gmail.com>
2024-01-15 07:54:18 +0530

    Added JAVA Code

--- HashMap/Find Players With Zero or One Losses.cpp
+++ HashMap/Find Players With Zero or One Losses.cpp
@@ -4,6 +4,10 @@
     Leetcode Link               : https://leetcode.com/problems/find-players-with-zero-or-one-losses/
 */
 
+
+/************************************************************ C++ ************************************************************/
+//T.C : O(n*logn)
+//S.C : O(n)
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
@@ -38,3 +42,39 @@ public:
         
     }
 };
+
+
+/************************************************************ JAVA ************************************************************/
+//T.C : O(n*logn)
+//S.C : O(n)
+class Solution {
+    public List<List<Integer>> findWinners(int[][] matches) {
+        Map<Integer, Integer> lost = new HashMap<>();
+
+        for (int[] it : matches) {
+            int lose = it[1];
+            lost.put(lose, lost.getOrDefault(lose, 0) + 1);
+        }
+
+        List<Integer> notLost = new ArrayList<>();
+        List<Integer> oneLos = new ArrayList<>();
+
+        for (int[] it : matches) {
+            int lose = it[1];
+            int win = it[0];
+
+            if (lost.get(lose) == 1) {
+                oneLos.add(lose);
+            }
+            if (!lost.containsKey(win)) {
+                notLost.add(win);
+                lost.put(win, 2);
+            }
+        }
+
+        Collections.sort(notLost);
+        Collections.sort(oneLos);
+
+        return Arrays.asList(notLost, oneLos);
+    }
+}
