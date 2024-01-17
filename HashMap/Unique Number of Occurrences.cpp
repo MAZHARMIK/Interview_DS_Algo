78dbd75fc11f34f3810ffc89f5ccaacbc00f8faa
MAZHAR <iiestshibpur605@gmail.com>
2024-01-17 07:35:30 +0530

    Added JAVA code also

--- HashMap/Unique Number of Occurrences.cpp
+++ HashMap/Unique Number of Occurrences.cpp
@@ -4,7 +4,10 @@
     Leetcode Link               : https://leetcode.com/problems/unique-number-of-occurrences/
 */
 
+/****************************************************************************** JAVA ******************************************************************************/
 //Approach-1 (Simple using hash map and set)
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
@@ -31,6 +34,8 @@ public:
 
 
 //Approach-2 (Making benefit of constraints)
+//T.C : O(nlogn)
+//S.C : O(1)
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
@@ -51,3 +56,55 @@ public:
         return true;
     }
 };
+
+
+
+/****************************************************************************** JAVA ******************************************************************************/
+//Approach-1 (Simple using hash map and set)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        // Store frequency of each number
+        for (int x : arr) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int freq : map.values()) {
+            if (set.contains(freq)) {
+                return false;
+            }
+            set.add(freq);
+        }
+        
+        return true;
+    }
+}
+
+
+//Approach-2 (Making benefit of constraints)
+//T.C : O(nlogn)
+//S.C : O(1)
+public class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        int[] vec = new int[2001];
+
+        for (int x : arr) {
+            vec[x + 1000]++;
+        }
+
+        Arrays.sort(vec);
+
+        for (int i = 1; i < 2001; i++) {
+            if (vec[i] != 0 && vec[i] == vec[i - 1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
