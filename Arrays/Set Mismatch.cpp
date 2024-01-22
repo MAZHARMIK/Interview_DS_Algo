a73cc46cce76c689017e835e0a9d258a4ccd3461
MAZHAR <iiestshibpur605@gmail.com>
2024-01-22 08:22:28 +0530

    Added JAVA code also

--- Arrays/Set Mismatch.cpp
+++ Arrays/Set Mismatch.cpp
@@ -4,7 +4,10 @@
     Leetcode Link              : https://leetcode.com/problems/set-mismatch/
 */
 
-//Smart Approach
+/****************************************************************** C++ ******************************************************************/
+//Approach-1 (Smart Approach)
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
@@ -34,7 +37,9 @@ public:
 };
 
 
-//Basic Approach
+//Approach-2 (Basic Approach)
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
@@ -57,3 +62,64 @@ public:
         return {dup, miss};
     }
 };
+
+
+/****************************************************************** C++ ******************************************************************/
+//Approach-1 (Smart Approach)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        
+        int dup = -1;
+        int missing = -1;
+        
+        for (int i = 0; i < n; i++) {
+            int num = Math.abs(nums[i]);
+            
+            if (nums[num - 1] < 0)
+                dup = num;
+            else
+                nums[num - 1] *= (-1);
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                missing = i + 1;
+                break;
+            }
+        }
+        
+        return new int[]{dup, missing};
+    }
+}
+
+
+
+//Approach-2 (Basic Approach)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int miss = 0, dup = 0;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int x : nums) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            if (map.containsKey(i)) {
+                if (map.get(i) == 2) {
+                    dup = i;
+                }
+            } else {
+                miss = i;
+            }
+        }
+        
+        return new int[]{dup, miss};
+    }
+}
