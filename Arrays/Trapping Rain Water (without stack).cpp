66fc8edcf54149d1e030dae547f6f766895b0389
MAZHAR <iiestshibpur605@gmail.com>
2024-01-24 18:10:02 +0530

    Update Trapping Rain Water (without stack).cpp

--- Arrays/Trapping Rain Water (without stack).cpp
+++ Arrays/Trapping Rain Water (without stack).cpp
@@ -4,6 +4,10 @@
     Leetcode Link                     : https://leetcode.com/problems/trapping-rain-water/
 */
 
+
+/********************************************************** C++ **********************************************************/
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
 public:
     vector<int> getLeftMax(vector<int>& height, int n) {
@@ -35,3 +39,43 @@ public:
         return sum;
     }
 };
+
+
+
+/********************************************************** JAVA **********************************************************/
+//T.C : O(n)
+//S.C : O(n)
+public class Solution {
+    public int[] getLeftMax(int[] height, int n) {
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+        return leftMax;
+    }
+
+    public int[] getRightMax(int[] height, int n) {
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+        return rightMax;
+    }
+
+    public int trap(int[] height) {
+        int n = height.length;
+        if (n == 1 || n == 0)
+            return 0;
+        
+        int[] leftMax = getLeftMax(height, n);
+        int[] rightMax = getRightMax(height, n);
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return sum;
+    }
+}
