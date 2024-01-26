3013138b984db070f58e2ed98ae7c560bb9e6b95
MAZHAR <iiestshibpur605@gmail.com>
2024-01-26 09:31:52 +0530

    Out of Boundary Paths

--- DP/Out of Boundary Paths.cpp
+++ DP/Out of Boundary Paths.cpp
@@ -1,121 +1,241 @@
 /*
-    Company Tags  : Baidu
-    Leetcode Link : https://leetcode.com/problems/out-of-boundary-paths/
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Baidu
+    Leetcode Link               : https://leetcode.com/problems/out-of-boundary-paths/
 */
 
+//#codestorywithMIK
 
-////////using map for memoization. Time : 156 ms (O(m*n*maxMove))//////////////////////////
-using ll = unsigned long long ;
+/*************************************************************************** C++ ***************************************************************************/
+//Approach-1 (Recursion + Memoization using 3D Array)
+//T.C : O(m * n * maxMove)
+//S.C : O(m * n * maxMove)
 class Solution {
 public:
-    int m, n;
-    unordered_map<string, ll> mp;
-    int solve(int maxMove, int i, int j) {
-        if(i < 0 || i >= m || j < 0 || j >= n) {
-            return 1;
-        }
-        if(maxMove == 0)
-            return 0;
-        
-        string key   = to_string(maxMove) + "_" + to_string(i) + "_" + to_string(j);
-            
-        if(mp.find(key) != mp.end())
-            return mp[key];
-        
-        ll left     = solve(maxMove-1, i, j-1);
-        ll right    = solve(maxMove-1, i, j+1);
-        ll up       = solve(maxMove-1, i-1, j);
-        ll down     = solve(maxMove-1, i+1, j);
-
-        return mp[key] = (left+right+up+down)%1000000007;
-    }
-
-    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        if(maxMove == 0)
-            return 0;
-        mp.clear();
-        this->m = m;
-        this->n  = n;
-
-        return solve(maxMove, startRow, startColumn);
-
-    }
-};
-
-////////using 3-D array for memoization. Time : 4 ms (O(m*n*maxMove))//////////////////////////
-using ll = unsigned long long ;
-class Solution {
-public:
-    int m, n;
+    int M;
+    int N;
+    int MOD = 1e9+7;
+    vector<vector<int>> directions{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     int t[51][51][51];
-    int solve(int maxMove, int i, int j) {
-        if(i < 0 || i >= m || j < 0 || j >= n) {
-            return 1;
+    int solve(int maxMove, int startRow, int startColumn) {
+        if(startRow < 0 || startRow >= M || startColumn < 0 || startColumn >= N) {
+            return 1; //Found one path out
         }
-        if(maxMove == 0)
-            return 0;
-        
-        if(t[maxMove][i][j] != -1)
-            return t[maxMove][i][j];
-        
-        ll left     = solve(maxMove-1, i, j-1);
-        ll right    = solve(maxMove-1, i, j+1);
-        ll up       = solve(maxMove-1, i-1, j);
-        ll down     = solve(maxMove-1, i+1, j);
 
-        return t[maxMove][i][j] = (left+right+up+down)%1000000007;
+        if(maxMove <= 0) 
+            return 0;
+
+        if(t[maxMove][startRow][startColumn] != -1) {
+            return t[maxMove][startRow][startColumn];
+        }
+
+        int result = 0;
+        for(auto &dir : directions) {
+            int x = startRow    + dir[0];
+            int y = startColumn + dir[1];
+            
+            result = (result + solve(maxMove-1, x, y)) % MOD;
+        }
+
+        return t[maxMove][startRow][startColumn] = result%MOD;
+
     }
 
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        if(maxMove == 0)
-            return 0;
-        
         memset(t, -1, sizeof(t));
-        this->m = m;
-        this->n  = n;
-
+        M = m;
+        N = n;
         return solve(maxMove, startRow, startColumn);
-
     }
 };
 
-////////using Bottom Up. Time : 4 ms (O(m*n*maxMove))//////////////
-/*I'ts a tricky one. I couldn't come up with this solution in the first time*/
-typedef unsigned long long ll;
+
+//Approach-2 (Bottom Up)
+//T.C : O(m * n * maxMove)
+//S.C : O(m * n * maxMove)
 class Solution {
 public:
+    int MOD = 1e9+7;
+    vector<vector<int>> directions{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        ll prev[51][51] = {0};
-        //t[i][j] = # ways to reach [i][j] with x moves in hand
-        prev[startRow][startColumn] = 1;
-        /*
-            There is only one(1) way to reach [startRow][startCol] with 0 moves in hand
-        */
-        
-        ll count = 0;
-        ll M     = 1e9+7;
-        for(int move = 1; move <= maxMove; move++) {
-            ll curr[51][51] = {0};
-            for(int i = 0; i<m; i++) {
-                for(int j = 0; j<n; j++) {
-                    if(i == 0)
-                        count = (count+prev[i][j])%M;
-                    if(i == m-1)
-                        count = (count+prev[i][j])%M;
-                    if(j == 0)
-                        count = (count+prev[i][j])%M;
-                    if(j == n-1)
-                        count = (count+prev[i][j])%M;
+
+        int dp[50][50][51];
+        memset(dp, 0, sizeof(dp));
+
+        for (int k = 1; k <= maxMove; ++k) {
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
                     
-                    curr[i][j] += (i > 0   ? prev[i-1][j] : 0)%M;   // we can come to [i][j] from UP    only if curr i > 0
-                    curr[i][j] += (i < m-1 ? prev[i+1][j] : 0)%M;   // we can come to [i][j] from DOWN  only if curr i < m-1
-                    curr[i][j] += (j > 0   ? prev[i][j-1] : 0)%M;   // we can come to [i][j] from LEFT  only if curr j > 0
-                    curr[i][j] += (j < n-1 ? prev[i][j+1] : 0)%M;   // we can come to [i][j] from RIGHT only if curr j < n-1
+                    for(auto &dir : directions) {
+                        int x = i + dir[0];
+                        int y = j + dir[1];
+                        if (x < 0 || x >= m || y < 0 || y >= n)
+                            dp[i][j][k] = (dp[i][j][k] + 1) % MOD;
+                        else
+                            dp[i][j][k] = (dp[i][j][k] + dp[x][y][k - 1]) % MOD;
+                        
+                    }
                 }
             }
-            swap(prev, curr);
         }
-        
-        return count;
+
+        return dp[startRow][startColumn][maxMove];
+
     }
 };
+
+
+//Approach-3 (Space and time optimized bottom up)
+//T.C : O(m * n)
+//S.C : O(m * n)
+class Solution {
+public:
+    int MOD = 1e9+7;
+    vector<vector<int>> directions{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+
+        vector<vector<int>> t(50, vector<int>(50));
+        t[startRow][startColumn] = 1;
+        int result = 0;
+
+        for (int k = 1; k <= maxMove; ++k) {
+            vector<vector<int>> temp(50, vector<int>(50));
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    
+                    for(auto &dir : directions) {
+                        int x = i + dir[0];
+                        int y = j + dir[1];
+                        if (x < 0 || x >= m || y < 0 || y >= n) {
+                            result = (result + t[i][j]) % MOD;
+                        }
+                        else {
+                            temp[x][y] = (temp[x][y] + t[i][j]) % MOD;
+                        }
+                    }
+                }
+            }
+            t = temp;
+        }
+
+        return result;
+
+    }
+};
+
+
+/*************************************************************************** JAVA ***************************************************************************/
+//Approach-1 (Recursion + Memoization using 3D Array)
+//T.C : O(m * n * maxMove)
+//S.C : O(m * n * maxMove)
+class Solution {
+    int M;
+    int N;
+    int MOD = (int)1e9+7;
+    int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private int[][][] t;
+
+    private int solve(int maxMove, int startRow, int startColumn) {
+        if (startRow < 0 || startRow >= M || startColumn < 0 || startColumn >= N) {
+            return 1; // Found one path out
+        }
+
+        if (maxMove <= 0) {
+            return 0;
+        }
+
+        if (t[maxMove][startRow][startColumn] != -1) {
+            return t[maxMove][startRow][startColumn];
+        }
+
+        int result = 0;
+        for (int[] dir : directions) {
+            int x = startRow + dir[0];
+            int y = startColumn + dir[1];
+
+            result = (result + solve(maxMove - 1, x, y)) % MOD;
+        }
+
+        return t[maxMove][startRow][startColumn] = result % MOD;
+    }
+
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        t = new int[maxMove + 1][m][n];
+        for (int i = 0; i <= maxMove; i++) {
+            for (int j = 0; j < m; j++) {
+                Arrays.fill(t[i][j], -1);
+            }
+        }
+
+        M = m;
+        N = n;
+        return solve(maxMove, startRow, startColumn);
+    }
+}
+
+
+//Approach-2 (Bottom Up)
+//T.C : O(m * n * maxMove)
+//S.C : O(m * n * maxMove)
+class Solution {
+    int MOD = 1000000007;
+    int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        
+        int[][][] dp = new int[m][n][maxMove + 1];
+        
+        for (int k = 1; k <= maxMove; ++k) {
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    for (int[] dir : directions) {
+                        int x = i + dir[0];
+                        int y = j + dir[1];
+                        if (x < 0 || x >= m || y < 0 || y >= n)
+                            dp[i][j][k] = (dp[i][j][k] + 1) % MOD;
+                        else
+                            dp[i][j][k] = (dp[i][j][k] + dp[x][y][k - 1]) % MOD;
+                    }
+                }
+            }
+        }
+        
+        return dp[startRow][startColumn][maxMove];
+    }
+}
+
+
+//Approach-3 (Space and time optimized bottom up)
+//T.C : O(m * n)
+//S.C : O(m * n)
+public class Solution {
+    int MOD = 1000000007;
+    int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        int[][] t = new int[50][50];
+        t[startRow][startColumn] = 1;
+        int result = 0;
+
+        for (int k = 1; k <= maxMove; ++k) {
+            int[][] temp = new int[50][50];
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+
+                    for (int[] dir : directions) {
+                        int x = i + dir[0];
+                        int y = j + dir[1];
+                        if (x < 0 || x >= m || y < 0 || y >= n) {
+                            result = (result + t[i][j]) % MOD;
+                        } else {
+                            temp[x][y] = (temp[x][y] + t[i][j]) % MOD;
+                        }
+                    }
+                }
+            }
+            t = Arrays.copyOf(temp, temp.length);
+        }
+
+        return result;
+    }
+}
