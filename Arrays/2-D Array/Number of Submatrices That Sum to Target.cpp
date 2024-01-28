27920d57d02df9cda8cfbf7cf7e81e1c78b32429
MAZHAR <iiestshibpur605@gmail.com>
2024-01-28 11:57:01 +0530

    Added JAVA code

--- Arrays/2-D Array/Number of Submatrices That Sum to Target.cpp
+++ Arrays/2-D Array/Number of Submatrices That Sum to Target.cpp
@@ -144,3 +144,42 @@ public class Solution {
 //Approach-2 (Using prefix sum)
 //T.C : O(n^2 * m)
 //S.C : O(m)
+public class Solution {
+    public int numSubmatrixSumTarget(int[][] matrix, int target) {
+        int rows = matrix.length; // m
+        int cols = matrix[0].length; // n
+
+        // First take the cumulative sum row-wise
+        for (int row = 0; row < rows; row++) {
+            for (int col = 1; col < cols; col++) {
+                matrix[row][col] += matrix[row][col - 1];
+            }
+        }
+
+        // Now, find the "No. of subarrays with sum k" in the downward direction
+        int result = 0;
+        for (int startCol = 0; startCol < cols; startCol++) {
+            for (int currCol = startCol; currCol < cols; currCol++) {
+                // We need to find all submatrices sum
+
+                // Now comes the concept of "No. of subarrays with sum k"
+                Map<Integer, Integer> map = new HashMap<>();
+                map.put(0, 1);
+                int sum = 0;
+
+                // Go downwards row-wise
+                for (int row = 0; row < rows; row++) {
+                    sum += matrix[row][currCol] - (startCol > 0 ? matrix[row][startCol - 1] : 0);
+
+                    if (map.containsKey(sum - target)) {
+                        result += map.get(sum - target);
+                    }
+
+                    map.put(sum, map.getOrDefault(sum, 0) + 1);
+                }
+            }
+        }
+
+        return result;
+    }
+}
