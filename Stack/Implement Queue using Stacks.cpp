07db3e6ca3b00a70ae63b8cc55b196367e60f939
MAZHAR <iiestshibpur605@gmail.com>
2024-01-29 06:52:31 +0530

    Added JAVA code also

--- Stack/Implement Queue using Stacks.cpp
+++ Stack/Implement Queue using Stacks.cpp
@@ -1,3 +1,4 @@
+/*                                             SCROLL BELOW FOR JAVA CODE ALSO                                         */
 /*
       MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=V1vqtGW0X0Y
       Company Tags                : Google, Amazon, Microsoft, Meta 
@@ -5,6 +6,10 @@
       GfG Link                    : https://practice.geeksforgeeks.org/problems/queue-using-stack/1
 */
 
+
+/************************************************************************ C++ ************************************************************************/
+//T.C : push - O(1), pop - Amortized O(1) , peek() - O(1), empty() - O(1)
+//S.C : O(n)
 class MyQueue {
 public:
     
@@ -51,3 +56,51 @@ public:
         return false;
     }
 };
+
+
+/************************************************************************ JAVA ************************************************************************/
+//T.C : push - O(1), pop - Amortized O(1) , peek() - O(1), empty() - O(1)
+//S.C : O(n)
+class MyQueue {
+    private Stack<Integer> input;
+    private Stack<Integer> output;
+    private int peekEl;
+
+    public MyQueue() {
+        input = new Stack<>();
+        output = new Stack<>();
+        peekEl = -1;
+    }
+
+    public void push(int x) {
+        if (input.empty()) {
+            peekEl = x;
+        }
+        input.push(x);
+    }
+
+    // Amortized O(1)
+    public int pop() {
+        if (output.empty()) {
+            // Transfer elements from input to output (O(n))
+            while (!input.empty()) {
+                output.push(input.pop());
+            }
+        }
+
+        int val = output.pop();
+        return val;
+    }
+
+    public int peek() {
+        if (output.empty()) {
+            return peekEl;
+        }
+
+        return output.peek();
+    }
+
+    public boolean empty() {
+        return input.empty() && output.empty();
+    }
+}
