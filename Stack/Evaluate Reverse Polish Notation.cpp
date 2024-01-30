9533413d4ac25add42cee2b53179f42a95ee0e99
MAZHAR <iiestshibpur605@gmail.com>
2024-01-30 08:06:19 +0530

    Added JAVA code

--- Stack/Evaluate Reverse Polish Notation.cpp
+++ Stack/Evaluate Reverse Polish Notation.cpp
@@ -1,10 +1,14 @@
+/*                                                        SCROLL DOWN FOR JAVA CODE ALSO                                   */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=BM-bu4tJz7U
     Company Tags                : Apple, Microsoft, Flipkart, Amazon, LinkedIn
     Leetcode Link               : https://leetcode.com/problems/evaluate-reverse-polish-notation/
 */
 
-//Basic approach
+/********************************************************************* C++ ************************************************************************/
+//Approach-1 (Basic stack approach)
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
 public:
     int operate(int &a, int &b, const string &s) {
@@ -77,3 +81,80 @@ public:
         return st.top();
     }
 };
+
+
+
+/********************************************************************* JAVA ************************************************************************/
+//Approach-1 (Basic stack approach)
+//T.C : O(n)
+//S.C : O(n)
+public class Solution {
+    public int operate(int a, int b, String s) {
+        if (s.equals("+"))
+            return a + b;
+
+        if (s.equals("-"))
+            return a - b;
+
+        if (s.equals("*"))
+            return (int) ((long) a * (long) b);
+
+        if (s.equals("/"))
+            return a / b;
+
+        return -1;
+    }
+
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        int result = 0;
+
+        for (String s : tokens) {
+            if (s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/")) {
+                int b = st.pop();
+                int a = st.pop();
+
+                result = operate(a, b, s);
+                st.push(result);
+            } else {
+                st.push(Integer.parseInt(s));
+            }
+        }
+
+        return st.pop();
+    }
+}
+
+/*
+    Using fancy Lambda on unordered_map
+    For my fancy STL collection with easy exlanation with examples, see the link below -
+    https://github.com/MAZHARMIK/Cpp-STL-Quick-Help/blob/main/README.md
+*/
+//Approach-2 (Using map and lambda)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        int result = 0;
+
+        Map<String, BiFunction<Integer, Integer, Integer>> mp = new HashMap<>();
+        mp.put("+", (a, b) -> a + b);
+        mp.put("-", (a, b) -> a - b);
+        mp.put("*", (a, b) -> (int)((long)a * (long)b));
+        mp.put("/", (a, b) -> a / b);
+
+        for (String s : tokens) {
+            if (mp.containsKey(s)) {
+                int b = st.pop();
+                int a = st.pop();
+
+                result = mp.get(s).apply(a, b);
+                st.push(result);
+            } else {
+                st.push(Integer.parseInt(s));
+            }
+        }
+        return st.pop();
+    }
+}
