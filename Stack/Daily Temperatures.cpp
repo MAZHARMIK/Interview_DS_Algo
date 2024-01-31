6b9b8dd1bdeffb9cdaf0d71fb7c9c53b72a12b7f
MAZHAR <iiestshibpur605@gmail.com>
2024-01-31 08:03:08 +0530

    Update Daily Temperatures.cpp

--- Stack/Daily Temperatures.cpp
+++ Stack/Daily Temperatures.cpp
@@ -1,9 +1,15 @@
+/**                                                SCROLL BELOW TO SEE JAVA CODE ALSO                                        **/
+
 /*
     MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=ZhZEmGv-sOY
     Company Tags                : Google, Meta, Tokopedia
     Leetcode Link               : https://leetcode.com/problems/daily-temperatures/
 */
 
+
+/******************************************************************** C++ ********************************************************************/
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
@@ -30,3 +36,34 @@ public:
         
     }
 };
+
+
+
+/******************************************************************** JAVA ********************************************************************/
+//T.C : O(n)
+//S.C :O(n)
+public class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        Stack<Integer> st = new Stack<>();
+        
+        int[] result = new int[n];
+        
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && temperatures[i] >= temperatures[st.peek()]) {
+                st.pop();
+            }
+            
+            if (st.isEmpty()) {
+                result[i] = 0;
+            } else {
+                result[i] = st.peek() - i; // days
+            }
+            
+            st.push(i);
+        }
+        
+        return result;
+    }
+}
+
