1cc679018e4d14fc666ec2aee6684d06ff63b59f
MAZHAR <iiestshibpur605@gmail.com>
2024-02-01 16:17:23 +0530

    Merge Sort C++ & Java

---
+++ Recursion/Merge Sort.cpp
@@ -0,0 +1,140 @@
+/*                                                        Scroll Down for JAVA code also                                           */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Paytm, Amazon, Microsoft, Snapdeal, Oracle, Goldman Sachs, Wipro, Qualcomm, Boomerang Commerce, Grofers, Target Corporation, Medlife
+    GfG Link                    : https://www.geeksforgeeks.org/problems/merge-sort/1
+*/
+
+
+/*********************************************************************** C++ ************************************************************************************/
+//T.C : O(nlogn)
+//S.C : O(n)
+class Solution
+{
+    public:
+    void merge(int arr[], int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // Create temporary arrays to store the two halves
+        int L[n1], R[n2];
+
+        // Copy data to temporary arrays L[] and R[]
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[m + 1 + j];
+
+        // Merge the temporary arrays back into arr[l..r]
+        int i = 0; // Initial index of first subarray
+        int j = 0; // Initial index of second subarray
+        int k = l; // Initial index of merged subarray
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy the remaining elements of L[], if there are any
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy the remaining elements of R[], if there are any
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    
+    public:
+    void mergeSort(int arr[], int l, int r) {
+        
+        if(l >= r) {
+            return;
+        }
+        
+        int mid = l + (r-l)/2;
+        
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid+1, r);
+        
+        merge(arr, l, mid ,r);
+        
+    }
+};
+
+
+
+/*********************************************************************** JAVA ************************************************************************************/
+//T.C : O(nlogn)
+//S.C : O(n)
+class Solution {
+    void merge(int arr[], int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // Create temporary arrays to store the two halves
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        // Copy data to temporary arrays L[] and R[]
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[m + 1 + j];
+
+        // Merge the temporary arrays back into arr[l..r]
+        int i = 0; // Initial index of first subarray
+        int j = 0; // Initial index of second subarray
+        int k = l; // Initial index of merged subarray
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy the remaining elements of L[], if there are any
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy the remaining elements of R[], if there are any
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    void mergeSort(int arr[], int l, int r) {
+
+        if (l >= r) {
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
+
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+
+        merge(arr, l, mid, r);
+    }
+}
