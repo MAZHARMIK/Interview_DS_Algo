07454842f3b26e8c2b9f955dafb9e9a647307f6c
MAZHAR <iiestshibpur605@gmail.com>
2024-02-05 11:14:55 +0530

    Added YouTube Link

--- strings/Easy Tagged/First Unique Character in a String.cpp
+++ strings/Easy Tagged/First Unique Character in a String.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=vqLiP9cdDLA
     Company Tags                : Amazon
     Leetcode Link               : https://leetcode.com/problems/first-unique-character-in-a-string/
 */
