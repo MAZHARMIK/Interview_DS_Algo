c36628d8e3bbcaf405d5dda9a977da89b90ee710
MAZHAR <iiestshibpur605@gmail.com>
2024-02-05 12:26:52 +0530

    Sorted insert for circular linked list

---
+++ Linked List/Sorted insert for circular linked list.cpp
@@ -0,0 +1,83 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Zoho, Amazon, Microsoft
+    GfG Link                    : https://www.geeksforgeeks.org/problems/sorted-insert-for-circular-linked-list/1
+*/
+
+/****************************************************************** C++ ******************************************************************/
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public:
+    Node *sortedInsert(Node* head, int data) {
+       Node *newNode = new Node(data);
+
+        // Case 1: Insert into an empty list
+        if (head == NULL)
+        {
+            newNode->next = newNode;
+            return newNode;
+        }
+
+        Node *current = head;
+
+        // Case 2: Insert at the beginning
+        if (data < head->data) {
+            while (current->next != head) {
+                current = current->next;
+            }
+            current->next = newNode;
+            newNode->next = head;
+            return newNode;
+        }
+
+        // Case 3: Insert in the middle or at the end
+        while (current->next != head && current->next->data < data) {
+            current = current->next;
+        }
+
+        newNode->next = current->next;
+        current->next = newNode;
+
+        return head;
+    }
+};
+
+
+
+/****************************************************************** C++ ******************************************************************/
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public Node sortedInsert(Node head, int data) {
+        Node newNode = new Node(data);
+
+        // Case 1: Insert into an empty list
+        if (head == null) {
+            newNode.next = newNode;
+            return newNode;
+        }
+
+        Node current = head;
+
+        // Case 2: Insert at the beginning
+        if (data < head.data) {
+            while (current.next != head) {
+                current = current.next;
+            }
+            current.next = newNode;
+            newNode.next = head;
+            return newNode;
+        }
+
+        // Case 3: Insert in the middle or at the end
+        while (current.next != head && current.next.data < data) {
+            current = current.next;
+        }
+
+        newNode.next = current.next;
+        current.next = newNode;
+
+        return head;
+    }
+}
