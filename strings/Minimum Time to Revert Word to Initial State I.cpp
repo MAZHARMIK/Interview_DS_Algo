c4c9926990a49e49623277443b37edcb59871bfa
MAZHAR <iiestshibpur605@gmail.com>
2024-02-06 17:34:29 +0530

    Minimum Time to Revert Word to Initial State I

---
+++ strings/Minimum Time to Revert Word to Initial State I.cpp
@@ -0,0 +1,152 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : will update soon
+    Leetcode Link               : https://leetcode.com/problems/minimum-time-to-revert-word-to-initial-state-i
+*/
+
+/***************************************************************** C++ *****************************************************************/
+//Approach-1 (Brute Force - Baby Steps)
+//T.C : O(n^2)
+//S.C : O(1)
+class Solution {
+public:
+    
+    bool check(string &word, int i) {
+        int j = 0;
+        
+        while(i < word.length()) {
+            if(word[i] == word[j]) {
+                i++;
+                j++;
+            } else {
+                break;
+            }
+        }
+        
+        return i == word.length();
+    }
+    
+    int minimumTimeToInitialState(string word, int k) {
+        int n = word.length();
+        
+        int count = 1;
+        int i = k;
+        
+        while(i < n) {
+            
+            if(check(word, i)) {
+                break;
+            }
+            
+            count++;
+            i += k;
+            
+        }
+        
+        return count;
+        
+    }
+};
+
+
+//Approach-2 (Brute Force - Smartly Clean Approach)
+//T.C : O(n^2)
+//S.C : O(1)
+class Solution {
+public:
+    
+    bool check(string &word, int i, int n) {
+        return word.substr(i, n-i) == word.substr(0, n-i);
+    }
+    
+    int minimumTimeToInitialState(string word, int k) {
+        int n = word.length();
+        
+        int count = 1;
+        int i = k;
+        
+        while(i < n) {
+            
+            if(check(word, i, n)) {
+                break;
+            }
+            
+            count++;
+            i += k;
+            
+        }
+        
+        return count;
+        
+    }
+};
+
+
+
+/***************************************************************** JAVA *****************************************************************/
+//Approach-1 (Brute Force - Baby Steps)
+//T.C : O(n^2)
+//S.C : O(1)
+public class Solution {
+    private boolean check(String word, int i) {
+        int j = 0;
+
+        while (i < word.length()) {
+            if (word.charAt(i) == word.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                break;
+            }
+        }
+
+        return i == word.length();
+    }
+
+    public int minimumTimeToInitialState(String word, int k) {
+        int n = word.length();
+
+        int count = 1;
+        int i = k;
+
+        while (i < n) {
+            if (check(word, i)) {
+                break;
+            }
+
+            count++;
+            i += k;
+        }
+
+        return count;
+    }
+}
+
+
+
+//Approach-2 (Brute Force - Smartly Clean Approach)
+//T.C : O(n^2)
+//S.C : O(1)
+public class Solution {
+    private boolean check(String word, int i, int n) {
+        return word.substring(i, n).equals(word.substring(0, n - i));
+    }
+
+    public int minimumTimeToInitialState(String word, int k) {
+        int n = word.length();
+
+        int count = 1;
+        int i = k;
+
+        while (i < n) {
+            if (check(word, i, n)) {
+                break;
+            }
+
+            count++;
+            i += k;
+        }
+
+        return count;
+    }
+}
