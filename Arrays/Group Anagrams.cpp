099b5c20e195a6d0c940e651a9545ffc9b0b9c79
MAZHAR <iiestshibpur605@gmail.com>
2024-02-06 07:15:08 +0530

    Added JAVA code also

--- Arrays/Group Anagrams.cpp
+++ Arrays/Group Anagrams.cpp
@@ -1,3 +1,4 @@
+/*                            Scroll below to see JAVA code also                                  */
 /*
   MY YOUTUBE VIDEO ON THIS Qn : Using Sortng     - https://www.youtube.com/watch?v=--k5-3EOObs&t=2s
                                 Without Sorting  - https://www.youtube.com/watch?v=TNe3gF4r128&t=1s
@@ -7,11 +8,10 @@
   GfG Link                    : https://practice.geeksforgeeks.org/problems/print-anagrams-together/1
 */
 
-
-/*
-Approach-1
-Time : O(n*klog(k))  (n = size of input, k = maximum length of a string in the input vector)
-*/
+/************************************************************** C++ **************************************************************/
+//Approach-1 (Using Sorting)
+//T.C : O(n*klog(k))  (n = size of input, k = maximum length of a string in the input vector)
+//S.C : O(n*k)
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
@@ -33,10 +33,9 @@ public:
 };
 
 
-/*
-Approach-2
-Time : O(n*k)  (n = size of input, k = maximum length of a string in the input vector)
-*/
+//Approach-2
+//T.C : O(n*k)  (n = size of input, k = maximum length of a string in the input vector)
+//S.C : O(n*k)
 class Solution {
 public:
     
@@ -77,3 +76,68 @@ public:
         
     }
 };
+
+
+/************************************************************** JAVA **************************************************************/
+//Approach-1 (Using Sorting)
+//T.C : O(n*klog(k))  (n = size of input, k = maximum length of a string in the input vector)
+//S.C : O(n*k)
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+
+            map.get(sortedStr).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
+
+
+//Approach-2
+//T.C : O(n*k)  (n = size of input, k = maximum length of a string in the input vector)
+//S.C : O(n*k)
+class Solution {
+    private String generate(String s) {
+        int[] count = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            count[ch - 'a']++;
+        }
+
+        StringBuilder newS = new StringBuilder();
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > 0) {
+                newS.append(String.valueOf((char) (i + 'a')).repeat(count[i]));
+            }
+        }
+
+        return newS.toString();
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            String newS = generate(s);
+
+            if (!map.containsKey(newS)) {
+                map.put(newS, new ArrayList<>());
+            }
+
+            map.get(newS).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
