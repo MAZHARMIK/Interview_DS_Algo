6bd07f36b33384586af253870125034bb57093ae
MAZHAR <iiestshibpur605@gmail.com>
2024-02-17 07:18:35 +0530

    Update Flatten BST to sorted list.cpp

--- Recursion/Flatten BST to sorted list.cpp
+++ Recursion/Flatten BST to sorted list.cpp
@@ -5,8 +5,8 @@
 */
 
 /********************************************************************* C++ ***********************************************************************************/
-//T.C : O(n)
-//S.C : AUxiliary Space is O(1) and Stack Space due to recursion is O(n)
+//T.C : O(n^2)
+//S.C : Auxiliary Space is O(1) and Stack Space due to recursion is O(n)
 class Solution
 {
 public:
@@ -34,6 +34,38 @@ public:
     }
 };
 
+//Approach-2
+//T.C : O(n)
+//S.C : Auxiliary Space is O(1) and Stack Space due to recursion is O(n)
+class Solution
+{
+public:
+    Node* prev = NULL;
+    Node *flattenBST(Node *root) {
+        
+        if(!root) {
+            return NULL;
+        }
+        
+        Node* head = flattenBST(root->left); //Trust
+        root->left = NULL;
+        if(prev) {
+            prev->right = root;
+            prev = root;
+        } else {
+            prev = root;
+        }
+        root->right = flattenBST(root->right); //Trust
+
+        if(head == NULL) {
+            return root;
+        }
+
+        return head;
+        
+    }
+};
+
 
 /********************************************************************* JAVA ***********************************************************************************/
 //T.C : O(n)
@@ -57,6 +89,38 @@ class Solution {
         } else {
             // If head is null, root becomes the head
             head = root;
+        }
+
+        return head;
+    }
+}
+
+
+//Approach-2
+//T.C : O(n)
+//S.C : Auxiliary Space is O(1) and Stack Space due to recursion is O(n)
+class Solution {
+    private Node prev = null;
+
+    public Node flattenBST(Node root) {
+        if (root == null) {
+            return null;
+        }
+
+        Node head = flattenBST(root.left); // Trust
+        root.left = null;
+        
+        if (prev != null) {
+            prev.right = root;
+            prev = root;
+        } else {
+            prev = root;
+        }
+        
+        root.right = flattenBST(root.right); // Trust
+
+        if (head == null) {
+            return root;
         }
 
         return head;
