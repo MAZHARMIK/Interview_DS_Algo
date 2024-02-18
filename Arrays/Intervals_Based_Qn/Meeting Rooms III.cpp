f209a48d46aec12363efbf3a561b5158057df964
MAZHAR <iiestshibpur605@gmail.com>
2024-02-18 09:33:40 +0530

    Meeting Rooms III C++ and JAVA

---
+++ Arrays/Intervals_Based_Qn/Meeting Rooms III.cpp
@@ -0,0 +1,243 @@
+/*                             Scroll Down to see JAVA code also                               */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : GOOGLE
+    Leetcode Link               : https://leetcode.com/problems/meeting-rooms-iii/description/?envType=daily-question&envId=2024-02-18
+*/
+
+/************************************************************ C++ ************************************************************************/
+//Approach-1 (Brute Force - Do as said)
+//T.C : O(mlogm +m*n) , where Let n = number of rooms, m =  number of meetings
+//S.C : O(n)
+class Solution {
+public:
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        int m = meetings.size();
+
+        sort(begin(meetings), end(meetings)); //sort by starting time of meetings
+
+        vector<int> roomsUsedCount(n, 0); //Each room is used 0 times in the beginning
+        vector<long long> lastAvailableAt(n, 0); //Each room will be last available at
+        
+
+        for(vector<int>& meet : meetings) {
+            int start  = meet[0];
+            int end    = meet[1];
+            bool found = false;
+
+            long long EarlyEndRoomTime = LLONG_MAX;
+            int EarlyEndRoom     = 0;
+
+            //Find the first available meeting room
+            for(int room = 0; room < n; room++) {
+                if(lastAvailableAt[room] <= start) {
+                    found = true;
+                    lastAvailableAt[room] = end;
+                    roomsUsedCount[room]++;
+                    break;
+                }
+
+                if(lastAvailableAt[room] < EarlyEndRoomTime) {
+                    EarlyEndRoom = room;
+                    EarlyEndRoomTime = lastAvailableAt[room];
+                }
+            }
+
+            if(!found) {
+                lastAvailableAt[EarlyEndRoom] += (end - start);
+                roomsUsedCount[EarlyEndRoom]++;
+            }
+
+        }
+
+        int resultRoom = -1;
+        int maxUse     = 0;  
+        for(int room = 0; room < n; room++) {
+            if(roomsUsedCount[room] > maxUse) {
+                maxUse = roomsUsedCount[room];
+                resultRoom = room;
+            }
+        }
+
+        return resultRoom;
+    }
+};
+
+
+//Approach-2 (Use priority Queue to find the first available meeting room)
+//T.C : O(mlogm + m*log(n)) , where Let n = number of rooms, m =  number of meetings
+//S.C : O(n)
+class Solution {
+public:
+    typedef pair<long long, int> P;
+
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        int m = meetings.size();
+
+        sort(begin(meetings), end(meetings)); //sort by starting time of meetings
+
+        vector<int> roomsUsedCount(n, 0); //Each room is used 0 times in the beginning
+
+        priority_queue<P, vector<P>, greater<P>> usedRooms;
+        //To store {earliest room empty time, room No.}
+        
+        priority_queue<int, vector<int>, greater<int>> unusedRooms;
+        //To store rooms that are used
+        for(int room = 0; room < n; room++) {
+            unusedRooms.push(room); //All rooms are unused in the beginning
+        }
+
+        for(vector<int>& meet : meetings) {
+            int start  = meet[0];
+            int end    = meet[1];
+
+            //First see, by this time, which rooms can be empty now
+            //And move them to unusedRooms
+            while(!usedRooms.empty() && usedRooms.top().first <= start) {
+                int room = usedRooms.top().second;
+                usedRooms.pop();
+                unusedRooms.push(room);
+            }
+
+            if(!unusedRooms.empty()) {
+                int room = unusedRooms.top();
+                unusedRooms.pop();
+                usedRooms.push({end, room});
+                roomsUsedCount[room]++;
+            } else { //We don't have any room available now. Pick earliest end room
+                int room          = usedRooms.top().second;
+                long long endTime = usedRooms.top().first;
+                usedRooms.pop();
+                usedRooms.push({endTime + (end-start), room});
+                roomsUsedCount[room]++;
+            }
+
+        }
+
+        int resultRoom = -1;
+        int maxUse     = 0;  
+        for(int room = 0; room < n; room++) {
+            if(roomsUsedCount[room] > maxUse) {
+                maxUse = roomsUsedCount[room];
+                resultRoom = room;
+            }
+        }
+
+        return resultRoom;
+    }
+};
+
+
+
+/************************************************************ JAVA ************************************************************************/
+//Approach-1 (Brute Force - Do as said)
+//T.C : O(mlogm +m*n) , where Let n = number of rooms, m =  number of meetings
+//S.C : O(n)
+class Solution {
+    public int mostBooked(int n, int[][] meetings) {
+        // sort by starting time of meetings
+        Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
+
+        // Each room is used 0 times in the beginning
+        int[] roomsUsedCount = new int[n];
+        // Each room will be last available at
+        long[] lastAvailableAt = new long[n];
+
+        for (int[] meet : meetings) {
+            int start = meet[0];
+            int end = meet[1];
+            boolean found = false;
+
+            long earlyEndRoomTime = Long.MAX_VALUE;
+            int earlyEndRoom = 0;
+
+            // Find the first available meeting room
+            for (int room = 0; room < n; room++) {
+                if (lastAvailableAt[room] <= start) {
+                    found = true;
+                    lastAvailableAt[room] = end;
+                    roomsUsedCount[room]++;
+                    break;
+                }
+
+                if (lastAvailableAt[room] < earlyEndRoomTime) {
+                    earlyEndRoom = room;
+                    earlyEndRoomTime = lastAvailableAt[room];
+                }
+            }
+
+            if (!found) {
+                lastAvailableAt[earlyEndRoom] += (end - start);
+                roomsUsedCount[earlyEndRoom]++;
+            }
+        }
+
+        int resultRoom = -1;
+        int maxUse = 0;
+        for (int room = 0; room < n; room++) {
+            if (roomsUsedCount[room] > maxUse) {
+                maxUse = roomsUsedCount[room];
+                resultRoom = room;
+            }
+        }
+
+        return resultRoom;
+    }
+}
+
+
+//Approach-2 (Use priority Queue to find the first available meeting room)
+//T.C : O(mlogm + m*log(n)) , where Let n = number of rooms, m =  number of meetings
+//S.C : O(n)
+class Solution {
+    public int mostBooked(int n, int[][] meetings) {
+        // sort by starting time of meetings
+        Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
+
+        // Each room is used 0 times in the beginning
+        int[] roomsUsedCount = new int[n];
+
+        // To store {earliest room empty time, room No.}
+        PriorityQueue<long[]> usedRooms = new PriorityQueue<>((a, b) -> Long.compare(a[0], b[0]));
+
+        // To store rooms that are used
+        PriorityQueue<Integer> unusedRooms = new PriorityQueue<>();
+        for (int room = 0; room < n; room++) {
+            unusedRooms.add(room); // All rooms are unused in the beginning
+        }
+
+        for (int[] meet : meetings) {
+            int start = meet[0];
+            int end = meet[1];
+
+            // First see, by this time, which rooms can be empty now
+            // And move them to unusedRooms
+            while (!usedRooms.isEmpty() && usedRooms.peek()[0] <= start) {
+                int room = (int) usedRooms.poll()[1];
+                unusedRooms.add(room);
+            }
+
+            if (!unusedRooms.isEmpty()) {
+                int room = unusedRooms.poll();
+                usedRooms.add(new long[]{end, room});
+                roomsUsedCount[room]++;
+            } else { // We don't have any room available now. Pick the earliest end room
+                int room = (int) usedRooms.peek()[1];
+                long endTime = usedRooms.poll()[0];
+                usedRooms.add(new long[]{endTime + (end - start), room});
+                roomsUsedCount[room]++;
+            }
+        }
+
+        int resultRoom = -1;
+        int maxUse = 0;
+        for (int room = 0; room < n; room++) {
+            if (roomsUsedCount[room] > maxUse) {
+                maxUse = roomsUsedCount[room];
+                resultRoom = room;
+            }
+        }
+
+        return resultRoom;
+    }
+}
