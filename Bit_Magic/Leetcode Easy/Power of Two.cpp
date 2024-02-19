0aae7ff5ada62ad425e3692583f323d1947a3248
MAZHAR <iiestshibpur605@gmail.com>
2024-02-19 06:21:06 +0530

    Create Power of Two.cpp

---
+++ Bit_Magic/Leetcode Easy/Power of Two.cpp
@@ -0,0 +1,84 @@
+/*                           Scroll down to see JAVA code also               */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Google, Aodbe
+    Leetcode Link               : https://leetcode.com/problems/power-of-two/
+*/
+
+
+/************************************************************************ C++ ************************************************************************/
+//Approach-1 (Using normal division) - Can also wite iterative version
+//T.C : O(log(n))
+//S.C : O(1)
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n <= 0)
+            return false;
+        if(n == 1)
+            return true;
+        
+        return n%2 == 0 && isPowerOfTwo(n/2);
+    }
+};
+
+//Approach-2 (Using bit magic)
+//T.C : O(1)
+//S.C : O(1)
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        return (n > 0) && ((n & (n - 1)) == 0);
+    }
+};
+
+//Approach-2 (__builtin_popcount)
+//T.C : O(1)
+//S.C : O(1)
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n <= 0)
+            return false;
+        
+        return __builtin_popcount(n) == 1;
+    }
+};
+
+
+
+/************************************************************************ C++ ************************************************************************/
+// Approach-1 (Using normal division) - Can also write iterative version
+// T.C : O(log(n))
+// S.C : O(1)
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0)
+            return false;
+        if (n == 1)
+            return true;
+
+        return n % 2 == 0 && isPowerOfTwo(n / 2);
+    }
+}
+
+// Approach-2 (Using bit magic)
+// T.C : O(1)
+// S.C : O(1)
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        return (n > 0) && ((n & (n - 1)) == 0);
+    }
+}
+
+// Approach-3 (__builtin_popcount)
+// T.C : O(1)
+// S.C : O(1)
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0)
+            return false;
+
+        return Integer.bitCount(n) == 1;
+    }
+}
