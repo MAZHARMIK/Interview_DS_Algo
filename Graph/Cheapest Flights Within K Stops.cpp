6bdbb1686e4cfaead3e64fd53195ee777509d246
MAZHAR <iiestshibpur605@gmail.com>
2024-02-23 06:45:30 +0530

    Added JAVA code also

--- Graph/Cheapest Flights Within K Stops.cpp
+++ Graph/Cheapest Flights Within K Stops.cpp
@@ -5,7 +5,10 @@
       GfG Link                    : https://practice.geeksforgeeks.org/problems/cheapest-flights-within-k-stops/1
 */
 
+/****************************************************************** C++ ******************************************************************/
 //Approach-1 (BFS)
+//T.C : O(V+E) - BFS traversal of Graph
+//S.C : O(V+E)
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
@@ -56,4 +59,53 @@ public:
 };
 
 
-//Other Approaches coming soon with youTube video link
+/****************************************************************** JAVA ******************************************************************/
+//Approach-1 (BFS)
+//T.C : O(V+E) - BFS traversal of Graph
+//S.C : O(V+E)
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int[] distance = new int[n];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+
+        Map<Integer, List<int[]>> adj = new HashMap<>();
+
+        for (int[] flight : flights) {
+            int u = flight[0];
+            int v = flight[1];
+            int cost = flight[2];
+
+            adj.computeIfAbsent(u, key -> new ArrayList<>()).add(new int[]{v, cost});
+        }
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{src, 0});
+        distance[src] = 0;
+
+        int level = 0;
+
+        while (!queue.isEmpty() && level <= k) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                int[] current = queue.poll();
+                int u = current[0];
+                int d = current[1];
+
+                List<int[]> neighbors = adj.getOrDefault(u, Collections.emptyList());
+                for (int[] neighbor : neighbors) {
+                    int v = neighbor[0];
+                    int cost = neighbor[1];
+
+                    if (distance[v] > d + cost) {
+                        distance[v] = d + cost;
+                        queue.offer(new int[]{v, d + cost});
+                    }
+                }
+            }
+            level++;
+        }
+
+        return distance[dst] == Integer.MAX_VALUE ? -1 : distance[dst];
+    }
+}
