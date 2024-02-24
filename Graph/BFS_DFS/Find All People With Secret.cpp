364a0b1e965f2445c95bc0d8e21297256ff234a7
MAZHAR <iiestshibpur605@gmail.com>
2024-02-24 15:32:39 +0530

    Added YouTube Link

--- Graph/BFS_DFS/Find All People With Secret.cpp
+++ Graph/BFS_DFS/Find All People With Secret.cpp
@@ -1,6 +1,6 @@
 /*                       Scroll down to see JAVA code also             */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=hxEWwnJJKcY
     Company Tags                : GOOGLE
     Leetcode Link               : https://leetcode.com/problems/find-all-people-with-secret/
 */
