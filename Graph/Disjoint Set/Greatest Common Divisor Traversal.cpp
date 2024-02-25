de9ce08e3c58e8b2735bb0373ea61bd28c40b6b1
MAZHAR <iiestshibpur605@gmail.com>
2024-02-25 13:59:58 +0530

    Added YouTube Link

--- Graph/Disjoint Set/Greatest Common Divisor Traversal.cpp
+++ Graph/Disjoint Set/Greatest Common Divisor Traversal.cpp
@@ -1,5 +1,5 @@
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=TI-xIXH4EnE
     Company Tags                : GOOGLE
     Leetcode Link               : https://leetcode.com/problems/greatest-common-divisor-traversal/
 */
