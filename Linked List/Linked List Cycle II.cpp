97d55d02f91c8b1160bb1e289ab896700b860fef
MAZHAR <iiestshibpur605@gmail.com>
2024-03-06 10:10:53 +0530

    Added JAVA code

--- Linked List/Linked List Cycle II.cpp
+++ Linked List/Linked List Cycle II.cpp
@@ -1,3 +1,4 @@
+/*                         Scroll below to see JAVA code also         */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=vlrxs-LPni4
     Company Tags  : Microsoft, Paytm, VMWare, Accolite, Amazon, OYO Rooms, Samsung
@@ -6,6 +7,10 @@
     Leetcode Link : https://leetcode.com/problems/linked-list-cycle-ii/
 */
 
+
+/********************************************************** C++ **********************************************************/
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
@@ -33,3 +38,39 @@ public:
         return entry;
     }
 };
+
+
+
+
+/********************************************************** JAVA **********************************************************/
+//T.C : O(n)
+//S.C : O(1)
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        if (fast != slow) {
+            return null;
+        }
+
+        ListNode entry = head;
+        while (entry != slow) {
+            entry = entry.next;
+            slow = slow.next;
+        }
+        return entry;
+    }
+}
