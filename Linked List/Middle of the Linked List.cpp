abf23496d946c6e6a0bd267fb15e2dcbffd96d21
MAZHAR <iiestshibpur605@gmail.com>
2024-03-07 08:33:50 +0530

    Added JAVA code also

--- Linked List/Middle of the Linked List.cpp
+++ Linked List/Middle of the Linked List.cpp
@@ -1,3 +1,4 @@
+/*         Scroll down to see JAVA code also            */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=I8h_hI_BF3Y
     Company Tags                : Adobe, Amazon, Flipkart, GE, Hike, IgniteWorld, MAQ Software, Microsoft, Morgan Stanley, 
@@ -6,6 +7,10 @@
     GfG Link                    : https://practice.geeksforgeeks.org/problems/finding-middle-element-in-a-linked-list/1
 */
 
+
+/****************************************************** C++ ******************************************************/
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
@@ -20,3 +25,22 @@ public:
         return first;
     }
 };
+
+
+
+/****************************************************** JAVA ******************************************************/
+//T.C : O(n)
+//S.C : O(1)
+public class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode first = head;
+        ListNode second = head;
+
+        while (first != null && second != null && second.next != null) {
+            first = first.next;
+            second = second.next.next;
+        }
+
+        return first;
+    }
+}
