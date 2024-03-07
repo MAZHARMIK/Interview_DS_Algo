61f4fc9505e5ea891cc877d2288c8c542c7792b7
MAZHAR <iiestshibpur605@gmail.com>
2024-03-07 11:55:31 +0530

    Added JAVA code also

--- Arrays/Two Pointer/3Sum.cpp
+++ Arrays/Two Pointer/3Sum.cpp
@@ -1,3 +1,4 @@
+/*        Scroll down to see JAVA code also            */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=_cBWWebTVpI&list=PLpIkg8OmuX-K6A0sEPFxOSJh4_AjCGAFf&index=4
     Company Tags                : Amazon, Facebook, Google
@@ -5,6 +6,10 @@
     Leetcode Qn Link            : https://leetcode.com/problems/3sum/
 */
 
+
+/************************************************ C++ ************************************************/
+//T.C : O(n^2)
+//S.C : O(1)
 class Solution {
 public:
     void twoSum(vector<int>& nums, int k, vector<vector<int>>& result, int target) {
@@ -39,3 +44,51 @@ public:
         return result;
     }
 };
+
+
+/************************************************ JAVA ************************************************/
+//T.C : O(n^2)
+//S.C : O(1)
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums.length < 3) {
+            return new ArrayList<>();
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            twoSum(nums, i + 1, result, -nums[i]);
+        }
+
+        return result;
+    }
+
+    private void twoSum(int[] nums, int k, List<List<Integer>> result, int target) {
+        int i = k, j = nums.length - 1;
+
+        while (i < j) {
+            if (nums[i] + nums[j] > target) {
+                j--;
+            } else if (nums[i] + nums[j] < target) {
+                i++;
+            } else {
+                result.add(Arrays.asList(-target, nums[i], nums[j]));
+
+                while (i < j && nums[i] == nums[i + 1]) {
+                    i++;
+                }
+                while (i < j && nums[j] == nums[j - 1]) {
+                    j--;
+                }
+
+                i++;
+                j--;
+            }
+        }
+    }
+}
