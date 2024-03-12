abb7ae7ac745d0506df9cef69a5676029cc81b07
MAZHAR <iiestshibpur605@gmail.com>
2024-03-12 10:23:47 +0530

    Simplified JAVA code

--- Recursion/Regular Expression Matching.cpp
+++ Recursion/Regular Expression Matching.cpp
@@ -106,32 +106,37 @@ public class Solution {
 //T.C : O(m*n)
 //S.C : O(m*n)
 class Solution {
-    boolean[][] t;
-
-    public boolean isMatch(String text, String pattern) {
-        t = new boolean[text.length() + 1][pattern.length() + 1];
-        return solve(0, 0, text, pattern);
-    }
+    int[][] t;
 
     public boolean solve(int i, int j, String text, String pattern) {
-        if (t[i][j]) {
-            return true;
-        }
-        boolean ans;
         if (j == pattern.length()) {
-            ans = i == text.length();
-        } else {
-            boolean first_match = (i < text.length() &&
-                                   (pattern.charAt(j) == text.charAt(i) ||
-                                    pattern.charAt(j) == '.'));
-
-            if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') {
-                ans = (solve(i, j + 2, text, pattern) ||
-                       first_match && solve(i + 1, j, text, pattern));
-            } else {
-                ans = first_match && solve(i + 1, j + 1, text, pattern);
-            }
+            return i == text.length();
         }
-        return t[i][j] = ans;
+
+        if (t[i][j] != -1) {
+            return t[i][j] == 1;
+        }
+
+        boolean ans = false;
+
+        boolean first_match = (i < text.length() &&
+                (pattern.charAt(j) == text.charAt(i) || pattern.charAt(j) == '.'));
+
+        if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') {
+            ans = (solve(i, j + 2, text, pattern) ||
+                    (first_match && solve(i + 1, j, text, pattern)));
+        } else {
+            ans = first_match && solve(i + 1, j + 1, text, pattern);
+        }
+
+        return (t[i][j] = ans ? 1 : 0) == 1;
+    }
+
+    public boolean isMatch(String text, String pattern) {
+        t = new int[21][21];
+        for (int[] row : t) {
+            Arrays.fill(row, -1);
+        }
+        return solve(0, 0, text, pattern);
     }
 }
