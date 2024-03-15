457e5d6b99e1685003e0e8f13f92ba91de996544
MAZHAR <iiestshibpur605@gmail.com>
2024-03-15 07:33:48 +0530

    Added JAVA code also

--- Arrays/Product of Array Except Self.cpp
+++ Arrays/Product of Array Except Self.cpp
@@ -1,11 +1,14 @@
+/*         Scroll down to see JAVA code also             */
 /*
-    Company Tags  : Apple, LinkedIn, Amazon, Facebook, Microsoft
-    Leetcode Link : https://leetcode.com/problems/product-of-array-except-self/
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Apple, LinkedIn, Amazon, Facebook, Microsoft
+    Leetcode Link               : https://leetcode.com/problems/product-of-array-except-self/
 */
 
-/********
-Approach-1  -> Using Division (Time : O(n))
-*********/
+/****************************************************************** C++ ******************************************************************/
+//Approach-1 (Using Division)
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
@@ -37,10 +40,10 @@ public:
     }
 };
 
-/********
-Approach-2  -> Without division and in O(n) space (Time : O(n))
-*********/
 
+//Approach-2 (Using extra space)
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
@@ -67,11 +70,9 @@ public:
     }
 };
 
-/********
-Approach-3  -> constant space complexity (The output array does not count as extra space for the purpose of space complexity analysis.)
-Time : O(n)
-*********/
-
+//Approach-3 (Using constant space)
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
@@ -92,3 +93,91 @@ public:
         return result;
     }
 };
+
+
+
+/****************************************************************** JAVA ******************************************************************/
+// Approach-1 (Using Division)
+// T.C: O(n)
+// S.C: O(1)
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int product = 0;
+        int productWithoutZero = 1;
+        for (int i : nums) {
+            if (i == 0)
+                product++;
+            else
+                productWithoutZero *= i;
+        }
+
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (product > 0)
+                    result[i] = 0;
+                else
+                    result[i] = productWithoutZero / nums[i];
+            } else {
+                if (product > 1)
+                    result[i] = 0;
+                else
+                    result[i] = productWithoutZero;
+            }
+        }
+        return result;
+    }
+}
+
+
+// Approach-2 (Using extra space)
+// T.C: O(n)
+// S.C: O(n)
+class Solution {
+    public int[] productExceptSelf2(int[] nums) {
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        left[0] = 1;
+        right[n - 1] = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = left[i] * right[i];
+        }
+
+        return result;
+    }
+}
+
+
+// Approach-3 (Using constant space)
+// T.C: O(n)
+// S.C: O(1)
+class Solution {
+    public int[] productExceptSelf3(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        result[0] = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = result[i] * right;
+            right *= nums[i];
+        }
+
+        return result;
+    }
+}
