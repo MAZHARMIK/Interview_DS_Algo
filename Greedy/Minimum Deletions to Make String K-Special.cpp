707ad1361002e27023679a46c69494898cf3ed5b
MAZHAR <iiestshibpur605@gmail.com>
2024-03-17 11:00:08 +0530

    Minimum Deletions to Make String K-Special

---
+++ Greedy/Minimum Deletions to Make String K-Special.cpp
@@ -0,0 +1,82 @@
+/*   Scroll below to see JAVA code also    */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn :
+    Company Tags                : will update soon
+    Leetcode Link               : https://leetcode.com/problems/minimum-deletions-to-make-string-k-special/description/
+*/
+
+
+/****************************************************************** C++ ****************************************************/
+//T.C : O(n) because other iterations are on a constant sized array
+//S.C : O(1) - 26 sized array
+class Solution {
+public:
+    int minimumDeletions(string word, int k) {
+        vector<int> freq(26, 0);
+
+        for(char &ch : word) {
+            freq[ch-'a']++;
+        }
+
+        sort(begin(freq), end(freq));
+
+        int result           = INT_MAX;
+        int deleted_till_now = 0;
+
+        for(int i = 0; i < 26; i++) {
+
+            int minFreq = freq[i];
+            int temp    = deleted_till_now; //temp taken to find deletion for j = 25 to j > i
+
+            for(int j = 25; j > i; j--) {
+                if(freq[j] - freq[i] <= k) 
+                    break;
+                
+                temp += freq[j] - minFreq - k;
+            }
+
+            result = min(result, temp);
+            deleted_till_now += minFreq;
+
+        }
+
+        return result;
+    }
+};
+
+
+
+/****************************************************************** JAVA ****************************************************/
+//T.C : O(n) because other iterations are on a constant sized array
+//S.C : O(1) - 26 sized array
+class Solution {
+    public int minimumDeletions(String word, int k) {
+        int[] freq = new int[26];
+
+        for(char ch : word.toCharArray()) {
+            freq[ch-'a']++;
+        }
+
+        Arrays.sort(freq);
+
+        int result = Integer.MAX_VALUE;
+        int deletedTillNow = 0;
+
+        for(int i = 0; i < 26; i++) {
+            int minFreq = freq[i];
+            int temp = deletedTillNow; //temp taken to find deletion for j = 25 to j > i
+
+            for(int j = 25; j > i; j--) {
+                if(freq[j] - freq[i] <= k) 
+                    break;
+                
+                temp += freq[j] - minFreq - k;
+            }
+
+            result = Math.min(result, temp);
+            deletedTillNow += minFreq;
+        }
+
+        return result;
+    }
+}
