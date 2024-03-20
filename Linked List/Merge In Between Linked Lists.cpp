31adae7100821c5b060b1915428ba47e057684ce
MAZHAR <iiestshibpur605@gmail.com>
2024-03-20 07:34:43 +0530

    Merge In Between Linked Lists (C++ & JAVA)

---
+++ Linked List/Merge In Between Linked Lists.cpp
@@ -0,0 +1,64 @@
+/*      Scroll below to see JAVA code also    */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : AMAZON
+    Leetcode Link               : https://leetcode.com/problems/merge-in-between-linked-lists/
+*/
+
+
+/*************************************************************************** C++ **********************************************************/
+//T.C : O(m+n)
+//S.C : O(1)
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        ListNode* left  = NULL;
+        ListNode* right = list1;
+        
+        for(int i = 0; i <= b; i++) {
+            if(i == a-1) {
+                left = right;
+            }
+            right = right->next;
+        }
+        
+        left->next = list2;
+        
+        ListNode* temp = list2;
+        while(temp && temp->next) {
+            temp = temp->next;
+        }
+        
+        temp->next = right;
+        
+        return list1;  
+    }
+};
+
+/*************************************************************************** JAVA **************************************************************/
+//T.C : O(m+n)
+//S.C : O(1)
+class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode left = null;
+        ListNode right = list1;
+        
+        for (int i = 0; i <= b; i++) {
+            if (i == a - 1) {
+                left = right;
+            }
+            right = right.next;
+        }
+        
+        left.next = list2;
+        
+        ListNode temp = list2;
+        while (temp != null && temp.next != null) {
+            temp = temp.next;
+        }
+        
+        temp.next = right;
+        
+        return list1;
+    }
+}
