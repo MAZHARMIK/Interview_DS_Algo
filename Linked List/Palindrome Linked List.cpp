17135379ed95bd8a2e0ea11ba89f3473ebff57e8
MAZHAR <iiestshibpur605@gmail.com>
2024-03-22 10:23:25 +0530

    Added youtube link

--- Linked List/Palindrome Linked List.cpp
+++ Linked List/Palindrome Linked List.cpp
@@ -1,6 +1,6 @@
 /*    Scroll below to see JAVA code also  */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=Sgi2BHiW0-Q
     Company Tags                : Google
     Leetcode Link               : https://leetcode.com/problems/palindrome-linked-list/description
 */
