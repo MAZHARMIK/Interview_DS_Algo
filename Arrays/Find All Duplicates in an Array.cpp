87dbea15ca702a42faa68cf6fdd31ba98095f487
MAZHAR <iiestshibpur605@gmail.com>
2024-03-25 06:26:52 +0530

    Find All Duplicates in an Array (C++ JAVA)

---
+++ Arrays/Find All Duplicates in an Array.cpp
@@ -0,0 +1,55 @@
+/*    Scroll below to see JAVA code also  */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Pocket Gems, Amazon, Meta
+    Leetcode Link               : https://leetcode.com/problems/find-all-duplicates-in-an-array
+*/
+
+
+/********************************************************************* C++ *****************************************************/
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> result; 
+        for(int i = 0; i < n; i++) {
+            int num = abs(nums[i]);
+            
+            if(nums[num-1] < 0) {
+                result.push_back(num);
+            } else {
+                nums[num-1] *= -1;
+            }
+        }
+        
+        return result;
+        
+    }
+};
+
+
+
+/********************************************************************* JAVA *****************************************************/
+//T.C : O(n)
+//S.C : O(1)
+public class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        int n = nums.length;
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int num = Math.abs(nums[i]);
+
+            if (nums[num - 1] < 0) {
+                result.add(num);
+            } else {
+                nums[num - 1] *= -1;
+            }
+        }
+
+        return result;
+    }
+}
