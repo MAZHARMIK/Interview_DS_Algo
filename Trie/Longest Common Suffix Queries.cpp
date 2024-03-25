29bf89ae3f25a90281d22d89f0146d6cd4c6e2b4
MAZHAR <iiestshibpur605@gmail.com>
2024-03-25 11:12:10 +0530

    Added YouTube Link

--- Trie/Longest Common Suffix Queries.cpp
+++ Trie/Longest Common Suffix Queries.cpp
@@ -1,6 +1,6 @@
 /*    Scroll below to see JAVA code also   */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=jZX2Z5DKR2M
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/longest-common-suffix-queries/
 */
