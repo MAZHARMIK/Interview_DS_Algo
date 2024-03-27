5f5cece25ff63fb7012419cc5f213e1e3d5a6b12
MAZHAR <iiestshibpur605@gmail.com>
2024-03-27 06:16:39 +0530

    Subarray Product Less Than K C++ JAVA

--- Sliding Window/Subarray Product Less Than K.cpp
+++ Sliding Window/Subarray Product Less Than K.cpp
@@ -1,10 +1,14 @@
+/*     Scroll below to see JAVA code also    */
 /*
-    Company Tags  : Yatra
-    Leetcode Link : https://leetcode.com/problems/subarray-product-less-than-k/
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Yatra
+    Leetcode Link               : https://leetcode.com/problems/subarray-product-less-than-k/
 */
 
-
-//Approach-1  : Sliding window , Time : O(N)
+/********************************************************************** C++ *****************************************************************/
+//Approach  : Standard Sliding window Problem
+//T.C : O(N)
+//S.C : O(1)
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
@@ -31,3 +35,36 @@ public:
         return count;
     }
 };
+
+
+/********************************************************************** JAVA *****************************************************************/
+//Approach  : Standard Sliding window Problem
+//T.C : O(N)
+//S.C : O(1)
+public class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1)
+            return 0;
+        
+        int n = nums.length;
+        int count = 0;
+        
+        int left = 0;
+        int right = 0;
+        int prod = 1;
+        
+        while (right < n) {
+            prod *= nums[right];
+            
+            while (prod >= k) {
+                prod /= nums[left];
+                left++;
+            }
+            
+            count += (right - left) + 1;
+            right++;
+        }
+        
+        return count;
+    }
+}
