c2c43235c8f5daad6affbec2bbbbfc79ed8a41a9
MAZHAR <iiestshibpur605@gmail.com>
2024-03-29 09:07:35 +0530

    Added JAVA also

--- Sliding Window/Count Subarrays Where Max Element Appears at Least K Times.cpp
+++ Sliding Window/Count Subarrays Where Max Element Appears at Least K Times.cpp
@@ -1,7 +1,7 @@
 /*    Scroll below to see JAVA code also   */
 /*
       MY YOUTUBE VIDEO ON THIS Qn : 
-      Company Tags                :
+      Company Tags                : will update soon
       Leetcode Link               : https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/
 */
 
@@ -78,3 +78,62 @@ public:
 //Approach-1 (Classic sliding window)
 //T.C : O(n)
 //S.C : O(1)
+class Solution {
+    public long countSubarrays(int[] nums, int k) {
+        int maxE = Arrays.stream(nums).max().getAsInt();
+
+        int n = nums.length;
+        int i = 0, j = 0;
+
+        long result = 0;
+        int countMax = 0;
+
+        while (j < n) {
+            if (nums[j] == maxE) {
+                countMax++;
+            }
+
+            while (countMax >= k) {
+                result += n - j;
+
+                if (nums[i] == maxE) {
+                    countMax--;
+                }
+                i++;
+            }
+            j++;
+        }
+
+        return result;
+    }
+}
+
+
+//Approach-2 (Without Sliding Window)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public long countSubarrays(int[] nums, int k) {
+        int maxE = Arrays.stream(nums).max().getAsInt();
+
+        int n = nums.length;
+
+        long result = 0;
+
+        List<Integer> maxIndices = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == maxE) {
+                maxIndices.add(i);
+            }
+
+            int size = maxIndices.size();
+            if (size >= k) {
+                int last_i = maxIndices.get(size - k);
+                result += last_i + 1;
+            }
+        }
+
+        return result;
+    }
+}
