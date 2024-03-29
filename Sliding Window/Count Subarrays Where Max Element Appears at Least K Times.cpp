c4bda9d3adf35ae96fab2236d73da224793feaf3
MAZHAR <iiestshibpur605@gmail.com>
2024-03-29 07:58:09 +0530

    Count Subarrays Where Max Element Appears at Least K Times C++ JAVA

---
+++ Sliding Window/Count Subarrays Where Max Element Appears at Least K Times.cpp
@@ -0,0 +1,80 @@
+/*    Scroll below to see JAVA code also   */
+/*
+      MY YOUTUBE VIDEO ON THIS Qn : 
+      Company Tags                :
+      Leetcode Link               : https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/
+*/
+
+
+/****************************************************************************** C++ ****************************************************************/
+//Approach-1 (Classic sliding window)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        int maxE = *max_element(begin(nums), end(nums));
+        
+        int n = nums.size();
+        int i = 0, j = 0;
+        
+        long long result = 0;
+        int countMax = 0;
+        
+        while(j < n) {
+            if(nums[j] == maxE) {
+                countMax++;
+            }
+            
+            while(countMax >= k) {
+                result += n-j;
+                
+                if(nums[i] == maxE) {
+                    countMax--;
+                }
+                i++;
+            }
+            j++;
+        }
+        
+        return result;
+    }
+};
+
+
+//Approach-2 (Without Sliding Window)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        int maxE = *max_element(begin(nums), end(nums));
+        
+        int n = nums.size();
+        
+        long long result = 0;
+
+        vector<int> maxIndices;
+        
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == maxE) {
+                maxIndices.push_back(i);
+            }
+            
+            int size = maxIndices.size();
+            if(size >= k) {
+                int last_i = maxIndices[size-k];
+                result += last_i+1;
+            }
+        }
+        
+        return result;
+    }
+};
+
+
+
+/****************************************************************************** JAVA ****************************************************************/
+//Approach-1 (Classic sliding window)
+//T.C : O(n)
+//S.C : O(1)
