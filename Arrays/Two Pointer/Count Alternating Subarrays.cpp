69d8184c3a9ae681a2983acaf951e496c9197c97
MAZHAR <iiestshibpur605@gmail.com>
2024-03-31 09:53:40 +0530

    Count Alternating Subarrays C++ & JAVA

---
+++ Arrays/Two Pointer/Count Alternating Subarrays.cpp
@@ -0,0 +1,57 @@
+/*    Scroll below to see JAVA code also     */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : will update soon
+    Leetcode Link               : https://leetcode.com/problems/count-alternating-subarrays
+*/
+
+
+/******************************************************************** C++ *************************************************************/
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    long long countAlternatingSubarrays(vector<int>& nums) {
+        long long count = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; ++i) {
+            int j = i;
+
+            while (j + 1 < n && nums[j] != nums[j + 1]) {
+                ++j;
+            }
+
+            int len = j - i + 1;
+            count += (long long)len * (len + 1) / 2;
+            i = j;
+        }
+
+        return count;
+    }
+};
+
+
+/******************************************************************** JAVA *************************************************************/
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public long countAlternatingSubarrays(int[] nums) {
+        long count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; ++i) {
+            int j = i;
+
+            while (j + 1 < n && nums[j] != nums[j + 1]) {
+                ++j;
+            }
+
+            int len = j - i + 1;
+            count += (long) len * (len + 1) / 2;
+            i = j;
+        }
+
+        return count;
+    }
+}
