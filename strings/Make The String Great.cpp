91d4d691133940c719b785f505fba5646433a308
MAZHAR <iiestshibpur605@gmail.com>
2024-04-05 08:16:33 +0530

    Added JAVA code also

--- strings/Make The String Great.cpp
+++ strings/Make The String Great.cpp
@@ -1,9 +1,14 @@
+/*         Scroll down to see JAVA code also     */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=J43ZIltH3AY
     Company Tags                : Google, Apple, LinkedIn, Microsoft
     Leetcode Link               : https://leetcode.com/problems/make-the-string-great/
 */
 
+
+/********************************************************************** C++ ********************************************************/
+//T.C : O(n)
+//S.C : O(1) -> Ignoring the space taken for result string
 class Solution {
 public:
     string makeGood(string s) {
@@ -20,3 +25,24 @@ public:
         return result;
     }
 };
+
+
+
+/********************************************************************** JAVA ********************************************************/
+//T.C : O(n)
+//S.C : O(1) -> Ignoring the space taken for result string
+public class Solution {
+    public String makeGood(String s) {
+        StringBuilder result = new StringBuilder();
+        
+        for (char ch : s.toCharArray()) {
+            if (result.length() > 0 && (ch + 32 == result.charAt(result.length() - 1) || ch - 32 == result.charAt(result.length() - 1))) {
+                result.deleteCharAt(result.length() - 1);
+            } else {
+                result.append(ch);
+            }
+        }
+        
+        return result.toString();
+    }
+}
