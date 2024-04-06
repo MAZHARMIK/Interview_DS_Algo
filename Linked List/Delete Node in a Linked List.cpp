234e27b32a2cab73c371baccb1b76bd4394596f0
MAZHAR <iiestshibpur605@gmail.com>
2024-04-06 17:35:15 +0530

    Added JAVA code

--- Linked List/Delete Node in a Linked List.cpp
+++ Linked List/Delete Node in a Linked List.cpp
@@ -1,3 +1,4 @@
+/*         Scroll Down to see JAVA code also      */
 /*
     MY YOUTUBE VIDEON ON THIS Qn : https://www.youtube.com/watch?v=TvLWOECe8mQ
     Company Tags                 : Apple, Adobe, Microsoft, Samsung
@@ -5,6 +6,9 @@
     Similar GfG Link             : https://practice.geeksforgeeks.org/problems/delete-a-node-in-single-linked-list/1
 */
 
+/**************************************** C++ *****************************/
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     void deleteNode(ListNode* node) {
@@ -19,3 +23,26 @@ public:
         prev->next = NULL;
     }
 };
+
+
+/**************************************** JAVA *****************************/
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public void deleteNode(ListNode node) {
+        ListNode prev = null;
+
+        while (node != null && node.next != null) {
+            node.val = node.next.val;
+            prev = node;
+            node = node.next;
+        }
+        
+        if (node != null) {
+            if (prev != null)
+                prev.next = null;
+            else
+                node = null;
+        }
+    }
+}
