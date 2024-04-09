ae8764c672aa9aef156de9782f16819cbd91edce
MAZHAR <iiestshibpur605@gmail.com>
2024-04-09 06:57:49 +0530

    Update Edit Distance.cpp

--- DP/Edit Distance.cpp
+++ DP/Edit Distance.cpp
@@ -4,7 +4,9 @@
 */
 
 
-//Approach-1 (Using Top Down (Recursive + Memo))
+//Approach-1 (Recur + Memo, starting from m, n)
+//T.C : O(m*n)
+//S.C : O(m*n)
 class Solution {
 public:
     int t[501][501];
@@ -35,8 +37,44 @@ public:
     }
 };
 
+//Approach-2 (Recur + Memo, starting from i = 0, j = 0)
+class Solution {
+public:
+    int t[501][501];
+    int m, n;
+    int solve(string& s1, string& s2, int i, int j) {
+        if(i == m)
+            return n-j;
+        else if(j == n)
+            return m-i;
+        
+        if(t[i][j] != -1)
+            return t[i][j];
+        
+        if(s1[i] == s2[j])
+            return t[i][j] = solve(s1, s2, i+1, j+1);
+        else {
+            int insertC  = 1 + solve(s1, s2, i, j+1);
+            int deleteC  = 1 + solve(s1, s2, i+1, j);
+            int replaceC = 1 + solve(s1, s2, i+1, j+1);
+            
+            return t[i][j] = min({insertC, deleteC, replaceC});
+        }
+        
+    }
+    
+    int minDistance(string s1, string s2) {
+        m = s1.length();
+        n = s2.length();
+        memset(t, -1, sizeof(t));
+        return solve(s1, s2, 0, 0);
+    }
+};
 
-//Approach-2 (Using Bottom Up)
+
+//Approach-3 (Using Bottom Up derived from Approach-1)
+//T.C : O(m*n)
+//S.C : O(m*n)
 class Solution {
 public:
     int editDistance(string s1, string s2, int m, int n) {
