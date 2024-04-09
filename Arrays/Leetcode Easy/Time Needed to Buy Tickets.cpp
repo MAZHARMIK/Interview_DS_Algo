18100a6d45914ad43888d427533567fb6618ccef
MAZHAR <iiestshibpur605@gmail.com>
2024-04-09 19:58:17 +0530

    Added YouTube Link

--- Arrays/Leetcode Easy/Time Needed to Buy Tickets.cpp
+++ Arrays/Leetcode Easy/Time Needed to Buy Tickets.cpp
@@ -1,6 +1,6 @@
 /*         Scroll down to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=r2LPa779amQ
     Company Tags                : 
     Leetcode Link               : https://leetcode.com/problems/time-needed-to-buy-tickets/
 */
