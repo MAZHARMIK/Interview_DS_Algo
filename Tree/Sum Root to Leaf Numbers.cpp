c014e03592535050ac9f3dfe5fcd0484380035d3
MAZHAR <iiestshibpur605@gmail.com>
2024-04-15 05:52:17 +0530

    Added JAVA also

--- Tree/Sum Root to Leaf Numbers.cpp
+++ Tree/Sum Root to Leaf Numbers.cpp
@@ -1,3 +1,4 @@
+/*     Scroll down to see JAVA code also    */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=KTwkq-MsJDw
     Company Tags                : Google, Meta, Accolite, Amazon, Microsoft, Samsung, FactSet, 
@@ -5,7 +6,11 @@
     Leetcode Link               : https://leetcode.com/problems/sum-root-to-leaf-numbers/
 */
 
-//Recursion
+
+/***************************************************** C++ *************************************/
+//Recursion-1
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     
@@ -32,7 +37,37 @@ public:
 };
 
 
-//Simple Recursion
+
+/***************************************************** JAVA *************************************/
+//Recursion-1
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public int find(TreeNode root, int curr) {
+        if (root == null)
+            return 0;
+
+        curr = curr * 10 + root.val;
+
+        if (root.left == null && root.right == null) {
+            return curr;
+        }
+
+        int leftNum = find(root.left, curr);
+        int rightNum = find(root.right, curr);
+
+        return leftNum + rightNum;
+    }
+
+    public int sumNumbers(TreeNode root) {
+        return find(root, 0);
+    }
+}
+
+
+//Recursion-2
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     int sumNumbers(TreeNode* root, int sum = 0) {
@@ -48,3 +83,27 @@ public:
         return ls + rs;
     }
 };
+
+
+
+//Recursion-2
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return sumNumbers(root, 0);
+    }
+    
+    private int sumNumbers(TreeNode root, int sum) {
+        if (root == null)
+            return 0;
+
+        if (root.left == null && root.right == null)
+            return sum * 10 + root.val;
+
+        int leftSum = sumNumbers(root.left, sum * 10 + root.val);
+        int rightSum = sumNumbers(root.right, sum * 10 + root.val);
+
+        return leftSum + rightSum;
+    }
+}
