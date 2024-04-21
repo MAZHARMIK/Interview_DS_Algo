a19e655a0331c00b03f55138e2a7195c35ca0168
MAZHAR <iiestshibpur605@gmail.com>
2024-04-21 06:56:56 +0530

    Added JAVA code

--- Sliding Window/Count Occurences of Anagrams.cpp
+++ Sliding Window/Count Occurences of Anagrams.cpp
@@ -1,3 +1,4 @@
+/*		Scroll down to see JAVA code also 		*/
 /*
     MY YOUTUBE VIDEO ON THIS QN : https://www.youtube.com/watch?v=mrUBUWb23hk
     Company Tags    		: Amazon, Intuit, Microsoft, Flipkart
@@ -5,6 +6,9 @@
 */
 
 
+/******************************************* C++ *********************************************/
+//T.C : O(n)
+//S.C : O(1)
 class Solution{
 public:
     bool allZero(vector<int>& count) {
@@ -37,3 +41,49 @@ public:
 	    return result;
 	}
 };
+
+
+/******************************************* JAVA *********************************************/
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    
+    private boolean allZero(int[] count) {
+        for (int num : count) {
+            if (num != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    int search(String pat, String txt) {
+        int k = pat.length();
+        int[] count = new int[26];
+        Arrays.fill(count, 0);
+        
+        for (char ch : pat.toCharArray()) {
+            count[ch - 'a']++;
+        }
+
+        int i = 0, j = 0;
+        int n = txt.length();
+        int result = 0;
+
+        while (j < n) {
+            int idx = txt.charAt(j) - 'a';
+            count[idx]--;
+
+            if (j - i + 1 == k) {
+                if (allZero(count)) {
+                    result++;
+                }
+
+                count[txt.charAt(i) - 'a']++;
+                i++;
+            }
+            j++;
+        }
+        return result;
+    }
+}
