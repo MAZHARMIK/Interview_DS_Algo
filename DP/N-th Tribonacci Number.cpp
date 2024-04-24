02b69132831d2f6d1bd8624bbfce52969fc2b0cc
MAZHAR <iiestshibpur605@gmail.com>
2024-04-24 06:27:40 +0530

    Update N-th Tribonacci Number.cpp

--- DP/N-th Tribonacci Number.cpp
+++ DP/N-th Tribonacci Number.cpp
@@ -36,20 +36,20 @@ public:
 //T.C : O(n)
 //S.C : O(n)  (But size of t is 38 which is constant hence O(1))
 class Solution {
-public:
-    int tribonacci(int n) {
-        int t[38];
+    public int tribonacci(int n) {
+        int[] t = new int[38];
 
         t[0] = 0;
         t[1] = 1;
         t[2] = 1;
         
-        for(int i = 0; i<=34; i++)
-            t[i+3] = t[i] + t[i+1] + t[i+2];
+        for (int i = 3; i <= 37; i++) {
+            t[i] = t[i-1] + t[i - 2] + t[i - 3];
+        }
         
         return t[n];
     }
-};
+}
 
 
 //APproach-3 (Using constant space)
@@ -114,8 +114,8 @@ class Solution {
         t[1] = 1;
         t[2] = 1;
         
-        for (int i = 0; i <= 34; i++) {
-            t[i + 3] = t[i] + t[i + 1] + t[i + 2];
+        for (int i = 3; i <= 37; i++) {
+            t[i] = t[i-1] + t[i - 2] + t[i - 3];
         }
         
         return t[n];
