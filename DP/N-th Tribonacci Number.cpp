0d1c7177fba4426b630aad94820792254573e0fe
MAZHAR <iiestshibpur605@gmail.com>
2024-04-24 06:07:36 +0530

    Update N-th Tribonacci Number.cpp

--- DP/N-th Tribonacci Number.cpp
+++ DP/N-th Tribonacci Number.cpp
@@ -38,11 +38,8 @@ public:
 class Solution {
 public:
     int tribonacci(int n) {
-        static int t[38];
-        memset(t, -1, sizeof(t));
-        
-        if(t[n] != -1) return t[n];
-        
+        int t[38];
+
         t[0] = 0;
         t[1] = 1;
         t[2] = 1;
@@ -112,16 +109,14 @@ class Solution {
 class Solution {
     public int tribonacci(int n) {
         int[] t = new int[38];
-        Arrays.fill(t, -1);
-        
-        if (t[n] != -1) return t[n];
-        
+
         t[0] = 0;
         t[1] = 1;
         t[2] = 1;
         
-        for (int i = 0; i <= 34; i++)
+        for (int i = 0; i <= 34; i++) {
             t[i + 3] = t[i] + t[i + 1] + t[i + 2];
+        }
         
         return t[n];
     }
