716aa9dcfb2d618ad9e9320839385fd281e33a78
MAZHAR <iiestshibpur605@gmail.com>
2024-04-26 09:31:23 +0530

    Improved

--- DP/Minimum Falling Path Sum II.cpp
+++ DP/Minimum Falling Path Sum II.cpp
@@ -13,34 +13,33 @@
 class Solution {
 public:
     int n;
-    
-    int solve(int row, int col, vector<vector<int>>& grid, map<pair<int, int>, int>& mp) {
+    int t[201][201];
+    int solve(int row, int col, vector<vector<int>>& grid) {
         if (row == n - 1) {
             return grid[row][col];
         }
 
-        if (mp.find({row, col}) != mp.end()) {
-            return mp[{row, col}];
+        if(t[row][col] != -1) {
+            return t[row][col];
         }
-
+        
         int ans = INT_MAX;
         for (int nextCol = 0; nextCol < n; nextCol++) {
             if (nextCol != col) {
-                ans = min(ans, solve(row + 1, nextCol, grid, mp));
+                ans = min(ans, solve(row + 1, nextCol, grid));
             }
         }
 
-        mp[{row, col}] = grid[row][col] + ans;
-        return mp[{row, col}];
+        return t[row][col] = grid[row][col] + ans;
     }
     
     int minFallingPathSum(vector<vector<int>>& grid) {
         n = grid.size();
-        map<pair<int, int>, int> mp;
+        memset(t, -1, sizeof(t));
 
         int result = INT_MAX;
         for (int col = 0; col < n; col++) {
-            result = min(result, solve(0, col, grid, mp));
+            result = min(result, solve(0, col, grid));
         }
 
         return result;
