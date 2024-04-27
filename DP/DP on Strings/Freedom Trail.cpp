96d90c6a0f0958dc1f7b74efc3c116efc214c296
MAZHAR <iiestshibpur605@gmail.com>
2024-04-27 11:20:36 +0530

    Update Freedom Trail.cpp

--- DP/DP on Strings/Freedom Trail.cpp
+++ DP/DP on Strings/Freedom Trail.cpp
@@ -51,5 +51,44 @@ public:
 
 
 //Approach-2 (Bottom Up)
-//T.C : 
-//S.C : 
+//T.C : O(n^2 * m)
+//S.C : O(n*m)
+//State Def - t[ringIndex][keyIndex] = minimum number of steps to get key[keyIndex] when the ring[ringIndex] is aligned with the "12:00" position.
+class Solution {
+public:
+    int countSteps(int curr, int next, int ringLength) {
+        int stepsBetween = abs(curr - next);
+        int stepsAround = ringLength - stepsBetween;
+        return min(stepsBetween, stepsAround);
+    }
+    
+    int findRotateSteps(string ring, string key) {
+        int n = ring.length();
+        int m = key.length(); 
+        
+        vector<vector<int>> t(n+1, vector<int>(m+1, INT_MAX));
+        //t[ringIndex][keyIndex] = minimum number of steps to get key[keyIndex] when the ring[ringIndex] is aligned with the "12:00" position.
+        
+        //If 
+        for(int ringIndex = 0; ringIndex < n; ringIndex++) {
+            t[ringIndex][m] = 0; //base case when all key chars are done (we reached index n)
+        }
+    
+        for(int keyIndex = m-1; keyIndex >= 0; keyIndex--) {
+            for(int ringIndex = 0; ringIndex < n; ringIndex++) {
+                
+                 int result = INT_MAX;
+                 for (int i = 0; i < ring.length(); i++) {
+                    if (ring[i] == key[keyIndex]) {
+                        int totalSteps = countSteps(ringIndex, i, ring.length()) + 1 +
+                                                    t[i][keyIndex + 1];
+                        result = min(result, totalSteps);
+                    }
+                }
+                t[ringIndex][keyIndex] = result;
+            }
+        }
+        return t[0][0];
+    }
+};
+
