4bba58140460b06d4956f10678da7971b27ef7eb
MAZHAR <iiestshibpur605@gmail.com>
2024-05-06 18:29:30 +0530

    Added JAVA code also

--- DP/Longest Palindromic Subsequence.cpp
+++ DP/Longest Palindromic Subsequence.cpp
@@ -1,3 +1,4 @@
+/*         Scroll down to see JAVA code also        */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=M4pfB3G-YQc
     Company Tags                : Amazon, LinkedIn, Paypal, Rivigo, Uber, Google
@@ -5,6 +6,74 @@
     GfG Link                    : https://practice.geeksforgeeks.org/problems/longest-palindromic-subsequence-1612327878/1
 */
 
+
+/******************************************************** C++ ********************************************************/
+//Approach-1 (Recursion + Memoization)
+//T.C : O(m*n)
+//S.C : O(m*n)
+class Solution {
+public:
+    int t[1001][1001];
+    int LPS(string& s, int i, int j) {
+        if(i > j)
+            return 0;
+        if(i == j)
+            return 1;
+        
+        if(t[i][j] != -1)
+            return t[i][j];
+        if(s[i] == s[j])
+            return t[i][j] = 2 + LPS(s, i+1, j-1);
+        else
+            return t[i][j] = max(LPS(s, i+1, j), LPS(s, i, j-1));
+    }
+    
+    int longestPalindromeSubseq(string s) {
+        int m = s.length();
+        memset(t, -1, sizeof(t));
+        
+        return LPS(s, 0, m-1); //Approach-1
+        
+    }
+};
+
+
+//Approach-2 (Bottom Up)
+//T.C : O(n*n)
+//S.C : O(n*n)
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.length();
+        
+        vector<vector<int>> t(n, vector<int>(n));
+        //t[i][j] = longest common subsequence in string from i to j indices;
+        //strings of length 1 are always a palindorme
+        for(int i = 0; i<n; i++) {
+            t[i][i] = 1;
+        }
+        
+        for(int L = 2; L<=n; L++) {
+            for(int i = 0; i<n-L+1; i++) {
+                int j = i + L - 1;
+                
+                if(s[i] == s[j] && L == 2)
+                    t[i][j] = 2;
+                else if(s[i] == s[j]) {
+                    t[i][j] = 2 + t[i+1][j-1];
+                } else {
+                    t[i][j] = max(t[i+1][j], t[i][j-1]);
+                }
+            }
+        }
+        return t[0][n-1];
+    }
+};
+
+
+//Approach-3 (Using LCS Concept)
+//T.C : O(m*n)
+//S.C : O(m*n)
 class Solution {
 public:
     //Approach-1 (Using Concept of LCS)
@@ -32,46 +101,6 @@ public:
         return t[m][n];
     }
     
-    //Approach-2 (Recursion with Memoization)
-    int LPS(string s, int i, int j, vector<vector<int>>& t) {
-        if(i > j)
-            return 0;
-        if(i == j)
-            return 1;
-        
-        if(t[i][j] != -1)
-            return t[i][j];
-        if(s[i] == s[j])
-            return t[i][j] = 2 + LPS(s, i+1, j-1, t);
-        else
-            return t[i][j] = max(LPS(s, i+1, j, t), LPS(s, i, j-1, t));
-    }
-    
-    //Approach-3 (Bottom-Up DP)
-    int LPS_Bottom_Up(string s, int n) {
-        vector<vector<int>> t(n, vector<int>(n));
-        //t[i][j] = longest common subsequence in string from i to j indices;
-        //strings of length 1 are always a palindorme
-        for(int i = 0; i<n; i++) {
-            t[i][i] = 1;
-        }
-        
-        for(int L = 2; L<=n; L++) {
-            for(int i = 0; i<n-L+1; i++) {
-                int j = i + L - 1;
-                
-                if(s[i] == s[j] && L == 2)
-                    t[i][j] = 2;
-                else if(s[i] == s[j]) {
-                    t[i][j] = 2 + t[i+1][j-1];
-                } else {
-                    t[i][j] = max(t[i+1][j], t[i][j-1]);
-                }
-            }
-        }
-        return t[0][n-1];
-    }
-    
     int longestPalindromeSubseq(string s) {
         string s1 = s;
         
@@ -80,9 +109,111 @@ public:
         
         return LCS(s, s1, m, m); //Approach-1
         
-        vector<vector<int>> t(m+1, vector<int>(m+1, -1));
-        return LPS(s, 0, m-1, t); //Approach-2
-        
-        return LPS_Bottom_Up(s, m); //Approach-3
     }
 };
+
+
+
+/******************************************************** JAVA ********************************************************/
+//Approach-1 (Recursion + Memoization)
+//T.C : O(m*n)
+//S.C : O(m*n)
+import java.util.Arrays;
+
+class Solution {
+    int[][] t = new int[1001][1001];
+
+    public int LPS(String s, int i, int j) {
+        if (i > j)
+            return 0;
+        if (i == j)
+            return 1;
+
+        if (t[i][j] != -1)
+            return t[i][j];
+        if (s.charAt(i) == s.charAt(j))
+            return t[i][j] = 2 + LPS(s, i + 1, j - 1);
+        else
+            return t[i][j] = Math.max(LPS(s, i + 1, j), LPS(s, i, j - 1));
+    }
+
+    public int longestPalindromeSubseq(String s) {
+        int m = s.length();
+        for (int[] row : t) {
+            Arrays.fill(row, -1);
+        }
+        return LPS(s, 0, m - 1); // Approach-1
+    }
+}
+
+
+//Approach-2 (Bottom Up)
+//T.C : O(n*n)
+//S.C : O(n*n)
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+
+        int[][] t = new int[n][n];
+        // t[i][j] = longest common subsequence in string from i to j indices;
+        // strings of length 1 are always a palindrome
+        for (int i = 0; i < n; i++) {
+            t[i][i] = 1;
+        }
+
+        for (int L = 2; L <= n; L++) {
+            for (int i = 0; i < n - L + 1; i++) {
+                int j = i + L - 1;
+
+                if (s.charAt(i) == s.charAt(j) && L == 2)
+                    t[i][j] = 2;
+                else if (s.charAt(i) == s.charAt(j)) {
+                    t[i][j] = 2 + t[i + 1][j - 1];
+                } else {
+                    t[i][j] = Math.max(t[i + 1][j], t[i][j - 1]);
+                }
+            }
+        }
+        return t[0][n - 1];
+    }
+}
+
+
+//Approach-3 (Using LCS Concept)
+//T.C : O(m*n)
+//S.C : O(m*n)
+import java.util.*;
+
+class Solution {
+    // Approach-1 (Using Concept of LCS)
+    public int LCS(String s1, String s2, int m, int n) {
+        int[][] t = new int[m + 1][n + 1];
+
+        for (int row = 0; row < m + 1; row++) {
+            t[row][0] = 0;
+        }
+
+        for (int col = 0; col < n + 1; col++) {
+            t[0][col] = 0;
+        }
+
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    t[i][j] = 1 + t[i - 1][j - 1];
+                } else {
+                    t[i][j] = Math.max(t[i][j - 1], t[i - 1][j]);
+                }
+            }
+        }
+
+        return t[m][n];
+    }
+
+    public int longestPalindromeSubseq(String s) {
+        String s1 = new StringBuilder(s).reverse().toString();
+        int m = s.length();
+
+        return LCS(s, s1, m, m); // Approach-1
+    }
+}
