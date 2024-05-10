8e3685abe91a74244ab761fb2d9b45cd7dbc2351
MAZHAR <iiestshibpur605@gmail.com>
2024-05-10 09:07:29 +0530

    Added JAVA code as well

--- Heap/K-th Smallest Prime Fraction.cpp
+++ Heap/K-th Smallest Prime Fraction.cpp
@@ -1,13 +1,14 @@
+/*         Scroll below to see JAVA code also        */
 /*
     MY YOUTUBE VIDEO ON THIS Qn :
     Company Tags                : Pony.ai
     Leetcode Link               : https://leetcode.com/problems/k-th-smallest-prime-fraction/
 */
 
-
+/****************************************************************** C++ ******************************************************************/
 //Approach-1 (Using simple Heap solution as it's asking for kth smallest)
 //T.C : O(n^2 * log(k))
-//S.C : 
+//S.C : O(k)
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
@@ -65,3 +66,63 @@ public:
         return {arr[i], arr[j]};
     }
 };
+
+
+
+/****************************************************************** JAVA ******************************************************************/
+//Approach-1 (Using simple Heap solution as it's asking for kth smallest)
+//T.C : O(n^2 * log(k))
+//S.C : O(k)
+class Solution {
+    public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+        PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                double div = (double) arr[i] / arr[j];
+                pq.offer(new double[]{div, (double) arr[i], (double) arr[j]});
+                if (pq.size() > k)
+                    pq.poll();
+            }
+        }
+
+        double[] vec = pq.peek();
+        int[] result = new int[2];
+        result[0] = (int) vec[1];
+        result[1] = (int) vec[2];
+        return result;
+    }
+}
+
+
+//Approach-2 (Optimising Approach-1)
+//T.C : O((n+k)logn)
+//S.C : O(n)
+import java.util.*;
+
+class Solution {
+    public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+        int n = arr.length;
+        PriorityQueue<double[]> pq = new PriorityQueue<>(Comparator.comparingDouble(a -> a[0]));
+
+        for (int i = 0; i < n; i++)
+            pq.offer(new double[]{1.0 * arr[i] / arr[n - 1], (double) i, (double) (n - 1)});
+
+        int smallest = 1;
+
+        while (smallest < k) {
+            double[] vec = pq.poll();
+
+            int i = (int) vec[1];
+            int j = (int) vec[2] - 1;
+
+            pq.offer(new double[]{1.0 * arr[i] / arr[j], (double) i, (double) j});
+            smallest++;
+        }
+
+        double[] vec = pq.peek();
+        int i = (int) vec[1];
+        int j = (int) vec[2];
+        return new int[]{arr[i], arr[j]};
+    }
+}
