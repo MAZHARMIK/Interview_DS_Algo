784b179c88a06b9e5dc5f0bfe419383b1f8be1b9
MAZHAR <iiestshibpur605@gmail.com>
2024-05-10 22:06:27 +0530

    Added YouTube Link

--- Heap/K-th Smallest Prime Fraction.cpp
+++ Heap/K-th Smallest Prime Fraction.cpp
@@ -1,6 +1,6 @@
 /*         Scroll below to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn :
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=92SDuUKAR3s
     Company Tags                : Pony.ai
     Leetcode Link               : https://leetcode.com/problems/k-th-smallest-prime-fraction/
 */
