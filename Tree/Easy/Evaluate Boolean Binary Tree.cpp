cc66f3602e71d60f028d746ed54c449597dec968
MAZHAR <iiestshibpur605@gmail.com>
2024-05-16 06:49:57 +0530

    Evaluate Boolean Binary Tree

---
+++ Tree/Easy/Evaluate Boolean Binary Tree.cpp
@@ -0,0 +1,45 @@
+/*     Scroll below to see JAVA code also    */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : will update soon
+    Leetcode Link               : https://leetcode.com/problems/evaluate-boolean-binary-tree/description/
+*/
+
+/************************************************************ C++ ************************************************************/
+//Using simple recursion
+//T.C : O(n)
+//S.C : O(depth of tree)
+class Solution {
+public:
+    bool evaluateTree(TreeNode* root) {
+        if(root->left == NULL && root->right == NULL) {
+            return root->val;
+        }
+        
+        if(root->val == 2) {
+            return evaluateTree(root->left) | evaluateTree(root->right);
+        }
+        
+        return evaluateTree(root->left) & evaluateTree(root->right);
+    }
+};
+
+
+
+/************************************************************ JAVA ************************************************************/
+//Using simple recursion
+//T.C : O(n)
+//S.C : O(depth of tree)
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return root.val != 0; // Convert int to boolean
+        }
+        
+        if (root.val == 2) {
+            return evaluateTree(root.left) | evaluateTree(root.right);
+        }
+        
+        return evaluateTree(root.left) & evaluateTree(root.right);
+    }
+}
