86e991de5b92c4dd0350e5179e13a55c2bc54231
MAZHAR <iiestshibpur605@gmail.com>
2024-05-17 07:22:32 +0530

    Delete Leaves With a Given Value

---
+++ Tree/Delete Leaves With a Given Value.cpp
@@ -0,0 +1,51 @@
+/*     MY YOUTUBE VIDEO ON THIS Qn    */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Dunzo, Sprinkler
+    Leetcode Link               : https://leetcode.com/problems/delete-leaves-with-a-given-value
+*/
+
+
+/************************************************************ C++ ************************************************************/
+//Simple Recursion story to code
+//T.C : O(n)
+//S.C : O(height)
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if(!root) {
+            return NULL;
+        }
+
+        root->left  = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
+
+        if(root->left == NULL && root->right == NULL && root->val == target)
+            return NULL;
+
+        return root;
+        
+    }
+};
+
+
+
+/************************************************************ JAVA ************************************************************/
+//Simple Recursion story to code
+//T.C : O(n)
+//S.C : O(height)
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if(root == null) {
+            return null;
+        }
+
+        root.left  = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+
+        if(root.left == null && root.right == null && root.val == target)
+            return null;
+
+        return root;
+    }
+}
