d2698116972bbe95feed1189928d8307df4dede1
MAZHAR <iiestshibpur605@gmail.com>
2024-05-18 20:23:47 +0530

    Update Distribute Coins in Binary Tree.cpp

--- Tree/Distribute Coins in Binary Tree.cpp
+++ Tree/Distribute Coins in Binary Tree.cpp
@@ -43,9 +43,8 @@ public:
 /****************************************************************** JAVA ****************************************************************************************/
 //T.C : O(n)
 //S.C : O(height of tree due to recursion)
-class Solution
-{
-    public static int distributeCoins(TreeNode root) {
+class Solution {
+    public static int distributeCandy(Node root) {
         if (root == null || (root.left == null && root.right == null)) {
             return 0;
         }
@@ -55,7 +54,7 @@ class Solution
 
         return moves[0];
     }
-    private static int solve(TreeNode root, int[] moves) {
+    private static int solve(Node root, int[] moves) {
         if (root == null) {
             return 0;
         }
@@ -63,7 +62,7 @@ class Solution
         int l = solve(root.left, moves);
         int r = solve(root.right, moves);
 
-        int totalExtraCandies = (l + r + root.val) - 1;
+        int totalExtraCandies = (l + r + root.data) - 1;
 
         moves[0] += Math.abs(l) + Math.abs(r);
 
