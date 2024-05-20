3a0d1c02960e06f6ef64b9fed63c1fe7539459d9
MAZHAR <iiestshibpur605@gmail.com>
2024-05-20 06:54:36 +0530

    Create Sum of All Subset XOR Totals.cpp

---
+++ Bit_Magic/Leetcode Easy/Sum of All Subset XOR Totals.cpp
@@ -0,0 +1,127 @@
+/*   Scroll below to see JAVA code also  */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Google, Meta
+    Leetcode Link               : https://leetcode.com/problems/sum-of-all-subset-xor-totals
+*/
+
+
+/******************************************************* C++ *******************************************************/
+//Approach-1 (Using simple backtracking to generate subsets)
+//T.C : O(n * 2^n)
+//S.C : O(n * 2^n)
+class Solution {
+public:
+
+    void solve(vector<int>& nums, int i, vector<int>& currSubset,
+                         vector<vector<int>>& subsets) {
+
+        if (i == nums.size()) {
+            subsets.push_back(currSubset);
+            return;
+        }
+
+        currSubset.push_back(nums[i]);
+        solve(nums, i + 1, currSubset, subsets);
+        currSubset.pop_back();
+        solve(nums, i + 1, currSubset, subsets);
+    }
+
+    int subsetXORSum(vector<int>& nums) {
+        vector<vector<int>> subsets;
+        vector<int> currSubset;
+        solve(nums, 0, currSubset, subsets);
+
+        int result = 0;
+        for (auto& currSubset : subsets) {
+            int Xor = 0;
+            for (int num : currSubset) {
+                Xor ^= num;
+            }
+            result += Xor;
+        }
+        return result;
+    }
+};
+
+
+//Approach-2 (Using optimal backtracking)
+//T.C : O(2^n)
+//S.C : O(n)
+class Solution {
+public:
+
+    int solve(int i, vector<int>& nums, int Xor) {
+        if(i == nums.size()) {
+            return Xor;
+        }
+
+        int include = solve(i+1, nums, nums[i] ^ Xor);
+        int exclude = solve(i+1, nums, Xor);
+
+        return include + exclude;
+    }
+
+    int subsetXORSum(vector<int>& nums) {
+        return solve(0, nums, 0);
+    }
+};
+
+/******************************************************* JAVA *******************************************************/
+//Approach-1 (Using simple backtracking to generate subsets)
+//T.C : O(n * 2^n)
+//S.C : O(n * 2^n)
+class Solution {
+
+    private void solve(int[] nums, int i, List<Integer> currSubset, List<List<Integer>> subsets) {
+        if (i == nums.length) {
+            subsets.add(new ArrayList<>(currSubset));
+            return;
+        }
+
+        currSubset.add(nums[i]);
+        solve(nums, i + 1, currSubset, subsets);
+        currSubset.remove(currSubset.size() - 1);
+        solve(nums, i + 1, currSubset, subsets);
+    }
+
+    public int subsetXORSum(int[] nums) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        List<Integer> currSubset = new ArrayList<>();
+        solve(nums, 0, currSubset, subsets);
+
+        int result = 0;
+        for (List<Integer> subset : subsets) {
+            int xor = 0;
+            for (int num : subset) {
+                xor ^= num;
+            }
+            result += xor;
+        }
+        return result;
+    }
+}
+
+
+
+
+//Approach-2 (Using optimal backtracking)
+//T.C : O(2^n)
+//S.C : O(n)
+class Solution {
+
+    private int solve(int i, int[] nums, int Xor) {
+        if (i == nums.length) {
+            return Xor;
+        }
+
+        int include = solve(i + 1, nums, nums[i] ^ Xor);
+        int exclude = solve(i + 1, nums, Xor);
+
+        return include + exclude;
+    }
+
+    public int subsetXORSum(int[] nums) {
+        return solve(0, nums, 0);
+    }
+}
