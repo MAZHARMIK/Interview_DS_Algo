fa786ee97b79d75db10e87be2a37b42c62216008
MAZHAR <iiestshibpur605@gmail.com>
2024-05-21 05:41:12 +0530

    Added JAVA code as well

--- Bit_Magic/Subsets.cpp
+++ Bit_Magic/Subsets.cpp
@@ -1,8 +1,11 @@
+/*                                       Scroll down to see JAVA code also                                      /*
 /*
     Company Tags  : Microsoft
     Leetcode Link : https://leetcode.com/problems/subsets/
 */
 
+
+/************************************************************ C++ ************************************************************/
 //Using Bit magic. You can also use Backtracking (See my Subsets II in Backtracking section)
 class Solution {
 public:
@@ -28,3 +31,28 @@ public:
         
     }
 };
+
+
+/************************************************************ JAVA ************************************************************/
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        int n = nums.length;
+        int limit = (1 << n);
+        
+        for (int mask = 0; mask < limit; mask++) {
+            List<Integer> temp = new ArrayList<>();
+            
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    temp.add(nums[i]);
+                }
+            }
+            
+            result.add(temp);
+        }
+        
+        return result;
+    }
+}
