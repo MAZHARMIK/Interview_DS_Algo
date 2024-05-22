3b8a4e72a26710dd0f4745bc0c6929d58f9b2230
MAZHAR <iiestshibpur605@gmail.com>
2024-05-22 05:44:35 +0530

    Added JAVA code as well

--- Backtracking/Palindrome Partitioning.cpp
+++ Backtracking/Palindrome Partitioning.cpp
@@ -1,3 +1,4 @@
+/*         Scroll below to see JAVA Code also        */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=jHR7KUAAEzw
     Company Tags                : Google, Amazon, Microsoft, Meta
@@ -5,7 +6,10 @@
     GfG Link                    : https://practice.geeksforgeeks.org/problems/find-all-possible-palindromic-partitions-of-a-string/1
 */
 
+/******************************************************** C++ ********************************************************/
 //Whenever a question asks for "Generating all possible" something, think about Backtracking once
+//T.C : O(n * 2^n) - For a string of length n, there are 2^(𝑛 − 1) potential ways to partition it (since each position can either be a cut or not). and we also check palindrome O(n)
+//S.C : O(n * 2^n) - Number of partitions * their length
 class Solution {
 public:
     int n;
@@ -58,3 +62,47 @@ public:
         
     }
 };
+
+
+/******************************************************** JAVA ********************************************************/
+//Whenever a question asks for "Generating all possible" something, think about Backtracking once
+//T.C : O(n * 2^n) - For a string of length n, there are 2^(𝑛 − 1) potential ways to partition it (since each position can either be a cut or not). and we also check palindrome O(n)
+//S.C : O(n * 2^n) - Number of partitions * their length
+class Solution {
+    private int n;
+    
+    private boolean isPalindrome(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r))
+                return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+    
+    private void backtrack(String s, int idx, List<String> curr, List<List<String>> result) {
+        if (idx == n) {
+            result.add(new ArrayList<>(curr));
+            return;
+        }
+        
+        for (int i = idx; i < n; i++) {
+            if (isPalindrome(s, idx, i)) {
+                curr.add(s.substring(idx, i + 1));
+                backtrack(s, i + 1, curr, result);
+                curr.remove(curr.size() - 1);
+            }
+        }
+    }
+    
+    public List<List<String>> partition(String s) {
+        n = s.length();
+        List<List<String>> result = new ArrayList<>();
+        List<String> curr = new ArrayList<>();
+        
+        backtrack(s, 0, curr, result);
+        
+        return result;
+    }
+}
