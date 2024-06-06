4cb0ab823e1ef4c07be9dd837ffb17942a8b6d15
MAZHAR <iiestshibpur605@gmail.com>
2024-06-06 10:49:42 +0530

    Hand of Straights

---
+++ HashMap/Hand of Straights.cpp
@@ -0,0 +1,82 @@
+/*   Scroll below to see JAVA code also  */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Google
+    Leetcode Link               : https://leetcode.com/problems/hand-of-straights
+*/
+
+
+//Approach (Simply using map)
+//T.C  : O(nlogn) + O(n*groupSize)
+//S.C  : O(n)
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+
+        if(n % groupSize) {
+            return false;
+        }
+
+        map<int, int> mp;
+        for(int &handNumber : hand) {
+            mp[handNumber]++; //O(nlogn)
+        }
+
+        while(!mp.empty()) { //O(n*groupSize)
+
+            int curr = mp.begin()->first; //->second : frequency
+
+            for(int i = 0; i < groupSize; i++) {
+                if(mp[curr + i] == 0) {
+                    return false;
+                }
+
+                mp[curr+i]--;
+                if(mp[curr+i] < 1) {
+                    mp.erase(curr+i);
+                }
+            }
+
+        }
+
+        return true;
+        
+    }
+};
+
+
+//Approach (Simply using map)
+//T.C  : O(nlogn) + O(n*groupSize)
+//S.C  : O(n)
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        int n = hand.length;
+
+        if (n % groupSize != 0) {
+            return false;
+        }
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int card : hand) {
+            map.put(card, map.getOrDefault(card, 0) + 1);
+        }
+
+        while (!map.isEmpty()) {
+            int curr = map.firstKey();
+
+            for (int i = 0; i < groupSize; i++) {
+                if (!map.containsKey(curr + i)) {
+                    return false;
+                }
+
+                map.put(curr + i, map.get(curr + i) - 1);
+                if (map.get(curr + i) == 0) {
+                    map.remove(curr + i);
+                }
+            }
+        }
+
+        return true;
+    }
+}
