c4c3de2fc4fad983ed748eaf550044e32af6a926
MAZHAR <iiestshibpur605@gmail.com>
2024-06-07 16:02:54 +0530

    Added JAVA code as well

---
+++ Trie/Replace Words.cpp
@@ -0,0 +1,154 @@
+/*    Scroll below to see JAVA code also    */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=EFskdMa-eBg
+    Company Tags                : UBER
+    Leetcode Link               : https://leetcode.com/problems/replace-words
+*/
+
+
+/*************************************************************************** C++ ******************************************************************************************/
+//Using Trie
+//T.C : o(n*l + m*l) , n = number of words in the dictionary, m be the number of words in the sentence, and l be the average length of each word.
+//S.C : O(n*l + m*l)
+class Solution {
+public:
+    struct trieNode { 
+        trieNode *children[26]; 
+        bool isEndOfWord; 
+    };
+    
+    trieNode* getNode() {
+        trieNode* newNode = new trieNode();
+        newNode->isEndOfWord = false;
+        
+        for (int i = 0; i < 26; i++) {
+			newNode->children[i] = NULL;
+		}
+        
+        return newNode;
+    }
+
+            
+    trieNode* root;
+
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        trieNode *crawler = root; 
+        
+        for (int i = 0; i < word.length(); i++) { 
+            int index = word[i] - 'a'; 
+            if (!crawler->children[index]) 
+                crawler->children[index] = getNode();
+            
+            crawler = crawler->children[index]; 
+        }
+        
+        crawler->isEndOfWord = true; 
+    }
+    
+    /** Returns if the word is in the trie. */
+    string search(string word) {
+        trieNode *crawler = root; 
+        
+        for (int i = 0; i < word.length(); i++) { 
+            int index = word[i] - 'a'; 
+            if (!crawler->children[index]) 
+                return word; 
+            crawler = crawler->children[index];
+            if(crawler->isEndOfWord) {
+                return word.substr(0, i+1);
+            }
+        }
+        return word;
+    }
+
+    string replaceWords(vector<string>& dictionary, string sentence) {
+
+        stringstream ss(sentence);
+        string word;
+        string result;
+        root = getNode();
+        for (string word : dictionary) {
+            insert(word);
+        }
+
+        while(getline(ss, word, ' ')) {
+            result += search(word) + " ";
+        }
+
+        result.pop_back();
+        return result;
+    }
+};
+
+
+
+/*************************************************************************** JAVA ******************************************************************************************/
+//Using Trie
+//T.C : o(n*l + m*l) , n = number of words in the dictionary, m be the number of words in the sentence, and l be the average length of each word.
+//S.C : O(n*l + m*l)
+class Solution {
+    class TrieNode {
+        TrieNode[] children;
+        boolean isEndOfWord;
+
+        TrieNode() {
+            children = new TrieNode[26];
+            isEndOfWord = false;
+        }
+    }
+
+    TrieNode root;
+
+    public Solution() {
+        root = new TrieNode();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode crawler = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (crawler.children[index] == null) {
+                crawler.children[index] = new TrieNode();
+            }
+            crawler = crawler.children[index];
+        }
+
+        crawler.isEndOfWord = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    public String search(String word) {
+        TrieNode crawler = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (crawler.children[index] == null) {
+                return word;
+            }
+            crawler = crawler.children[index];
+            if (crawler.isEndOfWord) {
+                return word.substring(0, i + 1);
+            }
+        }
+        return word;
+    }
+
+    public String replaceWords(List<String> dictionary, String sentence) {
+        for (String word : dictionary) {
+            insert(word);
+        }
+
+        StringTokenizer st = new StringTokenizer(sentence);
+        StringJoiner sj = new StringJoiner(" ");
+        
+        while (st.hasMoreTokens()) {
+            String word = st.nextToken();
+            sj.add(search(word));
+        }
+
+        return sj.toString();
+    }
+}
