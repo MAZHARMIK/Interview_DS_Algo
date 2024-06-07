670226e737e340c21ca9936b66344c2111103b0d
MAZHAR <iiestshibpur605@gmail.com>
2024-06-07 09:05:42 +0530

    Added JAVA code as well

---
+++ HashSet/Replace Words.cpp
@@ -0,0 +1,75 @@
+/*    Scroll below to see JAVA code also    */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : UBER
+    Leetcode Link               : https://leetcode.com/problems/replace-words
+*/
+
+
+/*************************************************************************** JAVA ******************************************************************************************/
+//Approach (Using normal hashset and generating all substrings)
+//T.C : o(n*l + m*l^2) , n = number of words in the dictionary, m be the number of words in the sentence, and l be the average length of each word.
+class Solution {
+public:
+
+    string findRoot(string& word, unordered_set<string>& st) {
+
+        //try all length substring starting from 0th index
+        for(int l = 1; l <= word.length(); l++) {
+            string root = word.substr(0, l);
+            if(st.count(root)) {
+                return root;
+            }
+        }
+
+        return word;
+
+    }
+
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        unordered_set<string> st(dictionary.begin(), dictionary.end());
+
+        stringstream ss(sentence);
+        string word;
+        string result;
+
+        while(getline(ss, word, ' ')) {
+            result += findRoot(word, st) + " ";
+        }
+
+        result.pop_back();
+        return result;
+
+    }
+};
+
+
+/*************************************************************************** JAVA ******************************************************************************************/
+//Approach (Using normal hashset and generating all substrings)
+//T.C : o(n*l + m*l^2) , n = number of words in the dictionary, m be the number of words in the sentence, and l be the average length of each word.
+class Solution {
+    private String findRoot(String word, Set<String> st) {
+        // Try all length substring starting from 0th index
+        for (int l = 1; l <= word.length(); l++) {
+            String root = word.substring(0, l);
+            if (st.contains(root)) {
+                return root;
+            }
+        }
+        return word;
+    }
+
+    public String replaceWords(List<String> dictionary, String sentence) {
+        Set<String> st = new HashSet<>(dictionary);
+        
+        StringBuilder result = new StringBuilder();
+        String[] words = sentence.split(" ");
+        
+        for (String word : words) {
+            result.append(findRoot(word, st)).append(" ");
+        }
+        
+        // Remove the trailing space
+        return result.toString().trim();
+    }
+}
