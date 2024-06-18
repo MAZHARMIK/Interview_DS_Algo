44f1d5841185218df891ac513eb2c7cb8a2320fa
MAZHAR <iiestshibpur605@gmail.com>
2024-06-18 11:42:56 +0530

    Added YouTube Link

--- Arrays/Binary Search/Most Profit Assigning Work.cpp
+++ Arrays/Binary Search/Most Profit Assigning Work.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code  also      */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=_HptwlLP8sI
     Comoany Tags                : Netease Games
     Leetcode Link               : https://leetcode.com/problems/most-profit-assigning-work/description/
 */
