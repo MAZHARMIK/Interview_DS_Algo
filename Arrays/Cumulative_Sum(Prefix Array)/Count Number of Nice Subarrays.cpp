9d64b1b494b05bb07125fd9dadaff2ec7b3241eb
MAZHAR <iiestshibpur605@gmail.com>
2024-06-22 17:45:06 +0530

    Added YouTube Link

--- Arrays/Cumulative_Sum(Prefix Array)/Count Number of Nice Subarrays.cpp
+++ Arrays/Cumulative_Sum(Prefix Array)/Count Number of Nice Subarrays.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=JpXlsCAD1kg
     Company Tags                : will soon update
     Leetcode Link               : https://leetcode.com/problems/count-number-of-nice-subarrays
 */
