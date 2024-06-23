9771a909a6671ab6ba1eb305abf4731230c0891a
MAZHAR <iiestshibpur605@gmail.com>
2024-06-23 07:40:18 +0530

    Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit

---
+++ Sliding Window/Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
@@ -0,0 +1,150 @@
+/*     Scroll below to see JAVA code   */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Uber
+    Leetcode Link               : https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/
+*/
+
+
+/******************************************************** C++ ****************************************************************/
+//Approach-1 (Using sliding window + heap)
+//T.C : O(nlogn)
+//S.C : O(n)
+class Solution {
+public:
+    typedef pair<int, int> P;
+
+    int longestSubarray(vector<int>& nums, int limit) {
+        int n = nums.size();
+        priority_queue<P> maxPq;
+        priority_queue<P, vector<P>, greater<P>> minPq;
+
+        int i = 0;
+        int j = 0;
+        int maxLength = 0;
+
+        while (j < n) {
+            maxPq.push({nums[j], j});
+            minPq.push({nums[j], j});
+
+            while (maxPq.top().first - minPq.top().first > limit) {
+                i = min(maxPq.top().second, minPq.top().second) + 1;
+
+                while (maxPq.top().second < i) {
+                    maxPq.pop();
+                }
+                while (minPq.top().second < i) {
+                    minPq.pop();
+                }
+            }
+
+            // Update maxLength with the length of the current valid window
+            maxLength = max(maxLength, j - i + 1);
+            j++;
+        }
+
+        return maxLength;
+    }
+};
+
+
+//Approach-2 (Using sliding window + multiset)
+//T.C : O(nlogn)
+//S.C : O(n)
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        int n = nums.size();
+
+        multiset<int> st;
+        int i = 0;
+        int j = 0;
+        int maxLength = 0;
+
+        while (j < n) {
+            st.insert(nums[j]);
+
+            while (*st.rbegin() - *st.begin() > limit) {
+                st.erase(st.find(nums[i]));
+                ++i;
+            }
+
+            maxLength = max(maxLength, j - i + 1);
+            j++;
+        }
+
+        return maxLength;
+    }
+};
+
+
+
+/******************************************************** JAVA ****************************************************************/
+//Approach-1 (Using sliding window + heap)
+//T.C : O(nlogn)
+//S.C : O(n)
+class Solution {
+    public int longestSubarray(int[] nums, int limit) {
+        int n = nums.length;
+        PriorityQueue<int[]> maxPq = new PriorityQueue<>((a, b) -> b[0] - a[0]); // max-heap
+        PriorityQueue<int[]> minPq = new PriorityQueue<>((a, b) -> a[0] - b[0]); // min-heap
+
+        int i = 0; // Left boundary of the sliding window
+        int j = 0; // Right boundary of the sliding window
+        int maxLength = 0;
+
+        while (j < n) {
+            maxPq.offer(new int[]{nums[j], j});
+            minPq.offer(new int[]{nums[j], j});
+
+            while (maxPq.peek()[0] - minPq.peek()[0] > limit) {
+                i = Math.min(maxPq.peek()[1], minPq.peek()[1]) + 1;
+
+                while (maxPq.peek()[1] < i) {
+                    maxPq.poll();
+                }
+                while (minPq.peek()[1] < i) {
+                    minPq.poll();
+                }
+            }
+
+            // Update maxLength with the length of the current valid window
+            maxLength = Math.max(maxLength, j - i + 1);
+            j++;
+        }
+
+        return maxLength;
+    }
+}
+
+
+//Approach-2 (Using sliding window + multiset)
+//T.C : O(nlogn)
+//S.C : O(n)
+class Solution {
+    public int longestSubarray(int[] nums, int limit) {
+        int n = nums.length;
+
+        TreeMap<Integer, Integer> st = new TreeMap<>();
+        int i = 0;
+        int j = 0;
+        int maxLength = 0;
+
+        while (j < n) {
+            st.put(nums[j], st.getOrDefault(nums[j], 0) + 1);
+
+            while (st.lastKey() - st.firstKey() > limit) {
+                st.put(nums[i], st.get(nums[i]) - 1);
+                if (st.get(nums[i]) == 0) {
+                    st.remove(nums[i]);
+                }
+                i++;
+            }
+
+            maxLength = Math.max(maxLength, j - i + 1);
+            j++;
+        }
+
+        return maxLength;
+    }
+}
