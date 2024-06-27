0c321e8e72767e78e086dfa9fc5ace25fb948516
MAZHAR <iiestshibpur605@gmail.com>
2024-06-27 06:48:48 +0530

    Find Center of Star Graph

---
+++ Graph/Easy/Find Center of Star Graph.cpp
@@ -0,0 +1,87 @@
+/*     Scroll below to see JAVA code also    */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : will soon update
+    Leetcode Link               : https://leetcode.com/problems/find-center-of-star-graph
+*/
+
+
+/************************************************************************ C++ ********************************************************/
+//Approach-1 (Using map)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        int n = edges.size();
+        unordered_map<int, int> degree;
+
+        for(auto &vec : edges) {
+            int u = vec[0];
+            int v = vec[1];
+
+            degree[u]++;
+            degree[v]++;
+        }
+
+        for(auto &it : degree) {
+            int node = it.first;
+            int deg  = it.second;
+
+            if(deg == n)
+                return node;
+        }
+
+        return -1;
+    }
+};
+
+
+//Approach-2 (Constant time)
+//T.C : O(1)
+//S.C : O(1)
+
+
+/************************************************************************ JAVA ********************************************************/
+//Approach-1 (Using map)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public int findCenter(int[][] edges) {
+        Map<Integer, Integer> degree = new HashMap<>();
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            degree.put(u, degree.getOrDefault(u, 0) + 1);
+            degree.put(v, degree.getOrDefault(v, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : degree.entrySet()) {
+            if (entry.getValue() == edges.length) {
+                return entry.getKey(); // star graph center node
+            }
+        }
+
+        return -1;
+    }
+}
+
+
+
+//Approach-2 (Constant time)
+//T.C : O(1)
+//S.C : O(1)
+class Solution {
+    public int findCenter(int[][] edges) {
+        int[] first = edges[0]; // {a, b}
+        int[] second = edges[1]; // {c, a}
+
+        if (first[0] == second[0] || first[0] == second[1]) {
+            return first[0];
+        }
+
+        return first[1];
+    }
+}
