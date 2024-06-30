80fae889c339cd9849ee29a3506ad13dfb65b3d1
MAZHAR <iiestshibpur605@gmail.com>
2024-06-30 12:15:21 +0530

    Added YouTube Link

--- Segment Tree/Sum of Query II.cpp
+++ Segment Tree/Sum of Query II.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=VJ67kQHYbv8
     Company Tags                : AMAZON
     GfG Link                    : https://www.geeksforgeeks.org/problems/sum-of-query-ii5310/1
 */
