656cc387c0233f816437dd0e5f27d840421d87ad
MAZHAR <iiestshibpur605@gmail.com>
2024-06-30 07:24:14 +0530

    Added JAVA code as well

--- Graph/Disjoint Set/Remove Max Number of Edges to Keep Graph Fully Traversable.cpp
+++ Graph/Disjoint Set/Remove Max Number of Edges to Keep Graph Fully Traversable.cpp
@@ -1,10 +1,16 @@
+/*         Scroll below to see JAVA code also        */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=8S5jpVM3kWw
     Company Tags                : GOOGLE
     Leetcode Link               : https://leetcode.com/problems/remove-max-number-of-edges-to-keep-graph-fully-traversable/
 */
 
+
+/**************************************************************** C++ ************************************************/
 //Using DSU
+//T.C : O(m log m) - sorting edges
+//      O(m * α(n)) union-find operations takes O(α(n)) time.The loop over all edges takes O(m) time, with each find and union operation inside the loop taking O(α(n)) time.
+//S.C : O(n)
 class DSU {
     private:
         vector<int> parent;
@@ -108,3 +114,103 @@ public:
         return -1;
     }
 };
+
+
+
+/**************************************************************** JAVA ************************************************/
+//Using DSU
+//T.C : O(m log m) - sorting edges
+//      O(m * α(n)) union-find operations takes O(α(n)) time.The loop over all edges takes O(m) time, with each find and union operation inside the loop taking O(α(n)) time.
+//S.C : O(n)
+class DSU {
+    private int[] parent;
+    private int[] rank;
+    private int components;
+
+    public DSU(int n) {
+        parent = new int[n + 1];
+        rank = new int[n + 1];
+        components = n;
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int find(int x) {
+        if (x == parent[x]) 
+            return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    public void union(int x, int y) {
+        int xParent = find(x);
+        int yParent = find(y);
+
+        if (xParent == yParent) 
+            return;
+
+        if (rank[xParent] > rank[yParent]) {
+            parent[yParent] = xParent;
+        } else if (rank[xParent] < rank[yParent]) {
+            parent[xParent] = yParent;
+        } else {
+            parent[xParent] = yParent;
+            rank[yParent]++;
+        }
+        components--;
+    }
+
+    public boolean isSingle() {
+        return components == 1;
+    }
+}
+
+class Solution {
+    public int maxNumEdgesToRemove(int n, int[][] edges) {
+        Arrays.sort(edges, (v1, v2) -> v2[0] - v1[0]);
+        DSU Alice = new DSU(n);
+        DSU Bob = new DSU(n);
+
+        int addedEdge = 0;
+
+        for (int[] edge : edges) {
+            int type = edge[0];
+            int u = edge[1];
+            int v = edge[2];
+
+            if (type == 3) {
+                boolean add = false;
+
+                if (Alice.find(u) != Alice.find(v)) {
+                    Alice.union(u, v);
+                    add = true;
+                }
+
+                if (Bob.find(u) != Bob.find(v)) {
+                    Bob.union(u, v);
+                    add = true;
+                }
+
+                if (add)
+                    addedEdge++;
+
+            } else if (type == 2) {
+                if (Bob.find(u) != Bob.find(v)) {
+                    Bob.union(u, v);
+                    addedEdge++;
+                }
+            } else {
+                if (Alice.find(u) != Alice.find(v)) {
+                    Alice.union(u, v);
+                    addedEdge++;
+                }
+            }
+        }
+
+        if (Alice.isSingle() && Bob.isSingle()) {
+            return edges.length - addedEdge;
+        }
+
+        return -1;
+    }
+}
