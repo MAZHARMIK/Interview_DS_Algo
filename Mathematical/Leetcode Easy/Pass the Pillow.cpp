ba2f3be2fe67a76b84f362e14368142169a17763
MAZHAR <iiestshibpur605@gmail.com>
2024-07-06 13:53:44 +0530

    Added YouTube Link

--- Mathematical/Leetcode Easy/Pass the Pillow.cpp
+++ Mathematical/Leetcode Easy/Pass the Pillow.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=tI8OO93t2zM
     Company Tags                : Microsoft
     Leetcode Link               : https://leetcode.com/problems/pass-the-pillow/description/
 */
