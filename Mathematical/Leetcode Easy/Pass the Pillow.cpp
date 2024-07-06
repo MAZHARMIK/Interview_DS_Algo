5bb2d8b1da0a035b98eeed7b9e74b78f64c83ed5
MAZHAR <iiestshibpur605@gmail.com>
2024-07-06 12:13:49 +0530

    Added JAVA code as well

---
+++ Mathematical/Leetcode Easy/Pass the Pillow.cpp
@@ -0,0 +1,91 @@
+/*     Scroll below to see JAVA code also    */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Microsoft
+    Leetcode Link               : https://leetcode.com/problems/pass-the-pillow/description/
+*/
+
+
+/************************************************************ C++ ************************************************************/
+//Approach-1 (Simple Simulation)
+//T.C : O(time)
+//S.C : O(1)
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int idx  = 1;
+        int dir  = 1;
+
+        while(time > 0) {
+            if(idx + dir >=1 && idx + dir <= n) {
+                idx += dir;
+                time--;
+            } else {
+                dir *= -1;
+            }
+        }
+        return idx;
+    }
+};
+
+
+
+//Approach-2 (Simple Maths)
+//T.C : O(1)
+//S.C : O(1)
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        
+        int totalFullRounds = time / (n-1);
+
+        int timeLeft        = time % (n-1);
+
+        if(totalFullRounds % 2 == 0) {
+            return timeLeft + 1;
+        } else {
+            return n - timeLeft;
+        }
+
+        return -1;
+    }
+};
+
+
+/************************************************************ JAVA ************************************************************/
+//Approach-1 (Simple Simulation)
+// T.C : O(time)
+// S.C : O(1)
+class Solution {
+    public int passThePillow(int n, int time) {
+        int idx = 1;
+        int dir = 1;
+
+        while (time > 0) {
+            if (idx + dir >= 1 && idx + dir <= n) {
+                idx += dir;
+                time--;
+            } else {
+                dir *= -1;
+            }
+        }
+        return idx;
+    }
+}
+
+
+//Approach-2 (Simple Maths)
+// T.C : O(1)
+// S.C : O(1)
+class Solution {
+    public int passThePillow(int n, int time) {
+        int totalFullRounds = time / (n - 1);
+        int timeLeft = time % (n - 1);
+
+        if (totalFullRounds % 2 == 0) {
+            return timeLeft + 1;
+        } else {
+            return n - timeLeft;
+        }
+    }
+}
