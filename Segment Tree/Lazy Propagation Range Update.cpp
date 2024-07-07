69210aeef0beeaee196a30eb2dceb712c2762b57
MAZHAR <iiestshibpur605@gmail.com>
2024-07-07 21:17:19 +0530

    Lazy Propagation Range Update

---
+++ Segment Tree/Lazy Propagation Range Update.cpp
@@ -0,0 +1,44 @@
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Google and many other companies ask problems based on this
+    GfG Link                    : https://www.geeksforgeeks.org/lazy-propagation-in-segment-tree/
+*/
+
+//NOTE - Below is just the function for updateRange
+//i.e. For each query, we are asked to update values of nodes in range [start....end]
+
+/*************************************************************** C++ ******************************************/
+//T.C : O(logn)
+//S.C : O(n)
+//Assuming we have defined segmentTree[4*n] and lazyTree[4*n]
+void updateRange(int start, int end, int i, int l, int r, int val) {
+    if (lazyTree[i] != 0) {
+        segTree[i] += (r - l + 1) * lazyTree[i];
+      
+        if (l != r) { //Not a leaf node
+            lazyTree[i * 2 + 1] += lazyTree[i]; //Left Child in Lazy Tree
+            lazyTree[i * 2 + 2] += lazyTree[i]; //Right Child in Lazy tree
+        }
+        lazyTree[i] = 0;
+    }
+
+    //Invalid or out of range
+    if (l > end || r < start || l > r)
+        return;
+
+    //[start...end[ is Exactly withing range of current nod [l..r]
+    if (l >= start && r <= end) {
+        segTree[i] += (r - l + 1) * val;
+        if (l != r) {
+            lazyTree[i * 2 + 1] += val;
+            lazyTree[i * 2 + 2] += val;
+        }
+        return;
+    }
+
+    //Call for left and right subtree
+    int mid = (l + r) / 2;
+    updateRange(start, end, 2*i+1, l, mid, val);
+    updateRange(start, end, 2*i+2, mid+1, r, val);
+    segTree[i] = segTree[i * 2 + 1] + segTree[i * 2 + 2];
+}
