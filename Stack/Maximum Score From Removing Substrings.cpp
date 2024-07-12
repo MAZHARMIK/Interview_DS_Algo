886eb56edd3e27f7703bdc05b37db78ca0e1b273
MAZHAR <iiestshibpur605@gmail.com>
2024-07-12 15:25:41 +0530

    Added JAVA code as well

--- Stack/Maximum Score From Removing Substrings.cpp
+++ Stack/Maximum Score From Removing Substrings.cpp
@@ -110,3 +110,89 @@ public:
 //Approach-1 (Using Stack)
 //T.C : O(n)
 //S.C : O(n)
+import java.util.Stack;
+
+class Solution {
+    public int maximumGain(String s, int x, int y) {
+        int n = s.length();
+        int score = 0;
+
+        String maxStr = (x > y) ? "ab" : "ba";
+        String minStr = (x < y) ? "ab" : "ba";
+
+        // First Pass
+        String tempFirst = removeSubstring(s, maxStr);
+        int removedPairsCount = (n - tempFirst.length()) / 2;
+        score += removedPairsCount * Math.max(x, y);
+
+        // Second Pass
+        String tempSecond = removeSubstring(tempFirst, minStr);
+        removedPairsCount = (tempFirst.length() - tempSecond.length()) / 2;
+        score += removedPairsCount * Math.min(x, y);
+
+        return score;
+    }
+
+    private String removeSubstring(String s, String matchStr) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty() && ch == matchStr.charAt(1) && stack.peek() == matchStr.charAt(0)) {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        StringBuilder remainStr = new StringBuilder();
+        while (!stack.isEmpty()) {
+            remainStr.append(stack.pop());
+        }
+        return remainStr.reverse().toString();
+    }
+}
+
+
+
+//Approach-2 (Without Stack)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public int maximumGain(String s, int x, int y) {
+        int n = s.length();
+        int score = 0;
+
+        String maxStr = (x > y) ? "ab" : "ba";
+        String minStr = (x < y) ? "ab" : "ba";
+
+        // First Pass
+        String tempFirst = removeSubstring(s, maxStr);
+        int L = tempFirst.length();
+        int removedPairsCount = (n - L) / 2;
+        score += removedPairsCount * Math.max(x, y);
+
+        // Second Pass
+        String tempSecond = removeSubstring(tempFirst, minStr);
+        removedPairsCount = (L - tempSecond.length()) / 2;
+        score += removedPairsCount * Math.min(x, y);
+
+        return score;
+    }
+
+    private String removeSubstring(String inputString, String matchStr) {
+        StringBuilder sb = new StringBuilder();
+        int j = 0;
+
+        for (int i = 0; i < inputString.length(); i++) {
+            sb.append(inputString.charAt(i));
+            j++;
+
+            if (j > 1 && sb.charAt(j - 2) == matchStr.charAt(0) && sb.charAt(j - 1) == matchStr.charAt(1)) {
+                sb.delete(j - 2, j);
+                j -= 2;
+            }
+        }
+
+        return sb.toString();
+    }
+}
