14e5942bff3706dbd0496c1b8140e02ddae820e1
MAZHAR <iiestshibpur605@gmail.com>
2024-07-13 10:52:51 +0530

    Robot Collisions

---
+++ Stack/Robot Collisions.cpp
@@ -0,0 +1,112 @@
+/*     Scroll below to see JAVA Code also    */
+/*
+      MY YOUTUBE VIDEO ON THIS Qn : 
+      Company Tags                : Will update soon
+      Leetcode Link               : https://leetcode.com/problems/robot-collisions/description
+*/
+
+
+/*********************************************************************** C++ ********************************************************************************/
+//Using Stack
+//T.C : O(nlogn)
+//T.C : O(n)
+class Solution {
+public:
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        int n = positions.size();
+        vector<int> j(n);
+        
+        iota(j.begin(), j.end(), 0); //This will fill the array as -> 0, 1, 2, 3, 4, n-1
+        stack<int> st;
+
+        auto lambda = [&](int i, int j) {
+            return positions[i] < positions[j];
+        };
+
+        sort(j.begin(), j.end(), lambda);
+
+        vector<int> result;
+        for (int currentIndex : j) {
+            if (directions[currentIndex] == 'R') {
+                st.push(currentIndex);
+            } else {
+                while (!st.empty() && healths[currentIndex] > 0) {
+                    int topIndex = st.top();
+                    st.pop();
+
+                    if (healths[topIndex] > healths[currentIndex]) {
+                        healths[topIndex] -= 1;
+                        healths[currentIndex] = 0;
+                        st.push(topIndex);
+                    } else if (healths[topIndex] < healths[currentIndex]) {
+                        healths[currentIndex] -= 1;
+                        healths[topIndex] = 0;
+                    } else {
+                        healths[currentIndex] = 0;
+                        healths[topIndex] = 0;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if (healths[i] > 0) {
+                result.push_back(healths[i]);
+            }
+        }
+        return result;
+    }
+};
+
+
+
+/*********************************************************************** JAVA ********************************************************************************/
+//Using Stack
+//T.C : O(nlogn)
+//T.C : O(n)
+class Solution {
+    public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+        int n = positions.length;
+        Integer[] j = new Integer[n];
+        
+        for (int i = 0; i < n; i++) {
+            j[i] = i;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        Arrays.sort(j, (i, k) -> Integer.compare(positions[i], positions[k]));
+
+        List<Integer> result = new ArrayList<>();
+        
+        for (int currentIndex : j) {
+            if (directions.charAt(currentIndex) == 'R') {
+                stack.push(currentIndex);
+            } else {
+                while (!stack.isEmpty() && healths[currentIndex] > 0) {
+                    int topIndex = stack.pop();
+
+                    if (healths[topIndex] > healths[currentIndex]) {
+                        healths[topIndex] -= 1;
+                        healths[currentIndex] = 0;
+                        stack.push(topIndex);
+                    } else if (healths[topIndex] < healths[currentIndex]) {
+                        healths[currentIndex] -= 1;
+                        healths[topIndex] = 0;
+                    } else {
+                        healths[currentIndex] = 0;
+                        healths[topIndex] = 0;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (healths[i] > 0) {
+                result.add(healths[i]);
+            }
+        }
+
+        return result;
+    }
+}
