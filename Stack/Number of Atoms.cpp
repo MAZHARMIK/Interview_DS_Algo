0cfe583d988844ff3a64039a7105e6aa5d7b6ac8
MAZHAR <iiestshibpur605@gmail.com>
2024-07-14 08:16:35 +0530

    Number of Atoms

---
+++ Stack/Number of Atoms.cpp
@@ -0,0 +1,143 @@
+/*     Scroll below to see JAVA code also    */
+/*
+        MY YOUTUBE VIDEO ON THIS Qn : 
+        Company Tags                : GOOGLE
+        Leetcode Link               : https://leetcode.com/problems/number-of-atoms
+*/
+
+
+/*************************************************************************** C++ *****************************************************************/
+//Using stack
+//T.C : O(n^2)
+//S.C : O(n)
+class Solution {
+public:
+    typedef unordered_map<string, int> MAP;
+
+    string countOfAtoms(string formula) {
+        int n = formula.length();
+        
+        stack<MAP> st;
+        st.push(MAP());
+
+        int i = 0;
+
+        while (i < n) {
+            if (formula[i] == '(') {
+                st.push(MAP());
+                i++;
+            } else if (formula[i] == ')') {
+                MAP currMap = st.top();
+                st.pop();
+                i++;
+                string multiplier;
+                while (i < formula.length() && isdigit(formula[i])) {
+                    multiplier += formula[i];
+                    i++;
+                }
+                if (!multiplier.empty()) {
+                    int mult = stoi(multiplier);
+                    for (auto& [atom, count] : currMap) {
+                        currMap[atom] = count * mult;
+                    }
+                }
+
+                for (auto& [atom, count] : currMap) {
+                    st.top()[atom] += count;
+                }
+            } else {
+                string currAtom;
+                currAtom += formula[i];
+                i++;
+                while (i < formula.length() && islower(formula[i])) {
+                    currAtom += formula[i];
+                    i++;
+                }
+
+                string currCount;
+                while (i < formula.length() && isdigit(formula[i])) {
+                    currCount += formula[i];
+                    i++;
+                }
+
+                int count = currCount.empty() ? 1 : stoi(currCount);
+                st.top()[currAtom] += count;
+            }
+ 
+        }
+        
+        map<string, int> sortedMap(begin(st.top()), end(st.top()));
+
+        string result;
+        for (auto& [atom, count] : sortedMap) {
+            result += atom;
+            if (count > 1) {
+                result += to_string(count);
+            }
+        }
+
+        return result;
+    }
+};
+
+
+/*************************************************************************** JAVA *****************************************************************/
+//Using stack
+//T.C : O(n^2)
+//S.C : O(n)
+class Solution {
+    public String countOfAtoms(String formula) {
+        int n = formula.length();
+
+        Stack<Map<String, Integer>> stack = new Stack<>();
+        
+        stack.push(new HashMap<>());
+
+        int i = 0;
+        while (i < n) {
+            if (formula.charAt(i) == '(') {
+                stack.push(new HashMap<>());
+                i++;
+            } else if (formula.charAt(i) == ')') {
+                Map<String, Integer> top = stack.pop();
+                i++;
+                StringBuilder sb = new StringBuilder();
+                while (i < n && Character.isDigit(formula.charAt(i))) {
+                    sb.append(formula.charAt(i));
+                    i++;
+                }
+                int multiplier = sb.length() > 0 ? Integer.parseInt(sb.toString()) : 1;
+                for (String key : top.keySet()) {
+                    int value = top.get(key);
+                    top.put(key, value * multiplier);
+                }
+                for (String key : top.keySet()) {
+                    stack.peek().put(key, stack.peek().getOrDefault(key, 0) + top.get(key));
+                }
+            } else {
+                StringBuilder element = new StringBuilder();
+                element.append(formula.charAt(i++));
+                while (i < n && Character.isLowerCase(formula.charAt(i))) {
+                    element.append(formula.charAt(i++));
+                }
+                StringBuilder sb = new StringBuilder();
+                while (i < n && Character.isDigit(formula.charAt(i))) {
+                    sb.append(formula.charAt(i++));
+                }
+                int count = sb.length() > 0 ? Integer.parseInt(sb.toString()) : 1;
+                stack.peek().put(element.toString(), stack.peek().getOrDefault(element.toString(), 0) + count);
+            }
+        }
+
+        TreeMap<String, Integer> sortedMap = new TreeMap<>(stack.peek());
+        StringBuilder result = new StringBuilder();
+        for (String key : sortedMap.keySet()) {
+            result.append(key);
+            int count = sortedMap.get(key);
+            if (count > 1) {
+                result.append(count);
+            }
+        }
+        return result.toString();
+    }
+}
