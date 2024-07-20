3d071e314776e607ecbdecb7053faadf845d6130
MAZHAR <iiestshibpur605@gmail.com>
2024-07-20 08:51:11 +0530

    Added YouTube Link

--- Arrays/2-D Array/Find Valid Matrix Given Row and Column Sums.cpp
+++ Arrays/2-D Array/Find Valid Matrix Given Row and Column Sums.cpp
@@ -1,6 +1,6 @@
 /*         Scroll below to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=9dKdLNlmxco
     Company Tags                : Facebook, Uber, Google, Microsoft
     Leetcode Link               : https://leetcode.com/problems/find-valid-matrix-given-row-and-column-sums/
 */
