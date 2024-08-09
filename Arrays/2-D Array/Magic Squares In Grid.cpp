4a364fcc1ed2fcfc83fbaa867235c701afb202af
MAZHAR <iiestshibpur605@gmail.com>
2024-08-09 17:54:25 +0530

    Added YouTube Link

--- Arrays/2-D Array/Magic Squares In Grid.cpp
+++ Arrays/2-D Array/Magic Squares In Grid.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also   */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=KgiW_AtaBPo
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/magic-squares-in-grid
 */
@@ -12,44 +12,58 @@
 //S.C : O(1)
 class Solution {
 public:
-    bool isMagicSquare(vector<vector<int>>& grid, int r, int c) {
-        // Must have Unique numbers between 1 and 9
-        unordered_set<int> nums;
-        for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
-                int num = grid[r + i][c + j];
-                if (num < 1 || num > 9 || nums.count(num)) {
+
+    bool isMagicGrid(vector<vector<int>>& grid, int r, int c) {
+        //uniqueness 1 to 9 and distinct
+        unordered_set<int> st;
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                int num = grid[r+i][c+j];
+                if(num < 1 || num > 9 || st.count(num)) {
                     return false;
                 } else {
-                    nums.insert(num);
+                    st.insert(num);
                 }
             }
         }
 
-        // Check sums
-        int sum = grid[r][c] + grid[r][c + 1] + grid[r][c + 2];
-        for (int i = 0; i < 3; ++i) {
-            if (grid[r + i][c] + grid[r + i][c + 1] + grid[r + i][c + 2] != sum || //Row sum
-                grid[r][c + i] + grid[r + 1][c + i] + grid[r + 2][c + i] != sum) { //Col sum
+        //check Sum - Rows and columns
+        int SUM = grid[r][c] + grid[r][c+1] + grid[r][c+2];
+        for(int i = 0; i < 3; i++) {
+            if(grid[r+i][c] + grid[r+i][c+1] + grid[r+i][c+2] != SUM) {
+                return false;
+            }
+
+            if(grid[r][c+i] + grid[r+1][c+i] + grid[r+2][c+i] != SUM) {
                 return false;
             }
         }
 
-        if (grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2] != sum || // Diagonal
-            grid[r][c + 2] + grid[r + 1][c + 1] + grid[r + 2][c] != sum) { // Anti Diagonal
+
+        //diaornal and anti-diagonal
+        if(grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2] != SUM) {
             return false;
         }
 
+        if(grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c] != SUM) {
+            return false;
+        }
+        
+
         return true;
+
+
     }
 
     int numMagicSquaresInside(vector<vector<int>>& grid) {
-        int count = 0;
-        int rows = grid.size(), cols = grid[0].size();
+        int rows = grid.size();
+        int cols = grid.size();
 
-        for (int i = 0; i <= rows - 3; i++) {
-            for (int j = 0; j <= cols - 3; j++) {
-                if (isMagicSquare(grid, i, j)) {
+        int count = 0;
+
+        for(int i = 0; i <= rows-3; i++) {
+            for(int j = 0; j <= cols-3; j++) {
+                if(isMagicGrid(grid, i, j)) {
                     count++;
                 }
             }
@@ -58,7 +72,6 @@ public:
         return count;
     }
 };
-
 
 
 //Approach-2 (Using Maths and Observation) - LET ME KNOW IF YOU NEED THIS VIDEO AS WELL
