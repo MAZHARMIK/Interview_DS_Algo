d1fbc4fa39567650c3d40853232c84497ae2d7c1
Bhavdeep Singh Nijhawan <143419096+BhavdeepSinghNijhawan@users.noreply.github.com>
2024-08-12 10:21:09 +0530

    Add Java Code

--- Heap/Kth Largest Element in a Stream.cpp
+++ Heap/Kth Largest Element in a Stream.cpp
@@ -5,6 +5,7 @@
 */
 
 //Approach - (Using min-heap)
+// C++
 class KthLargest {
 public:
     priority_queue<int, vector<int>, greater<int>> pq;
@@ -29,3 +30,35 @@ public:
         return pq.top();
     }
 };
+
+// Java
+/*
+class KthLargest {
+    private PriorityQueue<Integer> pq;
+    private int K;
+    public KthLargest(int k, int[] nums) {
+        K = k;
+        pq = new PriorityQueue<>();
+        for (int num : nums) {
+            pq.offer(num);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+    }
+
+    public int add(int val) {
+        pq.offer(val);
+        if (pq.size() > K) {
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
+*/
