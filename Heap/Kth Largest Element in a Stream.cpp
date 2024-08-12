e6929cfcfdae8ba5a098bd47d4efe577ff3ccbee
MAZHAR <iiestshibpur605@gmail.com>
2024-08-12 17:04:25 +0530

    Added JAVA code as well

--- Heap/Kth Largest Element in a Stream.cpp
+++ Heap/Kth Largest Element in a Stream.cpp
@@ -1,10 +1,15 @@
+/*     Scroll below to see JAVA code also        */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=3Bge5OCcXUk
     Company Tags                : Amazon
     Leetcode Qn Link            : https://leetcode.com/problems/kth-largest-element-in-a-stream/
 */
 
+
+/*************************************************************** C++ ***************************************************************/
 //Approach - (Using min-heap)
+//T.C : O(n*logK)
+//S.C : O(K)
 class KthLargest {
 public:
     priority_queue<int, vector<int>, greater<int>> pq;
@@ -29,3 +34,34 @@ public:
         return pq.top();
     }
 };
+
+
+
+/*************************************************************** JAVA ***************************************************************/
+//Approach - (Using min-heap)
+//T.C : O(n*logK)
+//S.C : O(K)
+class KthLargest {
+    private PriorityQueue<Integer> pq;
+    private int K;
+
+    public KthLargest(int k, int[] nums) {
+        K = k;
+        pq = new PriorityQueue<>(K);
+
+        for (int num : nums) {
+            pq.offer(num);
+            if (pq.size() > K) {
+                pq.poll();
+            }
+        }
+    }
+
+    public int add(int val) {
+        pq.offer(val);
+        if (pq.size() > K) {
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
