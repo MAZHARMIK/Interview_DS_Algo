2f63009c1f00dcb7b0b93864811b106466bbd8fc
MAZHAR <iiestshibpur605@gmail.com>
2024-08-29 11:00:50 +0530

    Added JAVA code as well

--- Graph/Disjoint Set/Most Stones Removed with Same Row or Column.cpp
+++ Graph/Disjoint Set/Most Stones Removed with Same Row or Column.cpp
@@ -1,3 +1,4 @@
+/*         Scroll below to see JAVA code also        */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : <soon>
     Company Tags                : Google
@@ -7,6 +8,9 @@
 */
 
 
+/******************************************************** C++ ********************************************************/
+//T.C : O(n^2 * α(n))
+//S.C : O(n)
 class Solution {
 public:
     vector<int> parent;
@@ -60,3 +64,70 @@ public:
         return n-groups;
     }
 };
+
+
+/******************************************************** JAVA ********************************************************/
+//T.C : O(n^2 * α(n))
+//S.C : O(n)
+class Solution {
+    private int[] parent;
+    private int[] rank;
+    private int n;
+
+    // Find function with path compression
+    private int find(int i) {
+        if (parent[i] != i) {
+            parent[i] = find(parent[i]); // Path compression
+        }
+        return parent[i];
+    }
+
+    // Union function with union by rank
+    private void union(int i, int j) {
+        int rootI = find(i);
+        int rootJ = find(j);
+        
+        if (rootI != rootJ) {
+            if (rank[rootI] > rank[rootJ]) {
+                parent[rootJ] = rootI;
+            } else if (rank[rootI] < rank[rootJ]) {
+                parent[rootI] = rootJ;
+            } else {
+                parent[rootJ] = rootI;
+                rank[rootI]++;
+            }
+        }
+    }
+
+    public int removeStones(int[][] stones) {
+        n = stones.length;
+        parent = new int[n];
+        rank = new int[n];
+
+        // Initialize parent and rank arrays
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+
+        // Union stones that are in the same row or column
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                    union(i, j);
+                }
+            }
+        }
+
+        // Count the number of disjoint sets (connected components)
+        int groups = 0;
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == i) {
+                groups++;
+            }
+        }
+
+        // The number of stones that can be removed is total stones - number of groups
+        return n - groups;
+    }
+}
