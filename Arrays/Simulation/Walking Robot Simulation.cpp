6f873dc16d81bc17d94a85315277042fcfaf877c
MAZHAR <iiestshibpur605@gmail.com>
2024-09-04 15:19:34 +0530

    Added YouTube Link

--- Arrays/Simulation/Walking Robot Simulation.cpp
+++ Arrays/Simulation/Walking Robot Simulation.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-      MY YOUTUBE VIDEO ON THIS Qn : 
+      MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=K8lMDgaAn4w
       Company Tags                : Jane Street
       Leetcode Link               : https://leetcode.com/problems/walking-robot-simulation/description/
 */
