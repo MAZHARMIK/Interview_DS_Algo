82547eae578ef9cfb11c994154ff8ffc686e0285
MAZHAR <iiestshibpur605@gmail.com>
2024-09-04 14:15:47 +0530

    Update Walking Robot Simulation.cpp

--- Arrays/Simulation/Walking Robot Simulation.cpp
+++ Arrays/Simulation/Walking Robot Simulation.cpp
@@ -1,7 +1,7 @@
 /*     Scroll below to see JAVA code also    */
 /*
       MY YOUTUBE VIDEO ON THIS Qn : 
-      Company Tags                : will update soon
+      Company Tags                : Jane Street
       Leetcode Link               : https://leetcode.com/problems/walking-robot-simulation/description/
 */
 
