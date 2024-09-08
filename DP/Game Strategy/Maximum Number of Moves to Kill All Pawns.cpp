b9c095ba232d1ed14b9ba96184d2427cfae23668
MAZHAR <iiestshibpur605@gmail.com>
2024-09-08 18:49:34 +0530

    Added YouTube Link\

--- DP/Game Strategy/Maximum Number of Moves to Kill All Pawns.cpp
+++ DP/Game Strategy/Maximum Number of Moves to Kill All Pawns.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=pz_slLUY7z0
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/maximum-number-of-moves-to-kill-all-pawns/
 */
