4bc08ae275575d523f5371cfabdfb8cdee2aeff3
MAZHAR <iiestshibpur605@gmail.com>
2024-09-13 09:16:57 +0530

    Added YouTube Link

--- Bit_Magic/XOR Queries of a Subarray.cpp
+++ Bit_Magic/XOR Queries of a Subarray.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=rZYdNpHXz0o
     Company Tags                : will soon update
     Leetcode Link               : https://leetcode.com/problems/xor-queries-of-a-subarray
 */
