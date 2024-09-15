5a1a466bbca4a339e6fc210aaae9009cedbb288b
Bhavdeep Singh Nijhawan <143419096+BhavdeepSinghNijhawan@users.noreply.github.com>
2024-09-15 22:23:48 +0530

    Add Java Code

--- Sliding Window/K Radius Subarray Averages.cpp
+++ Sliding Window/K Radius Subarray Averages.cpp
@@ -54,3 +54,44 @@ public:
         return result;
     }
 };
+
+/***************************************************************************Java*************************************************************************/
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] getAverages(int[] nums, int k) {
+        int n = nums.length;
+        if (k == 0) {
+            return nums;
+        }
+        int[] result = new int[n];
+        Arrays.fill(result, -1);
+        if (n < 2 * k + 1) {
+            return result;
+        }
+        long windowSum = 0;
+        int left = 0;
+        int right = 2 * k;
+        int i = k;
+        for (int j = left; j <= right; j++) {
+            windowSum += nums[j];
+        }
+        result[i] = (int) (windowSum / (2 * k + 1));
+        i++;
+        right++;
+        while (right < n) {
+            int outOfWindow = nums[left];
+            int cameToWindow = nums[right];
+            windowSum = windowSum - outOfWindow + cameToWindow;
+            result[i] = (int) (windowSum / (2 * k + 1));
+            i++;
+            left++;
+            right++;
+        }
+        return result;
+    }
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
