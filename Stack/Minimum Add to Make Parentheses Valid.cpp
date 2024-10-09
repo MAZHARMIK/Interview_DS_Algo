3c3f3c89a2b314ff19b843c858398918b06a0cf3
MAZHAR <iiestshibpur605@gmail.com>
2024-10-09 09:18:55 +0530

    Added YouTube Link

--- Stack/Minimum Add to Make Parentheses Valid.cpp
+++ Stack/Minimum Add to Make Parentheses Valid.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also   */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=lIdHFWyIrYE
     Company Tags                : Meta
     Leetcode Link               : https://leetcode.com/problems/minimum-add-to-make-parentheses-valid
 */
