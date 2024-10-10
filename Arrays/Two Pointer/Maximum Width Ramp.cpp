31ec698e335a86c3f8b5cde6255b1145af61d2ad
MAZHAR <iiestshibpur605@gmail.com>
2024-10-10 19:38:53 +0530

    Added YouTube Link

--- Arrays/Two Pointer/Maximum Width Ramp.cpp
+++ Arrays/Two Pointer/Maximum Width Ramp.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also   */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=mcJBhWXel-Y
     Company Tags                : Google, Amazon
     Leetcode Link               : https://leetcode.com/problems/maximum-width-ramp
 */
