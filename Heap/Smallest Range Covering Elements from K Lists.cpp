bef6cfdebbe543445b569cf764dc4a36aaac1648
MAZHAR <iiestshibpur605@gmail.com>
2024-10-13 16:19:16 +0530

    Added YouTube Link

--- Heap/Smallest Range Covering Elements from K Lists.cpp
+++ Heap/Smallest Range Covering Elements from K Lists.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=0IqFMBatlhU
     Company Tags                : will soon update
     Leetcode Link               : https://leetcode.com/problems/smallest-range-covering-elements-from-k-lists
 */
