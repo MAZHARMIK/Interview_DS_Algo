6b6aca2ca14d8baa1a976aa79ce229ab8915b3c4
MAZHAR <iiestshibpur605@gmail.com>
2024-10-19 13:08:57 +0530

    Added YouTube Link

--- Recursion/Find Kth Bit in Nth Binary String.cpp
+++ Recursion/Find Kth Bit in Nth Binary String.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=z4gmEqrhrkE
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/find-kth-bit-in-nth-binary-string
 */
