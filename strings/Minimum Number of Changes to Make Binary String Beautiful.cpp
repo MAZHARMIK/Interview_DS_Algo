89412dfa7cdd8c9940c77b64ecd975501b392865
MAZHAR <iiestshibpur605@gmail.com>
2024-11-05 15:03:43 +0530

    Added YouTube Link

--- strings/Minimum Number of Changes to Make Binary String Beautiful.cpp
+++ strings/Minimum Number of Changes to Make Binary String Beautiful.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=jvRiCOEflXA
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/minimum-number-of-changes-to-make-binary-string-beautiful
 */
