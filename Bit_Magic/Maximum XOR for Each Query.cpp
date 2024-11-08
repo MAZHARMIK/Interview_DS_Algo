177ae035a4be53e1b3eaa5168913e9fe25dba006
MAZHAR <iiestshibpur605@gmail.com>
2024-11-08 10:35:52 +0530

    Added YouTube Link

--- Bit_Magic/Maximum XOR for Each Query.cpp
+++ Bit_Magic/Maximum XOR for Each Query.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=qDtW0HRvNc0
     Company Tags                : will soon update
     Leetcode Link               : https://leetcode.com/problems/maximum-xor-for-each-query
 */
