e462179d7db86c08078928a2097ab2be67432cd8
MAZHAR <iiestshibpur605@gmail.com>
2024-11-09 09:16:31 +0530

    Added JAVA code as well

---
+++ Bit_Magic/Minimum Array End.cpp
@@ -0,0 +1,44 @@
+/*     Scroll below to see JAVA code also    */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : will soon update
+    Leetcode Link               : https://leetcode.com/problems/minimum-array-end/description/
+*/
+
+/******************************************************** C++ ********************************************************/
+//Approach - Simple using AND and OR
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        long long num = x;
+
+        for(int i = 1; i < n; i++) {
+
+            //num+1
+            num = (num+1) | x;
+            
+        }
+
+        return num;
+    }
+};
+
+
+/******************************************************** JAVA ********************************************************/
+//Approach - Simple using AND and OR
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public long minEnd(int n, int x) {
+        long num = x;
+
+        for (int i = 1; i < n; i++) {
+            // Increment num and perform bitwise OR with x
+            num = (num + 1) | x;
+        }
+
+        return num;
+    }
+}
