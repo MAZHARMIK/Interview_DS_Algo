f366eb7df9a8403f111b81ac77648c1b19ddf4dd
MAZHAR <iiestshibpur605@gmail.com>
2024-11-13 12:26:17 +0530

    Added YouTube Link

--- Arrays/Binary Search/Count the Number of Fair Pairs.cpp
+++ Arrays/Binary Search/Count the Number of Fair Pairs.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=r3EnymXRC9A
     Company Tags                : Will update soon
     Leetcode Link               : https://leetcode.com/problems/count-the-number-of-fair-pairs
 */
