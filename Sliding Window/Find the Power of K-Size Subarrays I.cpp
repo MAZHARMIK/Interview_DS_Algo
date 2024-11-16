fb292c96792dcaf123c420cc80a9a88e839d039d
MAZHAR <iiestshibpur605@gmail.com>
2024-11-16 10:43:27 +0530

    Added JAVA code as well

---
+++ Sliding Window/Find the Power of K-Size Subarrays I.cpp
@@ -0,0 +1,108 @@
+/*     Scroll below to see JAVA code also    */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Will update soon
+    Leetcode Link               : https://leetcode.com/problems/find-the-power-of-k-size-subarrays-i
+*/
+
+
+/**************************************************************** C++ ****************************************************************/
+//Approach (Using sliding window)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<int> result(n-k+1, -1);
+
+        int count = 1; //count of consecutive elements
+
+        //preprocess the first window
+        for(int i = 1; i < k; i++) {
+            if(nums[i] == nums[i-1]+1) {
+                count++;
+            } else {
+                count = 1;
+            }
+        }
+
+        if(count == k) {
+            result[0] = nums[k-1];
+        }
+
+        int i = 1;
+        int j = k;
+
+        while(j < n) {
+            if(nums[j] == nums[j-1]+1) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if(count >= k) {
+                result[i] = nums[j];
+            }
+
+            i++;
+            j++;
+        }
+
+        return result;
+
+    }
+};
+
+
+/**************************************************************** JAVA ****************************************************************/
+//Approach (Using sliding window)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public int[] resultsArray(int[] nums, int k) {
+        int n = nums.length;
+
+        int[] result = new int[n - k + 1];
+        // Initialize the result array with -1
+        java.util.Arrays.fill(result, -1);
+
+        int count = 1; // Count of consecutive elements
+
+        // Preprocess the first window
+        for (int i = 1; i < k; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                count++;
+            } else {
+                count = 1;
+            }
+        }
+
+        // Check if the first window satisfies the condition
+        if (count == k) {
+            result[0] = nums[k - 1];
+        }
+
+        int i = 1;
+        int j = k;
+
+        // Process the sliding window
+        while (j < n) {
+            if (nums[j] == nums[j - 1] + 1) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if (count >= k) {
+                result[i] = nums[j];
+            }
+
+            i++;
+            j++;
+        }
+
+        return result;
+    }
+}
