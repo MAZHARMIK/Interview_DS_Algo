329d1cdea5df1132730dd9b191c5c5e1e92fe372
MAZHAR <iiestshibpur605@gmail.com>
2024-11-16 16:42:41 +0530

    Added JAVA code as well

--- Sliding Window/Find the Power of K-Size Subarrays I.cpp
+++ Sliding Window/Find the Power of K-Size Subarrays I.cpp
@@ -1,13 +1,14 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=P1_yXMhqC50
+    MY YOUTUBE VIDEO ON THIS Qn : Approach-1 : https://www.youtube.com/watch?v=P1_yXMhqC50
+                                  Approach-2 : 
     Company Tags                : Will update soon
     Leetcode Link               : https://leetcode.com/problems/find-the-power-of-k-size-subarrays-i
 */
 
 
 /**************************************************************** C++ ****************************************************************/
-//Approach (Using sliding window)
+//Approach-1 (Using sliding window)
 //T.C : O(n)
 //S.C : O(1)
 class Solution {
@@ -56,6 +57,42 @@ public:
 };
 
 
+//Approach-2 (Using monotonic deque)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        deque<int> deq; //Monotonic deque
+        vector<int> result;
+
+        for(int j = 0; j < n; j++) {
+
+            if(deq.size() == k) {
+                deq.pop_front(); //deq ka size ab k-1, ab jth element ko hum insert karne ka soch sakte hain deque me
+            }
+
+            if(!deq.empty() && deq.back() != nums[j]-1) { //nums[j] is not consecutive to deq.back() element
+                deq.clear();
+            }
+
+            deq.push_back(nums[j]);
+
+            if(j >= k-1) {
+                if(deq.size() == k) {
+                    result.push_back(deq.back()); //max element hoga becau monotonic hai
+                } else {
+                    result.push_back(-1); //else -1
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
 /**************************************************************** JAVA ****************************************************************/
 //Approach (Using sliding window)
 //T.C : O(n)
@@ -101,6 +138,46 @@ class Solution {
 
             i++;
             j++;
+        }
+
+        return result;
+    }
+}
+
+
+//Approach-2 (Using monotonic deque)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public int[] resultsArray(int[] nums, int k) {
+        int n = nums.length;
+
+        Deque<Integer> deq = new LinkedList<>(); // Monotonic deque
+        int[] result = new int[n - k + 1];
+        int index = 0;
+
+        for (int j = 0; j < n; j++) {
+            // If deque size equals k, remove the front element
+            if (deq.size() == k) {
+                deq.pollFirst();
+            }
+
+            // If deque is not empty and current element is not consecutive to the last element
+            if (!deq.isEmpty() && deq.peekLast() != nums[j] - 1) {
+                deq.clear();
+            }
+
+            // Add the current element to the deque
+            deq.offerLast(nums[j]);
+
+            // Once we process the first k elements
+            if (j >= k - 1) {
+                if (deq.size() == k) {
+                    result[index++] = deq.peekLast(); // Last element is the max due to monotonic property
+                } else {
+                    result[index++] = -1; // Otherwise, add -1
+                }
+            }
         }
 
         return result;
