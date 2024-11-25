cc8cf729df6a2ff1821dc3317d96fe95b30fe7a9
MAZHAR <iiestshibpur605@gmail.com>
2024-11-25 08:47:03 +0530

    Added JAVA code as well

--- Arrays/2-D Array/Sliding Puzzle.cpp
+++ Arrays/2-D Array/Sliding Puzzle.cpp
@@ -13,123 +13,126 @@
 class Solution {
 public:
     int slidingPuzzle(vector<vector<int>>& board) {
-        string target = "123450";
         string start = "";
-        
-        // Convert 2D board to a string
-        for (auto& row : board) {
-            for (int num : row) {
-                start += to_string(num);
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < 3; j++) {
+                start += to_string(board[i][j]);
             }
         }
-        
-        unordered_set<string> visited;
-        vector<vector<int>> dirs = {
-            {1, 3},        // 0 can swap with 1 and 3
-            {0, 2, 4},     // 1 can swap with 0, 2, and 4
-            {1, 5},        // 2 can swap with 1 and 5
-            {0, 4},        // 3 can swap with 0 and 4
-            {1, 3, 5},     // 4 can swap with 1, 3, and 5
-            {2, 4}         // 5 can swap with 2 and 4
-        };
+        //positions = 6
+        //Total possible states = 6!
+        //Worst case me visit all states = O(6!)
+        //Space : O(6!)
+        string target = "123450";
 
         queue<string> que;
         que.push(start);
+        unordered_map<int, vector<int>> mp;
+        mp[0] = {1, 3};
+        mp[1] = {0, 2, 4};
+        mp[2] = {1, 5};
+        mp[3] = {0, 4};
+        mp[4] = {1, 3, 5};
+        mp[5] = {2, 4};
+
+        unordered_set<string> visited;
         visited.insert(start);
 
-        int steps = 0;
-
-        while (!que.empty()) {
+        int level = 0; //moves
+        while(!que.empty()) {
             int n = que.size();
-            while (n--) {
+
+            //Processing all elements at current level
+            while(n--) {
                 string curr = que.front();
                 que.pop();
 
-                // Check if we reached the target state
-                if (curr == target) {
-                    return steps;
+                if(curr == target) {
+                    return level;
                 }
 
-                // Find the index of '0' and explore its neighbors
-                int zeroIdx = curr.find('0');
-                for (int dir : dirs[zeroIdx]) {
-                    string next = curr;
-                    swap(next[zeroIdx], next[dir]);
-                    if (!visited.count(next)) {
-                        visited.insert(next);
-                        que.push(next);
+                int indexOfZero = curr.find('0'); //finding index of 0
+                for(int swapIdx : mp[indexOfZero]) {
+                    string newState = curr;
+                    swap(newState[indexOfZero], newState[swapIdx]);
+                    if(visited.find(newState) == visited.end()) {
+                        que.push(newState);
+                        visited.insert(newState);
                     }
                 }
             }
-            steps++;
+            level++;
         }
 
         return -1;
+
     }
 };
 
 
 /************************************************************ JAVA ************************************************************/
 //Approach - BFS (asking for minimum moves - and problem different states)
-//T.C : O(row*col)
-//S.C : O(1)
+//T.C : O(6!) : The puzzle has 6 ! = 720 possible permutations of the board, as there are 6 tiles, including the blank tile (0). In the worst case, BFS explores all permutations.
+//S.C : O(6!) All states are stored in map
 class Solution {
     public int slidingPuzzle(int[][] board) {
-        String target = "123450";
+        // Construct the start string from the board
         StringBuilder start = new StringBuilder();
-        
-        // Convert the 2D board to a string
-        for (int[] row : board) {
-            for (int num : row) {
-                start.append(num);
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                start.append(board[i][j]);
             }
         }
         
-        // Directions for swapping positions
-        int[][] dirs = {
-            {1, 3},        // 0 can swap with 1 and 3
-            {0, 2, 4},     // 1 can swap with 0, 2, and 4
-            {1, 5},        // 2 can swap with 1 and 5
-            {0, 4},        // 3 can swap with 0 and 4
-            {1, 3, 5},     // 4 can swap with 1, 3, and 5
-            {2, 4}         // 5 can swap with 2 and 4
-        };
+        String target = "123450";
+        
+        // Positions representing the adjacency list
+        Map<Integer, int[]> mp = new HashMap<>();
+        mp.put(0, new int[]{1, 3});
+        mp.put(1, new int[]{0, 2, 4});
+        mp.put(2, new int[]{1, 5});
+        mp.put(3, new int[]{0, 4});
+        mp.put(4, new int[]{1, 3, 5});
+        mp.put(5, new int[]{2, 4});
+        
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(start.toString());
         
         Set<String> visited = new HashSet<>();
-        Queue<String> queue = new LinkedList<>();
-        
-        queue.offer(start.toString());
         visited.add(start.toString());
         
-        int steps = 0;
+        int level = 0; // Tracks the number of moves
         
         while (!queue.isEmpty()) {
             int n = queue.size();
+            
+            // Process all elements at the current level
             for (int i = 0; i < n; i++) {
                 String curr = queue.poll();
                 
+                // If the target is reached, return the number of moves
                 if (curr.equals(target)) {
-                    return steps;
+                    return level;
                 }
                 
-                int zeroIdx = curr.indexOf('0');
-                
-                for (int dir : dirs[zeroIdx]) {
-                    char[] next = curr.toCharArray();
-                    // Swap the 0 with its adjacent position
-                    char temp = next[zeroIdx];
-                    next[zeroIdx] = next[dir];
-                    next[dir] = temp;
+                int indexOfZero = curr.indexOf('0'); // Find the index of '0'
+                for (int swapIdx : mp.get(indexOfZero)) {
+                    char[] newStateArray = curr.toCharArray();
+                    // Swap the positions
+                    char temp = newStateArray[indexOfZero];
+                    newStateArray[indexOfZero] = newStateArray[swapIdx];
+                    newStateArray[swapIdx] = temp;
                     
-                    String nextStr = new String(next);
+                    String newState = new String(newStateArray);
                     
-                    if (!visited.contains(nextStr)) {
-                        visited.add(nextStr);
-                        queue.offer(nextStr);
+                    // If not visited, add to the queue
+                    if (!visited.contains(newState)) {
+                        queue.offer(newState);
+                        visited.add(newState);
                     }
                 }
             }
-            steps++;
+            level++;
         }
         
         return -1;
