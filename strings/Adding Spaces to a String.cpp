057166499e651ff0a18f34987498cc5db6ba770e
MAZHAR <iiestshibpur605@gmail.com>
2024-12-03 07:08:41 +0530

    Added JAVA code as well

---
+++ strings/Adding Spaces to a String.cpp
@@ -0,0 +1,59 @@
+/*     Scroll below to see JAVA code also    */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : will update soon
+    Leetcode Link               : https://leetcode.com/problems/adding-spaces-to-a-string
+*/
+
+
+/**************************************************************** C++ ****************************************************************/
+//Approach (Simple traversal using two pointers)
+//T.C : O(m+n)
+//S.C : O(1)
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+        int m = s.length();
+        int n = spaces.size();
+
+        string result = "";
+
+        int j = 0; //to traverse in spaces vector
+        for(int i = 0; i < m; i++) {
+            if(j < n && i == spaces[j]) {
+                result += " ";
+                j++;
+            }
+
+            result += s[i];
+        }
+
+        return result;
+    }
+};
+
+
+
+/**************************************************************** JAVA ****************************************************************/
+//Approach (Simple traversal using two pointers)
+//T.C : O(m+n)
+//S.C : O(1)
+class Solution {
+    public String addSpaces(String s, int[] spaces) {
+        int m = s.length();
+        int n = spaces.length;
+
+        StringBuilder result = new StringBuilder(); // Using StringBuilder for efficiency
+        int j = 0; // Pointer to traverse the spaces array
+
+        for (int i = 0; i < m; i++) {
+            if (j < n && i == spaces[j]) {
+                result.append(" ");
+                j++;
+            }
+            result.append(s.charAt(i));
+        }
+
+        return result.toString();
+    }
+}
