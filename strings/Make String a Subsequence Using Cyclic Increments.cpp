7772a956726febca576249fd9cf9111479313451
MAZHAR <iiestshibpur605@gmail.com>
2024-12-04 07:18:48 +0530

    Added YouTube Link

---
+++ strings/Make String a Subsequence Using Cyclic Increments.cpp
@@ -0,0 +1,68 @@
+/*     Scroll below to see JAVA code also    */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : will update soon
+    Leetcode Link               : https://leetcode.com/problems/make-string-a-subsequence-using-cyclic-increments/
+*/
+
+
+/**************************************************************** C++ ****************************************************************/
+//Approach (Simple traversal using two pointers)
+//T.C : O(m+n)
+//S.C : O(1)
+class Solution {
+public:
+    bool canMakeSubsequence(string str1, string str2) {
+        int m = str1.length();
+        int n = str2.length();
+        if(n > m) {
+            return false;
+        }
+
+        int i = 0;
+        int j = 0;
+
+        while(i < m && j < n) {
+            if((str1[i] == str2[j])   ||
+               (str1[i]+1 == str2[j]) ||
+               (str1[i]-25 == str2[j])) {
+                j++;
+            }
+
+            i++;
+        }
+
+        return j == n;
+    }
+};
+
+
+
+/**************************************************************** JAVA ****************************************************************/
+//Approach (Simple traversal using two pointers)
+//T.C : O(m+n)
+//S.C : O(1)
+class Solution {
+    public boolean canMakeSubsequence(String str1, String str2) {
+        int m = str1.length();
+        int n = str2.length();
+        
+        if (n > m) {
+            return false;
+        }
+        
+        int i = 0;
+        int j = 0;
+        
+        while (i < m && j < n) {
+            if ((str1.charAt(i) == str2.charAt(j)) || 
+                (str1.charAt(i) + 1 == str2.charAt(j)) || 
+                (str1.charAt(i) - 25 == str2.charAt(j))) {
+                j++;
+            }
+            i++;
+        }
+        
+        return j == n;
+    }
+}
