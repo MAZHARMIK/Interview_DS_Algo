45a6912113c99f43100becc110e43af6df51da42
MAZHAR <iiestshibpur605@gmail.com>
2024-12-10 12:00:26 +0530

    Added YouTube Link

--- strings/Find Longest Special Substring That Occurs Thrice I & II.cpp
+++ strings/Find Longest Special Substring That Occurs Thrice I & II.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=fDUPCtPqzss
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/find-longest-special-substring-that-occurs-thrice-i
                                 : https://leetcode.com/problems/find-longest-special-substring-that-occurs-thrice-ii
