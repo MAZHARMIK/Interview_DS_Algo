8ec5bcc918126a428a0169bca03dd05cac7c5c1d
MAZHAR <iiestshibpur605@gmail.com>
2024-12-11 10:56:08 +0530

    Added YouTube Link

--- Arrays/Intervals_Based_Qn/Maximum Beauty of an Array After Applying Operation.cpp
+++ Arrays/Intervals_Based_Qn/Maximum Beauty of an Array After Applying Operation.cpp
@@ -1,6 +1,6 @@
 /*         Scroll below to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=VrFolxiw_yQ
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/maximum-beauty-of-an-array-after-applying-operation
 */
