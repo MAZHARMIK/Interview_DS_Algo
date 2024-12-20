ad30d10cda6138cf27678c0fdb2d371e7b63b6b4
MAZHAR <iiestshibpur605@gmail.com>
2024-12-20 08:29:44 +0530

    Added YouTube Link

--- Arrays/Cumulative_Sum(Prefix Array)/Max Chunks To Make Sorted.cpp
+++ Arrays/Cumulative_Sum(Prefix Array)/Max Chunks To Make Sorted.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=OwEggxOnszA
     Company Tags                : will soon update
     Leetcode Link               : https://leetcode.com/problems/max-chunks-to-make-sorted
 */
