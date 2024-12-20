756e910fac660bb1e6891eb945fa78d5c56160b5
MAZHAR <iiestshibpur605@gmail.com>
2024-12-20 08:31:29 +0530

    Added JAVA code as well

--- Arrays/Cumulative_Sum(Prefix Array)/Max Chunks To Make Sorted.cpp
+++ Arrays/Cumulative_Sum(Prefix Array)/Max Chunks To Make Sorted.cpp
@@ -64,7 +64,7 @@ public:
 };
 
 
-//Approach-2 (Using max check)
+//Approach-3 (Using max check)
 //T.C : O(n)
 //S.C : O(n)
 class Solution {
@@ -88,7 +88,79 @@ public:
 
 
 /**************************************************************** JAVA ****************************************************************/
-//Approach-1 (Using prefixMax and SuffixMin)
-//T.C : O(n)
-//S.C : O(n)
-//Will add tonight when I am free (currently travelling) :-) 
+// Approach-1 (Using prefixMax and SuffixMin)
+// T.C : O(n)
+// S.C : O(n)
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int n = arr.length;
+        int[] prefixMax = arr.clone();
+        int[] suffixMin = arr.clone();
+
+        for (int i = 1; i < n; i++) {
+            prefixMax[i] = Math.max(prefixMax[i - 1], prefixMax[i]);
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = Math.min(suffixMin[i + 1], suffixMin[i]);
+        }
+
+        int chunksCount = 0;
+        for (int i = 0; i < n; i++) {
+            int pehleKaMax = i > 0 ? prefixMax[i - 1] : -1;
+            int baadKaMin = suffixMin[i];
+
+            if (pehleKaMax < baadKaMin) {
+                chunksCount++;
+            }
+        }
+
+        return chunksCount;
+    }
+}
+
+
+// Approach-2 (Using cumulative Sum)
+// T.C : O(n)
+// S.C : O(1)
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int n = arr.length;
+        int cumSum = 0;
+        int normalSum = 0;
+
+        int chunksCount = 0;
+        for (int i = 0; i < n; i++) {
+            cumSum += arr[i];
+            normalSum += i;
+
+            if (cumSum == normalSum) {
+                chunksCount++;
+            }
+        }
+
+        return chunksCount;
+    }
+}
+
+
+// Approach-3 (Using max check)
+// T.C : O(n)
+// S.C : O(1)
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int n = arr.length;
+        int maxTillNow = -1;
+
+        int chunksCount = 0;
+        for (int i = 0; i < n; i++) {
+            maxTillNow = Math.max(maxTillNow, arr[i]);
+
+            if (maxTillNow == i) {
+                chunksCount++;
+            }
+        }
+
+        return chunksCount;
+    }
+}
