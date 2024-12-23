c9c6d2aef785034a3bef91ec973ace0eeb951599
MAZHAR <iiestshibpur605@gmail.com>
2024-12-24 02:27:55 +0530

    Added YouTube Link

--- Segment Tree/Find Building Where Alice and Bob Can Meet.cpp
+++ Segment Tree/Find Building Where Alice and Bob Can Meet.cpp
@@ -1,6 +1,6 @@
 /*	 Scroll below to see JAVA code also	*/
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn   : https://www.youtube.com/watch?v=5n2zzL8EqNY
     Company Tags  		          : will update later
     Leetcode Link 		          : https://leetcode.com/problems/find-building-where-alice-and-bob-can-meet
 */
