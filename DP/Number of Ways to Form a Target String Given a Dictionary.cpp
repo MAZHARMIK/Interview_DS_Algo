222367b279efa780199beb85725ba591ed9dfb78
MAZHAR <iiestshibpur605@gmail.com>
2024-12-29 06:28:00 +0530

    Added JAVA code as well

--- DP/Number of Ways to Form a Target String Given a Dictionary.cpp
+++ DP/Number of Ways to Form a Target String Given a Dictionary.cpp
@@ -1,3 +1,4 @@
+/*     Scroll below to see JAVA code also    */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : Recur+Memo - https://www.youtube.com/watch?v=w0mB7tEv17k 
                                 : Bottom Up  - https://www.youtube.com/watch?v=EtbCd9iETno
@@ -5,7 +6,11 @@
     Leetcode Link               : https://leetcode.com/problems/number-of-ways-to-form-a-target-string-given-a-dictionary/
 */
 
+
+/**************************************************************** C++ ****************************************************************/
 //Approach-1 (Recur + Memo)
+//T.C : O(n*k + m*k)
+//S.C : O(m*k)
 class Solution {
 public:
     int m;
@@ -54,6 +59,8 @@ public:
 
 
 //Approach-2 (Bottom Up DP)
+//T.C : O(n*k + m*k)
+//S.C : O(m*k)
 class Solution {
 public:
     const int MOD = 1e9+7;
@@ -97,3 +104,83 @@ public:
         return dp[m][k];
     }
 };
+
+
+
+/**************************************************************** JAVA ****************************************************************/
+//Approach-1 (Recur + Memo)
+//T.C : O(n*k + m*k)
+//S.C : O(m*k)
+class Solution {
+    public int numWays(String[] words, String target) {
+        int MOD = 1_000_000_007;
+        int k = words[0].length();
+        int m = target.length();
+
+        // Precompute frequency of each character in each column
+        long[][] freq = new long[26][k];
+        for (String word : words) {
+            for (int col = 0; col < k; col++) {
+                freq[word.charAt(col) - 'a'][col]++;
+            }
+        }
+
+        // DP array: dp[i][j] = ways to form first i chars of target using first j columns
+        long[][] dp = new long[m + 1][k + 1];
+        dp[0][0] = 1; // Base case: 1 way to form empty target
+
+        for (int i = 0; i <= m; i++) { // target length
+            for (int j = 0; j <= k; j++) { // columns
+                // Not taken
+                if (j < k) {
+                    dp[i][j + 1] = (dp[i][j + 1] + dp[i][j]) % MOD;
+                }
+                // Taken
+                if (i < m && j < k) {
+                    dp[i + 1][j + 1] = (dp[i + 1][j + 1] + freq[target.charAt(i) - 'a'][j] * dp[i][j]) % MOD;
+                }
+            }
+        }
+
+        return (int) dp[m][k];
+    }
+}
+
+
+//Approach-2 (Bottom Up)
+//T.C : O(n*k + m*k)
+//S.C : O(m*k)
+class Solution {
+    public int numWays(String[] words, String target) {
+        int MOD = 1_000_000_007;
+        int k = words[0].length();
+        int m = target.length();
+
+        // Precompute frequency of each character in each column
+        long[][] freq = new long[26][k];
+        for (String word : words) {
+            for (int col = 0; col < k; col++) {
+                freq[word.charAt(col) - 'a'][col]++;
+            }
+        }
+
+        // DP array: dp[i][j] = ways to form first i chars of target using first j columns
+        long[][] dp = new long[m + 1][k + 1];
+        dp[0][0] = 1; // Base case: 1 way to form empty target
+
+        for (int i = 0; i <= m; i++) { // Target length
+            for (int j = 0; j <= k; j++) { // Columns
+                // Not taken
+                if (j < k) {
+                    dp[i][j + 1] = (dp[i][j + 1] + dp[i][j]) % MOD;
+                }
+                // Taken
+                if (i < m && j < k) {
+                    dp[i + 1][j + 1] = (dp[i + 1][j + 1] + freq[target.charAt(i) - 'a'][j] * dp[i][j]) % MOD;
+                }
+            }
+        }
+
+        return (int) dp[m][k];
+    }
+}
