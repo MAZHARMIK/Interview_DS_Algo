c3d86d2ba338d5aa270aee765aecf58384423a90
MAZHAR <iiestshibpur605@gmail.com>
2024-12-30 08:18:56 +0530

    JAVA recursion+Memo

--- DP/Number of Ways to Form a Target String Given a Dictionary.cpp
+++ DP/Number of Ways to Form a Target String Given a Dictionary.cpp
@@ -112,37 +112,50 @@ public:
 //T.C : O(n*k + m*k)
 //S.C : O(m*k)
 class Solution {
-    public int numWays(String[] words, String target) {
-        int MOD = 1_000_000_007;
-        int k = words[0].length();
-        int m = target.length();
+    private int m;
+    private int k;
+    private final int MOD = (int) 1e9 + 7;
+    private int[][] memo;
 
-        // Precompute frequency of each character in each column
+    private int solve(int i, int j, long[][] freq, String target) {
+        if (i == m) {
+            return 1;
+        }
+
+        if (j == k) {
+            return 0;
+        }
+
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+
+        int notTaken = solve(i, j + 1, freq, target) % MOD;
+
+        int taken = (int) ((freq[target.charAt(i) - 'a'][j] * solve(i + 1, j + 1, freq, target)) % MOD);
+
+        return memo[i][j] = (notTaken + taken) % MOD;
+    }
+
+    public int numWays(String[] words, String target) {
+        k = words[0].length();
+        m = target.length();
+
         long[][] freq = new long[26][k];
+
+        // Populate frequency array
         for (String word : words) {
             for (int col = 0; col < k; col++) {
                 freq[word.charAt(col) - 'a'][col]++;
             }
         }
 
-        // DP array: dp[i][j] = ways to form first i chars of target using first j columns
-        long[][] dp = new long[m + 1][k + 1];
-        dp[0][0] = 1; // Base case: 1 way to form empty target
-
-        for (int i = 0; i <= m; i++) { // target length
-            for (int j = 0; j <= k; j++) { // columns
-                // Not taken
-                if (j < k) {
-                    dp[i][j + 1] = (dp[i][j + 1] + dp[i][j]) % MOD;
-                }
-                // Taken
-                if (i < m && j < k) {
-                    dp[i + 1][j + 1] = (dp[i + 1][j + 1] + freq[target.charAt(i) - 'a'][j] * dp[i][j]) % MOD;
-                }
-            }
+        memo = new int[m][k];
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
         }
 
-        return (int) dp[m][k];
+        return solve(0, 0, freq, target);
     }
 }
 
