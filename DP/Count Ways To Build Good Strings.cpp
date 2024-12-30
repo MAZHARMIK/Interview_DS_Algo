47f5afa14db9ab8a61c2e5b1c959288067569473
MAZHAR <iiestshibpur605@gmail.com>
2024-12-30 08:46:21 +0530

    Added JAVA code as well

--- DP/Count Ways To Build Good Strings.cpp
+++ DP/Count Ways To Build Good Strings.cpp
@@ -1,3 +1,4 @@
+/*    Scroll below to see JAVA code also        */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : Recur+Memo - https://www.youtube.com/watch?v=G-i4CveBSYI
                                   Bottom Up  - https://www.youtube.com/watch?v=SbB8tKCMtTE
@@ -5,7 +6,11 @@
     Leetcode Link               : https://leetcode.com/problems/count-ways-to-build-good-strings/
 */
 
+
+/******************************************************** C++ ****************************************************************/
 //Approach-1 (Using Recur + Memo)
+//T.C : O(H)
+//S.C : O(H)
 class Solution {
 public:
     int ans;
@@ -48,6 +53,8 @@ public:
 
 
 //Approach-2 (Bottom Up)
+//T.C : O(H)
+//S.C : O(H)
 class Solution {
 public:
     const int M = 1e9+7;
@@ -77,3 +84,82 @@ public:
         return ans;
     }
 };
+
+
+/******************************************************** JAVA ****************************************************************/
+//Approach-1 (Using Recur + Memo)
+//T.C : O(H)
+//S.C : O(H)
+class Solution {
+    int ans;
+    int L, H;
+    int Z, O;
+    final int M = 1_000_000_007;
+    int[] dp;
+
+    private int solve(int l) {
+        if (l > H) {
+            return 0;
+        }
+
+        if (dp[l] != -1) {
+            return dp[l];
+        }
+
+        boolean addOne = false;
+        if (l >= L && l <= H) {
+            addOne = true;
+        }
+
+        int takeZero = solve(l + Z);
+        int takeOne = solve(l + O);
+
+        dp[l] = (addOne ? 1 : 0) + takeZero + takeOne;
+        dp[l] %= M;
+
+        return dp[l];
+    }
+
+    public int countGoodStrings(int low, int high, int zero, int one) {
+        L = low;
+        H = high;
+        Z = zero;
+        O = one;
+
+        dp = new int[H + 1];
+        Arrays.fill(dp, -1);
+
+        return solve(0);
+    }
+}
+
+//Approach-1 (Using Bottom Up)
+//T.C : O(H)
+//S.C : O(H)
+class Solution {
+    private final int M = 1_000_000_007;
+
+    public int countGoodStrings(int low, int high, int zero, int one) {
+        int[] t = new int[high + 1];
+        // t[i] = Total number of good strings of length i
+
+        t[0] = 1; // Only one good string ""
+
+        for (int i = 1; i <= high; i++) {
+            if (i - zero >= 0) {
+                t[i] = (t[i] % M + t[i - zero] % M) % M;
+            }
+            if (i - one >= 0) {
+                t[i] = (t[i] % M + t[i - one] % M) % M;
+            }
+        }
+
+        int ans = 0;
+
+        for (int l = low; l <= high; l++) {
+            ans = (ans % M + t[l] % M) % M;
+        }
+
+        return ans;
+    }
+}
