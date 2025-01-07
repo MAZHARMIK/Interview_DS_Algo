a99b7773fa6e8b18c569c468f5534d1d4957cf00
MAZHAR <iiestshibpur605@gmail.com>
2025-01-07 14:09:44 +0530

    Added JAVA code as well

--- strings/Easy Tagged/String Matching in an Array.cpp
+++ strings/Easy Tagged/String Matching in an Array.cpp
@@ -121,16 +121,110 @@ public:
 
 
 /*************************************************************************** JAVA ***************************************************************************/
-//Approach-1 (Brute Force)
-//T.C : O(m*n^2) , m = length of longest string in words, n = size of words
-//S.C : O(1)
+// Approach-1 (Brute Force)
+// T.C : O(m*n^2), m = length of longest string in words, n = size of words
+// S.C : O(1)
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> result = new ArrayList<>();
+        int n = words.length;
 
-//Will add tonight when I get free time
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j)
+                    continue;
+                if (words[j].contains(words[i])) {
+                    result.add(words[i]);
+                    break;
+                }
+            }
+        }
 
+        return result;
+    }
+}
 
-//Approach-2 (Using KMP)
-//T.C : O(m*n^2), m = length of longest string in words, n = size of words
-//S.C : O(m)
+// Approach-2 (Using KMP)
+// T.C : O(m*n^2), m = length of longest string in words, n = size of words
+// S.C : O(m)
+class Solution {
+    // Function to compute the LPS (Longest Proper Prefix which is also Suffix) array
+    private void computeLPS(String pattern, int[] lps) {
+        int M = pattern.length();
+        int len = 0; // Length of the previous longest prefix & suffix
+    
+        lps[0] = 0; // Because there is no proper suffix and prefix of pattern[0..0]
+    
+        int i = 1;
+        while (i < M) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1]; // You can also write, len = len-1;
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+
+    private boolean searchKMP(String pat, String txt) {
+        int N = txt.length();
+        int M = pat.length();
+        
+        // Create an LPS array to store the longest proper prefix which is also a suffix
+        // lps[i] = the longest proper prefix of pat[0..i] which is also a suffix of pat[0..i].
+        int[] lps = new int[M];
+        computeLPS(pat, lps);
+    
+        int i = 0; // Index for text
+        int j = 0; // Index for pattern
+    
+        while (i < N) {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                i++;
+                j++;
+            }
+    
+            if (j == M) {
+                // result.add(i-j+1); // Pattern found at index i-j+1 (If you have to return 1 Based indexing, that's why added + 1)
+                // j = lps[j - 1];
+                return true;
+            } else if (i < N && pat.charAt(j) != txt.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        
+        return false;
+    }
+
+    public List<String> stringMatching(String[] words) {
+        int n = words.length;
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j)
+                    continue;
+
+                if (searchKMP(words[i], words[j])) {
+                    result.add(words[i]);
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
 
 
 //Will add tonight when I get free time
