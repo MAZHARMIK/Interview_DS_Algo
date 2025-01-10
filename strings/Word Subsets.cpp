c8d26275bc35c6ba0d680258a28fdb7504d03ce1
MAZHAR <iiestshibpur605@gmail.com>
2025-01-10 10:56:52 +0530

    Added YouTube Link

--- strings/Word Subsets.cpp
+++ strings/Word Subsets.cpp
@@ -1,6 +1,6 @@
 /*        Scroll below to see JAVA code as well     */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=EjXPzWYbviw
     Company Tags                : Amazon, Google
     Leetcode Link               : https://leetcode.com/problems/word-subsets/
 */
