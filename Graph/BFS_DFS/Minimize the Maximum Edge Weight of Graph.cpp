def37037bcc3d1a9f0fc9cfa34647f850bb31dfe
MAZHAR <iiestshibpur605@gmail.com>
2025-01-12 17:06:05 +0530

    Added YouTube Link

--- Graph/BFS_DFS/Minimize the Maximum Edge Weight of Graph.cpp
+++ Graph/BFS_DFS/Minimize the Maximum Edge Weight of Graph.cpp
@@ -1,6 +1,6 @@
 /*                       Scroll down to see JAVA code also             */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn  : 
+    MY YOUTUBE VIDEO ON THIS Qn  : https://www.youtube.com/watch?v=B5aIqLq50_8
     Company Tags                 : will update
     Leetcode Link                : https://leetcode.com/problems/minimize-the-maximum-edge-weight-of-graph/
 */
