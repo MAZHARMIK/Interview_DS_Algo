bc19a085d12eb05c969145202f46f416f75045a9
MAZHAR <iiestshibpur605@gmail.com>
2025-01-12 12:10:51 +0530

    Added JAVA code as well

--- Stack/Check if a Parentheses String Can Be Valid.cpp
+++ Stack/Check if a Parentheses String Can Be Valid.cpp
@@ -50,8 +50,6 @@ public:
 };
 
 
-
-/********************************************************************** JAVA **********************************************************************/
 //Approach-2 (Without using stack)
 //T.C : O(n)
 //S.C : O(1)
@@ -96,3 +94,91 @@ public:
 
     }
 };
+
+
+
+
+
+
+/********************************************************************** JAVA **********************************************************************/
+// Approach-1 (Using stack)
+// T.C : O(n)
+// S.C : O(n)
+class Solution {
+    public boolean canBeValid(String s, String locked) {
+        int n = s.length();
+
+        if (n % 2 != 0) { // odd
+            return false;
+        }
+
+        Stack<Integer> open = new Stack<>();
+        Stack<Integer> openClose = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            if (locked.charAt(i) == '0') {
+                openClose.push(i);
+            } else if (s.charAt(i) == '(') {
+                open.push(i);
+            } else if (s.charAt(i) == ')') {
+                if (!open.isEmpty()) {
+                    open.pop();
+                } else if (!openClose.isEmpty()) {
+                    openClose.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        while (!open.isEmpty() && !openClose.isEmpty() && open.peek() < openClose.peek()) {
+            open.pop();
+            openClose.pop();
+        }
+
+        return open.isEmpty(); // true
+    }
+}
+
+// Approach-2 (Without using stack)
+// T.C : O(n)
+// S.C : O(1)
+class Solution {
+    public boolean canBeValid(String s, String locked) {
+        int n = s.length();
+
+        if (n % 2 != 0) { // odd
+            return false;
+        }
+
+        // L to R
+        int open = 0;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '(' || locked.charAt(i) == '0') {
+                open++;
+            } else {
+                open--;
+            }
+
+            if (open < 0) {
+                return false;
+            }
+        }
+
+        // R to L
+        int close = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == ')' || locked.charAt(i) == '0') {
+                close++;
+            } else {
+                close--;
+            }
+
+            if (close < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
