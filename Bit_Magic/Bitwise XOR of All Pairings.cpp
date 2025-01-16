9dccc2ac2eb5b9741fb941b73c33b79f3fac1634
MAZHAR <iiestshibpur605@gmail.com>
2025-01-16 08:09:12 +0530

    Added YouTube Link

--- Bit_Magic/Bitwise XOR of All Pairings.cpp
+++ Bit_Magic/Bitwise XOR of All Pairings.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=19-Q_Krxj1w
     Company Tags                : will soon update
     Leetcode Link               : https://leetcode.com/problems/bitwise-xor-of-all-pairings
 */
