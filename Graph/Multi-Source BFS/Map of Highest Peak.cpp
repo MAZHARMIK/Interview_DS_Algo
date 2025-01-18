4bf0568b4ccb9957eb0a178b892b51f3eca8b637
MAZHAR <iiestshibpur605@gmail.com>
2025-01-18 12:19:09 +0530

    Added JAVA code as well

--- Graph/Multi-Source BFS/Map of Highest Peak.cpp
+++ Graph/Multi-Source BFS/Map of Highest Peak.cpp
@@ -10,54 +10,60 @@
 //Approach - Using Multi-Source BFS
 //T.C : O(m * n) , We will visit all cells once
 //S.C : O(m * n), in worst case queue will contain all the cells
-import java.util.*;
-
 class Solution {
-    public int[][] highestPeak(int[][] isWater) {
-        int m = isWater.length;
-        int n = isWater[0].length;
-        int[][] height = new int[m][n];
-        Queue<int[]> queue = new LinkedList<>();
-        
-        // Initialize the queue with all water cells and set their height to 0
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (isWater[i][j] == 1) {
-                    queue.offer(new int[]{i, j});
-                    height[i][j] = 0;
-                } else {
-                    height[i][j] = -1; // Mark unvisited land cells
+public:
+    typedef pair<int, int> P;
+    vector<vector<int>> directions{{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int m = isWater.size();
+        int n = isWater[0].size();
+
+
+        vector<vector<int>> height(m, vector<int>(n, -1));
+
+        queue<P> que;
+        //Height of water cells must be 0 only
+        //T.C : O(m*n)
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(isWater[i][j] == 1) { //this cell is water
+                    height[i][j] = 0; //This is a source for hitting BFS
+                    que.push({i, j});
                 }
             }
         }
-        
-        // Define directions for moving in the grid
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        
-        // Multi-Source BFS with level-based processing
-        while (!queue.isEmpty()) {
-            int size = queue.size(); // Number of nodes at the current level
-            while (size-- > 0) {
-                int[] curr = queue.poll();
-                int i = curr[0];
-                int j = curr[1];
-                
-                for (int[] dir : directions) {
-                    int newI = i + dir[0];
-                    int newJ = j + dir[1];
-                    
-                    // Check if the new cell is within bounds and unvisited
-                    if (newI >= 0 && newI < m && newJ >= 0 && newJ < n && height[newI][newJ] == -1) {
-                        height[newI][newJ] = height[i][j] + 1;
-                        queue.offer(new int[]{newI, newJ});
+
+        //Apply khandani template - Multi source BFS ka
+        //T.C : O(m*n)
+        //S.C : O(m*n)
+        while(!que.empty()) {
+            int N = que.size();
+
+            while(N--) {
+                P curr = que.front();
+                que.pop();
+
+                int i = curr.first;
+                int j = curr.second;
+
+                for(auto &dir : directions) {
+                    int i_ = i + dir[0];
+                    int j_ = j + dir[1];
+
+                    if(i_ >= 0 && i_ < m && j_ >= 0 && j_ < n && height[i_][j_] == -1) {
+                        height[i_][j_] = height[i][j] + 1;
+                        que.push({i_, j_});
                     }
                 }
             }
         }
-        
+
         return height;
+
+
     }
-}
+};
 
 
 /************************************************************ JAVA ************************************************/
