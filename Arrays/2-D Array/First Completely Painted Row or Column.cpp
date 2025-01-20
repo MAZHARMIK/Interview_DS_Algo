ad42626308cac4ccb25f5862c173fe9e9471a50d
MAZHAR <iiestshibpur605@gmail.com>
2025-01-20 08:57:24 +0530

    Added YouTube Link

--- Arrays/2-D Array/First Completely Painted Row or Column.cpp
+++ Arrays/2-D Array/First Completely Painted Row or Column.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also   */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=nqmR3aDwjwU
     Company Tags                : Will update later
     Leetcode Link               : https://leetcode.com/problems/first-completely-painted-row-or-column/
 */
