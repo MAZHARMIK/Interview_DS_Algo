24a803ef9de2ae632ada8e2b76e79c9d68ecbfdd
MAZHAR <iiestshibpur605@gmail.com>
2025-01-23 09:41:42 +0530

    Added YouTube Link

--- Arrays/2-D Array/Count Servers that Communicate.cpp
+++ Arrays/2-D Array/Count Servers that Communicate.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also   */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=GTahroSaPe0
     Company Tags                : Will update later
     Leetcode Link               : https://leetcode.com/problems/count-servers-that-communicate
 */
