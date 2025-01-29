33e7c33893e3daa58d1e8b5a90b13182e5ecaed0
MAZHAR <iiestshibpur605@gmail.com>
2025-01-29 20:26:51 +0530

    Added JAVA code as well

--- Graph/BFS_DFS/Making A Large Island.cpp
+++ Graph/BFS_DFS/Making A Large Island.cpp
@@ -1,14 +1,19 @@
+/*         Scroll below to see JAVA code as well        */
 /*
+    MY YOUTUBE VIDEO ON THIS Qn : 
     Company Tags  : Google, Uber
     Leetcode Link : https://leetcode.com/problems/making-a-large-island/
-    
-    Reference     : https://www.youtube.com/watch?v=_426VVOB8Vo
 */
 
+
+/********************************************************************** C++ **********************************************************************/
+//T.C : O(m*n)
+//S.C : O(m*n)
 class Solution {
 public:
     int m, n;
     vector<vector<int>> directions{{1, 0}, {-1, 0},{0, 1},{0, -1}};
+
     int DFS(vector<vector<int>>& grid, int i, int j, int& id) {
         if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != 1)
             return 0;
@@ -45,13 +50,6 @@ public:
         
         for(int i = 0; i<m; i++) {
             for(int j = 0; j<n; j++) {
-                cout << grid[i][j] << " ";
-            }
-            cout << endl;
-        }
-        
-        for(int i = 0; i<m; i++) {
-            for(int j = 0; j<n; j++) {
                 if(grid[i][j] == 0) {
                     unordered_set<int> st;
                     for(vector<int>& dir : directions) {
@@ -73,3 +71,72 @@ public:
         return maxArea;
     }
 };
+
+
+
+/********************************************************************** JAVA **********************************************************************/
+//T.C : O(m*n)
+//S.C : O(m*n)
+class Solution {
+    private int m, n;
+    private final int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    
+    private int DFS(int[][] grid, int i, int j, int id) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != 1) {
+            return 0;
+        }
+        
+        grid[i][j] = id;
+        int count = 1;
+        
+        for (int[] dir : directions) {
+            int x = i + dir[0];
+            int y = j + dir[1];
+            count += DFS(grid, x, y, id);
+        }
+        
+        return count;
+    }
+    
+    public int largestIsland(int[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
+        int maxArea = 0;
+        Map<Integer, Integer> islandSizes = new HashMap<>();
+        int islandId = 2;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    int size = DFS(grid, i, j, islandId);
+                    maxArea = Math.max(maxArea, size);
+                    islandSizes.put(islandId, size);
+                    islandId++;
+                }
+            }
+        }
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) {
+                    Set<Integer> uniqueIslands = new HashSet<>();
+                    for (int[] dir : directions) {
+                        int x = i + dir[0];
+                        int y = j + dir[1];
+                        if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] != 0) {
+                            uniqueIslands.add(grid[x][y]);
+                        }
+                    }
+                    
+                    int sum = 1; // converting current 0 to 1
+                    for (int id : uniqueIslands) {
+                        sum += islandSizes.get(id);
+                    }
+                    maxArea = Math.max(maxArea, sum);
+                }
+            }
+        }
+        
+        return maxArea;
+    }
+}
