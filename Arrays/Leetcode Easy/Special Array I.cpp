12d9b566b5b829d3e83c0936733118c59cad0766
MAZHAR <iiestshibpur605@gmail.com>
2025-02-01 10:13:38 +0530

    Added YouTube Link

--- Arrays/Leetcode Easy/Special Array I.cpp
+++ Arrays/Leetcode Easy/Special Array I.cpp
@@ -1,6 +1,6 @@
 /*         Scroll down to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=5tp8ddCyXbs
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/special-array-i
 */
