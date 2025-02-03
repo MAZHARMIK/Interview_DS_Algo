d6a64c1dbfeafb6fb007a699a1db19e40a62e33e
MAZHAR <iiestshibpur605@gmail.com>
2025-02-03 19:17:17 +0530

    Added YouTube Link

--- Arrays/Leetcode Easy/Longest Strictly Increasing or Strictly Decreasing Subarray.cpp
+++ Arrays/Leetcode Easy/Longest Strictly Increasing or Strictly Decreasing Subarray.cpp
@@ -1,6 +1,6 @@
 /*         Scroll down to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=4Hi4v5OaOEA
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/longest-strictly-increasing-or-strictly-decreasing-subarray
 */
