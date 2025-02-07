cbe6a7ac72a88d58b2f04bd9268cdfd78286a543
MAZHAR <iiestshibpur605@gmail.com>
2025-02-07 08:39:08 +0530

    Added YouTube Link

--- HashMap/Find the Number of Distinct Colors Among the Balls.cpp
+++ HashMap/Find the Number of Distinct Colors Among the Balls.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=G8YjG1MEoYg
     Company Tags                : Will update soon
     Leetcode Link               : https://leetcode.com/problems/find-the-number-of-distinct-colors-among-the-balls
 */
