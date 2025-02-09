215d1712b4aa530327a6a478c7e1758e4dbfb6f3
MAZHAR <iiestshibpur605@gmail.com>
2025-02-09 07:27:38 +0530

    Added JAVA code also

--- HashMap/Count Number of Bad Pairs.cpp
+++ HashMap/Count Number of Bad Pairs.cpp
@@ -38,6 +38,39 @@ public:
         int n = nums.size();
 
         long long result = 0;
+        for(int i = 0; i < n; i++) {
+            nums[i] = nums[i]-i;
+        }
+
+        unordered_map<int, int> mp;
+        mp[nums[0]] = 1;
+
+        for(int j = 1; j < n; j++) {
+            int countOfNumsj = mp[nums[j]];
+
+            int totalNumsBeforej = j;
+
+            int badPairs = totalNumsBeforej - countOfNumsj;
+
+            result += badPairs;
+
+            mp[nums[j]]++;
+        }
+
+        return result;
+    }
+};
+
+
+//Approach-3 (Using hashmap)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    long long countBadPairs(vector<int>& nums) {
+        int n = nums.size();
+
+        long long result = 0;
         unordered_map<int, int> mp;
 
         for(int i = 0; i < n; i++) {
@@ -77,7 +110,36 @@ class Solution {
     }
 }
 
+
 //Approach-2 (Using hashmap)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public long countBadPairs(int[] nums) {
+        int n = nums.length;
+        long result = 0;
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = nums[i] - i;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(nums[0], 1);
+
+        for (int j = 1; j < n; j++) {
+            int countOfNumsj = map.getOrDefault(nums[j], 0);
+            int totalNumsBeforej = j;
+            int badPairs = totalNumsBeforej - countOfNumsj;
+            result += badPairs;
+            
+            map.put(nums[j], countOfNumsj + 1);
+        }
+
+        return result;
+    }
+}
+
+//Approach-3 (Using hashmap)
 //T.C : O(n)
 //S.C : O(n)
 class Solution {
