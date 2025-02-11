dc18eeb3ef799c5929efac1c764acecd65c15926
MAZHAR <iiestshibpur605@gmail.com>
2025-02-11 18:14:56 +0530

    Added YouTube Link

--- Stack/Remove All Occurrences of a Substring.cpp
+++ Stack/Remove All Occurrences of a Substring.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also   */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=v6_q_fu1ev0
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/remove-all-occurrences-of-a-substring
 */
