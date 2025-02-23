3d4c1c6750a36249dad24e121782462420b0f62c
MAZHAR <iiestshibpur605@gmail.com>
2025-02-23 10:19:02 +0530

    Added YouTube Link

--- Tree/Construct Binary Tree from Preorder and Postorder Traversal.cpp
+++ Tree/Construct Binary Tree from Preorder and Postorder Traversal.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=6JDV3kIFyjU
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal
 */
