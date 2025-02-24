80793eed4da78815aa12fb39d178c52a5b1a2881
MAZHAR <iiestshibpur605@gmail.com>
2025-02-24 12:22:42 +0530

    Added YouTube Link

--- Graph/BFS_DFS/Most Profitable Path in a Tree.cpp
+++ Graph/BFS_DFS/Most Profitable Path in a Tree.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=uJiDHHChF58
     Company Tags                : Intuit
     Leetcode Link               : https://leetcode.com/problems/most-profitable-path-in-a-tree
 */
