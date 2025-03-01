db931a78211cf7d181fc2f73e8f48826e4d3fe56
MAZHAR <iiestshibpur605@gmail.com>
2025-03-01 15:19:36 +0530

    Added JAVA code also

--- strings/Concatenated Words.cpp
+++ strings/Concatenated Words.cpp
@@ -1,3 +1,4 @@
+/*            Scroll below to see JAVA code as well            */
 /*
       MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=zZsnMAgM6Q0
       Company Tags                : META
@@ -5,7 +6,10 @@
 */
 
 
-//Using DFS and Memoization - O(N*L^3)
+/********************************************************************** C++ ***************************************************************/
+//Using DFS and Memoization
+//T.C : O(n*L^3)
+//S.C : O(n*L)
 class Solution {
 public:
     int n;
@@ -45,3 +49,43 @@ public:
         return result;
     }
 };
+
+
+
+/********************************************************************** JAVA ***************************************************************/
+//Using DFS and Memoization
+//T.C : O(n*L^3)
+//S.C : O(n*L)
+class Solution {
+    public boolean isConcatenated(String word, Set<String> st, Map<String, Boolean> map) {
+        if(map.containsKey(word))
+            return map.get(word);
+        int l = word.length();
+        for(int i = 0 ; i< l; i++){
+            String prefix = word.substring(0, i+1);
+            String suffix = word.substring(i+1, l);
+            if((st.contains(prefix) && st.contains(suffix)) || (st.contains(prefix) && isConcatenated(suffix, st, map))){
+                map.put(word, true);
+                return true;
+            }
+        }
+        map.put(word, false);
+        return false;
+    }
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        int n = words.length;
+        Set<String> st = new HashSet<>();
+        Map<String, Boolean> map = new HashMap<>();
+        for(String s : words)
+            st.add(s);
+        List<String> result = new ArrayList<>();
+        for(int i = 0 ; i < n; i++){
+            String word = words[i];
+            
+            if(isConcatenated(word, st, map)){
+                result.add(word);
+            }
+        }
+        return result;
+    }
+}
