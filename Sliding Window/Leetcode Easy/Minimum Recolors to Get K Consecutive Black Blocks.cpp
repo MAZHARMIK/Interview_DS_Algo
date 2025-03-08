256a900b47d8a3a928fe2df8a78cd0e06305c84c
MAZHAR <iiestshibpur605@gmail.com>
2025-03-08 08:36:31 +0530

    Added YouTube Link

--- Sliding Window/Leetcode Easy/Minimum Recolors to Get K Consecutive Black Blocks.cpp
+++ Sliding Window/Leetcode Easy/Minimum Recolors to Get K Consecutive Black Blocks.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn   : 
+    MY YOUTUBE VIDEO ON THIS Qn   : https://www.youtube.com/watch?v=3X6pwCDWFUM
     Company Tags                  : Will update soon
     Leetcode Link                 : https://leetcode.com/problems/minimum-recolors-to-get-k-consecutive-black-blocks
 */
