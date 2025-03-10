6d1ad088bab21760218ee7fafb376d51af1c53b0
MAZHAR <iiestshibpur605@gmail.com>
2025-03-10 09:07:57 +0530

    Added YouTube Link

--- Sliding Window/Count of Substrings Containing Every Vowel and K Consonants II.cpp
+++ Sliding Window/Count of Substrings Containing Every Vowel and K Consonants II.cpp
@@ -1,6 +1,6 @@
 /*         Scroll below to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=8FP1kkYf2U4
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/count-of-substrings-containing-every-vowel-and-k-consonants-ii
 */
