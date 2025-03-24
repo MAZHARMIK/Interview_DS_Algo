847210a8bd5724aaa7a94ecf5ffdd2b673a76a66
MAZHAR <iiestshibpur605@gmail.com>
2025-03-24 20:47:06 +0530

    Added JAVA code also

--- Tree/Binary Tree Maximum Path Sum.cpp
+++ Tree/Binary Tree Maximum Path Sum.cpp
@@ -1,3 +1,4 @@
+/*        Scroll below to see JAVA code as well        */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=Op6YFcs8R9M
     Company Tags                : Google, Amazon, Meta, Flipkart
@@ -5,6 +6,9 @@
     GfG Link                    : https://practice.geeksforgeeks.org/problems/maximum-path-sum-from-any-node/1
 */
 
+/****************************************************** C++ ******************************************/
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
 public:
     
@@ -40,3 +44,36 @@ public:
         
     }
 };
+
+
+/****************************************************** JAVA ******************************************/
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    
+    private int maxSum;
+    
+    private int solve(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int left = solve(root.left);
+        int right = solve(root.right);
+        
+        int neecheHiMilgayaAnswer = left + right + root.val; // (1)
+        int koiEkAcha = Math.max(left, right) + root.val; // (2)
+        int onlyRootAcha = root.val; // (3)
+
+        maxSum = Math.max(maxSum, Math.max(neecheHiMilgayaAnswer, Math.max(koiEkAcha, onlyRootAcha)));
+        
+        // Most important part
+        return Math.max(koiEkAcha, onlyRootAcha);
+    }
+    
+    public int maxPathSum(TreeNode root) {
+        maxSum = Integer.MIN_VALUE;
+        solve(root);
+        return maxSum;
+    }
+}
