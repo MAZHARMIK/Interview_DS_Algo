30c4c3fd1b734ce45bdec91c234f3d4cec99a316
MAZHAR <iiestshibpur605@gmail.com>
2025-03-24 07:53:48 +0530

    Added JAVA code also

---
+++ Arrays/Intervals_Based_Qn/Count Days Without Meetings.cpp
@@ -0,0 +1,65 @@
+/*         Scroll below to see JAVA code also        */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : will update soon
+    Leetcode Link               : https://leetcode.com/problems/count-days-without-meetings
+*/
+
+
+/**************************************************** C++ ****************************************************/
+//Approach (Simple sorting based on starting point)
+//T.C : O(nlogn)
+//S.C : O(1)
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+        int n = meetings.size();
+
+        sort(begin(meetings), end(meetings));
+
+        int result = 0;
+        int start = 0;
+        int end = 0;
+
+        for(auto &meet : meetings) {
+            if(meet[0] > end) {
+                result += meet[0] - end - 1;
+            }
+
+            end = max(end, meet[1]);
+        }
+
+        if(days > end) {
+            result += days-end;
+        }
+
+        return result;
+    }
+};
+
+
+/**************************************************** JAVA ****************************************************/
+//Approach (Simple sorting based on starting point)
+//T.C : O(nlogn)
+//S.C : O(1)
+class Solution {
+    public int countDays(int days, int[][] meetings) {
+        Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        int result = 0;
+        int end = 0;
+        
+        for (int[] meet : meetings) {
+            if (meet[0] > end) {
+                result += meet[0] - end - 1;
+            }
+            end = Math.max(end, meet[1]);
+        }
+        
+        if (days > end) {
+            result += days - end;
+        }
+        
+        return result;
+    }
+}
