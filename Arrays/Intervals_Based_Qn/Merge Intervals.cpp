3c0463c58575fc328293c7e75550e32762094ebb
MAZHAR <iiestshibpur605@gmail.com>
2025-03-25 07:45:41 +0530

    Added JAVA code also

--- Arrays/Intervals_Based_Qn/Merge Intervals.cpp
+++ Arrays/Intervals_Based_Qn/Merge Intervals.cpp
@@ -1,9 +1,15 @@
+/*         Scroll below to see JAVA code as well        */
 /*
-    Company Tags     : Amazon, Google, Nutanix
-    Leetcode Qn Link : https://leetcode.com/problems/merge-intervals/
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Amazon, Google, Nutanix
+    Leetcode Qn Link            : https://leetcode.com/problems/merge-intervals/
 */
 
-//Approach-1 (Brute Force) O(n^2) : TLE on Leetcode (1 test case gives TLE)
+
+/************************************************************************ C++ ************************************************************/
+//Approach-1 (Brute Force)
+//T.C : O(n^2)
+//S.C : O(1)
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
@@ -37,7 +43,9 @@ public:
 };
 
 
-//Approach-2 (O(n))
+//Approach-2 (Using sorting)
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
@@ -66,3 +74,75 @@ public:
 
     }
 };
+
+
+
+/************************************************************************ Java ************************************************************/
+// Approach-1 (Brute Force)
+// T.C : O(n^2)
+// S.C : O(1)
+import java.util.*;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        int i = 0;
+        while (i < intervals.length - 1) {
+            int i_start = intervals[i][0];
+            int i_end = intervals[i][1];
+            boolean merge = false;
+            
+            for (int j = i + 1; j < intervals.length; j++) {
+                int j_start = intervals[j][0];
+                int j_end = intervals[j][1];
+                
+                if (!(i_end < j_start || i_start > j_end)) {
+                    // Overlap. Merge them
+                    intervals[i][0] = Math.min(i_start, j_start);
+                    intervals[i][1] = Math.max(i_end, j_end);
+                    
+                    // Erase j by shifting elements
+                    for (int k = j; k < intervals.length - 1; k++) {
+                        intervals[k] = intervals[k + 1];
+                    }
+                    
+                    intervals = Arrays.copyOf(intervals, intervals.length - 1);
+                    merge = true;
+                    break;
+                }
+            }
+            
+            if (!merge) {
+                i++;
+            }
+        }
+        return intervals;
+    }
+}
+
+
+// Approach-2 (Using Sorting)
+// T.C : O(n log n)
+// S.C : O(n)
+class Solution2 {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0])); // Sort based on start point
+        
+        List<int[]> result = new ArrayList<>();
+        result.add(intervals[0]);
+        
+        for (int i = 1; i < intervals.length; i++) {
+            int curr_start = intervals[i][0];
+            int curr_end = intervals[i][1];
+            
+            // If no overlap
+            if (curr_start > result.get(result.size() - 1)[1]) {
+                result.add(intervals[i]);
+            } else {
+                // There is an overlap
+                result.get(result.size() - 1)[1] = Math.max(result.get(result.size() - 1)[1], curr_end);
+            }
+        }
+        
+        return result.toArray(new int[result.size()][]);
+    }
+}
