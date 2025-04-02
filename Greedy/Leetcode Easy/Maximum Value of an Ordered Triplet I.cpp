d19183e37fc2bd5542c9ca28f0840e90343c1638
MAZHAR <iiestshibpur605@gmail.com>
2025-04-02 12:42:11 +0530

    Added YouTube Link

--- Greedy/Leetcode Easy/Maximum Value of an Ordered Triplet I.cpp
+++ Greedy/Leetcode Easy/Maximum Value of an Ordered Triplet I.cpp
@@ -1,6 +1,6 @@
 /*          Scroll down to see JAVA code also         */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=UQZEiyIl4ZA
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-i
 */
