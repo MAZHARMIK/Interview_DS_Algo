6ce57e73990f9509f17296d4a4f2364a8e90e7ec
MAZHAR <iiestshibpur605@gmail.com>
2025-04-10 09:02:01 +0530

    Added YouTube Link

--- HashSet/Leetcode Easy/Minimum Operations to Make Array Values Equal to K.cpp
+++ HashSet/Leetcode Easy/Minimum Operations to Make Array Values Equal to K.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code as well    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=dDl_tmwd6HQ
     Company Tags                : 
     Leetcode Link               : https://leetcode.com/problems/minimum-operations-to-make-array-values-equal-to-k
 */
