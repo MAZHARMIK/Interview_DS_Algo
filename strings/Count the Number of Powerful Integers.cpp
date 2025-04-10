316cc5255988549ca9ed8c35523f1ea37548da62
MAZHAR <iiestshibpur605@gmail.com>
2025-04-10 14:36:47 +0530

    Added YouTube Link

--- strings/Count the Number of Powerful Integers.cpp
+++ strings/Count the Number of Powerful Integers.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=DNbvaC-EqyQ
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/count-the-number-of-powerful-integers
 */
