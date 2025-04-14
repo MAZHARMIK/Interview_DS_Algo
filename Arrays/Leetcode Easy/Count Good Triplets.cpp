24f3222f1161be69923582736f10143442699d00
MAZHAR <iiestshibpur605@gmail.com>
2025-04-14 06:53:04 +0530

    Added YouTube Link

--- Arrays/Leetcode Easy/Count Good Triplets.cpp
+++ Arrays/Leetcode Easy/Count Good Triplets.cpp
@@ -1,6 +1,6 @@
 /*         Scroll down to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=wKrBZd2GAZM
     Company Tags                : 
     Leetcode Link               : https://leetcode.com/problems/count-good-triplets
 */
