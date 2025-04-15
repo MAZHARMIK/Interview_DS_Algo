d790c8575dbc34fcb769e26f0dfe065483539ace
MAZHAR <iiestshibpur605@gmail.com>
2025-04-15 19:25:20 +0530

    Added YouTube Link

--- Segment Tree/Count Good Triplets in an Array.cpp
+++ Segment Tree/Count Good Triplets in an Array.cpp
@@ -1,6 +1,6 @@
 /*	 Scroll below to see JAVA code also	*/
 /*
-    MY YOUTUBE VIDEO ON THIS Qn :
+    MY YOUTUBE VIDEO ON THIS Qn   : https://www.youtube.com/watch?v=gRF7k0dNr7k
     Company Tags  		          : will update later
     Leetcode Link 		          : https://leetcode.com/problems/count-good-triplets-in-an-array
 */
