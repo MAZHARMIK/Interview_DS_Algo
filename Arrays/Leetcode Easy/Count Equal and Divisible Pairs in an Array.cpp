2a35864e7a86faf3ebbf68135d5493850c9ef758
MAZHAR <iiestshibpur605@gmail.com>
2025-04-17 18:27:58 +0530

    Added YouTube Link

--- Arrays/Leetcode Easy/Count Equal and Divisible Pairs in an Array.cpp
+++ Arrays/Leetcode Easy/Count Equal and Divisible Pairs in an Array.cpp
@@ -1,6 +1,6 @@
 /*         Scroll down to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=6iODXcqICGQ
     Company Tags                : 
     Leetcode Link               : https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array
 */
