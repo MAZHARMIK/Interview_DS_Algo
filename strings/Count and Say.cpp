506de8e75ad957d12a3625567a1a5b260c377268
MAZHAR <iiestshibpur605@gmail.com>
2025-04-18 07:40:44 +0530

    Added JAVA code also

--- strings/Count and Say.cpp
+++ strings/Count and Say.cpp
@@ -1,11 +1,16 @@
+/*     Scroll below to see JAVA code also    */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=5uJitfSM3vk
-    Company Tags  : Facebook, Amazon, Microsoft, Zoho, Google (3 years ago)
-    Leetcode Link : https://leetcode.com/problems/count-and-say/
-    GfG Link      : https://practice.geeksforgeeks.org/problems/decode-the-pattern1138/1
+    Company Tags                : Facebook, Amazon, Microsoft, Zoho, Google (3 years ago)
+    Leetcode Link               : https://leetcode.com/problems/count-and-say/
+    GfG Link                    : https://practice.geeksforgeeks.org/problems/decode-the-pattern1138/1
 */
 
 
+/**************************************************************** C++ ****************************************************************/
+//Approach (Simple Recursion)
+//T.C : O(2^n)
+//S.C : O(2^n)
 class Solution {
 public:
     string countAndSay(int n) {
@@ -36,3 +41,34 @@ public:
         
     }
 };
+
+
+
+/**************************************************************** JAVA ****************************************************************/
+//Approach (Simple Recursion)
+//T.C : O(2^n)
+//S.C : O(2^n)
+class Solution {
+    public String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+
+        String say = countAndSay(n - 1);
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < say.length(); i++) {
+            int count = 1;
+            char ch = say.charAt(i);
+
+            while (i < say.length() - 1 && say.charAt(i) == say.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+
+            result.append(count).append(ch);
+        }
+
+        return result.toString();
+    }
+}
