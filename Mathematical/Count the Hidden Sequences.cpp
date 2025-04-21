628662d2ee93fa2cbbdc901e2ec12e08e09f61bc
MAZHAR <iiestshibpur605@gmail.com>
2025-04-21 10:14:19 +0530

    Added YouTube Link

--- Mathematical/Count the Hidden Sequences.cpp
+++ Mathematical/Count the Hidden Sequences.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=ednfb3I1qBo
     Company Tags                : will soon update
     Leetcode Link               : https://leetcode.com/problems/count-the-hidden-sequences
 */
