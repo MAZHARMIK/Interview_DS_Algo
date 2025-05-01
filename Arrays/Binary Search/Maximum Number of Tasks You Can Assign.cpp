dba3b2b5e7e0092a8298ce3f09d8a78d81320c7b
MAZHAR <iiestshibpur605@gmail.com>
2025-05-01 12:53:59 +0530

    Added JAVA code also

---
+++ Arrays/Binary Search/Maximum Number of Tasks You Can Assign.cpp
@@ -0,0 +1,137 @@
+/*     Scroll below to see JAVA code also    */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Will update soon
+    Leetcode Link               : https://leetcode.com/problems/maximum-number-of-tasks-you-can-assign
+*/
+
+
+
+/****************************************************************** C++ ******************************************************************/
+//Approach (Binary Search on Answer)
+//T.C : O(MLogM+NLogN+Min(M,N)∗Log(Min(M,N))∗Log(Min(M,N))), M = tasks.size(), N = workers.size()
+//S.C : O(M)
+class Solution {
+public:
+    bool check(vector<int>& tasks, vector<int>& workers, int pills, int strength, int mid) {
+        int pillsUsed = 0;
+        multiset<int> st(begin(workers), begin(workers) + mid); //best mid workers
+
+        for(int i = mid-1; i >= 0; i--) {
+            int reqrd = tasks[i];
+            auto it   = prev(st.end());
+
+            if(*it >= reqrd) {
+                st.erase(it);
+            } else if(pillsUsed >= pills) {
+                return false;
+            } else {
+                //find the weakest worker which can do this strong task using pills
+                auto weakestWorkerIt = st.lower_bound(reqrd - strength);
+                if(weakestWorkerIt == st.end()) {
+                    return false;
+                }
+                st.erase(weakestWorkerIt);
+                pillsUsed++;
+            }
+        }
+
+        return true;
+    }
+    int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills, int strength) {
+        int m = tasks.size();
+        int n = workers.size();
+
+        int l = 0;
+        int r = min(m, n);
+
+        sort(begin(tasks), end(tasks));
+        sort(begin(workers), end(workers), greater<int>());
+
+        int result = 0;
+
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+
+            if(check(tasks, workers, pills, strength, mid)) {
+                result = mid;
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+        }
+
+        return result;
+
+
+    }
+};
+
+
+
+/****************************************************************** JAVA ******************************************************************/
+//Approach (Binary Search on Answer)
+//T.C : O(MLogM+NLogN+Min(M,N)∗Log(Min(M,N))∗Log(Min(M,N))), M = tasks.size(), N = workers.size()
+//S.C : O(M)
+class Solution {
+
+    public int maxTaskAssign(
+        int[] tasks,
+        int[] workers,
+        int pills,
+        int strength
+    ) {
+        Arrays.sort(tasks);
+        Arrays.sort(workers);
+        int n = tasks.length, m = workers.length;
+        int left = 1, right = Math.min(m, n), ans = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (check(tasks, workers, pills, strength, mid)) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    private boolean check(
+        int[] tasks,
+        int[] workers,
+        int pills,
+        int strength,
+        int mid
+    ) {
+        int p = pills;
+        TreeMap<Integer, Integer> ws = new TreeMap<>();
+        for (int i = workers.length - mid; i < workers.length; ++i) {
+            ws.put(workers[i], ws.getOrDefault(workers[i], 0) + 1);
+        }
+
+        for (int i = mid - 1; i >= 0; --i) {
+            Integer key = ws.lastKey();
+            if (key >= tasks[i]) {
+                ws.put(key, ws.get(key) - 1);
+                if (ws.get(key) == 0) {
+                    ws.remove(key);
+                }
+            } else {
+                if (p == 0) {
+                    return false;
+                }
+                key = ws.ceilingKey(tasks[i] - strength);
+                if (key == null) {
+                    return false;
+                }
+                ws.put(key, ws.get(key) - 1);
+                if (ws.get(key) == 0) {
+                    ws.remove(key);
+                }
+                --p;
+            }
+        }
+        return true;
+    }
+}
