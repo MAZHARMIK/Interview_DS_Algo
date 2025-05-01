2ba78a1c81ac2940e6be7781d562ff7bcba4ef91
MAZHAR <iiestshibpur605@gmail.com>
2025-05-01 13:21:11 +0530

    Added YouTube Link

--- Arrays/Binary Search/Maximum Number of Tasks You Can Assign.cpp
+++ Arrays/Binary Search/Maximum Number of Tasks You Can Assign.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=7tGM7XA06MI
     Company Tags                : Will update soon
     Leetcode Link               : https://leetcode.com/problems/maximum-number-of-tasks-you-can-assign
 */
