f5dab86335f60dc8cbb1e53020c7642148a827cf
MAZHAR <iiestshibpur605@gmail.com>
2025-05-02 12:29:06 +0530

    Added YouTube Link

--- Arrays/Two Pointer/Push Dominoes.cpp
+++ Arrays/Two Pointer/Push Dominoes.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-	MY YOUTUBE VIDEO ON THIS Qn : 
+	MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=HrUZKTzStII
     	Company Tags                : Google, Microsoft
     	Leetcode Link               : https://leetcode.com/problems/push-dominoes/
 */
