1306b53609cd82254e693f40be5fb9b52a06c00b
MAZHAR <iiestshibpur605@gmail.com>
2025-05-03 09:41:00 +0530

    Added YouTube Link

--- Greedy/Minimum Domino Rotations For Equal Row.cpp
+++ Greedy/Minimum Domino Rotations For Equal Row.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=fCB9ZZ-o2Sc
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=R4qAV4rjTc4
     Company Tags                : Google
     Leetcode Link               : https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/
 */
