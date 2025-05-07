37230706aaacfa8c7478b4e271d52fb582571d76
MAZHAR <iiestshibpur605@gmail.com>
2025-05-07 07:57:26 +0530

    Added YouTube Link

--- Graph/Dijkstra'a Based Problems/Find Minimum Time to Reach Last Room I.cpp
+++ Graph/Dijkstra'a Based Problems/Find Minimum Time to Reach Last Room I.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=bvfBnQo5qiY
     Company Tags                : Will update soon
     Leetcode Link               : https://leetcode.com/problems/find-minimum-time-to-reach-last-room-i
 */
