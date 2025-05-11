9fcc9b2801e7dd03766c87b96b580e69c6a77463
MAZHAR <iiestshibpur605@gmail.com>
2025-05-11 10:38:02 +0530

    Added YouTube Link

--- Arrays/Leetcode Easy/Three Consecutive Odds.cpp
+++ Arrays/Leetcode Easy/Three Consecutive Odds.cpp
@@ -1,6 +1,6 @@
 /*         Scroll down to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=p6T-osneg9g
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/three-consecutive-odds
 */
