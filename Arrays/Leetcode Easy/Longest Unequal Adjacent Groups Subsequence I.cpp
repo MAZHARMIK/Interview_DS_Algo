59546eda8c8dd3f9655254e3c554ddaffa1214fa
MAZHAR <iiestshibpur605@gmail.com>
2025-05-15 10:09:24 +0530

    Added YouTube Link

--- Arrays/Leetcode Easy/Longest Unequal Adjacent Groups Subsequence I.cpp
+++ Arrays/Leetcode Easy/Longest Unequal Adjacent Groups Subsequence I.cpp
@@ -1,6 +1,6 @@
 /*         Scroll down to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=6zb5qH4oNSY
     Company Tags                : 
     Leetcode Link               : https://leetcode.com/problems/longest-unequal-adjacent-groups-subsequence-i
 */
