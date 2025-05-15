42971b01cc316f7f42c190b6e85698bddc690c3b
MAZHAR <iiestshibpur605@gmail.com>
2025-05-15 09:56:48 +0530

    Added JAVA code also

---
+++ Arrays/Leetcode Easy/Longest Unequal Adjacent Groups Subsequence I.cpp
@@ -0,0 +1,46 @@
+/*         Scroll down to see JAVA code also        */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : 
+    Leetcode Link               : https://leetcode.com/problems/longest-unequal-adjacent-groups-subsequence-i
+*/
+
+/*********************************************************** C++ **************************************************/
+//Approach (Counting groups)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        int n = words.size();
+
+        vector<string> result;
+        for(int i = 0; i < n; i++) {
+            if(i == 0 || groups[i] != groups[i-1]) { //new group started, so pick the first element
+                result.push_back(words[i]);
+            }
+        }
+
+        return result;
+    }
+};
+
+
+/*********************************************************** JAVA **************************************************/
+//Approach (Counting groups)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public List<String> getLongestSubsequence(String[] words, int[] groups) {
+        int n = words.length;
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || groups[i] != groups[i - 1]) {
+                result.add(words[i]); // new group started, so pick the first element
+            }
+        }
+
+        return result;
+    }
+}
