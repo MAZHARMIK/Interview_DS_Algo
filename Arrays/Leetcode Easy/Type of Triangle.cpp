5f6a893778c08799ff9d3b562951db2c82c009db
MAZHAR <iiestshibpur605@gmail.com>
2025-05-19 07:04:04 +0530

    Added YouTube Link

--- Arrays/Leetcode Easy/Type of Triangle.cpp
+++ Arrays/Leetcode Easy/Type of Triangle.cpp
@@ -1,6 +1,6 @@
 /*         Scroll down to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=X9uqA9KhID8
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/type-of-triangle
 */
