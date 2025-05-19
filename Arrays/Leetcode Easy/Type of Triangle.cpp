274d08110c3174f3f2ef3b00593913a2feb799bc
MAZHAR <iiestshibpur605@gmail.com>
2025-05-19 06:53:54 +0530

    Added JAVA code also

---
+++ Arrays/Leetcode Easy/Type of Triangle.cpp
@@ -0,0 +1,57 @@
+/*         Scroll down to see JAVA code also        */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : will update later
+    Leetcode Link               : https://leetcode.com/problems/type-of-triangle
+*/
+
+/*********************************************************** C++ **************************************************/
+//Approach (Simply checking triangle properties)
+//T.C : O(1) 
+//S.C : O(1)
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        bool valid = (nums[0] + nums[1] > nums[2]) &&
+                     (nums[0] + nums[2] > nums[1]) &&
+                     (nums[1] + nums[2] > nums[0]);
+
+        if(!valid) {
+            return "none";
+        }
+
+        if(nums[0] == nums[1] && nums[1] == nums[2]) {
+            return "equilateral";
+        } else if(nums[0] != nums[1] && nums[0] != nums[2] && nums[1] != nums[2]) {
+            return "scalene";
+        }
+
+        return "isosceles";
+    }
+};
+
+
+
+/*********************************************************** JAVA **************************************************/
+//Approach (Simply checking triangle properties)
+//T.C : O(1) 
+//S.C : O(1)
+class Solution {
+    public String triangleType(int[] nums) {
+        boolean valid = (nums[0] + nums[1] > nums[2]) &&
+                        (nums[0] + nums[2] > nums[1]) &&
+                        (nums[1] + nums[2] > nums[0]);
+
+        if (!valid) {
+            return "none";
+        }
+
+        if (nums[0] == nums[1] && nums[1] == nums[2]) {
+            return "equilateral";
+        } else if (nums[0] != nums[1] && nums[0] != nums[2] && nums[1] != nums[2]) {
+            return "scalene";
+        }
+
+        return "isosceles";
+    }
+}
