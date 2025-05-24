4d33b887360040f670aeb615590e26467a9095ed
MAZHAR <iiestshibpur605@gmail.com>
2025-05-24 11:38:57 +0530

    Added JAVA code also

---
+++ Arrays/Leetcode Easy/Find Words Containing Character.cpp
@@ -0,0 +1,43 @@
+/*         Scroll down to see JAVA code also        */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : will update later
+    Leetcode Link               : https://leetcode.com/problems/find-words-containing-character
+*/
+
+/*********************************************************** C++ **************************************************/
+//Approach (usin c++ STL)
+//T.C : O(m*n) 
+//S.C : O(1)
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> res;
+        int n = words.size();
+        for (int i = 0; i < n; ++i) {
+            if (words[i].find(x) != string::npos) {
+                res.push_back(i);
+            }
+        }
+        return res;
+    }
+};
+
+
+
+/*********************************************************** JAVA **************************************************/
+//T.C : O(m*n) 
+//S.C : O(1)
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].chars().anyMatch(ch -> ch == x)) {
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
+}
