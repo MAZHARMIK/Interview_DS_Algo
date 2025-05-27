395dcd0574ea98d1adf0cebc78115ae391f02021
MAZHAR <iiestshibpur605@gmail.com>
2025-05-27 09:14:43 +0530

    Java code typo

--- Mathematical/Divisible and Non-divisible Sums Difference.cpp
+++ Mathematical/Divisible and Non-divisible Sums Difference.cpp
@@ -27,11 +27,13 @@ public:
 //S.C : O(1)
 class Solution {
     public int differenceOfSums(int n, int m) {
-        int k = n / m;
+         int k = n / m;
 
+        //Using variables for clarity
         int totalSum = n * (n + 1) / 2;
         int divisibleSum = m * k * (k + 1) / 2;
+        int nonDivisibleSum = totalSum - divisibleSum;
 
-        return totalSum - divisibleSum;
+        return nonDivisibleSum - divisibleSum;
     }
 }
