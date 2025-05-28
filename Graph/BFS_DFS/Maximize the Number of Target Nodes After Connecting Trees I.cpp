df1f1b0595e5129313a11580d5b40b7c3f40ceb5
MAZHAR <iiestshibpur605@gmail.com>
2025-05-28 10:45:04 +0530

    Added YouTube Link

--- Graph/BFS_DFS/Maximize the Number of Target Nodes After Connecting Trees I.cpp
+++ Graph/BFS_DFS/Maximize the Number of Target Nodes After Connecting Trees I.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=tGtXUkUbYfo
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/maximize-the-number-of-target-nodes-after-connecting-trees-i
 */
