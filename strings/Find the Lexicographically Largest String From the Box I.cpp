9b57a5a198af11aee3eef82ff58f8573b4babc3b
MAZHAR <iiestshibpur605@gmail.com>
2025-06-04 14:07:09 +0530

    Added JAVA code also

--- strings/Find the Lexicographically Largest String From the Box I.cpp
+++ strings/Find the Lexicographically Largest String From the Box I.cpp
@@ -1,6 +1,7 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=P74WiyWY3Pk
+    MY YOUTUBE VIDEO ON THIS Qn : O(n^2) - https://www.youtube.com/watch?v=P74WiyWY3Pk
+                                  O(n)   - 
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/find-the-lexicographically-largest-string-from-the-box-i
 */
@@ -31,10 +32,50 @@ public:
     }
 };
 
-//Approach-2 (2 Pointer) - Coming soon
+
+//Approach-2 (2 Pointer)
 //T.C : O(n)
 //S.C : O(1)
+class Solution {
+public:
+    int bestStartingPoint(string &word, int n) {
+        int i = 0; //best starting point
+        int j = 1; //keep moving to find the best starting point
 
+        while(j < n) {
+            int k = 0;
+
+            //skipping equal characters
+            while( j+k < n && word[i+k] == word[j+k]) {
+                k++;
+            }
+
+            if(j+k < n && word[j+k] > word[i+k]) {
+                i = j; //updating best starting point
+                j = j+1;
+            } else {
+                j = j + k + 1; //skipping already checked characters
+            }
+        }
+
+        return i;
+    }
+
+    string answerString(string word, int numFriends) {
+        int n = word.length();
+
+        if(numFriends == 1)
+            return word;
+
+        int i = bestStartingPoint(word, n);
+
+        int longestPossibleLength = n - (numFriends-1);
+        int canTakePossible       = min(longestPossibleLength, n-i);
+
+        return word.substr(i, canTakePossible);
+
+    }
+};
 
 
 /**************************************************************** JAVA ****************************************************************/
@@ -67,3 +108,42 @@ class Solution {
 //Approach-2 (2 Pointer) - Coming soon
 //T.C : O(n)
 //S.C : O(1)
+public class Solution {
+
+    public int bestStartingPoint(String word, int n) {
+        int i = 0; // best starting point
+        int j = 1; // keep moving to find the best starting point
+
+        while (j < n) {
+            int k = 0;
+
+            // skipping equal characters
+            while (j + k < n && word.charAt(i + k) == word.charAt(j + k)) {
+                k++;
+            }
+
+            if (j + k < n && word.charAt(j + k) > word.charAt(i + k)) {
+                i = j; // updating best starting point
+                j = j + 1;
+            } else {
+                j = j + k + 1; // skipping already checked characters
+            }
+        }
+
+        return i;
+    }
+
+    public String answerString(String word, int numFriends) {
+        int n = word.length();
+
+        if (numFriends == 1) {
+            return word;
+        }
+
+        int i = bestStartingPoint(word, n);
+        int longestPossibleLength = n - (numFriends - 1);
+        int canTakePossible = Math.min(longestPossibleLength, n - i);
+
+        return word.substring(i, i + canTakePossible);
+    }
+}
