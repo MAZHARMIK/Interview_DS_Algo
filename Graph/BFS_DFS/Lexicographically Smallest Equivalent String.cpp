77a05e1bc38bd77fb1ba6bb06913a788d5a9b500
MAZHAR <iiestshibpur605@gmail.com>
2025-06-05 05:52:38 +0530

    Added JAVA code also

--- Graph/BFS_DFS/Lexicographically Smallest Equivalent String.cpp
+++ Graph/BFS_DFS/Lexicographically Smallest Equivalent String.cpp
@@ -1,12 +1,17 @@
+/*         Scroll below to see JAVA code as well        */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : DFS - https://www.youtube.com/watch?v=DsAIuJmiT1c
-                                  BFS - <soon>
-                                  DSU - <soon>
+                                  BFS - Let me know if you want a video on this)
+                                  DSU - Let me know if you want a video on this)
     Company Tags                : Amazon
     Leetcode Link               : https://leetcode.com/problems/lexicographically-smallest-equivalent-string/
 */
 
+
+/******************************************************************* C++ ************************************************************************/
 //Approach-1 (DFS)
+//T.C : O(m* ( V+E)), we try DFS m times
+//S.C : O(V+E)
 class Solution {
 public:
     
@@ -53,7 +58,62 @@ public:
 };
 
 
-//Approach-2 (BFS - SOON)
+//Approach-2 (BFS - Let me know if you want a video on this)
 
 
-//Approach-3 (DSU - SOON)
+//Approach-3 (DSU - Let me know if you want a video on this)
+
+
+
+/******************************************************************* JAVA ************************************************************************/
+//Approach-1 (DFS)
+//T.C : O(m* ( V+E)), we try DFS m times
+//S.C : O(V+E)
+class Solution {
+    
+    private char DFS(Map<Character, List<Character>> adj, char curr, boolean[] visited) {
+        visited[curr - 'a'] = true;
+        char minChar = curr;
+
+        for (char neighbor : adj.getOrDefault(curr, new ArrayList<>())) {
+            if (!visited[neighbor - 'a']) {
+                char nextMin = DFS(adj, neighbor, visited);
+                if (nextMin < minChar) {
+                    minChar = nextMin;
+                }
+            }
+        }
+
+        return minChar;
+    }
+
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        Map<Character, List<Character>> adj = new HashMap<>();
+        
+        // Build graph
+        for (int i = 0; i < s1.length(); i++) {
+            char u = s1.charAt(i);
+            char v = s2.charAt(i);
+            
+            adj.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
+            adj.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
+        }
+
+        StringBuilder result = new StringBuilder();
+        
+        for (char ch : baseStr.toCharArray()) {
+            boolean[] visited = new boolean[26];
+            result.append(DFS(adj, ch, visited));
+        }
+        
+        return result.toString();
+    }
+}
+
+
+//Approach-2 (BFS - Let me know if you want a video on this)
+
+
+//Approach-3 (DSU - Let me know if you want a video on this)
+
+
