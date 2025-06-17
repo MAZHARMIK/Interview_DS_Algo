34fff105a87b95b4f538740d462bd0b3015d17a5
MAZHAR <iiestshibpur605@gmail.com>
2025-06-17 09:25:11 +0530

    Added YouTube Link

--- Arrays/Binary Search/Minimize the Maximum Adjacent Element Difference.cpp
+++ Arrays/Binary Search/Minimize the Maximum Adjacent Element Difference.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=J_2DtfCHeSI
     Company Tags                : Will update soon
     Leetcode Link               : https://leetcode.com/problems/minimize-the-maximum-adjacent-element-difference/description/
 */
