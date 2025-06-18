868a798a6ef4f471102615906120d8311882e1c8
MAZHAR <iiestshibpur605@gmail.com>
2025-06-18 20:27:01 +0530

    Added JAVA code also

---
+++ Arrays/Partition Array Such That Maximum Difference Is K.cpp
@@ -0,0 +1,56 @@
+/*     Scroll below to see JAVA code as well    */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Will update soon
+    Leetcode Link               : https://leetcode.com/problems/partition-array-such-that-maximum-difference-is-k
+*/
+
+
+/************************************************************ C++ *****************************************************/
+//Approach (Using sorting + greedily partitioning)
+//T.C : O(nlogn + n)
+//S.C : O(1)
+class Solution {
+public:
+    int partitionArray(vector<int>& nums, int k) {
+        sort(begin(nums), end(nums));
+        int n = nums.size();
+
+        int minVal = nums[0];
+        int count  = 1;
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] - minVal > k) {
+                count++;
+                minVal = nums[i];
+            }
+        }
+
+        return count;
+    }
+};
+
+
+
+/************************************************************ JAVA *****************************************************/
+//Approach (Using sorting + greedily partitioning)
+//T.C : O(nlogn + n)
+//S.C : O(1)
+class Solution {
+    public int partitionArray(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        int minVal = nums[0];
+        int count = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] - minVal > k) {
+                count++;
+                minVal = nums[i];
+            }
+        }
+
+        return count;
+    }
+}
