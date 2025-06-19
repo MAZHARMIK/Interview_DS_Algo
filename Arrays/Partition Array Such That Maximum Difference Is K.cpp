233b3c9aef07884b2dc62d878e0499049c76be17
MAZHAR <iiestshibpur605@gmail.com>
2025-06-19 06:25:35 +0530

    Added YouTube Link

--- Arrays/Partition Array Such That Maximum Difference Is K.cpp
+++ Arrays/Partition Array Such That Maximum Difference Is K.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code as well    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=4Tptf8TvYag
     Company Tags                : Will update soon
     Leetcode Link               : https://leetcode.com/problems/partition-array-such-that-maximum-difference-is-k
 */
