3f168e086cd41f1758fde659506fc98ff039e5aa
MAZHAR <iiestshibpur605@gmail.com>
2025-06-25 17:43:36 +0530

    Added YouTube Link

--- Arrays/Binary Search/Kth Smallest Product of Two Sorted Arrays.cpp
+++ Arrays/Binary Search/Kth Smallest Product of Two Sorted Arrays.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=pjY5KaxYVIE
     Company Tags                : Will update soon
     Leetcode Link               : https://leetcode.com/problems/kth-smallest-product-of-two-sorted-arrays
 */
