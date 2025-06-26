2ebc7667e4fa1142d5e3007ce38d8da212ffcc1d
MAZHAR <iiestshibpur605@gmail.com>
2025-06-26 11:47:44 +0530

    Added YouTube Link

--- Greedy/Longest Binary Subsequence Less Than or Equal to K.cpp
+++ Greedy/Longest Binary Subsequence Less Than or Equal to K.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=ca04hcfuGlY
     Company Tags                : Will update soon
     Leetcode Link               : https://leetcode.com/problems/longest-binary-subsequence-less-than-or-equal-to-k
 */
