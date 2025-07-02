5cf690e3a28b396cbab5b94a6a8e486661e1c587
MAZHAR <iiestshibpur605@gmail.com>
2025-07-02 20:52:35 +0530

    Added YouTube Link

--- DP/Find the Original Typed String II.cpp
+++ DP/Find the Original Typed String II.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=Rfmhi6krtJU
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/find-the-original-typed-string-ii/description/
 */
