a42c3756c2040914d7c4e861389ad4c6450d679b
MAZHAR <iiestshibpur605@gmail.com>
2025-07-10 12:24:46 +0530

    Added JAVA code also

--- Greedy/Reschedule Meetings for Maximum Free Time II.cpp
+++ Greedy/Reschedule Meetings for Maximum Free Time II.cpp
@@ -1,6 +1,6 @@
 /*         Scroll below to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=nRQwEyJBW-I
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/reschedule-meetings-for-maximum-free-time-ii
 */
@@ -13,11 +13,10 @@
 class Solution {
 public:
     int maxFreeTime(int eventTime, vector<int>& startTime, vector<int>& endTime) {
-
         vector<int> freeArray; //store durations of free gaps
 
         //ith event
-        //ith start - (i-1)th ka end = free gap duration
+        //ith start - i-1th ka end = free gap duration
         freeArray.push_back(startTime[0]);
 
         for(int i = 1; i < startTime.size(); i++) {
@@ -26,34 +25,35 @@ public:
 
         freeArray.push_back(eventTime - endTime[endTime.size()-1]);
 
-        // Step 2: Precompute the largest free time to the right of each index
         int n = freeArray.size();
-        vector<int> maxRight(n, 0);
-        for (int i = n - 2; i >= 0; i--) {
-            maxRight[i] = max(maxRight[i + 1], freeArray[i + 1]);
+        vector<int> maxRightFree(n, 0);
+        vector<int> maxLeftFree(n, 0);
+        for(int i = n-2; i >= 0; i--) {
+            maxRightFree[i] = max(maxRightFree[i+1], freeArray[i+1]);
         }
 
-        vector<int> maxLeft(n, 0);
-        for (int i = 2; i < n; i++) {
-            maxLeft[i] = max(maxLeft[i - 1], freeArray[i - 2]);
+        for(int i = 1; i < n; i++) {
+            maxLeftFree[i] = max(maxLeftFree[i-1], freeArray[i-1]);
         }
 
-        // Step 3: Try moving each meeting and calculate max free time
+
         int result = 0;
-        for (int i = 1; i < n; ++i) {
-            int curMeetingDuration = endTime[i - 1] - startTime[i - 1];  // Duration of the (i-1)th meeting
+        //Iterating on the freeArray
+        for(int i = 1; i < n; i++) {
+            int currEventTime = endTime[i-1] - startTime[i-1]; //duration of event = d
 
-            //Move the meeting to another free spot
-            //But, check if moving is possible or not
-            if (curMeetingDuration <= max(maxLeft[i], maxRight[i])) {
-                result = max(result, freeArray[i - 1] + curMeetingDuration + freeArray[i]);
+            //Case-1 Moving completely out
+            if(currEventTime <= max(maxLeftFree[i-1], maxRightFree[i])) {
+                result = max(result, freeArray[i-1] + currEventTime + freeArray[i]);
             }
 
-            //Merge left and right free time
-            result = max(result, freeArray[i - 1] + freeArray[i]);
+            //case-2 shift left or right
+            result = max(result, freeArray[i-1] + freeArray[i]);
         }
 
         return result;
+
+
     }
 };
 
@@ -65,44 +65,39 @@ public:
 //S.C : O(n)
 class Solution {
     public int maxFreeTime(int eventTime, int[] startTime, int[] endTime) {
-        // Prepare freeArray to store free gaps
-        int nEvents = startTime.length;
-        int[] freeArray = new int[nEvents + 1];
-
-        // 1st free gap: before the first event
+        // Step 1: Compute freeArray (gaps between events)
+        int n = startTime.length;
+        int[] freeArray = new int[n + 1];
         freeArray[0] = startTime[0];
-        // gaps between events
-        for (int i = 1; i < nEvents; i++) {
+        for (int i = 1; i < n; i++) {
             freeArray[i] = startTime[i] - endTime[i - 1];
         }
-        // gap after the last event
-        freeArray[nEvents] = eventTime - endTime[nEvents - 1];
+        freeArray[n] = eventTime - endTime[n - 1];
 
-        int n = freeArray.length;
-        int[] maxRight = new int[n];
-        
-        // Precompute max free time to the right
-        for (int i = n - 2; i >= 0; i--) {
-            maxRight[i] = Math.max(maxRight[i + 1], freeArray[i + 1]);
+        int m = freeArray.length;
+        int[] maxRightFree = new int[m];
+        int[] maxLeftFree = new int[m];
+
+        // Step 2: Precompute max free to the right
+        for (int i = m - 2; i >= 0; i--) {
+            maxRightFree[i] = Math.max(maxRightFree[i + 1], freeArray[i + 1]);
+        }
+        // Step 3: Precompute max free to the left
+        for (int i = 1; i < m; i++) {
+            maxLeftFree[i] = Math.max(maxLeftFree[i - 1], freeArray[i - 1]);
         }
 
-        int[] maxLeft = new int[n];
-        // Precompute max free time to the left
-        for (int i = 2; i < n; i++) {
-            maxLeft[i] = Math.max(maxLeft[i - 1], freeArray[i - 2]);
-        }
-        
         int result = 0;
-        // Try moving each meeting and calculate max free time
-        for (int i = 1; i < n; ++i) {
-            int curMeetingDuration = endTime[i - 1] - startTime[i - 1];
+        // Step 4: Iterate for each possible movement/shift
+        for (int i = 1; i < m; i++) {
+            int currEventTime = endTime[i - 1] - startTime[i - 1];
 
-            // Move the meeting to another free spot if possible
-            if (curMeetingDuration <= Math.max(maxLeft[i], maxRight[i])) {
-                result = Math.max(result, freeArray[i - 1] + curMeetingDuration + freeArray[i]);
+            // Case 1: Move completely out
+            if (currEventTime <= Math.max(maxLeftFree[i - 1], maxRightFree[i])) {
+                result = Math.max(result, freeArray[i - 1] + currEventTime + freeArray[i]);
             }
 
-            // Merge left and right free time
+            // Case 2: Shift left or right
             result = Math.max(result, freeArray[i - 1] + freeArray[i]);
         }
 
