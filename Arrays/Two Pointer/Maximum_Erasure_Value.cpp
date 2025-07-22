d1cb950b763a9c53eb809332fd0c362125a7f92a
MAZHAR <iiestshibpur605@gmail.com>
2025-07-22 11:14:20 +0530

    Added JAVA code also

--- Arrays/Two Pointer/Maximum_Erasure_Value.cpp
+++ Arrays/Two Pointer/Maximum_Erasure_Value.cpp
@@ -1,26 +1,71 @@
+/*			Scroll below to see JAVA code also			*/
 /*
-    Leetcode Weekly Contest Qn
-    Company Tag   : NIL (Let me know if you find any :-) )
-    Leetcode Link : https://leetcode.com/problems/maximum-erasure-value/
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tag                 : will update later
+    Leetcode Link               : https://leetcode.com/problems/maximum-erasure-value/
 */
 
-//Approach-1 (Smart)
+
+/*************************************************************** C++ *************************************************/
+//Approach-1 (Sliding Window)
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
         int n = nums.size();
-        vector<int> cus(n+1, 0);
-        for(int i = 0; i<n; i++)
-            cus[i+1] = cus[i] + nums[i];
+        unordered_set<int> st;
+
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        int result = 0;
+
+        while(j < n) {
+            if(!st.count(nums[j])) {
+                sum += nums[j];
+                result = max(result, sum);
+                st.insert(nums[j]);
+                j++;
+            } else {
+                while(i < n && st.count(nums[j])) {
+                    sum -= nums[i];
+                    st.erase(nums[i]);
+                    i++;
+                }
+                
+            }
+        }
+
+        return result;
+    }
+};
+
+
+
+//Approach-2 (Using cumulative sum to find subarray sum)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> cus(n, 0);
+        cus[0] = nums[0];
+        for(int i = 1; i < n; i++)
+            cus[i] = cus[i-1] + nums[i];
         
-        vector<int> t(100001, -1);
-        int start  = 0;
+        vector<int> mp(10001, -1); //Using as a map 1 <= nums[i] <= 10^4
         int maxS   = 0;
+        int i      = 0;
         
-        for(int end  = 0; end<n; end++) {
-            start = max(start, t[nums[end]]+1);
-            maxS  = max(maxS, cus[end+1]-cus[start]);
-            t[nums[end]] = end;
+        for(int j  = 0; j < n; j++) {
+            i       = max(i, mp[nums[j]]+1);
+            int jthSum  = cus[j];
+            int ithSum  = i-1 < 0 ? 0 : cus[i-1];
+            maxS        = max(maxS, jthSum - ithSum);
+            mp[nums[j]] = j;
         }
         
         return maxS;
@@ -28,45 +73,63 @@ public:
 };
 
 
-//Approach-2 (Simple and intuitive)
-class Solution {
-public:
-    int maximumUniqueSubarray(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 1)
-            return nums[0];
-        
-        int sum = 0;
-        int i = 0,  j = 0;
-        unordered_map<int, int> mp;
-        
-        vector<int> cumS(n, 0);
-        cumS[0] = nums[0];
-        for(int i = 1; i<n; i++) {
-            cumS[i] = cumS[i-1] + nums[i];
-        }
 
-        while(i < n) {
-            if(!mp.count(nums[i])) {
-                mp[nums[i]] = i;
-                i++;
+/*************************************************************** JAVA *************************************************/
+//Approach-1 (Sliding Window)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        
+        int i = 0; 
+        int j = 0;
+        int sum = 0;
+        int max = 0;
+        
+        while(j < nums.length) {
+            if(!set.contains(nums[j])) {
+                sum += nums[j];
+                max = Math.max(max, sum); 
+                set.add(nums[j++]);
             } else {
-                if(mp[nums[i]] >= j) {
-                   int temp = cumS[i-1] - (j>0 ? cumS[j-1] : 0);
-                    sum = max(sum, temp);
-                    j = mp[nums[i]] + 1;
-                }
-                mp[nums[i]] = i;
-                i++;
+                sum -= nums[i];
+                set.remove(nums[i++]);
             }
         }
         
-        
-        int temp = 0;
-        if(j < n) {
-            temp = cumS[n-1] - (j>0 ? cumS[j-1] : 0);
-        }
-        
-        return max(sum, temp);
+        return max;
     }
-};
+}
+
+
+
+//Approach-2 (Using cumulative sum to find subarray sum)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        int n = nums.length;
+
+        int[] cus = new int[n]; 
+        cus[0] = nums[0];
+        for(int i = 1; i < n; i++)
+            cus[i] = cus[i-1] + nums[i];
+
+        int[] mp = new int[10001];
+        Arrays.fill(mp, -1);
+        
+        int maxS = 0;
+        int i = 0;
+
+        for(int j = 0; j < n; j++) {
+            i       = Math.max(i, mp[nums[j]] + 1); 
+            int jthSum  = cus[j];
+            int ithSum  = i-1 < 0 ? 0 : cus[i-1];
+            maxS        = Math.max(maxS, jthSum - ithSum);
+            mp[nums[j]] = j;
+        }
+
+        return maxS;
+    }
+}
