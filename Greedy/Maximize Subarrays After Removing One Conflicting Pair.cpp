f892c242393a9c8aa5b358f7bf1e0e6358621ad6
MAZHAR <iiestshibpur605@gmail.com>
2025-07-26 14:37:48 +0530

    Added YouTube Link

--- Greedy/Maximize Subarrays After Removing One Conflicting Pair.cpp
+++ Greedy/Maximize Subarrays After Removing One Conflicting Pair.cpp
@@ -1,6 +1,6 @@
 /*         Scroll below to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=gX3dCYnHpug
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/maximize-subarrays-after-removing-one-conflicting-pair
 */
