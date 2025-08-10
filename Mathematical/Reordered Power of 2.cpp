0ea86419ddf35bb20093f4bbe8a764da67408617
MAZHAR <iiestshibpur605@gmail.com>
2025-08-10 13:48:02 +0530

    Added JAVA code also

--- Mathematical/Reordered Power of 2.cpp
+++ Mathematical/Reordered Power of 2.cpp
@@ -128,3 +128,112 @@ public:
         return false;
     }
 };
+
+
+
+
+/****************************************************** JAVA **************************************************************/
+//Approach-1 (Using sorting)
+//T.C : O(dlogd), d = number of digits
+//S.C : O(d)
+class Solution {
+    public String getSortedStr(int n) {
+        char[] chars = String.valueOf(n).toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+
+    public boolean reorderedPowerOf2(int n) {
+        String sortedStr = getSortedStr(n);
+
+        // Check all powers of 2 up to 2^29
+        for (int p = 0; p <= 29; p++) {
+            if (sortedStr.equals(getSortedStr(1 << p))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+
+//Approach-2 (Using preprocessing and storing in a set)
+//T.C : O(dlogd), d = number of digits
+//S.C : O(d)
+class Solution {
+    private HashSet<String> set = new HashSet<>();
+
+    private void buildSet() {
+        for (int p = 0; p <= 29; p++) {
+            char[] chars = String.valueOf(1 << p).toCharArray();
+            Arrays.sort(chars);
+            set.add(new String(chars));
+        }
+    }
+
+    public boolean reorderedPowerOf2(int n) {
+        if (set.isEmpty()) {
+            buildSet(); // Call once
+        }
+
+        char[] chars = String.valueOf(n).toCharArray();
+        Arrays.sort(chars);
+        return set.contains(new String(chars));
+    }
+}
+
+
+
+//Approach-3 (count and store digits in a vector)
+//T.C : O(d), d = number of digits
+//S.C : O(1)
+class Solution {
+    private int[] getCountVector(int n) {
+        int[] count = new int[10];
+        while (n > 0) {
+            count[n % 10]++;
+            n /= 10;
+        }
+        return count;
+    }
+
+    public boolean reorderedPowerOf2(int n) {
+        int[] inputCount = getCountVector(n);
+
+        for (int p = 0; p <= 29; p++) {
+            if (Arrays.equals(inputCount, getCountVector(1 << p))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+
+//Approach-4 (count and store digits in a number)
+//T.C : O(d), d = number of digits
+//S.C : O(1)
+class Solution {
+    private int getCountNumber(int n) {
+        int num = 0;
+        while (n > 0) {
+            num += Math.pow(10, n % 10);
+            n /= 10;
+        }
+        return num;
+    }
+
+    public boolean reorderedPowerOf2(int n) {
+        int inputCount = getCountNumber(n);
+
+        for (int p = 0; p <= 29; p++) {
+            if (inputCount == getCountNumber(1 << p)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
