fbd7819251fb02e10dbee19ad8285a3b85384276
MAZHAR <iiestshibpur605@gmail.com>
2025-08-13 09:41:53 +0530

    Added YouTube Link

--- Mathematical/Leetcode Easy/Power of Three.cpp
+++ Mathematical/Leetcode Easy/Power of Three.cpp
@@ -1,6 +1,6 @@
 /*    Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn  : 
+    MY YOUTUBE VIDEO ON THIS Qn  : https://www.youtube.com/watch?v=11lCatB_H_A
     Company Tags                 : Google
     Leetcode Link                : https://leetcode.com/problems/power-of-three/description
 */
