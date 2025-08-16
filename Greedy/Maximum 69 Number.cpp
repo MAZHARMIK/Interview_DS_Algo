2ed08067179120c199129386c107e5b74a9321e5
MAZHAR <iiestshibpur605@gmail.com>
2025-08-16 07:52:56 +0530

    Added JAVA code also

--- Greedy/Maximum 69 Number.cpp
+++ Greedy/Maximum 69 Number.cpp
@@ -1,10 +1,16 @@
+/*           Scroll below to see JAVA code as well            */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=DMP2bqW6URA
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=6dh1H5Hv4TI
     Company Tags                : Oracle (Phone Interview)
     Leetcode Link               : https://leetcode.com/problems/maximum-69-number/
 */
 
-//Without converting to string
+
+
+/********************************************************************** C++ **********************************************************************/
+//Approach (Without converting to string)
+//T.C : O(d) , d = number of dogits in num
+//S.C : O(1)
 class Solution {
 public:
     int maximum69Number (int num) {
@@ -26,3 +32,30 @@ public:
         
     }
 };
+
+
+
+/********************************************************************** JAVA **********************************************************************/
+//Approach (Without converting to string)
+//T.C : O(d) , d = number of dogits in num
+//S.C : O(1)
+class Solution {
+    public int maximum69Number (int num) {
+        int place = 0;
+        int index = -1;
+        int temp = num;
+        
+        while (temp > 0) {
+            int remain = temp % 10;
+            if (remain == 6) {
+                index = place;
+            }
+            temp /= 10;
+            place++;
+        }
+        
+        if (index == -1) return num;
+        
+        return num + 3 * (int)Math.pow(10, index);
+    }
+}
