2a48ef2eea78138de913ab7fbd91e5a47376e3eb
MAZHAR <iiestshibpur605@gmail.com>
2025-08-25 06:29:07 +0530

    Added JAVA code also

--- Arrays/2-D Array/Diagonal Traverse.cpp
+++ Arrays/2-D Array/Diagonal Traverse.cpp
@@ -1,3 +1,4 @@
+/*         Scroll below to see JAVA code as well        */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : https://youtu.be/7HAKiGZSrWc
     Company Tags  : Google, Amazon
@@ -7,8 +8,13 @@
 /*
     Note : If you want to travel diaginally from right side, you can use the key map as (i-j) instead of (i+j)
     Related Qn : https://leetcode.com/problems/sort-the-matrix-diagonally/
+    Youtube Link : https://www.youtube.com/watch?v=mNWwJQ7_z4Q
 */
 
+
+/****************************************************************** C++ ******************************************************************/
+//T.C : O(m*n * log(m+n)), log because we are using ordered_map
+//S.C : O(m*n)
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
@@ -43,3 +49,50 @@ public:
         return result;
     }
 };
+
+
+/****************************************************************** JAVA ******************************************************************/
+//T.C : O(m*n * log(m+n)), log because we are using ordered_map
+//S.C : O(m*n)
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        
+        // Use HashMap to store diagonals
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        
+        // Fill the map with diagonals
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int key = i + j;
+                map.putIfAbsent(key, new ArrayList<>());
+                map.get(key).add(mat[i][j]);
+            }
+        }
+        
+        List<Integer> resultList = new ArrayList<>();
+        boolean flip = true;
+        
+        // Process diagonals in increasing order of keys
+        for (int k = 0; k <= m + n - 2; k++) {
+            List<Integer> diagonal = map.get(k);
+            if (diagonal == null) continue;
+            
+            if (flip) {
+                Collections.reverse(diagonal);
+            }
+            resultList.addAll(diagonal);
+            
+            flip = !flip;
+        }
+        
+        // Convert result list to array
+        int[] result = new int[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+            result[i] = resultList.get(i);
+        }
+        
+        return result;
+    }
+}
