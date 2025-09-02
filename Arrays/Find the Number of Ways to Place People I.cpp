5d459c0e5c1b4f4f6d8d22e0b5bf2712a1141e7b
MAZHAR <iiestshibpur605@gmail.com>
2025-09-02 14:30:58 +0530

    Find the Number of Ways to Place People I

---
+++ Arrays/Find the Number of Ways to Place People I.cpp
@@ -0,0 +1,19 @@
+/*     Scroll below to see JAVA code as well    */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Will update later
+    Leetcode Link               : https://leetcode.com/problems/find-the-number-of-ways-to-place-people-i
+*/
+
+
+/************************************************************ C++ *****************************************************/
+//Approach-1 (Brute Force)
+//T.C : O(n^3)
+//S.C : O(1)
+
+
+
+
+//Approach-2 (Using Sorting)
+//T.C : O(n^2)
+//S.C : O(1)
