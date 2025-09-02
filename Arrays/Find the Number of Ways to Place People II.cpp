848b3b0f561ae47986f7ef57a8e65c4ed2909553
MAZHAR <iiestshibpur605@gmail.com>
2025-09-03 00:05:29 +0530

    Added JAVA code also

---
+++ Arrays/Find the Number of Ways to Place People II.cpp
@@ -0,0 +1,102 @@
+/*     Scroll below to see JAVA code as well    */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : Will update later
+    Leetcode Link               : https://leetcode.com/problems/find-the-number-of-ways-to-place-people-ii
+    YouTube Video Of Part-I     : https://www.youtube.com/watch?v=f7iKx5lpqJ8
+*/
+
+
+/************************************************************ C++ *****************************************************/
+//Approach (Using Sorting - Approach-2 of Find the Number of Ways to Place People I in my video)
+//T.C : O(n^2)
+//S.C : O(1)
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        int n = points.size();
+
+        // Sort: x ascending, if x same then sort as y descending
+        auto lambda = [](vector<int>& point1, vector<int>& point2) {
+            if (point1[0] == point2[0]) {
+                return point1[1] > point2[1];
+            }
+            return point1[0] < point2[0];
+        };
+
+        sort(points.begin(), points.end(), lambda);
+
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];   // upper left
+
+            int bestY = INT_MIN;
+
+            for (int j = i + 1; j < n; j++) {
+                int x2 = points[j][0];
+                int y2 = points[j][1];   // lower right
+
+                // Condition: (x2, y2) must be above (x1, y1)
+                if (y2 > y1) { //not lower right
+                    continue;
+                }
+
+                if (y2 > bestY) {
+                    result++;
+                    bestY = y2;
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
+
+
+
+/************************************************************ JAVA *****************************************************/
+//Approach (Using Sorting - Approach-2 of Find the Number of Ways to Place People I in my video)
+//T.C : O(n^2)
+//S.C : O(1)
+class Solution {
+    public int numberOfPairs(int[][] points) {
+        int n = points.length;
+
+        // Sort: x ascending, if x same then sort as y descending
+        Arrays.sort(points, (point1, point2) -> {
+            if (point1[0] == point2[0]) {
+                return point2[1] - point1[1];
+            }
+            return point1[0] - point2[0];
+        });
+
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];   // upper left
+
+            int bestY = Integer.MIN_VALUE;
+
+            for (int j = i + 1; j < n; j++) {
+                int x2 = points[j][0];
+                int y2 = points[j][1];   // lower right
+
+                // Condition: (x2, y2) must be above (x1, y1)
+                if (y2 > y1) { //not lower right
+                    continue;
+                }
+
+                if (y2 > bestY) {
+                    result++;
+                    bestY = y2;
+                }
+            }
+        }
+
+        return result;
+    }
+};
