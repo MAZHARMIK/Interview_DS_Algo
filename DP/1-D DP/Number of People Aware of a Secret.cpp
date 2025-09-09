976ebadacc53bfb04afe23d78d2e0dd914e28113
MAZHAR <iiestshibpur605@gmail.com>
2025-09-09 12:44:21 +0530

    Added YouTube Link

--- DP/1-D DP/Number of People Aware of a Secret.cpp
+++ DP/1-D DP/Number of People Aware of a Secret.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=qSHfpqtuWBY
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/number-of-people-aware-of-a-secret
 */
