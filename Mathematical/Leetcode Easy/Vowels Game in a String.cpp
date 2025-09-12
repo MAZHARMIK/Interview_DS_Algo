6b57fcff89463da20571030f7238d56e3864cb90
MAZHAR <iiestshibpur605@gmail.com>
2025-09-12 09:45:05 +0530

    Added YouTube Link

--- Mathematical/Leetcode Easy/Vowels Game in a String.cpp
+++ Mathematical/Leetcode Easy/Vowels Game in a String.cpp
@@ -1,6 +1,6 @@
 /*    Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn  : 
+    MY YOUTUBE VIDEO ON THIS Qn  : https://www.youtube.com/watch?v=vJuMCE8ZiD4
     Company Tags                 : will update later
     Leetcode Link                : https://leetcode.com/problems/vowels-game-in-a-string
 */
