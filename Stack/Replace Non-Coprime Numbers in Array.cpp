25a972a3189633b1a8472fb98b20eb8e3ce4c2a3
MAZHAR <iiestshibpur605@gmail.com>
2025-09-16 12:18:50 +0530

    Added YouTube Link

--- Stack/Replace Non-Coprime Numbers in Array.cpp
+++ Stack/Replace Non-Coprime Numbers in Array.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also   */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=ll-m8RR7EcU
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/replace-non-coprime-numbers-in-array
 */
