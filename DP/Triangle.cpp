ed40d0f56d36a5af677159f2c43367f8036cc19c
MAZHAR <iiestshibpur605@gmail.com>
2025-09-25 12:51:00 +0530

    Added YouTube Link

--- DP/Triangle.cpp
+++ DP/Triangle.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn   :
+    MY YOUTUBE VIDEO ON THIS Qn   : https://www.youtube.com/watch?v=zABL4mxtlj0
     Company Tags                  : Will updat elater
     Leetcode Link                 : https://leetcode.com/problems/triangle/  
     
