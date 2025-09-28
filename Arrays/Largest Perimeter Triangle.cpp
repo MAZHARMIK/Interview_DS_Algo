ba0c794dea450d8a2083274841f650260ebad882
MAZHAR <iiestshibpur605@gmail.com>
2025-09-28 10:44:35 +0530

    Added JAVA code as well

--- Arrays/Largest Perimeter Triangle.cpp
+++ Arrays/Largest Perimeter Triangle.cpp
@@ -1,3 +1,4 @@
+/*     Scroll below to see JAVA code as well    */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=wukuOrHM3eg
     Company Tags                : C3.ai
@@ -5,6 +6,11 @@
     
 */
 
+
+/************************************************************ C++ *****************************************************/
+//Approach (Simply sort and check linearly)
+//T.C : O(nlogn))
+//S.C : O(1)
 class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
@@ -23,3 +29,26 @@ public:
         
     }
 };
+
+
+
+/************************************************************ JAVA *****************************************************/
+//Approach (Simply sort and check linearly)
+//T.C : O(nlogn))
+//S.C : O(1)
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        int n = nums.length;
+        
+        Arrays.sort(nums); // Sort the array
+        
+        for (int i = n - 3; i >= 0; i--) {
+            // nums[i] < nums[i+1] < nums[i+2]
+            if (nums[i] + nums[i + 1] > nums[i + 2]) { // a + b > c
+                return nums[i] + nums[i + 1] + nums[i + 2];
+            }
+        }
+        
+        return 0;
+    }
+}
