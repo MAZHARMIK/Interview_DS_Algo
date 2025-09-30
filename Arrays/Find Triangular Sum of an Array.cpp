5184f22ec701a887cb48232c62e1678584d6c493
MAZHAR <iiestshibpur605@gmail.com>
2025-09-30 12:08:06 +0530

    Added YouTube Link

--- Arrays/Find Triangular Sum of an Array.cpp
+++ Arrays/Find Triangular Sum of an Array.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code as well    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=5qlXato2VkE
     Company Tags                : Will update later
     Leetcode Link               : https://leetcode.com/problems/find-triangular-sum-of-an-array
 */
