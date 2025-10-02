e8137df7fcf49994931f1c5078eb46ccaddd9da4
MAZHAR <iiestshibpur605@gmail.com>
2025-10-03 00:34:51 +0400

    Added YouTube Link

--- Arrays/Simulation/Water Bottles II.cpp
+++ Arrays/Simulation/Water Bottles II.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-      MY YOUTUBE VIDEO ON THIS Qn : 
+      MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=RxFLUxwdXa4
       Company Tags                : will update later
       Leetcode Link               : https://leetcode.com/problems/water-bottles-ii
 */
