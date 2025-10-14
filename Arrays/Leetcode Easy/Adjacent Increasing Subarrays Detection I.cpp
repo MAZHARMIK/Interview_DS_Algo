2cd9a64fd7abbdb5817f425d1ab54998265eadb5
MAZHAR <iiestshibpur605@gmail.com>
2025-10-14 17:31:03 +0530

    Added YouTube Link

--- Arrays/Leetcode Easy/Adjacent Increasing Subarrays Detection I.cpp
+++ Arrays/Leetcode Easy/Adjacent Increasing Subarrays Detection I.cpp
@@ -1,6 +1,6 @@
 /*         Scroll down to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=DB0llpjZOhs
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/adjacent-increasing-subarrays-detection-i
 */
