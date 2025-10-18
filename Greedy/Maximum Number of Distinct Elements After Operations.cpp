444d68c3840d575cc2375f871912dd4eb1642ccc
MAZHAR <iiestshibpur605@gmail.com>
2025-10-18 12:48:01 +0530

    Added YouTube Link

--- Greedy/Maximum Number of Distinct Elements After Operations.cpp
+++ Greedy/Maximum Number of Distinct Elements After Operations.cpp
@@ -1,6 +1,6 @@
 /*         Scroll below to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=Vg3NjGAgOzU
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/maximum-number-of-distinct-elements-after-operations
 */
