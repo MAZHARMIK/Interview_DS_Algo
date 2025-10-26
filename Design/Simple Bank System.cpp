964a6ca2796a61e19ebbbff82955680c303aa8a9
MAZHAR <iiestshibpur605@gmail.com>
2025-10-26 11:16:50 +0530

    Added YouTube Link

--- Design/Simple Bank System.cpp
+++ Design/Simple Bank System.cpp
@@ -1,6 +1,6 @@
 /*        Scroll below to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO IN THIS QN : 
+    MY YOUTUBE VIDEO IN THIS QN : https://www.youtube.com/watch?v=tWjhWdHR4Zw
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/simple-bank-system
 */
