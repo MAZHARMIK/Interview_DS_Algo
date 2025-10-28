70b9f1ffc57ebc429ff445b231ba3d7d32c94a57
MAZHAR <iiestshibpur605@gmail.com>
2025-10-28 19:23:12 +0530

    Added YouTube Link

--- Arrays/Leetcode Easy/Make Array Elements Equal to Zero.cpp
+++ Arrays/Leetcode Easy/Make Array Elements Equal to Zero.cpp
@@ -1,6 +1,6 @@
 /*         Scroll down to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=1uMoX4soG38
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/make-array-elements-equal-to-zero
 */
