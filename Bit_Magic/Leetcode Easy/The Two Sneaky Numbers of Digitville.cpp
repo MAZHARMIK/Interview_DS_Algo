177792c8b07718e4c92174092bc6adfe2be9cdcc
MAZHAR <iiestshibpur605@gmail.com>
2025-10-31 11:59:30 +0530

    Added YouTube Link

--- Bit_Magic/Leetcode Easy/The Two Sneaky Numbers of Digitville.cpp
+++ Bit_Magic/Leetcode Easy/The Two Sneaky Numbers of Digitville.cpp
@@ -1,6 +1,6 @@
 /*   Scroll below to see JAVA code also  */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=5Vjt_w5Zi-A
     Company Tags                : Will udpate later
     Leetcode Link               : https://leetcode.com/problems/the-two-sneaky-numbers-of-digitville
 */
