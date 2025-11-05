db30b1f168bad5cae9da91c3afbaa6197eb89b33
MAZHAR <iiestshibpur605@gmail.com>
2025-11-05 10:12:14 +0530

    Added YouTube Link

--- Sliding Window/Find X-Sum of All K-Long Subarrays II.cpp
+++ Sliding Window/Find X-Sum of All K-Long Subarrays II.cpp
@@ -1,6 +1,6 @@
 /*			Scroll below to see JAVA code also			*/
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=ZyE1zCe_gSQ
     Company Tag                 : will update later
     Leetcode Link               : https://leetcode.com/problems/find-x-sum-of-all-k-long-subarrays-ii/
 */
