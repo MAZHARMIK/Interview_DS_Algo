72364f55f0df7cb6b31450c4fd15140ad83f56f5
MAZHAR <iiestshibpur605@gmail.com>
2025-11-06 23:16:50 +0530

    Added DSU Approach

--- Graph/BFS_DFS/Power Grid Maintenance.cpp
+++ Graph/BFS_DFS/Power Grid Maintenance.cpp
@@ -158,8 +158,91 @@ public:
 
 
 //Approach-3 (Using DSU)
-//Coming soon
+//T.C : O((n+c+q)×α(c)), n = connections.size(), c = number of nodes, q = number of queries
+//S.C : O(n + c + q)
+class Solution {
+public:
 
+    vector<int> parent;
+    vector<int> rank;
+
+    int Find(int x){
+        if(x == parent[x]) 
+            return x;
+        
+        return parent[x] = Find(parent[x]);
+    }
+
+    void Union(int x, int y){
+        int x_par = Find(x);
+        int y_par = Find(y);
+
+        if(x_par == y_par)
+            return;
+
+        if(rank[x_par] > rank[y_par]){
+            parent[y_par] = x_par;
+            rank[x_par]++;
+        }
+        else if(rank[x_par] < rank[y_par]){
+            parent[x_par] = y_par;
+            rank[y_par]++;
+        }
+        else{
+            parent[y_par] = x_par;
+            rank[x_par]++;
+        }
+    }
+
+    vector<int> processQueries(int c, vector<vector<int>>& connections, vector<vector<int>>& queries) {
+        vector<int> result;
+        int n = connections.size();
+        
+        rank.resize(c+1, 0);
+        parent.resize(c+1);
+        for(int i = 1; i <= c; i++) {
+            parent[i] = i;
+        }
+
+
+        for(auto& edge : connections) {
+            int u = edge[0];
+            int v = edge[1];
+            Union(u, v);
+        }
+
+        unordered_map<int, set<int>> mp; //id -> {ordered set of nodes}
+        vector<int> nodeId(c+1);
+
+        for(int i = 1;i <= c; i++){
+            int par = Find(i);
+            mp[par].insert(i);
+            nodeId[i] = par;
+        }
+
+        for(auto& query : queries){
+            int type = query[0]; //1, 2
+            int node = query[1]; //station/node
+            int id = nodeId[node];
+
+            if(type == 1) {
+                if(mp[id].find(node) != mp[id].end()){
+                    result.push_back(node);
+                } else if(!mp[id].empty()) {
+                    result.push_back(*mp[id].begin());
+                } else {
+                    result.push_back(-1);
+                }
+            }
+            else {
+                mp[id].erase(node);
+            }
+        }
+
+        return result;
+
+    }
+};
 
 
 
@@ -315,5 +398,89 @@ class Solution {
 }
 
 
-//Approach-3 (Using DSU)
-//Coming soon
+//Approach-3 (Using DSU / Union-Find)
+//T.C : O((c + n + q) log c)
+//S.C : O(c + n + q)
+class Solution {
+    int[] parent;
+    int[] rank;
+
+    private int find(int x) {
+        if (parent[x] == x)
+            return x;
+        return parent[x] = find(parent[x]); // Path compression
+    }
+
+    private void union(int x, int y) {
+        int xPar = find(x);
+        int yPar = find(y);
+
+        if (xPar == yPar)
+            return;
+
+        if (rank[xPar] > rank[yPar]) {
+            parent[yPar] = xPar;
+            rank[xPar]++;
+        } else if (rank[xPar] < rank[yPar]) {
+            parent[xPar] = yPar;
+            rank[yPar]++;
+        } else {
+            parent[yPar] = xPar;
+            rank[xPar]++;
+        }
+    }
+
+    public int[] processQueries(int c, int[][] connections, int[][] queries) {
+        int n = connections.length;
+        parent = new int[c + 1];
+        rank = new int[c + 1];
+
+        for (int i = 1; i <= c; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+
+        // Build components
+        for (int[] edge : connections) {
+            int u = edge[0];
+            int v = edge[1];
+            union(u, v);
+        }
+
+        // Build mapping of parent -> sorted set of nodes
+        Map<Integer, TreeSet<Integer>> mp = new HashMap<>();
+        int[] nodeId = new int[c + 1];
+
+        for (int i = 1; i <= c; i++) {
+            int par = find(i);
+            mp.computeIfAbsent(par, k -> new TreeSet<>()).add(i);
+            nodeId[i] = par;
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int[] query : queries) {
+            int type = query[0]; // 1 or 2
+            int node = query[1];
+            int id = nodeId[node];
+            TreeSet<Integer> set = mp.get(id);
+
+            if (type == 1) {
+                if (set != null && set.contains(node)) {
+                    result.add(node);
+                } else if (set != null && !set.isEmpty()) {
+                    result.add(set.first());
+                } else {
+                    result.add(-1);
+                }
+            } else { // type == 2
+                if (set != null) {
+                    set.remove(node);
+                }
+            }
+        }
+
+        // Convert List<Integer> to int[]
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+}
