851eb0c783dac537126f8f333ad482efc0e67c71
MAZHAR <iiestshibpur605@gmail.com>
2025-11-06 23:02:25 +0530

    added BFS

--- Graph/BFS_DFS/Power Grid Maintenance.cpp
+++ Graph/BFS_DFS/Power Grid Maintenance.cpp
@@ -13,72 +13,147 @@
 //S.C : O(c + n)
 class Solution {
 public:
-    unordered_map<int, set<int>> componentStations; 
+    //O(c + n)
+    void dfs(int node, unordered_map<int, vector<int>> &adj, int id, vector<int>& nodeId, 
+            unordered_map<int, set<int>>& mp, vector<bool>& visited) {
+        visited[node] = true;
+        mp[id].insert(node);
+        nodeId[node] = id;
 
-    void dfs(int node, unordered_map<int, vector<int>>& adj,
-             vector<int>& visited, int componentId, vector<int>& componentOf)  {
-        visited[node] = 1;
-        componentOf[node] = componentId;
-        componentStations[componentId].insert(node);
-
-        for (int neighbor : adj[node]) {
-            if (!visited[neighbor]) {
-                dfs(neighbor, adj, visited, componentId, componentOf);
+        for(int &ngbr : adj[node]) {
+            if(!visited[ngbr]) {
+                dfs(ngbr, adj, id, nodeId, mp, visited);
             }
         }
     }
 
-    vector<int> processQueries(int c, vector<vector<int>>& connections,
-                               vector<vector<int>>& queries) {
-        unordered_map<int, vector<int>> adj; 
-        vector<int> visited(c + 1, 0);
-        vector<int> componentOf(c + 1);
+    vector<int> processQueries(int c, vector<vector<int>>& connections, vector<vector<int>>& queries) {
+        unordered_map<int, vector<int>> adj;
 
-        for (auto& edge : connections) {
+        for(auto &edge :  connections) {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<bool> visited(c+1, false);
+        vector<int> nodeId(c+1);
+        //id -> {}
+        unordered_map<int, set<int>> mp;
+
+        //O(c+n)
+        for(int node = 1; node <= c; node++) {
+            if(!visited[node]) {
+                int id = node;
+                dfs(node, adj, id, nodeId, mp, visited);
+            }
+        }
+
+        vector<int> result;
+        //O(q * log(c))
+        for(auto &query : queries) {
+            int type = query[0]; //1, 2
+            int node = query[1]; //station/node
+
+            if(type == 1) {
+                int id = nodeId[node];
+                if(mp[id].count(node)) {
+                    result.push_back(node);
+                } else if(!mp[id].empty()) {
+                    result.push_back(*mp[id].begin());
+                } else {
+                    result.push_back(-1);
+                }
+            } else {
+                int id = nodeId[node];
+                mp[id].erase(node); //log(c)
+            }
+
+        }
+        return result;
+    }
+};
+
+
+//Approach-2 (Using BFS)
+//T.C : O((c + n) + q log c)
+//S.C : O(c + n)
+class Solution {
+public:
+    // O(c + n)
+    void bfs(int start, unordered_map<int, vector<int>> &adj, int id,
+             vector<int>& nodeId, unordered_map<int, set<int>>& mp,
+             vector<bool>& visited) {
+
+        queue<int> q;
+        q.push(start);
+        visited[start] = true;
+        nodeId[start] = id;
+        mp[id].insert(start);
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (int &ngbr : adj[node]) {
+                if (!visited[ngbr]) {
+                    visited[ngbr] = true;
+                    nodeId[ngbr] = id;
+                    mp[id].insert(ngbr);
+                    q.push(ngbr);
+                }
+            }
+        }
+    }
+
+    vector<int> processQueries(int c, vector<vector<int>>& connections, vector<vector<int>>& queries) {
+        unordered_map<int, vector<int>> adj;
+
+        for (auto &edge : connections) {
             int u = edge[0];
             int v = edge[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
 
+        vector<bool> visited(c + 1, false);
+        vector<int> nodeId(c + 1);
+        unordered_map<int, set<int>> mp;
+
+        // O(c + n)
         for (int node = 1; node <= c; node++) {
             if (!visited[node]) {
-                dfs(node, adj, visited, node, componentOf);
+                int id = node;
+                bfs(node, adj, id, nodeId, mp, visited);
             }
         }
 
         vector<int> result;
-
-        // Process queries
-        for (auto& q : queries) {
-            int type = q[0];
-            int x = q[1];
-            int compId = componentOf[x];
+        // O(q * log(c))
+        for (auto &query : queries) {
+            int type = query[0]; // 1 or 2
+            int node = query[1]; // station/node
 
             if (type == 1) {
-                if (componentStations[compId].count(x)) {
-                    result.push_back(x);
-                } 
-                else if (!componentStations[compId].empty()) {
-                    result.push_back(*componentStations[compId].begin());
-                } 
-                else {
+                int id = nodeId[node];
+                if (mp[id].count(node)) {
+                    result.push_back(node);
+                } else if (!mp[id].empty()) {
+                    result.push_back(*mp[id].begin());
+                } else {
                     result.push_back(-1);
                 }
-            } 
-            else { 
-                componentStations[compId].erase(x);
+            } else {
+                int id = nodeId[node];
+                mp[id].erase(node); // O(log c)
             }
         }
 
         return result;
     }
 };
-
-
-
-//Approach-2 (Using BFS)
-//Coming soon
 
 
 
@@ -93,89 +168,151 @@ public:
 //T.C : O((c + n) + q log c)
 //S.C : O(c + n)
 class Solution {
+    // O(c + n)
+    private void dfs(int node, Map<Integer, List<Integer>> adj, int id,
+                     int[] nodeId, Map<Integer, TreeSet<Integer>> mp,
+                     boolean[] visited) {
+        visited[node] = true;
+        mp.computeIfAbsent(id, k -> new TreeSet<>()).add(node);
+        nodeId[node] = id;
 
-    Map<Integer, TreeSet<Integer>> componentStations = new HashMap<>();
-
-    private void dfs(int node,
-                     Map<Integer, List<Integer>> adj,
-                     int[] visited,
-                     int componentId,
-                     int[] componentOf) {
-
-        visited[node] = 1;
-        componentOf[node] = componentId;
-
-        componentStations.putIfAbsent(componentId, new TreeSet<>());
-        componentStations.get(componentId).add(node);
-
-        for (int neighbor : adj.getOrDefault(node, new ArrayList<>())) {
-            if (visited[neighbor] == 0) {
-                dfs(neighbor, adj, visited, componentId, componentOf);
+        for (int ngbr : adj.getOrDefault(node, new ArrayList<>())) {
+            if (!visited[ngbr]) {
+                dfs(ngbr, adj, id, nodeId, mp, visited);
             }
         }
     }
 
     public int[] processQueries(int c, int[][] connections, int[][] queries) {
-
         Map<Integer, List<Integer>> adj = new HashMap<>();
 
-        // Build adjacency list
         for (int[] edge : connections) {
             int u = edge[0];
             int v = edge[1];
-
             adj.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
             adj.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
         }
 
-        int[] visited = new int[c + 1];
-        int[] componentOf = new int[c + 1];
+        boolean[] visited = new boolean[c + 1];
+        int[] nodeId = new int[c + 1];
+        Map<Integer, TreeSet<Integer>> mp = new HashMap<>();
 
-        // DFS to assign component ids
+        // O(c + n)
         for (int node = 1; node <= c; node++) {
-            if (visited[node] == 0) {
-                dfs(node, adj, visited, node, componentOf);
+            if (!visited[node]) {
+                int id = node;
+                dfs(node, adj, id, nodeId, mp, visited);
             }
         }
 
-        List<Integer> resultList = new ArrayList<>();
-
-        // Process queries
-        for (int[] q : queries) {
-            int type = q[0];
-            int x = q[1];
-
-            int compId = componentOf[x];
-            TreeSet<Integer> set = componentStations.get(compId);
+        List<Integer> result = new ArrayList<>();
+        // O(q * log(c))
+        for (int[] query : queries) {
+            int type = query[0];
+            int node = query[1];
+            int id = nodeId[node];
 
             if (type == 1) {
-                if (set.contains(x)) {
-                    resultList.add(x);
-                } else if (!set.isEmpty()) {
-                    resultList.add(set.first());
+                TreeSet<Integer> set = mp.get(id);
+                if (set != null && set.contains(node)) {
+                    result.add(node);
+                } else if (set != null && !set.isEmpty()) {
+                    result.add(set.first());
                 } else {
-                    resultList.add(-1);
+                    result.add(-1);
                 }
             } else {
-                set.remove(x); //offline
+                TreeSet<Integer> set = mp.get(id);
+                if (set != null) {
+                    set.remove(node); // O(log c)
+                }
             }
         }
 
-        int[] result = new int[resultList.size()];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = resultList.get(i);
-        }
-
-        return result;
+        // convert List<Integer> to int[]
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
 
 
 
-
 //Approach-2 (Using BFS)
-//Coming soon
+//T.C : O((c + n) + q log c)
+//S.C : O(c + n)
+class Solution {
+    // O(c + n)
+    private void bfs(int start, Map<Integer, List<Integer>> adj, int id,
+                     int[] nodeId, Map<Integer, TreeSet<Integer>> mp,
+                     boolean[] visited) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        visited[start] = true;
+        nodeId[start] = id;
 
+        mp.computeIfAbsent(id, k -> new TreeSet<>()).add(start);
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            for (int ngbr : adj.getOrDefault(node, new ArrayList<>())) {
+                if (!visited[ngbr]) {
+                    visited[ngbr] = true;
+                    nodeId[ngbr] = id;
+                    mp.get(id).add(ngbr);
+                    q.offer(ngbr);
+                }
+            }
+        }
+    }
+
+    public int[] processQueries(int c, int[][] connections, int[][] queries) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+
+        for (int[] edge : connections) {
+            int u = edge[0];
+            int v = edge[1];
+            adj.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
+            adj.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
+        }
+
+        boolean[] visited = new boolean[c + 1];
+        int[] nodeId = new int[c + 1];
+        Map<Integer, TreeSet<Integer>> mp = new HashMap<>();
+
+        // O(c + n)
+        for (int node = 1; node <= c; node++) {
+            if (!visited[node]) {
+                int id = node;
+                bfs(node, adj, id, nodeId, mp, visited);
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        // O(q * log(c))
+        for (int[] query : queries) {
+            int type = query[0];
+            int node = query[1];
+            int id = nodeId[node];
+
+            if (type == 1) {
+                TreeSet<Integer> set = mp.get(id);
+                if (set != null && set.contains(node)) {
+                    result.add(node);
+                } else if (set != null && !set.isEmpty()) {
+                    result.add(set.first());
+                } else {
+                    result.add(-1);
+                }
+            } else {
+                TreeSet<Integer> set = mp.get(id);
+                if (set != null) {
+                    set.remove(node); // O(log c)
+                }
+            }
+        }
+
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+}
 
 
 //Approach-3 (Using DSU)
