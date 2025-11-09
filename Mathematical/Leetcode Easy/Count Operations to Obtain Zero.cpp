b118c89575b145c8c409c38e23cb9967d0bc65a1
MAZHAR <iiestshibpur605@gmail.com>
2025-11-09 11:50:29 +0530

    Added YouTube Link

--- Mathematical/Leetcode Easy/Count Operations to Obtain Zero.cpp
+++ Mathematical/Leetcode Easy/Count Operations to Obtain Zero.cpp
@@ -1,6 +1,6 @@
 /*    Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn  : 
+    MY YOUTUBE VIDEO ON THIS Qn  : https://www.youtube.com/watch?v=1Vq8EjnWTNE
     Company Tags                 : will update later
     Leetcode Link                : https://leetcode.com/problems/count-operations-to-obtain-zero
 */
