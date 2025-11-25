092c210d7cf5de062577e96e7ec4c6f298d4173a
MAZHAR <iiestshibpur605@gmail.com>
2025-11-25 09:54:50 +0530

    Added YouTube Link

--- Mathematical/Smallest Integer Divisible by K.cpp
+++ Mathematical/Smallest Integer Divisible by K.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=yqCSa29Ugxc
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/smallest-integer-divisible-by-k/
 */
