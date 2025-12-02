65ca6de4365c42d0d3312a62b73dfbf82547ea89
MAZHAR <iiestshibpur605@gmail.com>
2025-12-02 17:33:29 +0530

    Added YouTube Link

--- Mathematical/Count Number of Trapezoids I.cpp
+++ Mathematical/Count Number of Trapezoids I.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn                : 
+    MY YOUTUBE VIDEO ON THIS Qn                : https://www.youtube.com/watch?v=YI5ALyXyv_Q
     Company Tags                               : will soon update
     Leetcode Link                              : https://leetcode.com/problems/count-number-of-trapezoids-i
     
