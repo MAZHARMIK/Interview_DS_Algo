326e585a8c915e0532b155451d1a2d51c1e5eacf
MAZHAR <iiestshibpur605@gmail.com>
2025-12-05 10:49:40 +0530

    Added YouTube Link

--- Arrays/Leetcode Easy/Count Partitions with Even Sum Difference.cpp
+++ Arrays/Leetcode Easy/Count Partitions with Even Sum Difference.cpp
@@ -1,6 +1,6 @@
 /*         Scroll down to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=6FMrZXz2kMA
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/count-partitions-with-even-sum-difference
 */
