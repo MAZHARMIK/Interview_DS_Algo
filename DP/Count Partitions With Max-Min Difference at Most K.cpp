291f1cafbf9c776a95cc17c52638cbf6fb7a9ed1
MAZHAR <iiestshibpur605@gmail.com>
2025-12-06 23:33:19 +0530

    Added YouTube Link

--- DP/Count Partitions With Max-Min Difference at Most K.cpp
+++ DP/Count Partitions With Max-Min Difference at Most K.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=0mRKXFfXHQU
     Company Tags                : Will update later
     Leetcode Link               : https://leetcode.com/problems/count-partitions-with-max-min-difference-at-most-k
 */
