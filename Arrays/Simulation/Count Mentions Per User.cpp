1a68191860fff837ae6bb215a6b4432e35efb460
MAZHAR <iiestshibpur605@gmail.com>
2025-12-12 12:10:12 +0530

    Added YouTube Link

--- Arrays/Simulation/Count Mentions Per User.cpp
+++ Arrays/Simulation/Count Mentions Per User.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-      MY YOUTUBE VIDEO ON THIS Qn : 
+      MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=3WN6qHE4K_E
       Company Tags                : will update later
       Leetcode Link               : https://leetcode.com/problems/count-mentions-per-user
 */
