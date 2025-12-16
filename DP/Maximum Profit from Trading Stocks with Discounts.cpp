24205a114ce6d319e47d3f961de55f416f5467f9
MAZHAR <iiestshibpur605@gmail.com>
2025-12-16 21:39:57 +0530

    Added YouTube Link

--- DP/Maximum Profit from Trading Stocks with Discounts.cpp
+++ DP/Maximum Profit from Trading Stocks with Discounts.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=S-kmwk0PqOU
     Company Tags                : Will update later
     Leetcode Link               : https://leetcode.com/problems/maximum-profit-from-trading-stocks-with-discounts
 */
