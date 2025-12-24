deddfd56feeaad0595393014ed331e6c23b50932
MAZHAR <iiestshibpur605@gmail.com>
2025-12-24 09:43:26 +0530

    Added YouTube Link

--- Arrays/Leetcode Easy/Apple Redistribution into Boxes.cpp
+++ Arrays/Leetcode Easy/Apple Redistribution into Boxes.cpp
@@ -1,6 +1,6 @@
 /*         Scroll down to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=e7ZHyvP_3Sk
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/apple-redistribution-into-boxes
 */
