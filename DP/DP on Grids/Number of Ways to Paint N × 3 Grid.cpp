8984037d33be6a1d2115ce1657cafc301a713f43
MAZHAR <iiestshibpur605@gmail.com>
2026-01-03 14:25:01 +0530

    Added YouTube Link

--- DP/DP on Grids/Number of Ways to Paint N × 3 Grid.cpp
+++ DP/DP on Grids/Number of Ways to Paint N × 3 Grid.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=WXdLIuRhiRg
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/number-of-ways-to-paint-n-3-grid
 */
