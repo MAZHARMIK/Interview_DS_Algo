82f6b46e1377d1e355840796d9f21ce36c043c65
MAZHAR <iiestshibpur605@gmail.com>
2026-01-16 15:46:49 +0530

    Added youtube link

--- HashSet/Maximum Square Area by Removing Fences From a Field.cpp
+++ HashSet/Maximum Square Area by Removing Fences From a Field.cpp
@@ -1,6 +1,6 @@
 /*    Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=7H_tzns_G3M
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/maximum-square-area-by-removing-fences-from-a-field
 */
