3017174b7abe3144be1ea730aa51cca60396964a
MAZHAR <iiestshibpur605@gmail.com>
2026-01-22 08:58:22 +0530

    Added youtube link

--- Arrays/Leetcode Easy/Minimum Pair Removal to Sort Array I.cpp
+++ Arrays/Leetcode Easy/Minimum Pair Removal to Sort Array I.cpp
@@ -1,6 +1,6 @@
 /*         Scroll down to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=kwrBFKA0Huo
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/minimum-pair-removal-to-sort-array-i
 */
