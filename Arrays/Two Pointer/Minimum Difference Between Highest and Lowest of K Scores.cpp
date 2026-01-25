8ecfd1aae2f74db7ac6d5e198465f65d9c4d53dc
MAZHAR <iiestshibpur605@gmail.com>
2026-01-25 07:57:11 +0530

    Added youtube link

--- Arrays/Two Pointer/Minimum Difference Between Highest and Lowest of K Scores.cpp
+++ Arrays/Two Pointer/Minimum Difference Between Highest and Lowest of K Scores.cpp
@@ -1,6 +1,6 @@
 /*			Scroll below to see JAVA code also			*/
 /*
-	    MY YOUTUBE VIDEO ON THIS Qn : 
+	    MY YOUTUBE VIDEO ON THIS Qn   : https://www.youtube.com/watch?v=k-ZtDvayDys
     	Company Tags  		          : Will update later
     	Leetcode Link 		          : https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores
 */
