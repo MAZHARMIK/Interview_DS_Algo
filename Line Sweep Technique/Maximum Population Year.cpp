e98c7f0b5f704588dbf412a51f92513f3b27b7f3
MAZHAR <iiestshibpur605@gmail.com>
2026-01-25 19:54:51 +0530

    company tags + youtube link

--- Line Sweep Technique/Maximum Population Year.cpp
+++ Line Sweep Technique/Maximum Population Year.cpp
@@ -1,7 +1,7 @@
 /*     Scroll below to see JAVA code as well    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
-    Company Tags                : Will update later
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=hpbYXxKk-Sg
+    Company Tags                : Atlassian, Goldman Sachs, Wayfair, Oracle, Agoda, TripAdvisor, Morgan Stanley, Adobe, Amazon
     Leetcode Link               : https://leetcode.com/problems/maximum-population-year
 */
 
