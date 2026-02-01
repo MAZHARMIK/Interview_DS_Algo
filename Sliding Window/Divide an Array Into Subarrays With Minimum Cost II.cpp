ed06b37e65244cb1ece816ebb2dce4d990700d28
MAZHAR <iiestshibpur605@gmail.com>
2026-02-02 01:19:24 +0530

    Added JAVA code as well

---
+++ Sliding Window/Divide an Array Into Subarrays With Minimum Cost II.cpp
@@ -0,0 +1,155 @@
+/*			Scroll below to see JAVA code also			*/
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tag                 : will update later
+    Leetcode Link               : https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-ii
+*/
+
+
+/*************************************************************** C++ *************************************************/
+//Approach (Sliding Window using ordered sets)
+//T.C : O(n * log(k)), set operations are logarithmic
+//S.C : O(k), set stores (k-1) elements
+class Solution {
+public:
+    typedef long long ll;
+    typedef pair<ll, ll> P;
+
+    ll minimumCost(vector<int>& nums, int k, int dist) {
+        int n = nums.size();
+
+        set<P> kMinimum; //stores (k-1) minimum elements
+        set<P> remaining;
+
+        ll sum = 0; //stores sum of (k-1) min elements
+
+        int i = 1;
+        while(i - dist < 1) {
+            kMinimum.insert({nums[i], i});
+            sum += nums[i];
+            if(kMinimum.size() > k-1) {
+                P temp = *kMinimum.rbegin();
+                sum -= temp.first;
+                remaining.insert(temp);
+                kMinimum.erase(temp);
+            }
+            i++;
+        }
+
+        ll result = LONG_MAX; //stores min sum of (k-1) elements
+        //0, 1, ...... i
+        while(i < n) {
+            kMinimum.insert({nums[i], i});
+            sum += nums[i];
+
+            if(kMinimum.size() > k-1) {
+                P temp = *kMinimum.rbegin();
+                sum -= temp.first;
+                remaining.insert(temp);
+                kMinimum.erase(temp);
+            }
+
+            result = min(result, sum);
+
+            //shift window
+            //i-dist wala element will be removed
+            P remove = {nums[i-dist], i-dist};
+            if(kMinimum.count(remove)) {
+                kMinimum.erase(remove);
+                sum -= remove.first;
+
+                if(!remaining.empty()) {
+                    P temp = *remaining.begin();
+                    kMinimum.insert(temp);
+                    sum += temp.first;
+                    remaining.erase(temp);
+                }
+            } else {
+                remaining.erase(remove);
+            }
+
+            i++;
+        }
+
+        return nums[0] + result;
+    }
+};
+
+
+
+/*************************************************************** JAVA *************************************************/
+//Approach (Sliding Window using ordered sets)
+//T.C : O(n * log(k)), set operations are logarithmic
+//S.C : O(k), set stores (k-1) elements
+
+class Solution {
+    public long minimumCost(int[] nums, int k, int dist) {
+        int n = nums.length;
+
+        // Sort by value first, then index
+        TreeSet<int[]> kMinimum = new TreeSet<>((a, b) -> {
+            if (a[0] != b[0]) return Integer.compare(a[0], b[0]);
+            return Integer.compare(a[1], b[1]);
+        });
+
+        TreeSet<int[]> remaining = new TreeSet<>((a, b) -> {
+            if (a[0] != b[0]) return Integer.compare(a[0], b[0]);
+            return Integer.compare(a[1], b[1]);
+        });
+
+        long sum = 0;   // sum of (k-1) smallest in window
+
+        int i = 1;
+
+        // Build first window [1 ... dist+1]
+        while (i < n && i - dist < 1) {
+            int[] cur = new int[]{nums[i], i};
+            kMinimum.add(cur);
+            sum += nums[i];
+
+            if (kMinimum.size() > k - 1) {
+                int[] largest = kMinimum.pollLast();
+                sum -= largest[0];
+                remaining.add(largest);
+            }
+            i++;
+        }
+
+        long result = Long.MAX_VALUE;
+
+        // Sliding window
+        while (i < n) {
+            int[] cur = new int[]{nums[i], i};
+            kMinimum.add(cur);
+            sum += nums[i];
+
+            if (kMinimum.size() > k - 1) {
+                int[] largest = kMinimum.pollLast();
+                sum -= largest[0];
+                remaining.add(largest);
+            }
+
+            result = Math.min(result, sum);
+
+            // Remove expired index (i - dist)
+            int remIdx = i - dist;
+            int[] toRemove = new int[]{nums[remIdx], remIdx};
+
+            if (kMinimum.remove(toRemove)) {
+                sum -= nums[remIdx];
+
+                if (!remaining.isEmpty()) {
+                    int[] promote = remaining.pollFirst();
+                    kMinimum.add(promote);
+                    sum += promote[0];
+                }
+            } else {
+                remaining.remove(toRemove);
+            }
+
+            i++;
+        }
+
+        return nums[0] + result;
+    }
+}
