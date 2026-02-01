2627cc4ec7e8abbc40d075db0e1b14cfd94b5480
MAZHAR <iiestshibpur605@gmail.com>
2026-02-01 08:11:56 +0530

    Added YouTube Link

--- Arrays/Leetcode Easy/Divide an Array Into Subarrays With Minimum Cost I.cpp
+++ Arrays/Leetcode Easy/Divide an Array Into Subarrays With Minimum Cost I.cpp
@@ -1,6 +1,6 @@
 /*         Scroll down to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=zuBKtLVXAgY
     Company Tags                : American Express
     Leetcode Link               : https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-i
 */
