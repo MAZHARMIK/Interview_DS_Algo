16888c77c27a58e91ff8595775b0a1bff5ca6305
MAZHAR <iiestshibpur605@gmail.com>
2026-02-02 05:47:08 +0530

    Added YouTube Link

--- Sliding Window/Divide an Array Into Subarrays With Minimum Cost II.cpp
+++ Sliding Window/Divide an Array Into Subarrays With Minimum Cost II.cpp
@@ -1,6 +1,6 @@
 /*			Scroll below to see JAVA code also			*/
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=hnj5JrPVqZk
     Company Tag                 : will update later
     Leetcode Link               : https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-ii
 */
