7ae1d7c6f101315d6a690ec8a8c0269e89870fd2
MAZHAR <iiestshibpur605@gmail.com>
2026-02-06 01:04:01 +0530

    Added JAVA code as well

---
+++ Bit_Magic/Leetcode Easy/Binary Watch.cpp
@@ -0,0 +1,59 @@
+/*   Scroll below to see JAVA code also  */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : GOOGLE
+    Leetcode Link               : https://leetcode.com/problems/binary-watch/submissions/1909541136/
+*/
+
+
+/******************************************************* C++ *******************************************************/
+//Approach (Simple enumeration)
+//T.C : O(1)
+//S.C : O(1)
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> result;
+
+        for (int HH = 0; HH <= 11; HH++) {
+
+            for (int MM = 0; MM <= 59; MM++) {
+                
+                if (__builtin_popcount(HH) + __builtin_popcount(MM) == turnedOn) {
+                    string hour   = to_string(HH);
+                    string minute = (MM < 10 ? "0" : "") + to_string(MM);
+                    
+                    result.push_back(hour + ":" + minute);
+                }
+            }
+        }
+        return result;
+    }
+};
+
+
+
+
+/******************************************************* JAVA *******************************************************/
+//Approach (Simple enumeration)
+//T.C : O(1)
+//S.C : O(1)
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> result = new ArrayList<>();
+
+        for (int hh = 0; hh <= 11; hh++) {
+            for (int mm = 0; mm <= 59; mm++) {
+
+                if (Integer.bitCount(hh) + Integer.bitCount(mm) == turnedOn) {
+                    String hour = String.valueOf(hh);
+                    String minute = (mm < 10 ? "0" : "") + mm;
+                    result.add(hour + ":" + minute);
+                }
+
+            }
+        }
+
+        return result;
+    }
+}
