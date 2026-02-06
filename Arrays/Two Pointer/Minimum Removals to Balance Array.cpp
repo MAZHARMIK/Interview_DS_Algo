9ac48823e484669ff5ac579d013095ae64b07e59
MAZHAR <iiestshibpur605@gmail.com>
2026-02-06 07:46:00 +0530

    Added YouTube Link

--- Arrays/Two Pointer/Minimum Removals to Balance Array.cpp
+++ Arrays/Two Pointer/Minimum Removals to Balance Array.cpp
@@ -1,6 +1,6 @@
 /*			Scroll below to see JAVA code also			*/
 /*
-	    MY YOUTUBE VIDEO ON THIS Qn : 
+	    MY YOUTUBE VIDEO ON THIS Qn   : https://www.youtube.com/watch?v=d4qAUbuEqvw 
     	Company Tags  		          : Will update later
     	Leetcode Link 		          : https://leetcode.com/problems/minimum-removals-to-balance-array
 */
