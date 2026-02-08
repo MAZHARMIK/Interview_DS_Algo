bbae7c4710aad73ab63ab45dcd7d64cc43923845
MAZHAR <iiestshibpur605@gmail.com>
2026-02-08 07:34:32 +0530

    Added company tags

--- Tree/Easy/Balanced Binary Tree.cpp
+++ Tree/Easy/Balanced Binary Tree.cpp
@@ -1,7 +1,7 @@
 /*     Scroll below to see JAVA code also    */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : 
-    Company Tags                : will update soon
+    Company Tags                : Adobe, Apple, Capgemini, Meta, CBSL Group (India)
     Leetcode Link               : https://leetcode.com/problems/balanced-binary-tree
 */
 
