f09eb94ebafef2b7042040c9021e4b8dc196e304
MAZHAR <iiestshibpur605@gmail.com>
2026-02-13 08:17:27 +0530

    Added YouTube Link

--- HashMap/Longest Balanced Substring II.cpp
+++ HashMap/Longest Balanced Substring II.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=eBhYqD4G4rw
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/longest-balanced-substring-ii/
 */
