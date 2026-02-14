adc6ef1d13fa7ea1120d40bf124fa7a95706c125
MAZHAR <iiestshibpur605@gmail.com>
2026-02-14 22:48:48 +0530

    Added JAVA code as well

---
+++ Line Sweep Technique/Corporate Flight Bookings.cpp
@@ -0,0 +1,71 @@
+/*     Scroll below to see JAVA code as well    */
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : 
+    Company Tags                : will update later
+    Leetcode Link               : https://leetcode.com/problems/corporate-flight-bookings
+*/
+
+
+/******************************************************************************* C++ ************************************************************************************************/
+//Approach (Using Line Sweep/Difference Array Technique)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> diff(n+2, 0);
+
+        for(auto &b : bookings) {
+            int start = b[0];
+            int end = b[1];
+            int count = b[2];
+
+            diff[start] += count;
+            diff[end+1] -= count;
+        }
+
+        //Cumulative sum
+        vector<int> result;
+        int cumSum = 0;
+        for(int i = 1; i <= n; i++) {
+            cumSum += diff[i];
+            result.push_back(cumSum);
+        }
+
+        return result;
+    }
+};
+
+
+
+
+/******************************************************************************* JAVA ************************************************************************************************/
+//Approach (Using Line Sweep/Difference Array Technique)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        
+        int[] diff = new int[n + 2];
+        
+        for (int[] b : bookings) {
+            int start = b[0];
+            int end = b[1];
+            int count = b[2];
+            
+            diff[start] += count;
+            diff[end + 1] -= count;
+        }
+        
+        // Prefix sum to build result
+        int[] result = new int[n];
+        int cumSum = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            cumSum += diff[i];
+            result[i - 1] = cumSum;  // shift because result is 0-based
+        }
+        
+        return result;
+    }
+}
