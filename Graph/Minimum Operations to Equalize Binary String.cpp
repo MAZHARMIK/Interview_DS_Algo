5a93092258067e828c3cbdf3a0b230041456752a
MAZHAR <iiestshibpur605@gmail.com>
2026-02-27 20:12:10 +0530

    updated

--- Graph/Minimum Operations to Equalize Binary String.cpp
+++ Graph/Minimum Operations to Equalize Binary String.cpp
@@ -28,6 +28,58 @@ public:
         vector<int> operations(n+1, -1);
         //operations[z] = min operations required to reach state in which we have z 0s
 
+        queue<int> que;
+        que.push(startZeros);
+        operations[startZeros] = 0;
+
+        while(!que.empty()) {
+            int z = que.front();
+            que.pop();
+
+            int minF = max(0, k-n+z);
+            int maxF = min(k, z);
+
+            for(int f = minF; f <= maxF; f++) {
+                int newZ = z + k - 2*f;
+
+                if(operations[newZ] == -1) {
+                    operations[newZ] = operations[z] + 1;
+
+                    if(newZ == 0) {
+                        return operations[newZ];
+                    }
+
+                    que.push(newZ);
+                }
+            }
+        }
+
+        return -1;
+    }
+};
+
+
+//Approach-2 - Optimal - Skipping already visited states
+//T.C : O(n*logn)
+//S.C : O(n)
+class Solution {
+public:
+    int minOperations(string s, int k) {
+        int n = s.length();
+
+        int startZeros = 0;
+        for(char &ch : s) {
+            if(ch == '0')
+                startZeros++;
+        }
+
+        if(startZeros == 0) {
+            return 0; //no operations required
+        }
+
+        vector<int> operations(n+1, -1);
+        //operations[z] = min operations required to reach state in which we have z 0s
+
         set<int> evenSet;
         set<int> oddSet;
 
@@ -79,59 +131,6 @@ public:
         return -1;
     }
 };
-
-
-//Approach-2 - Optimal - Skipping already visited states
-//T.C : O(n*logn)
-//S.C : O(n)
-class Solution {
-public:
-    int minOperations(string s, int k) {
-        int n = s.length();
-
-        int startZeros = 0;
-        for(char &ch : s) {
-            if(ch == '0')
-                startZeros++;
-        }
-
-        if(startZeros == 0) {
-            return 0; //no operations required
-        }
-
-        vector<int> operations(n+1, -1);
-        //operations[z] = min operations required to reach state in which we have z 0s
-
-        queue<int> que;
-        que.push(startZeros);
-        operations[startZeros] = 0;
-
-        while(!que.empty()) {
-            int z = que.front();
-            que.pop();
-
-            int minF = max(0, k-n+z);
-            int maxF = min(k, z);
-
-            for(int f = minF; f <= maxF; f++) {
-                int newZ = z + k - 2*f;
-
-                if(operations[newZ] == -1) {
-                    operations[newZ] = operations[z] + 1;
-
-                    if(newZ == 0) {
-                        return operations[newZ];
-                    }
-
-                    que.push(newZ);
-                }
-            }
-        }
-
-        return -1;
-    }
-};
-
 
 
 /************************************************************ JAVA ************************************************/
