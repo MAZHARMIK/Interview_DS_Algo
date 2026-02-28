713c07dff4452a6ba32d0b178bb669ac4e711dd0
MAZHAR <iiestshibpur605@gmail.com>
2026-02-28 07:01:11 +0530

    Added YouTube Link

--- Bit_Magic/Concatenation of Consecutive Binary Numbers.cpp
+++ Bit_Magic/Concatenation of Consecutive Binary Numbers.cpp
@@ -1,6 +1,6 @@
 /*   Scroll below to see JAVA code also  */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=JwiTzOEzy44
     Company Tags                : Will udpate later
     Leetcode Link               : https://leetcode.com/problems/concatenation-of-consecutive-binary-numbers/
 */
