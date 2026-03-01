4bea4c774cda6451282d5e2e45ff18aae23424f8
MAZHAR <iiestshibpur605@gmail.com>
2026-03-01 11:26:13 +0530

    Added JAVA code as well

--- Greedy/Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
+++ Greedy/Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
@@ -42,8 +42,8 @@ public:
 //S.C : O(1)
 class Solution {
 public:
-    int minPartitions(string str) {
-        return *max_element(begin(str), end(str))-'0';
+    int minPartitions(string n) {
+        return *max_element(begin(n), end(n))-'0';
     }
 };
 
@@ -54,11 +54,45 @@ public:
 //Method-1 (This method explains why Method-2 trick worked)
 //T.C : O(n*D), where D is the max digit which can be <= 9 ~= O(n)
 //S.C : O(1)
+class Solution {
+    public int minPartitions(String n) {
 
+        char[] arr = n.toCharArray();
+        int count = 0;
 
+        while (true) {
+            boolean changed = false;
 
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] != '0') {
+                    arr[i]--;      // subtract 1
+                    changed = true;
+                }
+            }
+
+            if (!changed)
+                break;
+
+            count++;
+        }
+
+        return count;
+    }
+}
 
 
 //Method-2 (The largest character will take time to reach 0)
 //T.C : o(length)
 //S.C : O(1)
+class Solution {
+    public int minPartitions(String n) {
+
+        char maxCh = '0';
+
+        for (char ch : n.toCharArray()) {
+            maxCh = (char)Math.max(maxCh, ch);
+        }
+
+        return maxCh - '0';
+    }
+}
