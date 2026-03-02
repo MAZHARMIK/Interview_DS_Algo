90ee711db028d058802a71af837f8863b31a240e
MAZHAR <iiestshibpur605@gmail.com>
2026-03-02 19:37:40 +0530

    Added YouTube Link

--- Arrays/2-D Array/Minimum Swaps to Arrange a Binary Grid.cpp
+++ Arrays/2-D Array/Minimum Swaps to Arrange a Binary Grid.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also   */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=FxOl81Rwdrk
     Company Tags                : Ajira Software
     Leetcode Link               : https://leetcode.com/problems/minimum-swaps-to-arrange-a-binary-grid
 */
