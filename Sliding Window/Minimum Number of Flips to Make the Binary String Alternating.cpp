f0e0d6a708601091fc924f2b13bbeeebcf6f79b0
MAZHAR <iiestshibpur605@gmail.com>
2026-03-05 22:46:32 +0530

    Added YouTube Link

--- Sliding Window/Minimum Number of Flips to Make the Binary String Alternating.cpp
+++ Sliding Window/Minimum Number of Flips to Make the Binary String Alternating.cpp
@@ -1,6 +1,6 @@
 /*			Scroll below to see JAVA code also			*/
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=T3lhIT7hbl4
     Company Tag                 : will update later
     Leetcode Link               : https://leetcode.com/problems/minimum-number-of-flips-to-make-the-binary-string-alternating
 */
