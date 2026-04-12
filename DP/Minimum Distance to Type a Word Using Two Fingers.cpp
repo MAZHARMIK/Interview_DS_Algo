efc23e6f3e87731d306e0bbda7a29dbcbad39380
MAZHAR <iiestshibpur605@gmail.com>
2026-04-12 17:02:55 +0530

    Added YouTube Link

--- DP/Minimum Distance to Type a Word Using Two Fingers.cpp
+++ DP/Minimum Distance to Type a Word Using Two Fingers.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=26jlH4R9cf8
     Company Tags                : Will update later
     Leetcode Link               : https://leetcode.com/problems/minimum-distance-to-type-a-word-using-two-fingers
 */
