44dfcbcdcbcfc588a0acddcc4a5db27cf853ad23
MAZHAR <iiestshibpur605@gmail.com>
2026-04-12 15:48:30 +0530

    Added JAVA code as well

--- DP/Minimum Distance to Type a Word Using Two Fingers.cpp
+++ DP/Minimum Distance to Type a Word Using Two Fingers.cpp
@@ -7,7 +7,7 @@
 
 
 /**************************************************************** C++ ****************************************************************/
-//Approach (Recursion + Memo with 5-D Memoization DP)
+//Approach-1 (Recursion + Memo with 5-D Memoization DP)
 //T.C : O(n*27*27) ~= O(n)
 //S.C : O(n*27*27) ~= O(n)
 class Solution {
@@ -69,7 +69,7 @@ public:
 
 
 
-//Approach (Recursion + Memo with 3-D Memoization DP)
+//Approach-2 (Recursion + Memo with 3-D Memoization DP)
 //T.C : O(n*27*27) ~= O(n)
 //S.C : O(n*27*27) ~= O(n)
 class Solution {
@@ -121,6 +121,71 @@ public:
         //finger2 = value of character
         memset(dp, -1, sizeof(dp));
         return solve(word, 0, 26, 26);
+    }
+};
+
+
+
+//Approach-3 (Bottom Up)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int dp[301][27][27];
+
+    pair<int, int> getCoord(int pos) {
+        return {pos/6, pos%6};
+    }
+
+    int getDistance(int pos1, int pos2) {
+        if (pos1 == 26 || pos2 == 26) return 0;
+
+        auto [x1, y1] = getCoord(pos1);
+        auto [x2, y2] = getCoord(pos2);
+        return abs(x1 - x2) + abs(y1 - y2);
+    }
+
+    int minimumDistance(string word) {
+        int n = word.length();
+
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(27, vector<int>(27, 0)));
+
+        //3D DP needs 3 for loops to fill the dp array
+        //Why i = n-1 to 0 because we need to find current answer using future values
+        //For example : To find, dp[i][f1][f2] = we need to know dp[i+1][curr][f2];
+        //So, better to solve from n-1 to 0
+
+        for (int i = n - 1; i >= 0; i--) {
+            int curr = word[i] - 'A';
+
+            for (int f1 = 0; f1 < 27; f1++) {
+                for (int f2 = 0; f2 < 27; f2++) {
+
+                    //None of them are used
+                    if (f1 == 26 && f2 == 26) {
+                        dp[i][f1][f2] = dp[i+1][curr][f2]; //solve(i+1, curr, f2);
+                        continue;
+                    }
+
+                    //F2 not used
+                    if (f2 == 26) {
+                        int moveF2 = dp[i+1][f1][curr]; //solve(i+1, f1, curr);
+                        int moveF1 = getDistance(f1, curr) + dp[i+1][curr][f2]; //solve(i+1, curr, f2);
+
+                        dp[i][f1][f2] = min(moveF1, moveF2);
+                        continue;
+                    }
+
+                    //F1 and F2 both used
+                    int moveF1 = getDistance(f1, curr) + dp[i+1][curr][f2]; //solve(i+1, curr, f2);
+                    int moveF2 = getDistance(f2, curr) + dp[i+1][f1][curr]; //solve(i+1, f1, curr);
+
+                    dp[i][f1][f2] = min(moveF1, moveF2);
+                }
+            }
+        }
+
+        return dp[0][26][26]; //solve(0, 26, 26);
     }
 };
 
@@ -252,5 +317,71 @@ class Solution {
                 Arrays.fill(dp[i][j], -1);
 
         return solve(word, 0, 26, 26);
+    }
+}
+
+
+//Approach-3 (Bottom Up)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+
+    public int[] getCoord(int pos) {
+        return new int[]{pos/6, pos%6};
+    }
+
+    public int getDistance(int pos1, int pos2) {
+        if (pos1 == 26 || pos2 == 26) return 0;
+
+        int[] c1 = getCoord(pos1);
+        int[] c2 = getCoord(pos2);
+
+        int x1 = c1[0], y1 = c1[1];
+        int x2 = c2[0], y2 = c2[1];
+
+        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+    }
+
+    public int minimumDistance(String word) {
+        int n = word.length();
+
+        int[][][] dp = new int[n + 1][27][27];
+
+        //3D DP needs 3 for loops to fill the dp array
+        //Why i = n-1 to 0 because we need to find current answer using future values
+        //For example : To find, dp[i][f1][f2] = we need to know dp[i+1][curr][f2];
+        //So, better to solve from n-1 to 0
+
+        for (int i = n - 1; i >= 0; i--) {
+            int curr = word.charAt(i) - 'A';
+
+            for (int f1 = 0; f1 < 27; f1++) {
+                for (int f2 = 0; f2 < 27; f2++) {
+
+                    //None of them are used
+                    if (f1 == 26 && f2 == 26) {
+                        dp[i][f1][f2] = dp[i+1][curr][f2]; //solve(i+1, curr, f2);
+                        continue;
+                    }
+
+                    //F2 not used
+                    if (f2 == 26) {
+                        int moveF2 = dp[i+1][f1][curr]; //solve(i+1, f1, curr);
+                        int moveF1 = getDistance(f1, curr) + dp[i+1][curr][f2]; //solve(i+1, curr, f2);
+
+                        dp[i][f1][f2] = Math.min(moveF1, moveF2);
+                        continue;
+                    }
+
+                    //F1 and F2 both used
+                    int moveF1 = getDistance(f1, curr) + dp[i+1][curr][f2]; //solve(i+1, curr, f2);
+                    int moveF2 = getDistance(f2, curr) + dp[i+1][f1][curr]; //solve(i+1, f1, curr);
+
+                    dp[i][f1][f2] = Math.min(moveF1, moveF2);
+                }
+            }
+        }
+
+        return dp[0][26][26]; //solve(0, 26, 26);
     }
 }
