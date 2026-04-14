01cd0ec9a43423d499bb20df4532ea7bdecb221f
Mayank sangwani <87940332+guru340@users.noreply.github.com>
2026-04-14 18:00:29 +0530

    Add Java solution for network connectivity problem

--- Graph/Disjoint Set/Number of Operations to Make Network Connected.cpp
+++ Graph/Disjoint Set/Number of Operations to Make Network Connected.cpp
@@ -56,3 +56,51 @@ public:
         return components-1;
     }
 };
+
+
+// Java Solution 
+class Solution {
+    int []parent;
+    int []rank;
+
+    int find(int x){
+        if(parent[x]==x){
+            return x;
+        }
+        return parent[x]=find(parent[x]);
+    }
+    void union(int x,int y){
+        int x_parent=find(x);
+        int y_parent=find(y);
+        if(rank[x_parent]>rank[y_parent]){
+            parent[y_parent]=x_parent;
+        }else if(rank[x_parent]<rank[y_parent]){
+            parent[x_parent]=y_parent;
+        }
+        else{
+            parent[x_parent]=y_parent;
+            rank[y_parent]++;
+        }
+
+    }
+    public int makeConnected(int n, int[][] connections) {
+        if(connections.length<n-1){
+            return -1;
+        }
+        parent=new int[n];
+        rank=new int[n];
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            
+        }   
+
+        int components=n;
+        for(int []vec:connections){
+            if(find(vec[0])!=find(vec[1])){
+                components--;
+                union(vec[0],vec[1]);
+            }
+        }
+        return components-1;
+    }
+}
