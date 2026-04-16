72c5fb4b6f01fc1302a5842fd0ae6c76e9c3e1f8
MAZHAR <iiestshibpur605@gmail.com>
2026-04-16 21:29:52 +0530

    Added YouTube Link

--- Arrays/Two Pointer/Leetcode Easy/Two Furthest Houses With Different Colors.cpp
+++ Arrays/Two Pointer/Leetcode Easy/Two Furthest Houses With Different Colors.cpp
@@ -1,6 +1,6 @@
 /*			Scroll below to see JAVA code also			*/
 /*
-	    MY YOUTUBE VIDEO ON THIS Qn : 
+	    MY YOUTUBE VIDEO ON THIS Qn   : https://www.youtube.com/watch?v=TGaKkfMe_00
     	Company Tags  		          : Will update later
     	Leetcode Link 		          : https://leetcode.com/problems/two-furthest-houses-with-different-colors
 */
