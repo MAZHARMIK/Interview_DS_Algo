01225787e2ea57f33c73985f65d3d7b3792f1e7d
MAZHAR <iiestshibpur605@gmail.com>
2026-04-26 10:59:14 +0530

    cleaned

--- Graph/BFS_DFS/Detect Cycles in 2D Grid.cpp
+++ Graph/BFS_DFS/Detect Cycles in 2D Grid.cpp
@@ -14,49 +14,46 @@
 class Solution {
 public:
     int m, n;
-    vector<pair<int,int>> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-    
+    vector<vector<int>> directions = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+
     bool cycleDetectDFS(int r, int c, int prev_r, int prev_c, 
-                        vector<vector<char>>& grid, vector<vector<bool>>& visited) {
-        
-        if (visited[r][c]) {
+                    vector<vector<char>>& grid, vector<vector<bool>>& visited ) {
+
+        if(visited[r][c]) {
             return true;
         }
 
         visited[r][c] = true;
 
-        for (auto &d : directions) {
-            int new_r = r + d.first;
-            int new_c = c + d.second;
+        //explore neighbours
+        for(auto &dir : directions) {
+            int new_r = r + dir[0]; 
+            int new_c = c + dir[1];
 
-            if (new_r >= 0 && new_r < m && new_c >= 0 && new_c < n 
+            if(new_r >= 0 && new_r < m && new_c >= 0 && new_c < n
                 && grid[new_r][new_c] == grid[r][c]) {
-                
-                if (new_r == prev_r && new_c == prev_c) {
-                    continue;
-                } else {
-                    if (cycleDetectDFS(new_r, new_c, r, c, grid, visited)) {
+                    if(new_r == prev_r && new_c == prev_c)
+                        continue;
+                    
+                    if(cycleDetectDFS(new_r, new_c, r, c, grid, visited)) {
                         return true;
                     }
                 }
-            }
         }
 
         return false;
-    }
 
+    }
     bool containsCycle(vector<vector<char>>& grid) {
         m = grid.size();
         n = grid[0].size();
 
         vector<vector<bool>> visited(m, vector<bool>(n, false));
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j]) {
-                    if (cycleDetectDFS(i, j, i, j, grid, visited)) {
-                        return true;
-                    }
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(!visited[i][j] && cycleDetectDFS(i, j, i, j, grid, visited)) {
+                    return true;
                 }
             }
         }
@@ -73,36 +70,38 @@ public:
 class Solution {
 public:
     int m, n;
-    vector<pair<int,int>> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-    
+    vector<vector<int>> directions = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+
     bool cycleDetectBFS(int r, int c, vector<vector<char>>& grid, vector<vector<bool>>& visited) {
-        queue<tuple<int,int,int,int>> q; // {r, c, prev_r, prev_c}
-        
-        q.push({r, c, -1, -1});
+        //r, c, prev_r, prev_c
+        queue<tuple<int, int, int, int>> que;
+
+        que.push({r, c, -1, -1});
         visited[r][c] = true;
 
-        while (!q.empty()) {
-            auto [curr_r, curr_c, prev_r, prev_c] = q.front();
-            q.pop();
+        while(!que.empty()) {
+            auto [curr_r, curr_c, prev_r, prev_c] = que.front();
+            que.pop();
 
-            for (auto &d : directions) {
-                int new_r = curr_r + d.first;
-                int new_c = curr_c + d.second;
+            //explore neighbours
+            for(auto &dir : directions) {
+                int new_r = curr_r + dir[0];
+                int new_c = curr_c + dir[1];
 
-                if (new_r >= 0 && new_r < m && new_c >= 0 && new_c < n 
-                    && grid[new_r][new_c] == grid[curr_r][curr_c]) {
-
-                    if (new_r == prev_r && new_c == prev_c) {
+                if(new_r >= 0 && new_r < m && new_c >= 0 && new_c < n
+                && grid[new_r][new_c] == grid[curr_r][curr_c]) {
+                    
+                    if(new_r == prev_r && new_c == prev_c)
                         continue;
-                    }
-
-                    if (visited[new_r][new_c]) {
-                        return true; // cycle found
-                    }
-
+                    
+                    if(visited[new_r][new_c])
+                        return true;
+                    
                     visited[new_r][new_c] = true;
-                    q.push({new_r, new_c, curr_r, curr_c});
+
+                    que.push({new_r, new_c, curr_r, curr_c});
                 }
+
             }
         }
 
@@ -115,12 +114,10 @@ public:
 
         vector<vector<bool>> visited(m, vector<bool>(n, false));
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j]) {
-                    if (cycleDetectBFS(i, j, grid, visited)) {
-                        return true;
-                    }
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(!visited[i][j] && cycleDetectBFS(i, j, grid, visited)) {
+                    return true;
                 }
             }
         }
