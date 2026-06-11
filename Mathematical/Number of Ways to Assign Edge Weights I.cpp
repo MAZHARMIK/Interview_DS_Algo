698db97113ef55750b7e04c4308643c1655e2186
MAZHAR <iiestshibpur605@gmail.com>
2026-06-11 13:59:06 +0530

    Added YouTube Link

--- Mathematical/Number of Ways to Assign Edge Weights I.cpp
+++ Mathematical/Number of Ways to Assign Edge Weights I.cpp
@@ -1,6 +1,6 @@
 /*         Scroll down to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=VQ2I1vn21iA
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/number-of-ways-to-assign-edge-weights-i
 */
