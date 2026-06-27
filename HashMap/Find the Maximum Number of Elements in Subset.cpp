2a0f9cf516133002d6c559e34d5e785b99d894e4
MAZHAR <iiestshibpur605@gmail.com>
2026-06-27 10:39:50 +0530

    Update Find the Maximum Number of Elements in Subset.cpp

--- HashMap/Find the Maximum Number of Elements in Subset.cpp
+++ HashMap/Find the Maximum Number of Elements in Subset.cpp
@@ -1,3 +1,4 @@
+/*         Scroll down to see JAVA code also        */
 /*
     MY YOUTUBE VIDEO ON THIS Qn : 
     Company Tags                : will update soon
@@ -5,59 +6,80 @@
 */
 
 /*************************************************************** C++ ***************************************************************/
-//T.C : O(n*logn(n))
+//T.C : O(n * log log M), M = maximum number in the nums
 //S.C : O(n)
 class Solution {
 public:
-    int maximumLength(vector<int>& nums) {
-        long long result = 0;
-        
-        map<int, int> mp;
-        //Why ordered map ?
-        //We want to start from smallest to largest
-        //Example : [14,14,196,196,38416,38416]
-        
-        for(auto &num: nums)
-            mp[num]++;
-        
-        for(auto &it : mp) {
+    typedef long long ll;
 
-            long long val  = it.first;
-            long long freq = it.second;
-            
-            long long length = 0;
-            //Handling 1 separately because its square will be 1 only
-            //example : [1,1,1,1,1,1,1,1,1,1,2,4,8,16,32,64,128,256,512,1024]
-            if(val == 1) { 
-                length += freq;
-                mp[val] = 0; 
-                if(length % 2 == 0)
-                    length--;
-                result = max(result, length);
-                continue;
-            } 
-            
-            while(val <= 1e9 && mp[val] > 0){
-                length += 2;
-                
-                if(mp[val] == 1) //We need atleast 2
-                    break; 
-                
-                mp[val] = 0;    // set to zero, so that we do not check for this sequence again
-                
-                val = val * val;
-            }
-            
-            //Series length will be odd example : {2, 4, 2}, {2, 4, 16, 4, 2}
-            length--;
-            
-            result = max(result, length);
+    int maximumLength(vector<int>& nums) {
+        unordered_map<ll, int> mp;
+
+        for(int &num : nums) {
+            mp[num]++;
         }
+
+        int result = 0;
+
+        if(mp[1]%2) { //Odd
+            result = mp[1];
+        } else { //even
+            result = mp[1]-1;
+        }
+
+        for(auto &[num, _] : mp) {
+            if(num == 1) continue;
+
+            ll curr = num;
+            int len = 0;
+
+            while(mp.count(curr) && mp[curr] > 1) {
+                len += 2;
+                curr = curr*curr;
+            }
+
+            if(mp.count(curr) == 1) {
+                len += 1;
+            } else {
+                len -= 1;
+            }
+
+            result = max(result, len);
+        }
+
         return result;
     }
 };
 
 /*************************************************************** JAVA ***************************************************************/
-//T.C : O(n*logn(n))
+//T.C : O(n * log log M), M = maximum number in the nums
 //S.C : O(n)
-//Will soon update
+class Solution {
+    public int maximumLength(int[] nums) {
+        Map<Long, Integer> cnt = new HashMap<>();
+        for (int num : nums) {
+            cnt.merge((long) num, 1, Integer::sum);
+        }
+
+        int result = 0;
+       
+        int ones = cnt.getOrDefault(1L, 0);
+        result = (ones % 2 == 1) ? ones : ones - 1;
+
+        for (long num : cnt.keySet()) {
+            if (num == 1) continue;
+
+            long curr = num;
+            int len = 0;
+            while (cnt.getOrDefault(curr, 0) > 1) {
+                len += 2;
+                curr = curr * curr;
+            }
+            len += cnt.containsKey(curr) ? 1 : -1;
+
+            result = Math.max(result, len);
+        }
+
+        return result;
+    }
+}
