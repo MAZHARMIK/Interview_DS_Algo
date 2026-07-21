32847a7a43e2b011c4029e8f9d1eaff4fbf9426d
MAZHAR <iiestshibpur605@gmail.com>
2026-07-21 13:22:11 +0530

    Added YouTube Link

--- strings/Maximize Active Section with Trade I.cpp
+++ strings/Maximize Active Section with Trade I.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=JP-N2krnCTc
     Company Tags                : will update soon
     Leetcode Link               : https://leetcode.com/problems/maximize-active-section-with-trade-i
 */
