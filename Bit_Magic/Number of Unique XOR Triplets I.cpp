5a3586fbcc639ca81f4aebb8359055c4ea6a5602
MAZHAR <iiestshibpur605@gmail.com>
2026-07-23 14:06:44 +0530

    Added YouTube Link

--- Bit_Magic/Number of Unique XOR Triplets I.cpp
+++ Bit_Magic/Number of Unique XOR Triplets I.cpp
@@ -1,6 +1,6 @@
 /*   Scroll below to see JAVA code also  */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=FbD3Tfwhi0Q
     Company Tags                : Will udpate later
     Leetcode Link               : https://leetcode.com/problems/number-of-unique-xor-triplets-i
 */
