8df68931dbefd44511cd4971c1600f6404f39b52
MAZHAR <iiestshibpur605@gmail.com>
2026-08-15 08:17:30 +0530

    Added YouTube Link

--- Bit_Magic/Longest Subsequence With Non-Zero Bitwise XOR.cpp
+++ Bit_Magic/Longest Subsequence With Non-Zero Bitwise XOR.cpp
@@ -1,6 +1,6 @@
 /*   Scroll below to see JAVA code also  */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=t0kb8GtOsWs
     Company Tags                : Will udpate later
     Leetcode Link               : https://leetcode.com/problems/longest-subsequence-with-non-zero-bitwise-xor
 */
