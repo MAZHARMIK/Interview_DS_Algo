236659ac627d9aa40c2d00cb3c5751f6a5f21c90
MAZHAR <iiestshibpur605@gmail.com>
2026-08-21 19:22:36 +0530

    Added YouTube Link

--- Mathematical/Kth Smallest Amount With Single Denomination Combination.cpp
+++ Mathematical/Kth Smallest Amount With Single Denomination Combination.cpp
@@ -1,6 +1,6 @@
 /*         Scroll down to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=QRlIMH6GmtU
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/kth-smallest-amount-with-single-denomination-combination
 */
