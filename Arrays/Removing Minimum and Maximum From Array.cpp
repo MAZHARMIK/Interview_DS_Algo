5b0bf414b3c6fb50bb2c7e92bdaddc70239bb97d
MAZHAR <iiestshibpur605@gmail.com>
2026-08-30 08:34:35 +0530

    Update Removing Minimum and Maximum From Array.cpp

--- Arrays/Removing Minimum and Maximum From Array.cpp
+++ Arrays/Removing Minimum and Maximum From Array.cpp
@@ -1,6 +1,6 @@
 /*         Scroll down to see JAVA code also        */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=rslpUxSz9BI
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/removing-minimum-and-maximum-from-array
 */
