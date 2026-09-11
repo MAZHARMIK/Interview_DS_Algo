d3268aa1f51115c4078b89ba40ce8dea696c97b5
MAZHAR <iiestshibpur605@gmail.com>
2026-09-11 19:54:59 +0530

    added 5 approaches

--- DP/DP on Strings/Maximum Number of Non-overlapping Palindrome Substrings.cpp
+++ DP/DP on Strings/Maximum Number of Non-overlapping Palindrome Substrings.cpp
@@ -54,7 +54,65 @@ public:
 };
 
 
-//Approach-2 (Bottom Up of Approach-1 Above)
+//Approach-2 (Using Blueprint of Palindrome Strings to conver isPalindrome check to O(1))
+//T.C : O(n^2)
+//S.C : O(n^3)
+class Solution {
+public:
+    vector<vector<bool>> isPalindrome;
+
+    //max non-overlapping palindromic pieces (length >= k) from s[i..j]
+    int solve(const string& s, int k, int i, int j, vector<vector<int>>& t) {
+        int n = s.size();
+        if (i >= n || j >= n) 
+            return 0;
+            
+        if (t[i][j] != -1) 
+            return t[i][j];
+
+        if (isPalindrome[i][j]) { //O(1)
+            int growWindow  = solve(s, k, i, j + 1, t);
+            int takeIt      = 1 + solve(s, k, j + 1, j + k, t);
+            int slideWindow = solve(s, k, i + 1, j + 1, t);
+
+            return t[i][j] = max({growWindow, takeIt, slideWindow});
+        }
+
+        int slideWindow = solve(s, k, i + 1, j + 1, t);
+        int growWindow  = solve(s, k, i, j + 1, t);
+
+        return t[i][j] = max(slideWindow, growWindow);
+    }
+
+    int maxPalindromes(string s, int k) {
+        int n = s.size();
+        if (k == 1)
+            return n; //each character can be a substring
+
+        isPalindrome.assign(n+1, vector<bool>(n+1));
+        //Palindromic substring Blueprint
+        for(int L = 1; L <= n; L++) {
+            for(int i = 0; i+L <= n; i++) {
+                int j = i + L - 1;
+
+                if(i == j) {
+                    isPalindrome[i][i] = true; //Single characters are palindrome
+                } else if(i+1 == j) {
+                    isPalindrome[i][j] = (s[i] == s[j]); //Strings of 2 Length
+                } else {
+                    isPalindrome[i][j] = ((s[i] == s[j]) && isPalindrome[i+1][j-1] == true);
+                }
+            }
+        }
+
+        vector<vector<int>> t(n, vector<int>(n, -1));
+
+        return solve(s, k, 0, k - 1, t);
+    }
+};
+
+
+//Approach-3 (Bottom Up of Approach-1 Above)
 //T.C : O(n^3)
 //S.C : O(n^2)
 class Solution {
@@ -95,7 +153,58 @@ public:
 };
 
 
-//Approach-3 (Using My favourite BluePrint for solving palindromic DP problems
+//Approach-4 (Using Blueprint of Palindrome Strings to conver isPalindrome check to O(1))
+//T.C : O(n^3)
+//S.C : O(n^2)
+class Solution {
+public:
+    int maxPalindromes(string s, int k) {
+        int n = s.size();
+        if (k == 1) 
+            return n; //each character can be a substring
+
+        vector<vector<bool>> isPalindrome(n, vector<bool>(n, false));
+
+        //Palindromic substring Blueprint
+        for(int L = 1; L <= n; L++) {
+            for(int i = 0; i+L <= n; i++) {
+                int j = i + L - 1;
+
+                if(i == j) {
+                    isPalindrome[i][i] = true; //Single characters are palindrome
+                } else if(i+1 == j) {
+                    isPalindrome[i][j] = (s[i] == s[j]); //Strings of 2 Length
+                } else {
+                    isPalindrome[i][j] = ((s[i] == s[j]) && isPalindrome[i+1][j-1] == true);
+                }
+            }
+        }
+
+        vector<vector<int>> t(n + 1, vector<int>(n + 1, 0));
+
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = n - 1; j >= i; --j) {
+
+                if (isPalindrome[i][j]) {
+                    int growWindow  = t[i][j + 1];
+                    int takeIt      = 1 + (j + k <= n ? t[j + 1][j + k] : 0);
+                    int slideWindow = t[i + 1][j + 1];
+
+                    t[i][j] = max({growWindow, takeIt, slideWindow});
+                }
+                
+                int slideWindow = t[i + 1][j + 1];
+                int growWindow  = t[i][j + 1];
+                t[i][j] = max({t[i][j], slideWindow, growWindow});
+            }
+        }
+
+        return t[0][k - 1];
+    }
+};
+
+
+//Approach-5 (Using My BluePrint for solving palindromic DP problems)
 //T.C : O(n^2)
 //S.C : O(n^2)
 class Solution {
