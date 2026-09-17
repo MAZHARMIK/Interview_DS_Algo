89aa37347f2a2a17c8f02c18f834ca56e505db59
MAZHAR <iiestshibpur605@gmail.com>
2026-09-17 07:27:36 +0530

    Added YouTube Link

--- Sliding Window/Find Two Non-overlapping Sub-arrays Each With Target Sum.cpp
+++ Sliding Window/Find Two Non-overlapping Sub-arrays Each With Target Sum.cpp
@@ -1,6 +1,6 @@
 /*			Scroll below to see JAVA code also			*/
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=OozgrdhmWWA
     Company Tag                 : will update later
     Leetcode Link               : https://leetcode.com/problems/find-two-non-overlapping-sub-arrays-each-with-target-sum
 */
